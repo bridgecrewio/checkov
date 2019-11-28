b23c47c2b936335616099283036091191df9f958
barak <schosterbarak@gmail.com>
2019-11-28 11:45:44 +0200

    s3 versioning

--- bridgecrew/terraformscanner/scanners/S3Versioning.py
+++ bridgecrew/terraformscanner/scanners/S3Versioning.py
@@ -2,10 +2,10 @@ from bridgecrew.terraformscanner.models.enums import ScanResult, ScanCategories
 from bridgecrew.terraformscanner.scanner import Scanner
 
 
-class S3EncryptionScanner(Scanner):
+class S3VersioningScanner(Scanner):
     def __init__(self):
         name = "Ensure all data stored in the S3 bucket is securely encrypted at rest"
-        scan_id = "BC_AWS_S3_14"
+        scan_id = "BC_AWS_S3_16"
         supported_resource = 'aws_s3_bucket'
         categories = [ScanCategories.LOGGING]
         super().__init__(name=name, scan_id=scan_id, categories=categories, supported_resource=supported_resource)
@@ -17,15 +17,11 @@ class S3EncryptionScanner(Scanner):
         :param conf: aws_s3_bucket configuration
         :return: <ScanResult>
         """
-        if 'server_side_encryption_configuration' in conf.keys():
-            sse_block = conf['server_side_encryption_configuration']
-            if 'rule' in sse_block[0].keys():
-                rule_block = sse_block[0]['rule']
-                if 'apply_server_side_encryption_by_default' in rule_block[0].keys():
-                    encryption_block = rule_block[0]['apply_server_side_encryption_by_default']
-                    if  'sse_algorithm' in encryption_block[0].keys():
-                        return ScanResult.SUCCESS
+        if 'versioning' in conf.keys():
+            versioning_block = conf['versioning'][0]
+            if versioning_block['enabled'][0]:
+                return ScanResult.SUCCESS
         return ScanResult.FAILURE
 
 
-scanner = S3EncryptionScanner()
+scanner = S3VersioningScanner()
---
+++ tests/terraformscanner/scanners/test_S3Versioning.py
@@ -0,0 +1,35 @@
+import unittest
+
+from bridgecrew.terraformscanner.models.enums import ScanResult
+from bridgecrew.terraformscanner.scanners.S3Versioning import scanner
+
+
+class TestS3Versioning(unittest.TestCase):
+
+    def test_failure(self):
+        resource_conf = {"region": ["us-west-2"],
+                         "bucket": ["my_bucket"],
+                         "acl": ["public-read"],
+                         "force_destroy": [True],
+                         "tags": [{"Name": "my-bucket"}]}
+        scan_result = scanner.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(ScanResult.FAILURE, scan_result)
+
+    def test_success(self):
+        resource_conf = {"region": ["us-west-2"],
+                         "bucket": ["my_bucket"],
+                         "acl": ["public-read"],
+                         "force_destroy": [True],
+                         "tags": [{"Name": "my-bucket"}],
+                         "logging": [{"target_bucket": "logging-bucket",
+                                      "target_prefix": "log/"
+                                      }],
+                         "versioning": [
+                             {"enabled": [True]}]
+                         }
+        scan_result = scanner.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(ScanResult.SUCCESS, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
