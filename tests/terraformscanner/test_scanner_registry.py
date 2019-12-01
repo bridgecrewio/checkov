c8b74efa16a51a0cc95ee9f6ecd8bb912eb3e509
barak <schosterbarak@gmail.com>
2019-12-01 08:48:35 +0200

    add elasticsearch encryption check

--- tests/terraformscanner/test_scanner_registry.py
+++ tests/terraformscanner/test_scanner_registry.py
@@ -11,7 +11,7 @@ class TestScannerRegistry(unittest.TestCase):
         for key in list(registry.scanners.keys()):
             scanners_counter+=len(registry.scanners[key])
 
-        self.assertEqual(27,scanners_counter)
+        self.assertEqual(29,scanners_counter)
 
 
 
