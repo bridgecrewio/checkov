8880273acd16105a0b24eef8dd7f34c90852f9cf
barak <schosterbarak@gmail.com>
2019-12-01 11:43:34 +0200

    add GCP SSL Policy Check

--- tests/terraformscanner/test_scanner_registry.py
+++ tests/terraformscanner/test_scanner_registry.py
@@ -11,7 +11,7 @@ class TestScannerRegistry(unittest.TestCase):
         for key in list(registry.scanners.keys()):
             scanners_counter+=len(registry.scanners[key])
 
-        self.assertEqual(37,scanners_counter)
+        self.assertEqual(38,scanners_counter)
 
 
 
