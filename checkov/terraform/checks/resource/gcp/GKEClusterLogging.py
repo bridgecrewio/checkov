f1ca0013ff824400a439f777e0596dcc78c313e0
barak <schosterbarak@gmail.com>
2019-12-11 11:05:29 +0200

    add gke cluster logging check

---
+++ checkov/terraform/checks/resource/gcp/GKEClusterLogging.py
@@ -0,0 +1,26 @@
+from checkov.terraform.checks.resource.base_check import BaseResourceCheck
+from checkov.terraform.models.enums import CheckResult, CheckCategories
+
+
+class GKEClusterLogging(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure Stackdriver Logging is set to Enabled on Kubernetes Engine Clusters"
+        id = "BC_GCP_GKE_1"
+        supported_resources = ['google_container_cluster']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Looks for password configuration at azure_instance:
+            https://www.terraform.io/docs/providers/google/r/compute_ssl_policy.html
+        :param conf: google_compute_ssl_policy configuration
+        :return: <CheckResult>
+        """
+        if 'logging_service' in conf.keys():
+            if conf['logging_service'][0] == "none":
+                return CheckResult.FAILURE
+        return CheckResult.SUCCESS
+
+
+check = GKEClusterLogging()
