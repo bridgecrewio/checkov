6b52af059ff11fd7192b9e1afea07ab9c8915331
barak <schosterbarak@gmail.com>
2020-01-03 21:41:24 +0200

    fix #38 referencing a security_group instead of cidr_block in a security group rule causes an exception

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -125,3 +125,26 @@ resource "aws_iam_account_password_policy" "password-policy" {
   require_symbols                = true
   allow_users_to_change_password = true
 }
+
+resource "aws_security_group" "bar-sg" {
+  name   = "sg-bar"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    security_groups = [
+    aws_security_group.foo-sg.id]
+    description = "foo"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+}
