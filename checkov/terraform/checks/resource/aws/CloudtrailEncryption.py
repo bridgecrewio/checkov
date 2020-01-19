115894f6e278a5669b6559375e8e1a774c661fb8
barak <schosterbarak@gmail.com>
2020-01-19 21:51:43 +0200

    add cloudtrail checks

---
+++ checkov/terraform/checks/resource/aws/CloudtrailEncryption.py
@@ -0,0 +1,25 @@
+from checkov.terraform.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_check import BaseResourceCheck
+
+
+class CloudtrailEncryption(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure CloudTrail logs are encrypted at rest using KMS CMKs"
+        id = "CKV_AWS_35"
+        supported_resources = ['aws_cloudtrail']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Looks for encryption configuration at cloudtrail:
+            https://www.terraform.io/docs/providers/aws/r/cloudtrail.html
+        :param conf: cloudtrail configuration
+        :return: <CheckResult>
+        """
+        if "kms_key_id" in conf.keys():
+            return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = CloudtrailEncryption()
