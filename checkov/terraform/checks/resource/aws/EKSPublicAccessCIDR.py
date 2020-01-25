04ac61ba304861f6d116526f1ddf164384b02ef8
jonjozwiak <jonjozwiak@users.noreply.github.com>
2020-01-24 21:15:56 -0600

    Add 3 EKS checks (#84)

---
+++ checkov/terraform/checks/resource/aws/EKSPublicAccessCIDR.py
@@ -0,0 +1,35 @@
+from checkov.terraform.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_check import BaseResourceCheck
+
+class EKSPublicAccessCIDR(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure Amazon EKS public endpoint not accessible to 0.0.0.0/0"
+        id = "CKV_AWS_38"
+        supported_resources = ['aws_eks_cluster']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Looks for public_access_cidrs at aws_eks_cluster:
+            https://www.terraform.io/docs/providers/aws/r/eks_cluster.html
+        :param conf: aws_eks_cluster configuration
+        :return: <CheckResult>
+        """
+        if "vpc_config" in conf.keys():
+           if "endpoint_public_access" in conf["vpc_config"][0].keys() and conf["vpc_config"][0]["endpoint_public_access"][0] == False:
+               return CheckResult.PASSED
+           elif "public_access_cidrs" in conf["vpc_config"][0].keys():
+               if not len(conf["vpc_config"][0]["public_access_cidrs"][0]):
+                   return CheckResult.FAILED
+               elif "0.0.0.0/0" in conf["vpc_config"][0]["public_access_cidrs"][0]:
+                   return CheckResult.FAILED
+               else:
+                   return CheckResult.PASSED
+           else:
+               return CheckResult.FAILED
+        else:
+           return CheckResult.UNKNOWN
+     
+check = EKSPublicAccessCIDR()
+
---
+++ tests/terraform/checks/resource/aws/test_EKSControlPlaneLogging.py
@@ -0,0 +1,32 @@
+import unittest
+
+from checkov.terraform.checks.resource.aws.EKSControlPlaneLogging import check
+from checkov.terraform.models.enums import CheckResult
+
+
+class TestEKSControlPlaneLogging(unittest.TestCase):
+
+    def test_failure(self):
+        resource_conf = {'name': ['testcluster'], 'enabled_cluster_log_types': [['api', 'audit']]}
+
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_failure_empty(self):
+        resource_conf = {'name': ['testcluster']}
+
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+
+    def test_success(self):
+        resource_conf = {'name': ['testcluster'], 'enabled_cluster_log_types': [['api', 'audit', 'authenticator', 'controllerManager', 'scheduler']]}
+
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
