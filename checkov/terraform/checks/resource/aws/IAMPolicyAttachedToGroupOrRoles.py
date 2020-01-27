c162532301137d773ab0a389902f12775c8db7b5
barak <schosterbarak@gmail.com>
2020-01-27 18:51:01 +0200

    add check: Ensure IAM policies are attached only to groups or roles fix: https://github.com/bridgecrewio/checkov/issues/56

--- checkov/terraform/checks/resource/aws/IAMPolicyAttachedToGroupOrRoles.py
+++ checkov/terraform/checks/resource/aws/IAMPolicyAttachedToGroupOrRoles.py
@@ -7,13 +7,13 @@ class IAMPolicyAttachedToGroupOrRoles(BaseResourceCheck):
         name = "Ensure IAM policies are attached only to groups or roles (Reducing access management complexity may " \
                "in-turn reduce opportunity for a principal to inadvertently receive or retain excessive privileges.) "
         id = "CKV_AWS_40"
-        supported_resources = ['aws_iam_user_policy_attachment', 'aws_iam_user_policy']
+        supported_resources = ['aws_iam_user_policy_attachment', 'aws_iam_user_policy', 'aws_iam_policy_attachment']
         categories = [CheckCategories.IAM]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        # fail if this kind of resources is identified - since policies should only be attached to groups and roles
-        return CheckResult.FAILED
-
+        if 'user' in conf.keys() or 'users' in conf.keys():
+            return CheckResult.FAILED
+        return CheckResult.PASSED
 
 check = IAMPolicyAttachedToGroupOrRoles()
--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -387,6 +387,16 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = "${aws_iam_user.user.name}"
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
+resource "aws_iam_policy_attachment" "test-attach" {
+  name = "test-attachment"
+  users = [
+  "${aws_iam_user.user.name}"]
+  roles = [
+  "${aws_iam_role.role.name}"]
+  groups = [
+  "${aws_iam_group.group.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
 
 resource "aws_iam_user_policy" "lb_ro" {
   name = "test"
