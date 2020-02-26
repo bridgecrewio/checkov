4568e4b5984a5794a4d033d3deaabc88987225cf
James Woolfenden <james.woolfenden@gmail.com>
2020-02-26 16:54:11 +0000

    Fix to add missing windows support for checkov; closes #99 (#107)
    * Add windows batach and add file to be included with setup.py
    
    * Fix line ending

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     author_email="support@bridgecrew.io",
     url="https://github.com/bridgecrewio/checkov",
     packages=setuptools.find_packages(exclude=["tests*"]),
-    scripts=["bin/checkov"],
+    scripts=["bin/checkov","bin/checkov.bat"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
