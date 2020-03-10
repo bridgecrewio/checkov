8142812c15e175bea963269424a120b0ce500c26
barak <schosterbarak@gmail.com>
2020-03-10 15:03:41 +0200

    add cloudfront and alb tests

--- checkov/common/output/report.py
+++ checkov/common/output/report.py
@@ -17,6 +17,11 @@ class Report:
     skipped_checks = []
     parsing_errors = []
 
+    def __init__(self):
+        self.passed_checks = []
+        self.failed_checks = []
+        self.skipped_checks = []
+        self.parsing_errors = []
     def add_parsing_errors(self, files):
         for file in files:
             self.add_parsing_error(file)
@@ -111,3 +116,19 @@ class Report:
 
     def print_json(self):
         print(self.get_json())
+
+    def get_new_report_for_check_id(self, check_id):
+        new_report = Report()
+        for record in self.passed_checks:
+            if record.check_id == check_id:
+                new_report.add_record(record)
+        for record in self.failed_checks:
+            if record.check_id == check_id:
+                new_report.add_record(record)
+        for record in self.skipped_checks:
+            if record.check_id == check_id:
+                new_report.add_record(record)
+        for record in self.parsing_errors:
+            if record.check_id == check_id:
+                new_report.add_record(record)
+        return new_report
