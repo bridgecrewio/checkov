be632cbc1f21eee4e143defcbf7d0c6c4189ff5e
James Woolfenden <james.woolfenden@gmail.com>
2020-03-15 11:44:18 +0000

    Test for emvyption setup for kinesis stream

---
+++ tests/terraform/checks/resource/aws/test_KinesisStreamEncryptionType.py
@@ -0,0 +1,35 @@
+import unittest
+
+from checkov.terraform.checks.resource.aws.KinesisStreamEncryptionType import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestKinesisStreamEncryptionType(unittest.TestCase):
+
+
+  
+    def test_failure(self):
+        resource_conf =  {
+		    'name': ["terraform-kinesis-test"],
+			'shard_count': [1],
+			'retention_period' : [48]
+
+
+		}
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success(self):
+        resource_conf =  {
+            'name': ["terraform-kinesis-test"],
+			'shard_count': [1],
+			'retention_period' : [48],
+            'encryption_type': ["KMS"]
+            }
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
