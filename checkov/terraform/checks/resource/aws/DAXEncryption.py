99244888c17425c45b5109847e0bb9f49a9f01f4
James Woolfenden <James.Woolfenden@gmail.com>
2020-03-29 18:30:28 +0100

    test for DAX encyption being enabled: Resolves #164

--- checkov/terraform/checks/resource/aws/DAXEncryption.py
+++ checkov/terraform/checks/resource/aws/DAXEncryption.py
@@ -2,15 +2,15 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 from checkov.common.models.enums import CheckCategories
 
 
-class DynamodbEncryption(BaseResourceValueCheck):
+class DAXEncryption(BaseResourceValueCheck):
     def __init__(self):
-        name = "Ensure Dynamodb is encrypted at rest"
-        id = "CKV_AWS_45"
-        supported_resources = ['aws_dynamodb_table']
+        name = "Ensure DAX is encrypted at rest (default is unencrypted)"
+        id = "CKV_AWS_47"
+        supported_resources = ['aws_dax_cluster']
         categories = [CheckCategories.BACKUP_AND_RECOVERY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
         return "server_side_encryption/[0]/enabled"
 
-check = DynamodbEncryption()
+check = DAXEncryption()
--- tests/terraform/checks/resource/aws/test_DAXEncryption.py
+++ tests/terraform/checks/resource/aws/test_DAXEncryption.py
@@ -8,13 +8,29 @@ class TestDAXEncryption(unittest.TestCase):
 
     def test_failure(self):
         resource_conf =  {
-		'cluster_name': ['myDAXcluster']}
+		      "cluster_name": "${var.cluster_name}",
+              "iam_role_arn": "${var.iam_role_arn}",
+              "parameter_group_name": "${aws_dax_parameter_group.example.name}",
+              "subnet_group_name": "${aws_dax_subnet_group.example.name}",
+              "tags": "${var.common_tags}"
+            }
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_success(self):
-        resource_conf =  {	'cluster_name': ['myDAXcluster'],
-		'server_side_encryption':{'enabled': [True]}}
+        resource_conf =  {
+              "cluster_name": "${var.cluster_name}",
+              "iam_role_arn": "${var.iam_role_arn}",
+              "parameter_group_name": "${aws_dax_parameter_group.example.name}",
+              "server_side_encryption": [
+                {
+                  "enabled": [True]
+                }
+              ],
+              "subnet_group_name": "${aws_dax_subnet_group.example.name}",
+              "tags": "${var.common_tags}"
+            }
+
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
