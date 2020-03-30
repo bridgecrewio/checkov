28b836822961bbd1a84ed2fe2def13a9594ad5fc
James Woolfenden <james.woolfenden@gmail.com>
2020-03-30 10:02:49 +0100

    Check for MQ Broker: to enable general logging to Cloudwatch, #172 (#205)
    * Test to Ensure MQ Broker logging is enabled
    
    Co-authored-by: James Woolfenden <james.woolfenden@slalom.com>

---
+++ checkov/terraform/checks/resource/aws/MQBrokerLogging.py
@@ -0,0 +1,16 @@
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class MQBrokerLogging(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure MQ Broker logging is enabled"
+        id = "CKV_AWS_48"
+        supported_resources = ['aws_mq_broker']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "logs/[0]/general"
+
+check = MQBrokerLogging()
