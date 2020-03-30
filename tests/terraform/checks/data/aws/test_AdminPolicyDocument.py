f24b01e4f89eec5edee6a827f7f8f09dafadf694
Nimrod Kor <nimrodkor@gmail.com>
2020-03-30 15:20:29 +0300

    Fix tests to conform with HCL2 - everything is a list

--- tests/terraform/checks/data/aws/test_AdminPolicyDocument.py
+++ tests/terraform/checks/data/aws/test_AdminPolicyDocument.py
@@ -7,24 +7,12 @@ from checkov.common.models.enums import CheckResult
 class TestAdminPolicyDocument(unittest.TestCase):
 
     def test_success(self):
-        resource_conf = {
-            "statement": [{
-                "actions": ["s3:Describe*"],
-                "resources": ["arn:aws:s3:::my_corporate_bucket/*"],
-                "effect": ["Allow"]
-            }]
-        }
+        resource_conf = {'version': ['2012-10-17'], 'statement': [{'actions': [['s3:Describe*']], 'resources': [['*']], 'effect': ['Allow']}]}
         scan_result = check.scan_data_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
     def test_failure(self):
-        resource_conf = {
-            "statement": [{
-                "actions": ["*"],
-                "resources": ["*"],
-                "effect": ["Allow"]
-            }]
-        }
+        resource_conf = {'version': ['2012-10-17'], 'statement': [{'actions': [['*']], 'resources': [['*']], 'effect': ['Allow']}]}
         scan_result = check.scan_data_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
