3f02ff98a6065f62d505defc4d87f135e6321e30
barak <schosterbarak@gmail.com>
2020-04-26 19:48:37 +0300

    main refactor

--- checkov/main.py
+++ checkov/main.py
@@ -5,6 +5,7 @@ import logging
 
 from checkov.cloudformation.runner import Runner as cfn_runner
 from checkov.common.runners.runner_registry import RunnerRegistry
+from checkov.common.util.banner import banner as checkov_banner
 from checkov.common.util.docs_generator import print_checks
 from checkov.terraform.runner import Runner as tf_runner
 from checkov.version import version
@@ -19,10 +20,10 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 console.setFormatter(formatter)
 
 
-def run():
+def run(banner = checkov_banner):
     parser = argparse.ArgumentParser(description='Infrastructure as code static analysis')
     parser.add_argument('-v', '--version',
-                        help='Checkov version', action='store_true')
+                        help='version', action='store_true')
     parser.add_argument('-d', '--directory', action='append',
                         help='IaC root directory (can not be used together with --file). Can be repeated')
     parser.add_argument('-f', '--file', action='append',
@@ -35,7 +36,7 @@ def run():
     parser.add_argument('-s', '--soft-fail',
                         help='Runs checks but suppresses error code', action='store_true')
     args = parser.parse_args()
-    runner_registry = RunnerRegistry(tf_runner(), cfn_runner())
+    runner_registry = RunnerRegistry(banner, tf_runner(), cfn_runner())
     if args.version:
         print(version)
         return
@@ -52,7 +53,7 @@ def run():
         scan_reports = runner_registry.run(None, external_checks_dir=args.external_checks_dir, files=args.file)
         runner_registry.print_reports(scan_reports, args)
     else:
-        print("No argument given. Try `checkov --help` for further information")
+        print("No argument given. Try ` --help` for further information")
 
 
 if __name__ == '__main__':
