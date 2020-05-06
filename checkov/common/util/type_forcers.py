cad0a78d0084ec0484c767343d0c3e88f709de61
ArielK <63583491+arielkru@users.noreply.github.com>
2020-05-06 16:09:15 +0300

    handle force_int with string exception

--- checkov/common/util/type_forcers.py
+++ checkov/common/util/type_forcers.py
@@ -5,6 +5,9 @@ def force_list (var):
 
 
 def force_int (var):
-    if not isinstance(var, int):
-        var = int(var)
-    return var
+    try:
+        if not isinstance(var, int):
+            var = int(var)
+        return var
+    except:
+        return None
