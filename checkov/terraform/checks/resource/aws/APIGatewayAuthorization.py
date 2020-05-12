52a6892a3bb3746e9c5961ae01a5dc230bfc89ce
ArielK <63583491+arielkru@users.noreply.github.com>
2020-05-12 14:10:46 +0300

    allow http_method to have no authorization & change check category

--- checkov/terraform/checks/resource/aws/APIGatewayAuthorization.py
+++ checkov/terraform/checks/resource/aws/APIGatewayAuthorization.py
@@ -8,13 +8,12 @@ class APIGatewayAuthorization(BaseResourceCheck):
         name = "Ensure there is no open access to back-end resources through API "
         id = "CKV_AWS_59"
         supported_resources = ['aws_api_gateway_method']
-        categories = [CheckCategories.SECRETS]
+        categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if 'authorization' in conf.keys():
-            if conf['authorization'][0] == "NONE":
-                return CheckResult.FAILED
+        if conf['http_method'][0] != "OPTIONS" and conf['authorization'][0] == "NONE":
+            return CheckResult.FAILED
         return CheckResult.PASSED
 
 
