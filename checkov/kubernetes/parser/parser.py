3475f9fd2e6ab55ed7f79d2c21d6a938cc6536fd
barak <schosterbarak@gmail.com>
2020-05-14 14:50:41 +0300

    run k8 only on specific suffix

--- checkov/kubernetes/parser/parser.py
+++ checkov/kubernetes/parser/parser.py
@@ -1,5 +1,4 @@
 import logging
-
 from yaml import YAMLError
 
 from checkov.kubernetes.parser import k8_yaml, k8_json
@@ -11,32 +10,27 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-def parse( filename):
+
+def parse(filename):
     template = None
     template_lines = None
     try:
-        (template, template_lines) = k8_yaml.load(filename)
+        if filename.endswith(".yaml") or filename.endswith(".yml"):
+            (template, template_lines) = k8_yaml.load(filename)
+        if filename.endswith(".json"):
+            (template, template_lines) = k8_json.load(filename)
     except IOError as e:
         if e.errno == 2:
             logger.error('Template file not found: %s', filename)
         elif e.errno == 21:
             logger.error('Template references a directory, not a file: %s',
-                              filename)
+                         filename)
         elif e.errno == 13:
             logger.error('Permission denied when accessing template file: %s',
-                              filename)
+                         filename)
     except UnicodeDecodeError as err:
         logger.error('Cannot read file contents: %s', filename)
     except YAMLError as err:
-        if err.problem in [
-            'expected \'<document start>\', but found \'{\'']:
-            try:
-                (template, template_lines) = k8_json.load(filename)
-            except Exception as json_err:  # pylint: disable=W0703
-                logger.error(
-                    'Template %s is malformed: %s', filename, err.problem)
-                pass
+        pass
 
     return template, template_lines
-
-
