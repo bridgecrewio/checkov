b73405b7788dd721cfd57793c005c09cc3a16148
Nimrod Kor <nimrodkor@gmail.com>
2020-05-15 10:55:36 +0300

    Fix SG checks + refactor to abs class

--- checkov/terraform/checks/resource/aws/SecurityGroupUnrestrictedIngress3389.py
+++ checkov/terraform/checks/resource/aws/SecurityGroupUnrestrictedIngress3389.py
@@ -1,37 +1,9 @@
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.util.type_forcers import force_list
-
-PORT = 3389
+from checkov.terraform.checks.resource.aws.AbsSecurityGroupUnrestrictedIngress import AbsSecurityGroupUnrestrictedIngress
 
 
-class SecurityGroupUnrestrictedIngress3389(BaseResourceCheck):
+class SecurityGroupUnrestrictedIngress3389(AbsSecurityGroupUnrestrictedIngress):
     def __init__(self):
-        name = "Ensure no security groups allow ingress from 0.0.0.0:0 to port %d" % PORT
-        id = "CKV_AWS_25"
-        supported_resources = ['aws_security_group']
-        categories = [CheckCategories.NETWORKING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-
-    def scan_resource_conf(self, conf):
-        """
-            Looks for configuration at security group ingress rules :
-            https://www.terraform.io/docs/providers/aws/r/security_group.html
-        :param conf: aws_security_group configuration
-        :return: <CheckResult>
-        """
-        if 'ingress' in conf.keys():
-            ingress_conf = conf['ingress']
-            for rule in ingress_conf:
-                if isinstance(rule, dict):
-                    if isinstance(force_list(rule['from_port'])[0], int) and isinstance(force_list(rule['to_port'])[0], int):
-                        if rule['from_port'] == [PORT] and rule['to_port'] == [PORT]:
-                            if 'cidr_blocks' in rule.keys():
-                                if rule['cidr_blocks'] == [["0.0.0.0/0"]] and 'security_groups' not in rule.keys():
-                                    return CheckResult.FAILED
-
-
-        return CheckResult.PASSED
+        super().__init__(check_id="CKV_AWS_24", port=3389)
 
 
 check = SecurityGroupUnrestrictedIngress3389()
--- tests/terraform/checks/resource/aws/test_SecurityGroupUnrestrictedIngress22.py
+++ tests/terraform/checks/resource/aws/test_SecurityGroupUnrestrictedIngress22.py
@@ -1,32 +1,62 @@
 import unittest
 
-from checkov.terraform.checks.resource.aws.SecurityGroupUnrestrictedIngress22 import check
+import hcl2
+
 from checkov.common.models.enums import CheckResult
+from checkov.terraform.checks.resource.aws.SecurityGroupUnrestrictedIngress22 import check
 
 
 class TestSecurityGroupUnrestrictedIngress22(unittest.TestCase):
 
     def test_failure(self):
-        resource_conf = {'name': ['foo'],
-                         'vpc_id': ['${var.vpc_id}'], 'ingress': [
-                {'from_port': [22], 'to_port': [22], 'protocol': ['TCP'], 'cidr_blocks': [['0.0.0.0/0']]},
-                {'from_port': [443], 'to_port': [443], 'protocol': ['TCP'], 'cidr_blocks': [['0.0.0.0/0']]}],
-                         'egress': [
-                             {'from_port': [0], 'to_port': [0], 'protocol': ['-1'], 'cidr_blocks': [['0.0.0.0/0']]}]
-                        }
+        hcl_res = hcl2.loads("""
+resource "aws_security_group" "bar-sg" {
+  name   = "sg-bar"
+  vpc_id = aws_vpc.main.id
 
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "foo"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}  
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_security_group']['bar-sg']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_success(self):
-        resource_conf =  {'name': ['foo'],
-                          'vpc_id': ['${var.vpc_id}'], 'ingress': [
-                {'from_port': [80], 'to_port': [80], 'protocol': ['TCP'], 'cidr_blocks': [['0.0.0.0/0']]},
-                {'from_port': [443], 'to_port': [443], 'protocol': ['TCP'], 'cidr_blocks': [['0.0.0.0/0']]}],
-                          'egress': [
-                              {'from_port': [0], 'to_port': [0], 'protocol': ['-1'], 'cidr_blocks': [['0.0.0.0/0']]}],
-                          'tags': [{'kubernetes.io/cluster/${var.cluster_name}': 'owned',
-                                    'kubernetes:application': '${local.name}'}]}
+        hcl_res = hcl2.loads("""
+resource "aws_security_group" "bar-sg" {
+  name   = "sg-bar"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    security_groups = [aws_security_group.foo-sg.id]
+    description = "foo"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_security_group']['bar-sg']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
