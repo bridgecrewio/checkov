43a29e5f576093e67f0607cea08dbc779e42924f
Nimrod Kor <nimrodkor@gmail.com>
2020-05-16 12:28:14 +0300

    Fix ID

--- checkov/terraform/checks/resource/aws/SecurityGroupUnrestrictedIngress3389.py
+++ checkov/terraform/checks/resource/aws/SecurityGroupUnrestrictedIngress3389.py
@@ -3,7 +3,7 @@ from checkov.terraform.checks.resource.aws.AbsSecurityGroupUnrestrictedIngress i
 
 class SecurityGroupUnrestrictedIngress3389(AbsSecurityGroupUnrestrictedIngress):
     def __init__(self):
-        super().__init__(check_id="CKV_AWS_24", port=3389)
+        super().__init__(check_id="CKV_AWS_25", port=3389)
 
 
 check = SecurityGroupUnrestrictedIngress3389()
