3d115e4b0093b985e6c8af32cb05d0d094257cf6
ArielK <63583491+arielkru@users.noreply.github.com>
2020-05-26 11:09:44 +0300

    CKV_GCP_26 - example.tf update

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1173,3 +1173,23 @@ resource "aws_iam_role" "example_2_allowing_all_aws_principals" {
 }
 EOF
 }
+
+resource "google_compute_subnetwork" "subnet without logging" {
+          name          = "log-test-subnetwork"
+          ip_cidr_range = "10.2.0.0/16"
+          region        = "us-central1"
+          network       = google_compute_network.custom-test.id
+        }
+
+resource "google_compute_subnetwork" "subnet with logging" {
+          name          = "log-test-subnetwork"
+          ip_cidr_range = "10.2.0.0/16"
+          region        = "us-central1"
+          network       = google_compute_network.custom-test.id
+
+          log_config {
+            aggregation_interval = "INTERVAL_10_MIN"
+            flow_sampling        = 0.5
+            metadata             = "INCLUDE_ALL_METADATA"
+          }
+        }
