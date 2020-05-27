acf2ccbdcd4ef4b0af467db86c7e5adad3aa858a
ArielK <63583491+arielkru@users.noreply.github.com>
2020-05-27 14:49:13 +0300

    CKV_GCP_25 - BaseResourceValueCheck class implementation (fix)

--- checkov/terraform/checks/resource/gcp/GKEPrivateClusterConfig.py
+++ checkov/terraform/checks/resource/gcp/GKEPrivateClusterConfig.py
@@ -1,5 +1,6 @@
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckCategories
+from checkov.common.models.consts import ANY_VALUE
 
 
 class GKEPodSecurityPolicyEnabled(BaseResourceValueCheck):
@@ -10,13 +11,11 @@ class GKEPodSecurityPolicyEnabled(BaseResourceValueCheck):
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    # def scan_resource_conf(self, conf):
-    #     if conf.get('private_cluster_config'):
-    #         return CheckResult.PASSED
-    #     return CheckResult.FAILED
-
     def get_inspected_key(self):
         return 'private_cluster_config'
+
+    def get_expected_values(self):
+        return [ANY_VALUE]
 
 
 
