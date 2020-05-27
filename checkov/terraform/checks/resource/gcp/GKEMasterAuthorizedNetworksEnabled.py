6e9ccd03630545108bf6a985cf7def056996b710
ArielK <63583491+arielkru@users.noreply.github.com>
2020-05-27 14:44:01 +0300

    CKV_GCP_20 - BaseResourceValueCheck class implementation

--- checkov/terraform/checks/resource/gcp/GKEMasterAuthorizedNetworksEnabled.py
+++ checkov/terraform/checks/resource/gcp/GKEMasterAuthorizedNetworksEnabled.py
@@ -1,8 +1,9 @@
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.common.models.consts import ANY_VALUE
 
 
-class GKEMasterAuthorizedNetworksEnabled(BaseResourceCheck):
+class GKEMasterAuthorizedNetworksEnabled(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure master authorized networks is set to enabled in GKE clusters"
         id = "CKV_GCP_20"
@@ -10,16 +11,11 @@ class GKEMasterAuthorizedNetworksEnabled(BaseResourceCheck):
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        """
-            Looks for password configuration at azure_instance:
-            https://www.terraform.io/docs/providers/google/r/compute_ssl_policy.html
-        :param conf: google_compute_ssl_policy configuration
-        :return: <CheckResult>
-        """
-        if 'master_authorized_networks_config' in conf.keys():
-            return CheckResult.PASSED
-        return CheckResult.FAILED
+    def get_inspected_key(self):
+        return 'master_authorized_networks_config'
+
+    def get_expected_values(self):
+        return [ANY_VALUE]
 
 
 check = GKEMasterAuthorizedNetworksEnabled()
