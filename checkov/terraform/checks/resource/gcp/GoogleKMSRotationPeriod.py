4fd26adfeba75d70a8e60bc9f53f1abf918b0394
ArielK <63583491+arielkru@users.noreply.github.com>
2020-06-01 11:45:23 +0300

    CKV_GCP_44 - check file (GENERAL_SECURITY)

---
+++ checkov/terraform/checks/resource/gcp/GoogleKMSRotationPeriod.py
@@ -0,0 +1,23 @@
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+NINETY_DAYS = {'d': 90, 'h': 2160, 'm': 129600, 's': 7776000}
+
+class GoogleKMSKeyRotationPeriod(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure KMS encryption keys are rotated within a period of 90 days"
+        id = "CKV_GCP_44"
+        supported_resources = ['google_kms_crypto_key']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if 'rotation_period' in conf.keys():
+            time_unit = conf['rotation_period'][0][-1]
+            time = int(conf['rotation_period'][0][:-1])
+            if time <= NINETY_DAYS[time_unit]:
+                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = GoogleKMSKeyRotationPeriod()
