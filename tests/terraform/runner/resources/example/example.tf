d6052dc58174b710f608cedbb2e402e68ae522b9
ArielK <63583491+arielkru@users.noreply.github.com>
2020-06-04 18:10:08 +0300

    CKV_AZURE_13 - example update (GENERAL_SECURITY)

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1386,3 +1386,17 @@ retention_policy {
 }
 }
 
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  auth_settings {
+    enabled                       = true
+    issuer                        = "https://sts.windows.net/d13958f6-b541-4dad-97b9-5a39c6b01297"
+    default_provider              = "AzureActiveDirectory"
+    unauthenticated_client_action = "RedirectToLoginPage"
+  }
+}
+
