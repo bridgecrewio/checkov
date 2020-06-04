edc3f271481ee1468a747a26e4d47897eb1f31d0
ArielK <63583491+arielkru@users.noreply.github.com>
2020-06-04 15:43:37 +0300

    CKV_AZURE_19 - example.tf update (NETWORKING)

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1351,4 +1351,18 @@ resource "azurerm_network_security_rule" "inbound-rdp" {
   network_security_group_name = azurerm_network_security_group.example.name
 }
 
+resource "azurerm_network_security_rule" "inbound-ssh" {
+  name                        = "test123"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "TCP"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
 
