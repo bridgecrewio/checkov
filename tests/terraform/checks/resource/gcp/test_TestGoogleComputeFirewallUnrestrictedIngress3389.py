062350d5269da927b39d98c425fccfa38e4b70b0
ArielK <63583491+arielkru@users.noreply.github.com>
2020-06-04 11:18:37 +0300

    CKV_AZURE_10 - example.tf update (NETWORKING)

--- tests/terraform/checks/resource/gcp/test_TestGoogleComputeFirewallUnrestrictedIngress3389.py
+++ tests/terraform/checks/resource/gcp/test_TestGoogleComputeFirewallUnrestrictedIngress3389.py
@@ -1,5 +1,7 @@
 import unittest
 
+import hcl2
+
 from checkov.terraform.checks.resource.gcp.GoogleComputeFirewallUnrestrictedIngress3389 import check, PORT
 from checkov.common.models.enums import CheckResult
 
@@ -15,11 +17,31 @@ class TestGoogleComputeFirewallUnrestrictedIngress3389(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
-    def test_success(self):
+    def test_success_1(self):
         resource_conf = {'name': ['${var.name}-${var.region}-mesos-ssh'],
                          'network': ['${google_compute_network.mesos-global-net.name}'],
                          'allow': [{'protocol': ['tcp'], 'ports': [[
                              PORT]]}], 'target_tags': [['ssh']], 'source_ranges': [['172.1.2.3/32']]}
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_2(self):
+        hcl_res = hcl2.loads("""
+                    resource "google_compute_firewall" "no-allow-blocks" {
+                      name        = "deny-all-egress-all"
+                      description = "Prevent all egress traffic by default"
+                      disabled = true
+                    
+                      network        = google_compute_network.vpc_network.name
+                      enable_logging = true
+                    
+                      priority           = 65534
+                      direction          = "EGRESS"
+                      destination_ranges = ["0.0.0.0/0"]
+                      deny { protocol = "all" }
+                    }
+                        """)
+        resource_conf = hcl_res['resource'][0]['google_compute_firewall']['no-allow-blocks']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
