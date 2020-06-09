05889206b2a9ed35c132fd35483561deb975c8d2
arielk <ar.kruszyn@gmail.com>
2020-06-09 09:16:32 +0300

    CKV_AZURE_15 - resource update

--- checkov/terraform/checks/resource/azure/AppServiceMinTLSVersion.py
+++ checkov/terraform/checks/resource/azure/AppServiceMinTLSVersion.py
@@ -6,7 +6,7 @@ class AppServiceMinTLSVersion(BaseResourceCheck):
     def __init__(self):
         name = "Ensure web app is using the latest version of TLS encryption"
         id = "CKV_AZURE_15"
-        supported_resources = ['azurerm_resource_group']
+        supported_resources = ['azurerm_app_service']
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
