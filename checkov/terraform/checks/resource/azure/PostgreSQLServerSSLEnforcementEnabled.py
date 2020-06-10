a7635c6865608cebc0bf5f14ae6ca91ea7d485fd
arielk <ar.kruszyn@gmail.com>
2020-06-10 16:06:41 +0300

    CKV_AZURE_29

---
+++ checkov/terraform/checks/resource/azure/PostgreSQLServerSSLEnforcementEnabled.py
@@ -0,0 +1,18 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.consts import ANY_VALUE
+
+
+class PostgreSQLServerSSLEnforcementEnabled(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure 'Enforce SSL connection' is set to 'ENABLED' for PostgreSQL Database Server"
+        id = "CKV_AZURE_29"
+        supported_resources = ['azurerm_postgre_server']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'ssl_enforcement_enabled'
+
+
+check = PostgreSQLServerSSLEnforcementEnabled()
--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1471,5 +1471,21 @@ resource "azurerm_mysql_server" "example" {
   ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
+resource "azurerm_postgresql_server" "example" {
+  name                = "example-psqlserver"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  administrator_login          = "psqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+  sku_name   = "GP_Gen5_4"
+  version    = "9.6"
+  storage_mb = 640000
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = true
+  auto_grow_enabled            = true
+  public_network_access_enabled    = false
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
 
 
