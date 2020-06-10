e918d0273135f486167f643755c7b21116f8a815
arielk <ar.kruszyn@gmail.com>
2020-06-10 15:49:08 +0300

    CKV_AZURE_28

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1450,5 +1450,26 @@ resource "azurerm_mssql_server_security_alert_policy" "example" {
   email_account_admins = true
 }
 
+resource "azurerm_mysql_server" "example" {
+  name                = "example-mysqlserver"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = "mysqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = true
+  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
 
 
