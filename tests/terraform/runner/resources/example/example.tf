4a828173aafedcebff5f41ffac918b6ff2a03a99
arielk <ar.kruszyn@gmail.com>
2020-06-10 11:56:02 +0300

    CKV_AZURE_23

--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1422,4 +1422,20 @@ resource "azurerm_security_center_contact" "good-example" {
   alerts_to_admins    = true
 }
 
+resource "azurerm_sql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+  }
+
 
