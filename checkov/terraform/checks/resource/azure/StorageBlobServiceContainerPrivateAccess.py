eccb81f871e0e9422adace606177469f1585806d
arielkru <63583491+arielkru@users.noreply.github.com>
2020-06-15 09:32:15 +0300

    Update StorageBlobServiceContainerPrivateAccess.py

--- checkov/terraform/checks/resource/azure/StorageBlobServiceContainerPrivateAccess.py
+++ checkov/terraform/checks/resource/azure/StorageBlobServiceContainerPrivateAccess.py
@@ -7,7 +7,7 @@ class StorageBlobServiceContainerPrivateAccess(BaseResourceCheck):
         name = "Ensure that 'Public access level' is set to Private for blob containers"
         id = "CKV_AZURE_34"
         supported_resources = ['azurerm_storage_container']
-        categories = [CheckCategories.LOGGING]
+        categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
