3e40826b038cd5a4a63453566beb8a5df1986078
arielk <ar.kruszyn@gmail.com>
2020-06-15 14:48:38 +0300

    CKV_AZURE_40

---
+++ tests/terraform/checks/resource/azure/test_KeyExpirationDate.py
@@ -0,0 +1,58 @@
+import unittest
+
+import hcl2
+
+from checkov.terraform.checks.resource.azure.KeyExpirationDate import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestKeyExpirationDate(unittest.TestCase):
+
+    def test_failure(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_key_vault_key" "generated" {
+              name         = "generated-certificate"
+              key_vault_id = azurerm_key_vault.example.id
+              key_type     = "RSA"
+              key_size     = 2048
+            
+              key_opts = [
+                "decrypt",
+                "encrypt",
+                "sign",
+                "unwrapKey",
+                "verify",
+                "wrapKey",
+              ]
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_key_vault_key']['generated']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_key_vault_key" "generated" {
+              name         = "generated-certificate"
+              key_vault_id = azurerm_key_vault.example.id
+              key_type     = "RSA"
+              key_size     = 2048
+            
+              key_opts = [
+                "decrypt",
+                "encrypt",
+                "sign",
+                "unwrapKey",
+                "verify",
+                "wrapKey",
+              ]
+              expiration_date = "2020-12-30T20:00:00Z"
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_key_vault_key']['generated']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/terraform/runner/resources/example/example.tf
+++ tests/terraform/runner/resources/example/example.tf
@@ -1599,4 +1599,21 @@ resource "azurerm_role_definition" "example" {
   ]
 }
 
+resource "azurerm_key_vault_key" "generated" {
+  name         = "generated-certificate"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+  expiration_date = "2020-12-30T20:00:00Z"
+}
+
 
