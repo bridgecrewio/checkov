32e0e9a6815128f6ef81f5a16512552840cc7114
barak <schosterbarak@gmail.com>
2020-06-21 21:07:42 +0300

    CKV_GCP_22 should not trigger with remove_default_node_pool=true
    issue: https://github.com/bridgecrewio/checkov/issues/364

--- tests/terraform/checks/resource/gcp/test_GKEUseCosImage.py
+++ tests/terraform/checks/resource/gcp/test_GKEUseCosImage.py
@@ -32,7 +32,7 @@ class TestGKEUseCosImage(unittest.TestCase):
                       # We can't create a cluster with no node pool defined, but we want to only use
                       # separately managed node pools. So we create the smallest possible default
                       # node pool and immediately delete it.
-                      
+                      remove_default_node_pool = true
                       initial_node_count       = 1
                     
                       master_auth {
