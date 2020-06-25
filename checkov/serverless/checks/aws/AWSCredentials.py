40faf9e6a6b8e0fe5e23be30787f5bf10a6f088b
tronxd <tron47@gmail.com>
2020-06-25 12:47:35 +0300

    secret AWS access key and secret detection WIP

--- checkov/serverless/checks/aws/AWSCredentials.py
+++ checkov/serverless/checks/aws/AWSCredentials.py
@@ -1,27 +1,28 @@
 import re
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.provider.base_check import BaseProviderCheck
+from checkov.serverless.checks.base_function_check import BaseFunctionCheck
 from checkov.common.models.consts import access_key_pattern, secret_key_pattern
 
 
-class AWSCredentials(BaseProviderCheck):
+class AWSCredentials(BaseFunctionCheck):
 
     def __init__(self):
         name = "Ensure no hard coded AWS access key and and secret key exists in provider"
         id = "CKV_AWS_41"
-        supported_provider = ['aws']
+        supported_entities = ['serverless_aws']
         categories = [CheckCategories.SECRETS]
-        super().__init__(name=name, id=id, categories=categories, supported_provider=supported_provider)
+        super().__init__(name=name, id=id, categories=categories,  supported_entities=supported_entities)
 
-    def scan_provider_conf(self, conf):
+    def scan_function_conf(self, conf):
         """
         see: https://www.terraform.io/docs/providers/aws/index.html#static-credentials
         """
-        if self.secret_found(conf, "access_key", access_key_pattern):
-            return CheckResult.FAILED
-        if self.secret_found(conf, "secret_key", secret_key_pattern):
-            return CheckResult.FAILED
-        return CheckResult.PASSED
+        # TODO
+        # if self.secret_found(conf, "access_key", access_key_pattern):
+        #     return CheckResult.FAILED
+        # if self.secret_found(conf, "secret_key", secret_key_pattern):
+        #     return CheckResult.FAILED
+        # return CheckResult.PASSED
 
     @staticmethod
     def secret_found(conf, field, pattern):
