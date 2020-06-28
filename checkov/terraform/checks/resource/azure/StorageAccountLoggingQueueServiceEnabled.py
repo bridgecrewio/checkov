ad4ce760ad384503b6358b1f242a1c27f48d59d5
Hong Viet Le <hvle@octo.com>
2020-06-28 16:11:48 +0200

    CKV_AZURE_33 queue logging and account kind
    queue_properties doesn't apply for other account kind than Storage and StorageV2

--- checkov/terraform/checks/resource/azure/StorageAccountLoggingQueueServiceEnabled.py
+++ checkov/terraform/checks/resource/azure/StorageAccountLoggingQueueServiceEnabled.py
@@ -11,6 +11,9 @@ class StorageAccountLoggingQueueServiceEnabled(BaseResourceCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
+        if 'account_kind' in conf and (conf['account_kind'][0] != 'Storage' or conf['account_kind'][0] != 'StorageV2'):
+            # queue_properties block doesn't apply for other account kind
+            return CheckResult.PASSED
         if 'queue_properties' in conf and 'logging' in conf['queue_properties'][0]:
             logging = conf['queue_properties'][0]['logging'][0]
             if logging['delete'][0] and logging['write'][0] and logging['read'][0]:
