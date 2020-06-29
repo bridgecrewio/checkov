27404bed782b4c009f4cd15666ed38e9ea5edbed
arielk <ar.kruszyn@gmail.com>
2020-06-29 20:48:56 +0300

    CKV_AWS_8 - bug fix - check for encryption in all of the block_devices before defining result

--- tests/terraform/checks/resource/aws/test_LaunchConfigurationEBSEncryption.py
+++ tests/terraform/checks/resource/aws/test_LaunchConfigurationEBSEncryption.py
@@ -1,5 +1,7 @@
 import unittest
 
+import hcl2
+
 from checkov.terraform.checks.resource.aws.LaunchConfigurationEBSEncryption import check
 from checkov.common.models.enums import CheckResult
 
@@ -17,9 +19,47 @@ class TestS3Encryption(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_failure_multiple_blocks(self):
+        hcl_res = hcl2.loads("""
+                            resource "aws_instance" "test" {
+                              ami                  = var.ami_id
+                              instance_type        = var.instance_type
+                              key_name             = var.key_name
+
+                              root_block_device {
+                                volume_type = "gp2"
+                                volume_size = var.root_volume_size
+                                encrypted = true
+                              }
+
+
+                              ebs_block_device {
+                                volume_type = "gp2"
+                                volume_size = var.ebs_volume_size
+                                device_name = "/dev/xvdb"
+                                encrypted = false
+                              }
+                            }
+                                """)
+        resource_conf = hcl_res['resource'][0]['aws_instance']['test']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
     def test_success(self):
-        resource_conf = {'image_id': ['ami-123'], 'instance_type': ['t2.micro'],
-                         'root_block_device': [{'encrypted': [True]}]}
+        hcl_res = hcl2.loads("""
+                        resource "aws_instance" "test" {
+                              ami                  = var.ami_id
+                              instance_type        = var.instance_type
+                              key_name             = var.key_name
+                            
+                              root_block_device {
+                                volume_type = "gp2"
+                                volume_size = var.root_volume_size
+                                encrypted = true
+                              }
+                            }
+                        """)
+        resource_conf = hcl_res['resource'][0]['aws_instance']['test']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
