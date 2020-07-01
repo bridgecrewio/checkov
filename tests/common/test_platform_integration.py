ba854e83f4d3e481f543e6903a781b03e3b02a30
Guy Sulema <48174831+guy-suli@users.noreply.github.com>
2020-07-01 19:04:02 +0300

    WIP Add CI/CD type to PUT method (#399)
    * Add CI/CD type to PUT
    
    * Add source type to PUT as ENV
    
    * Add test for BC_SOURCE
    
    * Add CI/CD type to PUT
    
    * Add CI/CD type to PUT
    
    * Add CI/CD type to PUT
    
    * Delete unused and fix blank line
    
    * Change unit tests
    
    Co-authored-by: guy sulema <suli@virtue.im>

--- tests/common/test_platform_integration.py
+++ tests/common/test_platform_integration.py
@@ -1,16 +1,24 @@
 import os
 import unittest
-
 from unittest import mock
-
+from checkov.common.bridgecrew.platform_integration import BcPlatformIntegration
 
 
 class TestBCApiUrl(unittest.TestCase):
 
     @mock.patch.dict(os.environ,{'BC_API_URL':'foo'})
     def test_overriding_bc_api_url(self):
-        from checkov.common.bridgecrew.platform_integration import BC_API_URL
-        self.assertEqual(BC_API_URL,"foo")
+        instance = BcPlatformIntegration()
+        self.assertEqual(instance.bc_api_url,"foo")
+
+    @mock.patch.dict(os.environ,{'BC_SOURCE':'foo'})
+    def test_overriding_bc_source(self):
+        instance = BcPlatformIntegration()
+        self.assertEqual(instance.bc_source,"foo")
+
+    def test_default_bc_source(self):
+        instance = BcPlatformIntegration()
+        self.assertEqual(instance.bc_source,"cli")
 
 
 if __name__ == '__main__':
