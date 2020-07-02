406630f2262e0171b8982ef641b4e8db4463ec78
barak <schosterbarak@gmail.com>
2020-07-02 21:50:58 +0300

    add hcl dependency

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         "junit-xml==1.8",
         "lark-parser==0.7.8",
         "python-hcl2==0.2.5",
+        "pyhcl==0.4.4",
         "pyyaml==5.2",
         "requests==2.22.0",
         "six==1.13.0",
