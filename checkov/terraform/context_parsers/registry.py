c8f8c9ae190909813f325bd0826a3ad3edbf63c9
Barak Schoster Goihman <barak@bridgecrew.io>
2020-07-09 17:05:58 +0300

    re add bc_source (#427)

--- checkov/terraform/context_parsers/registry.py
+++ checkov/terraform/context_parsers/registry.py
@@ -18,13 +18,14 @@ class ParserRegistry:
     def enrich_definitions_context(self, definitions):
         supported_definitions = [parser_type for parser_type in self.context_parsers.keys()]
         (tf_file, definition_blocks_types) = definitions
-        definition_blocks_types = {x: definition_blocks_types[x] for x in definition_blocks_types.keys()}
-        for definition_type in definition_blocks_types.keys():
-            if definition_type in supported_definitions:
-                dpath.new(self.definitions_context, [tf_file, definition_type], {})
-                context_parser = self.context_parsers[definition_type]
-                definition_blocks = definition_blocks_types[definition_type]
-                self.definitions_context[tf_file][definition_type] = context_parser.run(tf_file, definition_blocks)
+        if definition_blocks_types:
+            definition_blocks_types = {x: definition_blocks_types[x] for x in definition_blocks_types.keys()}
+            for definition_type in definition_blocks_types.keys():
+                if definition_type in supported_definitions:
+                    dpath.new(self.definitions_context, [tf_file, definition_type], {})
+                    context_parser = self.context_parsers[definition_type]
+                    definition_blocks = definition_blocks_types[definition_type]
+                    self.definitions_context[tf_file][definition_type] = context_parser.run(tf_file, definition_blocks)
 
         return self.definitions_context
 
