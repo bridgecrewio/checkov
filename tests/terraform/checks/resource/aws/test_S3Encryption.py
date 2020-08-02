05b102159cde20f53ecdf62b86cdb096e5e390a6
barak <schosterbarak@gmail.com>
2020-08-02 21:10:46 +0300

    fix hcl2 parser

--- tests/terraform/checks/resource/aws/test_S3Encryption.py
+++ tests/terraform/checks/resource/aws/test_S3Encryption.py
@@ -1,5 +1,7 @@
 import unittest
 
+import hcl2
+
 from checkov.terraform.checks.resource.aws.S3Encryption import check
 from checkov.common.models.enums import CheckResult
 
@@ -33,6 +35,26 @@ class TestS3Encryption(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
+    def test_success_oneline(self):
+        hcl_res = hcl2.loads("""
+        resource "aws_s3_bucket" "my" {
+          bucket = "my-${lower(var.environment)}-my-${lower(var.my)}"
+          acl    = "private"
+          # Delete S3 Bucket even it has some objects in it
+          force_destroy = "true"
+          server_side_encryption_configuration {
+            rule {
+              apply_server_side_encryption_by_default { sse_algorithm = "AES256" }
+            }
+          }
+          versioning {
+            enabled = false
+          }
+        }
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_s3_bucket']['my']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
 
 if __name__ == '__main__':
     unittest.main()
