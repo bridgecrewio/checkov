1f5d4b8d9d0a57a917861eff46c832244d58dcee
Mike Urbanski <mike@bridgecrew.io>
2020-08-12 19:29:51 -0500

    add tests for bucket_policy object

--- tests/terraform/checks/resource/aws/test_S3AllowsAnyPrincipal.py
+++ tests/terraform/checks/resource/aws/test_S3AllowsAnyPrincipal.py
@@ -37,6 +37,36 @@ POLICY
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_failure_policyobj(self):
+        hcl_res = hcl2.loads("""
+resource "aws_s3_bucket_policy" "s3" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "*"
+            }
+        }
+    ]
+}
+POLICY
+}        
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_s3_bucket_policy']['s3']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
     def test_failure_array(self):
         hcl_res = hcl2.loads("""
 resource "aws_s3_bucket" "s3" {
@@ -150,6 +180,36 @@ POLICY
 }        
         """)
         resource_conf = hcl_res['resource'][0]['aws_s3_bucket']['s3']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_policyobj(self):
+        hcl_res = hcl2.loads("""
+resource "aws_s3_bucket_policy" "s3" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "some_arn"
+            }
+        }
+    ]
+}
+POLICY
+}        
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_s3_bucket_policy']['s3']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
