d116873282acb1d4cf2996c00ce2825730f4fbbd
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-08-13 04:26:43 -0500

    Add any principal check (#493)
    * handle AWS principal as string or array
    
    * add check for S3 policy allowing any principal
    
    * handle deny statement
    
    * add tests for bucket_policy object
    
    * fix tests
    
    Co-authored-by: barak <schosterbarak@gmail.com>

--- tests/terraform/checks/resource/aws/test_IAMRoleAllowsPublicAssume.py
+++ tests/terraform/checks/resource/aws/test_IAMRoleAllowsPublicAssume.py
@@ -40,6 +40,39 @@ EOF
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_failure_array(self):
+        hcl_res = hcl2.loads("""
+resource "aws_iam_role" "lambdaRole" {
+    name = "test-role"
+    assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal" : {"Service": "lambda.amazonaws.com"},
+"Effect": "Allow"
+},
+{
+"Action": "sts:AssumeRole",
+"Principal" : {"AWS": ["*"]},
+"Effect": "Allow"
+},
+{
+"Action": "sts:AssumeRole",
+"Principal" : {"Service": "events.amazonaws.com"},
+"Effect": "Allow"
+}
+]
+}
+
+EOF
+}        
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_iam_role']['lambdaRole']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
     def test_success(self):
         hcl_res = hcl2.loads("""
 resource "aws_iam_role" "lambdaRole" {
@@ -52,6 +85,29 @@ resource "aws_iam_role" "lambdaRole" {
 "Action": "sts:AssumeRole",
 "Principal" : {"Service": "lambda.amazonaws.com"},
 "Effect": "Allow"
+}
+]
+}
+
+EOF
+}        
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_iam_role']['lambdaRole']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_deny(self):
+        hcl_res = hcl2.loads("""
+resource "aws_iam_role" "lambdaRole" {
+    name = "test-role"
+    assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal" : {"AWS": "*"},
+"Effect": "Deny"
 }
 ]
 }
