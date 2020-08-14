26185c37e6845629afe86ac4898383c0449f7e22
Jonas Eckert <j.eckert@ostfalia.de>
2020-08-14 12:17:56 +0200

    Add test for _fix_scan_entity_conf

---
+++ tests/common/checks/test_base_check.py
@@ -0,0 +1,61 @@
+import unittest
+
+from checkov.common.checks.base_check import BaseCheck
+from checkov.common.checks.base_check_registry import BaseCheckRegistry
+from checkov.common.models.enums import CheckResult
+
+
+class TestCheckTypeNotInSignature(BaseCheck):
+
+    def __init__(self):
+        name = "Example check"
+        categories = []
+        id = "CKV_T_1"
+        supported_entities = ["module"]
+        block_type = "module"
+        super().__init__(name=name, id=id, categories=categories, supported_entities=supported_entities,
+                         block_type=block_type)
+
+    # noinspection PyMethodOverriding
+    def scan_entity_conf(self, conf):
+        return CheckResult.PASSED
+
+
+# noinspection DuplicatedCode
+class TestBaseCheck(unittest.TestCase):
+
+    def test_entity_type_is_not_required_in_signature(self):
+        registry = BaseCheckRegistry()
+        check = TestCheckTypeNotInSignature()
+        registry.register(check)
+
+        # noinspection PyArgumentList
+        scan_result = check.scan_entity_conf({}, "Some name")
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_invalid_signature_is_detected(self):
+        with self.assertRaises(NotImplementedError) as context:
+            class TestCheckUnknownSignature(BaseCheck):
+
+                def __init__(self):
+                    name = "Example check"
+                    categories = []
+                    id = "CKV_T_1"
+                    supported_entities = ["module"]
+                    block_type = "module"
+                    super().__init__(name=name, id=id, categories=categories, supported_entities=supported_entities,
+                                     block_type=block_type)
+
+                # noinspection PyMethodOverriding
+                def scan_entity_conf(self, conf, some_unexpected_parameter_123):
+                    return CheckResult.PASSED
+        self.assertIsInstance(context.exception, NotImplementedError)
+        self.assertEquals(
+            f'The signature {["self", "conf", "some_unexpected_parameter_123"]} for scan_entity_conf is not supported. '
+            f'See {BaseCheck.__module__}.{BaseCheck.__name__}.{BaseCheck.scan_entity_conf.__name__}.',
+            context.exception.args[0]
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
