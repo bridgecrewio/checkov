3f712273151715ec51796aee982ee6e85c871dc5
arielk <ar.kruszyn@gmail.com>
2020-08-19 19:24:31 +0300

    members_conf lines to ternary operator

--- checkov/terraform/checks/resource/gcp/AbsGoogleIAMMemberDefaultServiceAccount.py
+++ checkov/terraform/checks/resource/gcp/AbsGoogleIAMMemberDefaultServiceAccount.py
@@ -13,11 +13,7 @@ class AbsGoogleIAMMemberDefaultServiceAccount(BaseResourceCheck):
         super().__init__(name, id, categories, supported_resources)
 
     def scan_resource_conf(self, conf):
-        members_conf = []
-        if 'members' in conf:
-            members_conf = conf['members'][0]
-        elif 'member' in conf:
-            members_conf = conf['member']
+        members_conf = conf['members'][0] if 'members' in conf else conf.get('member', [])
         if any(re.match(DEFAULT_SA, member) for member in members_conf):
             return CheckResult.FAILED
         return CheckResult.PASSED
