fac6d7f5eb7ee054c96b9e031175f68e2726b9c1
arielk <ar.kruszyn@gmail.com>
2020-08-19 17:28:42 +0300

    GCP_47 - default service account at org level

---
+++ checkov/terraform/checks/resource/gcp/GoogleOrgMemberDefaultServiceAccount.py
@@ -0,0 +1,13 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.gcp.AbsGoogleIAMMemberDefaultServiceAccount import AbsGoogleIAMMemberDefaultServiceAccount
+
+class GoogleOrgMemberDefaultServiceAccount(AbsGoogleIAMMemberDefaultServiceAccount):
+    def __init__(self):
+        name = "Ensure default service account is not used at an organization level"
+        id = "CKV_GCP_47"
+        supported_resources = ['google_organization_iam_member', 'google_organization_iam_binding']
+        categories = [CheckCategories.IAM]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+
+check = GoogleOrgMemberDefaultServiceAccount()
