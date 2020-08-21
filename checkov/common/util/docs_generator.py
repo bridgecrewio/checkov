383140abbadd4fc3be0c111cbb4a85f34915dbe3
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-08-21 08:54:26 -0500

    sort checks in the same framework by number (#495)
    * sort checks in the same framework by number
    
    * Create a comparison key based on the check id (#500)
    
    The id is split into text and numbers. They are compared from left to
    right. It also handles leading zeros, which are placed after te number
    with one leading zero fewer. (1 -> 01 -> 001 ...)
    
    Co-authored-by: Jonas Eckert <j.eckert@ostfalia.de>
    
    Co-authored-by: Jonas Eckert <59320234+angatha@users.noreply.github.com>
    Co-authored-by: Jonas Eckert <j.eckert@ostfalia.de>

--- checkov/common/util/docs_generator.py
+++ checkov/common/util/docs_generator.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 
+import re
+
 from tabulate import tabulate
 
-from checkov.terraform.checks.data.registry import data_registry
-from checkov.terraform.checks.resource.registry import resource_registry
-from checkov.terraform.checks.provider.registry import provider_registry
+from checkov.arm.registry import arm_registry
 from checkov.cloudformation.checks.resource.registry import cfn_registry as cfn_registry
 from checkov.kubernetes.registry import registry as k8_registry
 from checkov.serverless.registry import sls_registry
-from checkov.arm.registry import arm_registry
+from checkov.terraform.checks.data.registry import data_registry
+from checkov.terraform.checks.provider.registry import provider_registry
+from checkov.terraform.checks.resource.registry import resource_registry
+
+ID_PARTS_PATTERN = re.compile(r'(\D*)(\d*)')
+
+
+def get_compare_key(c):
+    res = []
+    for match in ID_PARTS_PATTERN.finditer(c[0]):
+        text, number = match.groups()
+        numeric_value = int(number) if number else 0
+        # count number of leading zeros
+        same_number_ordering = len(number) - len(number.lstrip('0'))
+        res.append((text, numeric_value, same_number_ordering))
+    return res
 
 
 def print_checks(framework="all"):
@@ -47,7 +62,8 @@ def get_checks(framework="all"):
         for key in arm_registry.checks.keys():
             for check in arm_registry.checks[key]:
                 printable_checks_list.append([check.id, "resource", key, check.name, "arm"])
-    return sorted(printable_checks_list, key=lambda x: x[0])
+    return sorted(printable_checks_list, key=get_compare_key)
+
 
 if __name__ == '__main__':
     print_checks()
