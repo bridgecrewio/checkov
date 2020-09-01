8b75ad89cab30da4d02bf628aff8972d1d44265b
Jonas Eckert <j.eckert@ostfalia.de>
2020-09-01 19:25:57 +0200

    Add a test to check that entity_type is set correctly
    This only tests for terraform resources.

---
+++ tests/terraform/runner/resources/valid_tf_only_resource_usage/example.tf
@@ -0,0 +1,7 @@
+resource "type_1" "resource" {
+  a = 1
+}
+
+resource "type_2" "resource" {
+  b = 2
+}
--- tests/terraform/runner/test_runner.py
+++ tests/terraform/runner/test_runner.py
@@ -4,7 +4,6 @@ import unittest
 import dpath.util
 
 from checkov.runner_filter import RunnerFilter
-from checkov.terraform.checks.module.base_module_check import BaseModuleCheck
 from checkov.terraform.context_parsers.registry import parser_registry
 from checkov.terraform.runner import Runner
 
@@ -200,41 +199,49 @@ class TestRunnerValid(unittest.TestCase):
         self.assertEqual(len(result.passed_checks), 1)
         self.assertIn('module.some-module', map(lambda record: record.resource, result.passed_checks))
 
-    def test_typed_terraform_module_checks_are_performed(self):
-        check_name = "CKV_TF_M_1"
+    def test_typed_terraform_resource_checks_are_performed(self):
         test_self = self
+        check_name = "TF_M_2"
+        test_dir = "resources/valid_tf_only_resource_usage"
 
         from checkov.common.models.enums import CheckResult
-        from checkov.terraform.checks.module.registry import module_registry
+        from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+        from checkov.terraform.checks.resource.registry import resource_registry
 
-        class ModuleCheck(BaseModuleCheck):
+        class ResourceCheck(BaseResourceCheck):
 
             def __init__(self):
                 name = "Test check"
                 id = check_name
-                supported_resources = ['module']
+                supported_resources = ['*']
                 categories = []
                 super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-            def scan_module_conf(self, conf, entity_type):
-                nonlocal test_self
-                test_self.assertEqual("module", entity_type)
+            def scan_resource_conf(self, conf, entity_type):
+                if entity_type == 'type_1':
+                    test_self.assertIn('a', conf)
+                    test_self.assertEquals([1], conf['a'])
+                elif entity_type == 'type_2':
+                    test_self.assertIn('b', conf)
+                    test_self.assertEquals([2], conf['b'])
+                else:
+                    test_self.fail(f'Unexpected entity_type: {entity_type}. Expected type_1 or type_2, because no '
+                                   f'other resources are defined in the files inside of {test_dir}.')
                 return CheckResult.PASSED
 
-        check = ModuleCheck()
+        check = ResourceCheck()
 
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        valid_dir_path = os.path.join(current_dir, "resources/valid_tf_only_module_usage")
+        valid_dir_path = os.path.join(current_dir, test_dir)
         runner = Runner()
         result = runner.run(root_folder=valid_dir_path, external_checks_dir=None,
                             runner_filter=RunnerFilter(checks=check_name))
 
         # unregister check
         for resource in check.supported_resources:
-            module_registry.checks[resource].remove(check)
+            resource_registry.wildcard_checks[resource].remove(check)
 
-        self.assertEqual(len(result.passed_checks), 1)
-        self.assertIn('module.some-module', map(lambda record: record.resource, result.passed_checks))
+        self.assertEqual(len(result.passed_checks), 2)
 
     def tearDown(self):
         parser_registry.definitions_context = {}
