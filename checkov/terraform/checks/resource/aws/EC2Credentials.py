dbdb2a93577fc732a29a1cec0d902cb98e94a7d6
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-09-04 11:30:23 -0500

    refactor secrets regexes to be more expandable (#538)
    * refactor secrets regexes to be more expandable
    
    * add secret method docstring
    
    * make AWS secret checks only check AWS creds (to keep functionality the same)
    
    * fix docstring
    
    * fix test secrets to not trigger security alert
    
    * fix docstring typo
    
    * use constants instead of strings for regex categories

--- checkov/terraform/checks/resource/aws/EC2Credentials.py
+++ checkov/terraform/checks/resource/aws/EC2Credentials.py
@@ -1,14 +1,12 @@
-import re
-
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.terraform.checks.utils.consts import access_key_pattern, secret_key_pattern
+from checkov.common.util.secrets import string_has_secrets, AWS
 
 
 class EC2Credentials(BaseResourceCheck):
 
     def __init__(self):
-        name = "Ensure no hard coded AWS access key and and secret key exists in EC2 user data"
+        name = "Ensure no hard coded AWS access key and secret key exists in EC2 user data"
         id = "CKV_AWS_46"
         supported_resources = ['aws_instance']
         categories = [CheckCategories.SECRETS]
@@ -18,8 +16,7 @@ class EC2Credentials(BaseResourceCheck):
         if 'user_data' in conf.keys():
             user_data = conf['user_data'][0]
             if isinstance(user_data, str):
-                if re.match(".*{}.*".format(access_key_pattern), conf['user_data'][0], re.DOTALL) or re.match(
-                        ".*{}.*".format(secret_key_pattern), conf['user_data'][0], re.DOTALL):
+                if string_has_secrets(user_data, AWS):
                     return CheckResult.FAILED
         return CheckResult.PASSED
 
---
+++ tests/unit/test_secrets.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+
+from checkov.common.util.secrets import *
+
+
+class TestSecrets(unittest.TestCase):
+
+    def test_secrets(self):
+        test_strings = [
+            'AKIAIOSFODNN7EXAMPLE',
+            'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+            '-----BEGIN RSA PRIVATE KEY-----\n',
+            'Hello from Bridgecrew'
+        ]
+
+        # check that no category checks all
+        self.assertEqual(3, sum(1 for s in test_strings if string_has_secrets(s)))
+
+        # check one category
+        self.assertEqual(2, sum(1 for s in test_strings if string_has_secrets(s, AWS)))
+
+        # check two categories
+        self.assertEqual(3, sum(1 for s in test_strings if string_has_secrets(s, AWS, GENERAL)))
+
+        # check explicit all
+        self.assertEqual(3, sum(1 for s in test_strings if string_has_secrets(s, ALL)))
+
+        # check explicit all plus another category
+        self.assertEqual(3, sum(1 for s in test_strings if string_has_secrets(s, ALL, AWS)))
