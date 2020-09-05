cadf3a456348d639601b6a527935f65bbf832195
Jonas Eckert <j.eckert@ostfalia.de>
2020-09-05 09:40:48 +0200

    Make supported_resources deprecated

--- checkov/terraform/checks/module/base_module_check.py
+++ checkov/terraform/checks/module/base_module_check.py
@@ -5,7 +5,20 @@ from checkov.terraform.checks.module.registry import module_registry
 
 
 class BaseModuleCheck(BaseCheck):
-    def __init__(self, name, id, categories, supported_resources):
+    def __init__(self, name, id, categories, supported_resources=None):
+        """
+        Base class for terraform module call related checks.
+
+        :param name: an error message that is shown, when the check failed.
+        :param id: the id of the check
+        :param categories: categories of the check
+        :param supported_resources: DEPRECATED the resources that this check applies to.
+
+            This is deprecated because there is only one resource type that is valid for
+            checks that extend this class.
+        """
+        if supported_resources is None:
+            supported_resources = ['module']
         super().__init__(name=name, id=id, categories=categories, supported_entities=supported_resources,
                          block_type="module")
         self.supported_resources = supported_resources
