043cf99f8850b867487ee6bd13377b1663edb095
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-09-06 19:02:51 +0300

    Update DocDBLogging.py

--- checkov/terraform/checks/resource/aws/DocDBLogging.py
+++ checkov/terraform/checks/resource/aws/DocDBLogging.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 class DocDBLogging(BaseResourceCheck):
     def __init__(self):
         name = "Ensure DocDB Logging is enabled"
-        id = "CKV_AWS_83"
+        id = "CKV_AWS_85"
         supported_resources = ['aws_docdb_cluster']
         categories = [CheckCategories.LOGGING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
