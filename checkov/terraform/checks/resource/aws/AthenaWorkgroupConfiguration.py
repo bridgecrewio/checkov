239e0b935bac43cac7ce2a3d99a25ddab7fbf5a2
drfaust92 <ilia.lazebnik@gmail.com>
2020-09-06 08:58:19 +0300

    simplify check

--- checkov/terraform/checks/resource/aws/AthenaWorkgroupConfiguration.py
+++ checkov/terraform/checks/resource/aws/AthenaWorkgroupConfiguration.py
@@ -1,8 +1,9 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class AthenaWorkgroupConfiguration(BaseResourceCheck):
+class AthenaWorkgroupConfiguration(BaseResourceValueCheck):
+
     def __init__(self):
         name = "Ensure Athena Workgroup should enforce configuration to prevent client disabling encryption"
         id = "CKV_AWS_82"
@@ -10,14 +11,8 @@ class AthenaWorkgroupConfiguration(BaseResourceCheck):
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        if 'configuration' in conf.keys():
-            if 'enforce_workgroup_configuration' in conf['configuration'][0]:
-                if conf['configuration'][0]['enforce_workgroup_configuration'] is True:
-                    return CheckResult.PASSED
-                else:
-                    return CheckResult.FAILED
-        return CheckResult.PASSED
+    def get_inspected_key(self):
+        return "configuration/[0]/enforce_workgroup_configuration"
 
 
 check = AthenaWorkgroupConfiguration()
