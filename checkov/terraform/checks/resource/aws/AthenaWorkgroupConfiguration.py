f6ecd4bdeb17d8fa97405caf399d5d2237df63f4
drfaust92 <ilia.lazebnik@gmail.com>
2020-09-06 10:11:00 +0300

    success on missing block

--- checkov/terraform/checks/resource/aws/AthenaWorkgroupConfiguration.py
+++ checkov/terraform/checks/resource/aws/AthenaWorkgroupConfiguration.py
@@ -1,4 +1,4 @@
-from checkov.common.models.enums import CheckCategories
+from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
@@ -9,7 +9,8 @@ class AthenaWorkgroupConfiguration(BaseResourceValueCheck):
         id = "CKV_AWS_82"
         supported_resources = ['aws_athena_workgroup']
         categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED)
 
     def get_inspected_key(self):
         return "configuration/[0]/enforce_workgroup_configuration"
