6feee09f80a797ecb67fb7c7645c5fd37cedce3a
Nimrod Kor <nimrodkor@gmail.com>
2020-10-07 15:21:59 +0300

    Bump urllib3 version to 1.25.10

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
             "sphinxcontrib-jsmath==1.0.1",
             "sphinxcontrib-qthelp==1.0.2",
             "sphinxcontrib-serializinghtml==1.1.3",
-            "urllib3==1.25.7",
+            "urllib3==1.25.10",
             "wcwidth==0.1.7",
             "zipp==0.6.0",
             "GitPython==3.1.7",
@@ -81,7 +81,7 @@ setup(
         "six==1.15.0",
         "tabulate==0.8.6",
         "termcolor==1.1.0",
-        "urllib3==1.25.7",
+        "urllib3==1.25.10",
         "dpath==1.5.0",
         "GitPython==3.1.7",
         "gitdb==4.0.5"
