9cd76bbda48a833abcd749edad4872b2216197b7
Nimrod Kor <nimrodkor@gmail.com>
2020-10-21 11:25:13 +0300

    Handle case where NSG rule is not a dict

--- tests/terraform/checks/resource/azure/test_NSGRuleSSHAccessRestricted.py
+++ tests/terraform/checks/resource/azure/test_NSGRuleSSHAccessRestricted.py
@@ -48,6 +48,37 @@ class TestNSGRuleSSHAccessRestricted(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
+    def test_unknown(self):
+        hcl_res = hcl2.loads("""
+resource "azurerm_network_security_group" "mynsg" {
+    name = var.nsg_name
+    resource_group_name = azurerm_resource_group.rg.name
+    location = var.location
+
+    security_rule = [for rule in var.security_rules : {
+        name = rule.name
+        priority = rule.priority
+        source_address_prefix = lookup(rule, "source_address_prefixes", []) == [] ? lookup(rule, "source_address_prefix", var.nsg_default_source_address_prefix) : ""
+        source_address_prefixes = lookup(rule, "source_address_prefixes", [])
+        access = lookup(rule, "access", var.nsg_default_access)
+        destination_port_range = lookup(rule, "destination_port_ranges", []) == [] ? lookup(rule, "destination_port_range", var.nsg_default_destination_port_range) : ""
+        destination_port_ranges = lookup(rule, "destination_port_ranges", [])
+        direction = lookup(rule, "direction", var.nsg_default_direction)
+        protocol = lookup(rule, "protocol", var.nsg_default_protocol)
+        source_port_range = lookup(rule, "source_port_range", var.nsg_default_source_port_range)
+        description = ""
+        destination_address_prefix = "*"
+        destination_address_prefixes = []
+        destination_application_security_group_ids = []
+        source_application_security_group_ids = []
+        source_port_ranges = []
+    }]
+}
+        """)
+
+        resource_conf = hcl_res['resource'][0]['azurerm_network_security_group']['mynsg']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
 
 
 if __name__ == '__main__':
