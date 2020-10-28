91f2758a0cd7e16bbe17bdf8440b0466d9a83a68
robeden <rob@robeden.com>
2020-10-28 16:34:26 -0500

    Various comments and trivial formatting for clarity

--- checkov/terraform/parser.py
+++ checkov/terraform/parser.py
@@ -37,22 +37,26 @@ class Parser:
             filter_ignored_directories(d_names)
             self._mark_parsed(os.path.abspath(root))
             for file in f_names:
-                if file.endswith(".tf"):
-                    tf_file = os.path.join(root, file)
-                    if tf_file not in tf_definitions.keys():
-                        try:
-                            tf_definition = self._parse_tf_definitions(tf_file)
-                            if tf_definition:
-                                tf_definitions[tf_file] = tf_definition
-                            for modules in tf_definition.get("module", []):
-                                for module in modules.values():
-                                    relative_path = module['source'][0]
-                                    abs_path = os.path.abspath(os.path.join(root, relative_path))
-                                    if not self._is_parsed(abs_path):
-                                        modules_scan.add(abs_path)
-                        except Exception as e:
-                            self.logger.debug(f'failed while parsing file {tf_file}', exc_info=e)
-                            parsing_errors[tf_file] = e
+                if not file.endswith(".tf"):
+                    continue
+
+                tf_file = os.path.join(root, file)
+                if tf_file in tf_definitions.keys():
+                    continue
+
+                try:
+                    tf_definition = self._parse_tf_definitions(tf_file)
+                    if tf_definition:
+                        tf_definitions[tf_file] = tf_definition
+                    for modules in tf_definition.get("module", []):
+                        for module in modules.values():
+                            relative_path = module['source'][0]
+                            abs_path = os.path.abspath(os.path.join(root, relative_path))
+                            if not self._is_parsed(abs_path):
+                                modules_scan.add(abs_path)
+                except Exception as e:
+                    self.logger.debug(f'failed while parsing file {tf_file}', exc_info=e)
+                    parsing_errors[tf_file] = e
         for m in modules_scan:
             if path.exists(m):
                 self.hcl2(directory=m, tf_definitions=tf_definitions)
