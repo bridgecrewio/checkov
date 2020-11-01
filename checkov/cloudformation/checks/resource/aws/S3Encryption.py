8643beca0331a58889b85ecf6f363e66b4942b1f
Mike Urbanski <mike@bridgecrew.io>
2020-11-01 09:42:06 -0600

    update S3Encryption to use fix PR

--- checkov/cloudformation/checks/resource/aws/S3Encryption.py
+++ checkov/cloudformation/checks/resource/aws/S3Encryption.py
@@ -1,8 +1,9 @@
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
 
 
-class S3Encryption(BaseResourceCheck):
+class S3Encryption(BaseResourceValueCheck):
+
     def __init__(self):
         name = "Ensure the S3 bucket has server-side-encryption enabled"
         id = "CKV_AWS_19"
@@ -10,13 +11,14 @@ class S3Encryption(BaseResourceCheck):
         categories = [CheckCategories.LOGGING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        if conf.get('Properties'):
-            if conf['Properties'].get('ServerSideEncryptionRule') or \
-                (conf['Properties'].get('BucketEncryption') and
-                 len(conf['Properties']['BucketEncryption'].get('ServerSideEncryptionConfiguration', [])) > 0):
-                return CheckResult.PASSED
-        return CheckResult.FAILED
+    def get_inspected_key(self):
+        return 'Properties/BucketEncryption/ServerSideEncryptionConfiguration/0/ServerSideEncryptionByDefault/SSEAlgorithm'
+
+    def get_expected_value(self):
+        return 'AES256'
+
+    def get_expected_values(self):
+        return [self.get_expected_value(), 'aws:kms']
 
 
 check = S3Encryption()
