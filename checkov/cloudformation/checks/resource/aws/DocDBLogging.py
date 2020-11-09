efe3816645945039ac9136dc97f87015281e7980
Nick <njgibbon@outlook.com>
2020-11-09 21:38:46 +0000

    DocDB Export to CloudWatch Logs - Cloudformation Implementation (#660)
    * added check and tests for docdb encryption for cloudformation
    
    * added MasterUsername and password parameters based on CF lint failure from actions
    
    * Check and tests for ensuring DocDB TLS is not disabled as it is enabled by default - Corresponds with terraform check already completed under https://github.com/bridgecrewio/checkov/issues/165
    
    * update to latest
    
    * check - cloudformation implementation of DocDB export to cloudwatch logs with logic coming from already existing terraform check
    
    * deleting some accidentally added files
    
    * unix eols

---
+++ checkov/cloudformation/checks/resource/aws/DocDBLogging.py
@@ -0,0 +1,22 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class DocDBLogging(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure DocDB Logging is enabled"
+        id = "CKV_AWS_85"
+        supported_resources = ['AWS::DocDB::DBCluster']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        log_types = ["profiler", "audit"]
+        if 'Properties' in conf.keys():
+            if 'EnableCloudwatchLogsExports' in conf['Properties'].keys():
+                if all(elem in conf['Properties']['EnableCloudwatchLogsExports'] for elem in log_types):
+                    return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = DocDBLogging()
