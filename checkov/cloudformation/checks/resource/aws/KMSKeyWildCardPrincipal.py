19b8f04d9e1586de552ba1cba854cd0391d68f33
Avishay Bar <avishay.il@gmail.com>
2020-11-19 12:34:28 +0200

    - more improvements

--- checkov/cloudformation/checks/resource/aws/KMSKeyWildCardPrincipal.py
+++ checkov/cloudformation/checks/resource/aws/KMSKeyWildCardPrincipal.py
@@ -2,6 +2,34 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
+def get_recursively(search_dict, field):
+    """
+    Takes a dict with nested lists and dicts,
+    and searches all dicts for a key of the field
+    provided.
+    """
+    fields_found = []
+
+    for key, value in search_dict.items():
+
+        if key == field:
+            fields_found.append(value)
+
+        elif isinstance(value, dict):
+            results = get_recursively(value, field)
+            for result in results:
+                fields_found.append(result)
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    more_results = get_recursively(item, field)
+                    for another_result in more_results:
+                        fields_found.append(another_result)
+
+    return fields_found
+
+
 class KMSKeyWildCardPrincipal(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure KMS key policy does not contain wildcard (*) principal"
@@ -17,15 +45,16 @@ class KMSKeyWildCardPrincipal(BaseResourceValueCheck):
         if conf.get('Properties'):
             if conf['Properties'].get('KeyPolicy'):
                 policy_block = conf['Properties']['KeyPolicy']
-                if 'Statement' in policy_block.keys():
-                    for policy_property in policy_block:
-                        if policy_property == 'Statement':
-                            for policy_statement in policy_block['Statement']:
-                                if 'Principal' in policy_statement.keys():
-                                    for policy_principal_property in policy_statement['Principal']:
-                                        policy_principal_value = policy_statement['Principal'][policy_principal_property]
-                                        if str(policy_principal_value) == '*':
-                                            return CheckResult.FAILED
+                principals_list = get_recursively(policy_block, 'Principal')
+                for principal in principals_list:
+                    if isinstance(principal, dict):
+                        for principal_key, principal_value in principal.items():
+                            if principal_value == '*':
+                                return CheckResult.FAILED
+                    else:
+                        if principal == '*':
+                            return CheckResult.FAILED
+
         return CheckResult.PASSED
 
 
