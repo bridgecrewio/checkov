eae7dd80f9a2d839b195488602f20a8203aa298c
Barak Schoster Goihman <barak@bridgecrew.io>
2020-11-29 09:59:44 +0200

    add child modules scanning. fix https://github.com/bridgecrewio/checkov/issues/691 (#697)

--- checkov/terraform/plan_parser.py
+++ checkov/terraform/plan_parser.py
@@ -65,5 +65,11 @@ def parse_tf_plan(tf_plan_file):
         resource_block[resource['type']] = {}
         resource_block[resource['type']][resource.get('name', "default")] = _hclify(resource['values'])
         tf_defintions[tf_plan_file]['resource'].append(resource_block)
+    for child_module in template.get('planned_values', {}).get("root_module", {}).get("child_modules",[]):
+        for resource in child_module.get("resources", []):
+            resource_block = {}
+            resource_block[resource['type']] = {}
+            resource_block[resource['type']][resource.get('name', "default")] = _hclify(resource['values'])
+            tf_defintions[tf_plan_file]['resource'].append(resource_block)
 
     return tf_defintions, template_lines
