8c1ed61da7f3be539d248729eb484abfc772c73e
Nick <njgibbon@outlook.com>
2020-12-14 20:53:36 +0000

    Fix - Check - CF - CKV_AWS_16 - Aurora Encryption (#731)
    * added check and tests for docdb encryption for cloudformation
    
    * added MasterUsername and password parameters based on CF lint failure from actions
    
    * Check and tests for ensuring DocDB TLS is not disabled as it is enabled by default - Corresponds with terraform check already completed under https://github.com/bridgecrewio/checkov/issues/165
    
    * update to latest
    
    * check - cloudformation implementation of DocDB export to cloudwatch logs with logic coming from already existing terraform check
    
    * deleting some accidentally added files
    
    * unix eols
    
    * Merge remote-tracking branch 'upstream/master'
    
    * check - cloudformation - APIGatewayAccessLogging to be in line with terraform version of check
    
    * version data got out of sync
    
    * Merge branch 'master' of https://github.com/njgibbon/checkov
    
    * version data got out of sync
    
    * unix eol
    
    * version sync off again
    
    * Merge branch 'master' of https://github.com/njgibbon/checkov
    
    * version sync off again
    
    * Somehow doc did not get merged from upstream - syncing up
    
    * sorry, gh actions has been working against me. Sorted now.
    
    * Updated test data fieldsbased off failed cflint - Turns out the docs provide bad test data
    
    * refactored the check into 2 checks based on discovering the that V1 and V2 API GW Stage has different paths for access logging - AccessLogSetting vs AccessLogSettings
    
    * fix - seperate api g2 check v1 and v2 in to different checks with different IDs because the API has changed
    
    * Cloudformation check for ECRImmutableTags following the same logic as the terraform one
    
    * Consistency of string marks
    
    * Update to latest cflint version
    
    * check - cf - api gateway - xray enabled - same as tf
    
    * check - cf - apigw authorizer inline with tf check CKV_AWS_59
    
    * corrext line endings
    
    * check - cloudformation - CloudfrontDistributionLogging
    
    * cflint issues
    
    * cflint issues - fixing test data
    
    * cflint issues - misaligned yaml
    
    * Fix - Check - Terraform - Github - i714 - CKV_GIT_1 - Updating check to reflect newer provider API
    
    * fix - check - CKV_AWS_21 - fundamental issue with terraform base resource check class
    
    * fix - check - CKV_AWS_21 - remove some commented out test data
    
    * fix - check - CKV_AWS_21 - remove some commented out test data and revert to before to see the blast of trying to fix this class which is used a lot
    
    * refix
    
    * fix - azure rbac and eks publix endpoint issues which were hidden by issue.
    
    * fix - gcp storge bucket uniform access check and test fix - hopefully to finish off the change
    
    * fix - check - Aurora config different than rest of RDS - Issue 586
    
    * fix - ctn lint
    
    * fix - missed colon on condition
    
    * fix - cfnlint and stablise change
    
    * fix - uname pword cflint on db instance stuff
    
    * fix - reimplementation with overriding parent to check exception to the normal case
    
    * fix - sorted out the testing in a good way to repeat
    
    * fix - remove copied over code

--- checkov/cloudformation/checks/resource/aws/RDSEncryption.py
+++ checkov/cloudformation/checks/resource/aws/RDSEncryption.py
@@ -9,8 +9,19 @@ class RDSEncryption(BaseResourceValueCheck):
         supported_resources = ['AWS::RDS::DBInstance']
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+    
+    def scan_resource_conf(self, conf):
+        # If DB is Aurora then Encryption is set in other resource
+        # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-rds-database-instance.html#cfn-rds-dbinstance-storageencrypted
+        if 'Properties' in conf.keys():
+            if 'Engine' in conf['Properties'].keys():
+                if 'aurora' in conf['Properties']['Engine']:
+                    return CheckResult.UNKNOWN
+        # Database is not Aurora; Use base class implementation
+        return super().scan_resource_conf(conf)
 
     def get_inspected_key(self):
         return 'Properties/StorageEncrypted'
+
 
 check = RDSEncryption()
