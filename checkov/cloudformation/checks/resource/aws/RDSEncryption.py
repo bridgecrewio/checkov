da677c7dc6329ccf8f3fb2169d2b8becf0db27fc
njgibbon <njgibbon@outlook.com>
2020-12-14 16:28:44 +0000

    fix - check - Aurora config different than rest of RDS - Issue 586

--- checkov/cloudformation/checks/resource/aws/RDSEncryption.py
+++ checkov/cloudformation/checks/resource/aws/RDSEncryption.py
@@ -1,8 +1,8 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceCheck
 
 
-class RDSEncryption(BaseResourceValueCheck):
+class RDSEncryption(BaseResourceCheck):
     def __init__(self):
         name = "Ensure all data stored in the RDS is securely encrypted at rest"
         id = "CKV_AWS_16"
@@ -10,7 +10,20 @@ class RDSEncryption(BaseResourceValueCheck):
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return 'Properties/StorageEncrypted'
+    def scan_resource_conf(self, conf):
+        # If DB is Aurora then Encryption is set in other resource
+        # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-rds-database-instance.html#cfn-rds-dbinstance-storageencrypted
+        if 'Properties' in conf.keys():
+            if 'Engine' in conf['Properties'].keys()
+                if 'aurora' in conf['Properties']['Engine']:
+                    return CheckResult.PASSED
+
+        if 'Properties' in conf.keys():
+            if 'StorageEncrypted' in conf['Properties'].keys()
+                if conf['Properties']['StorageEncrypted'] == True:
+                    return CheckResult.PASSED
+
+        return CheckResult.FAILED
+
 
 check = RDSEncryption()
