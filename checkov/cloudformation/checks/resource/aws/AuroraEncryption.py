299bceaa52d78b0fb4d511684459aa0f130666b6
njgibbon <njgibbon@outlook.com>
2020-12-14 22:58:54 +0000

    fix - doc error in cloudformation causing a bit of confusion - resolved that

--- checkov/cloudformation/checks/resource/aws/AuroraEncryption.py
+++ checkov/cloudformation/checks/resource/aws/AuroraEncryption.py
@@ -14,8 +14,9 @@ class AuroraEncryption(BaseResourceValueCheck):
         # If you specify the SnapshotIdentifier or SourceDBInstanceIdentifier property, don't specify this property. 
         # The value is inherited from the snapshot or source DB instance.
         # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-rds-dbcluster.html#cfn-rds-dbcluster-storageencrypted
+        # Doc refers to 'SourceDBInstanceIdentifier' but that is not an available field. This is a doc error. 'SourceDBClusterIdentifier' is correct.
         if 'Properties' in conf.keys():
-            if 'SnapshotIdentifier' in conf['Properties'].keys() or 'SourceDBInstanceIdentifier' in conf['Properties'].keys():
+            if 'SnapshotIdentifier' in conf['Properties'].keys() or 'SourceDBClusterIdentifier' in conf['Properties'].keys():
                 return CheckResult.UNKNOWN
         # No snapshot or source DB; Use base class implementation
         return super().scan_resource_conf(conf)
