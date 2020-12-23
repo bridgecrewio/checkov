11d96a7415b6720cb6e4948312396b06e08b642d
Christophe Tafani-Dereeper <christophe@tafani-dereeper.me>
2020-12-23 16:04:15 +0100

    Fix false positive for MSK cluster encryption (closes #747)

--- checkov/terraform/checks/resource/aws/MSKClusterEncryption.py
+++ checkov/terraform/checks/resource/aws/MSKClusterEncryption.py
@@ -11,16 +11,18 @@ class MSKClusterEncryption(BaseResourceCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
+        # Note: As long as the 'encryption_info' block is specified, the cluster
+        # will be encrypted at rest even if 'encryption_at_rest_kms_key_arn' is not specified
+        # See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/msk_cluster#encryption_at_rest_kms_key_arn
         if 'encryption_info' in conf.keys():
             encryption = conf['encryption_info'][0]
-            if 'encryption_at_rest_kms_key_arn' in encryption:
-                if 'encryption_in_transit' in encryption:
-                    transit = encryption['encryption_in_transit'][0]
-                    if 'client_broker' in transit and transit['client_broker'][0] != 'TLS' or \
-                            'in_cluster' in transit and transit['in_cluster'][0] is False:
-                        return CheckResult.FAILED
-                    return CheckResult.PASSED
+            if 'encryption_in_transit' in encryption:
+                transit = encryption['encryption_in_transit'][0]
+                if 'client_broker' in transit and transit['client_broker'][0] != 'TLS' or \
+                        'in_cluster' in transit and transit['in_cluster'][0] is False:
+                    return CheckResult.FAILED
                 return CheckResult.PASSED
+            return CheckResult.PASSED
         return CheckResult.FAILED
 
 
--- tests/terraform/checks/resource/aws/test_MSKClusterEncryption.py
+++ tests/terraform/checks/resource/aws/test_MSKClusterEncryption.py
@@ -13,23 +13,6 @@ class TestMSKClusterEncryption(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
-    def test_failure_no_kms(self):
-        resource_conf = {
-            "name": "test-project",
-            "encryption_info": [
-                {
-                    "encryption_in_transit": [
-                        {
-                            "client_broker": ["TLS"],
-                            "in_cluster": ["true"],
-                        }
-                    ],
-                }
-            ],
-        }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
-
     def test_failure_non_tls(self):
         resource_conf = {
             "name": "test-project",
@@ -90,6 +73,33 @@ class TestMSKClusterEncryption(unittest.TestCase):
             "encryption_info": [
                 {
                     "encryption_at_rest_kms_key_arn": ["aws_kms_key.kms.arn"],
+                }
+            ],
+        }
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    # Regression test for https://github.com/bridgecrewio/checkov/issues/747
+    def test_success_no_encryption_at_rest_kms_key_arn_specified(self):
+        resource_conf = {
+            "name": "test-project",
+            "encryption_info": [{}],
+        }
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    # Regression test for https://github.com/bridgecrewio/checkov/issues/747
+    def test_success_encryption_in_transit_and_no_encryption_at_rest_kms_key_arn_specified(self):
+        resource_conf = {
+            "name": "test-project",
+            "encryption_info": [
+                {
+                    "encryption_in_transit": [
+                        {
+                            "client_broker": ["TLS"],
+                            "in_cluster": [True],
+                        }
+                    ],
                 }
             ],
         }
