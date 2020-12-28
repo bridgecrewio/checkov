080fb2620f4f4487143e60a513b059a86fe81d1a
tronxd <tron47@gmail.com>
2020-12-28 16:55:10 +0200

    added UT for empty iam policies

--- tests/terraform/checks/resource/aws/test_IAMRoleAllowAssumeFromAccount.py
+++ tests/terraform/checks/resource/aws/test_IAMRoleAllowAssumeFromAccount.py
@@ -52,6 +52,19 @@ class TestIAMRoleAllowAssumeFromAccount(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
+    def test_empty_iam_role(self):
+        resource_conf = {
+            'name': ['${var.name}-default'],
+            'assume_role_policy': ""}
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_empty_iam_role_2(self):
+        resource_conf = {
+            'name': ['${var.name}-default'],
+        }
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
 
 if __name__ == '__main__':
     unittest.main()
