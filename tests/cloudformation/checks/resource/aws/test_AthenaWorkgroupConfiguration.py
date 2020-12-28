0baf53bb4da7816f6a875df753a4a6051283b682
Nick <njgibbon@outlook.com>
2020-12-28 15:37:51 +0000

    check - cf - athena workgroup config - inline with terraform (#751)
    * check - cf - athena workgroup config - inline with terraform

---
+++ tests/cloudformation/checks/resource/aws/test_AthenaWorkgroupConfiguration.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+
+from checkov.cloudformation.checks.resource.aws.AthenaWorkgroupConfiguration import check
+from checkov.cloudformation.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+
+class TestAthenaWorkgroupConfiguration(unittest.TestCase):
+
+    def test_summary(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # There is some internal conflcits on "Tags" in the Athena WG Docs. And corresponding specs.
+        # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-athena-workgroup.html
+        # This may mean that the "Tags" in this test data needs to correspondingly change when CF / CF Lint are updated in the future.
+        # CF Lint Issue: https://github.com/aws-cloudformation/cfn-python-lint/issues/1577
+        test_files_dir = current_dir + "/example_AthenaWorkgroupConfiguration"
+        report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        self.assertEqual(summary['passed'], 1)
+        self.assertEqual(summary['failed'], 2)
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
