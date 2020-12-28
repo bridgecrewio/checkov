2cf769f492cd73619037498f35dcf2098107ac5d
njgibbon <njgibbon@outlook.com>
2020-12-28 14:16:30 +0000

    fix cflint and unit test feedback

--- tests/cloudformation/checks/resource/aws/test_AthenaWorkgroupConfiguration.py
+++ tests/cloudformation/checks/resource/aws/test_AthenaWorkgroupConfiguration.py
@@ -6,7 +6,7 @@ from checkov.cloudformation.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestAuroraEncryption(unittest.TestCase):
+class TestAthenaWorkgroupConfiguration(unittest.TestCase):
 
     def test_summary(self):
         runner = Runner()
