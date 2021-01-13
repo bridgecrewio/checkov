43d6969d5477e338a9302c4a2bff6d57483be00c
njgibbon <njgibbon@outlook.com>
2021-01-13 18:40:33 +0000

    check - cf - cloudtrail multi region to correspond with tf

---
+++ checkov/cloudformation/checks/resource/aws/CloudtrailMultiRegion.py
@@ -0,0 +1,16 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+
+
+class CloudtrailMultiRegion(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure CloudTrail is enabled in all Regions"
+        id = "CKV_AWS_67"
+        supported_resources = ['AWS::CloudTrail::Trail']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'Properties/IsMultiRegionTrail'
+
+check = CloudtrailMultiRegion()
