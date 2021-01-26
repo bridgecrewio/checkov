aeabeb074ca6122200c4f20e154cf80d4647a0b0
Mike Urbanski <mike@bridgecrew.io>
2021-01-26 15:01:02 -0600

    handle generic CFN tag parsing error

--- checkov/cloudformation/cfn_utils.py
+++ checkov/cloudformation/cfn_utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from checkov.cloudformation.checks.resource.registry import cfn_registry
 from checkov.cloudformation.parser.node import dict_node, list_node
 
@@ -13,16 +15,19 @@ def get_resource_tags(entity, registry=cfn_registry):
     if type(entity_config) not in (dict, dict_node):
         return None
 
-    properties = entity_config.get('Properties')
-    if properties:
-        tags = properties.get('Tags')
-        if tags:
-            if type(tags) == list_node:
-                tag_dict = {tag['Key']: str(get_entity_value_as_string(tag['Value'])) for tag in tags}
-                return tag_dict
-            elif type(tags) == dict_node:
-                tag_dict = {str(key): str(get_entity_value_as_string(value)) for key, value in tags.items() if key not in ('__startline__', '__endline__')}
-                return tag_dict
+    try:
+        properties = entity_config.get('Properties')
+        if properties:
+            tags = properties.get('Tags')
+            if tags:
+                if type(tags) == list_node:
+                    tag_dict = {tag['Key']: str(get_entity_value_as_string(tag['Value'])) for tag in tags}
+                    return tag_dict
+                elif type(tags) == dict_node:
+                    tag_dict = {str(key): str(get_entity_value_as_string(value)) for key, value in tags.items() if key not in ('__startline__', '__endline__')}
+                    return tag_dict
+    except:
+        logging.warning(f'Failed to parse tags for entity {entity}')
 
     return None
 
