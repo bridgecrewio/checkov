d792fdb63c615188bab6225cdf5e6ec37536ba48
Mike Urbanski <mike@bridgecrew.io>
2021-01-26 14:53:50 -0600

    handle CFN tags in key=value form

--- checkov/cloudformation/cfn_utils.py
+++ checkov/cloudformation/cfn_utils.py
@@ -1,5 +1,5 @@
 from checkov.cloudformation.checks.resource.registry import cfn_registry
-from checkov.cloudformation.parser.node import dict_node
+from checkov.cloudformation.parser.node import dict_node, list_node
 
 
 def get_resource_tags(entity, registry=cfn_registry):
@@ -17,8 +17,12 @@ def get_resource_tags(entity, registry=cfn_registry):
     if properties:
         tags = properties.get('Tags')
         if tags:
-            tag_dict = {tag['Key']: str(get_entity_value_as_string(tag['Value'])) for tag in tags}
-            return tag_dict
+            if type(tags) == list_node:
+                tag_dict = {tag['Key']: str(get_entity_value_as_string(tag['Value'])) for tag in tags}
+                return tag_dict
+            elif type(tags) == dict_node:
+                tag_dict = {str(key): str(get_entity_value_as_string(value)) for key, value in tags.items() if key not in ('__startline__', '__endline__')}
+                return tag_dict
 
     return None
 
@@ -43,12 +47,18 @@ def get_entity_value_as_string(value):
     :return:
     """
     if type(value) in (dict, dict_node):
-        value = list(value.values())[0]
+        (function, value) = list(value.items())[0]
         # If the value is a long-form function, then the first element is the template string (technically str_node)
         # Otherwise the dict value is the template string
         if type(value) == list:
-            return value[0]
+            if 'Join' in function:
+                # Join looks like !Join [, [V1, V2, V3]]
+                join_str = str(value[0])
+                return join_str.join([str(v) for v in value[1]])
+            else:
+                return value[0]
         else:
             return value
     else:
         return value
+
