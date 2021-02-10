1a2d6e0ccf7fa306355a76aab01f468897e3daf5
Matt Johnson <matt@bridgecrew.io>
2021-02-10 15:15:29 +0000

    added integration test for -o json --quiet

--- integration_tests/test_checkov_json_report.py
+++ integration_tests/test_checkov_json_report.py
@@ -33,6 +33,13 @@ class TestCheckovJsonReport(unittest.TestCase):
             self.assertEqual(data["summary"]["parsing_errors"], 0, "expecting 0 parsing errors")
             self.assertGreater(data["summary"]["failed"], 1, "expecting more then 1 failed checks")
 
+    def validate_json_quiet(self):
+        report_path = current_dir + "/../checkov_report_cfngoat_quiet.json"
+        with open(report_path) as json_file:
+            data = json.load(json_file)
+            self.assertTrue(data["results"]["failed_checks"])
+            self.assertFalse(data["results"]["passed_checks"])
+            self.assertTrue(data["summary"])
 
 if __name__ == '__main__':
     unittest.main()
