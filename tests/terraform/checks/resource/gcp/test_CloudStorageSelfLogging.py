59111bae0b57e3a0c9ce6e60bf6a1fca6094d9a1
James Woolfenden <james.woolfenden@gmail.com>
2021-02-17 22:00:55 +0000

    A bucket shouldnt log to itself (#878)

---
+++ tests/terraform/checks/resource/gcp/test_CloudStorageSelfLogging.py
@@ -0,0 +1,45 @@
+import unittest
+
+import hcl2
+
+from checkov.terraform.checks.resource.gcp.CloudStorageSelfLogging import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestCloudStorageSelfLogging(unittest.TestCase):
+
+    def test_failure(self):
+        hcl_res = hcl2.loads("""
+                resource "google_storage_bucket" "static-site" {
+                  name          = "image-store.com"
+                  location      = "EU"
+                  force_destroy = true
+                  logging {
+                    log_bucket="image-store.com"
+                  }
+                }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_storage_bucket']['static-site']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+
+    def test_success(self):
+         hcl_res = hcl2.loads("""
+                         resource "google_storage_bucket" "static-site" {
+                           name          = "image-store.com"
+                           location      = "EU"
+                           force_destroy = true
+                           uniform_bucket_level_access = true
+                           logging {
+                             log_bucket="mybestbucket"
+                             }
+                         }
+                         """)
+         resource_conf = hcl_res['resource'][0]['google_storage_bucket']['static-site']
+         scan_result = check.scan_resource_conf(conf=resource_conf)
+         self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
