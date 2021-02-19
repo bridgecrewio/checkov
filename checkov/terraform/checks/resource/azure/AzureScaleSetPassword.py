72854b898908d8e02b60e5ab73fad94e4c91d3ec
Barak Schoster Goihman <barak@bridgecrew.io>
2021-02-19 21:58:56 +0200

    Update AzureScaleSetPassword.py

--- checkov/terraform/checks/resource/azure/AzureScaleSetPassword.py
+++ checkov/terraform/checks/resource/azure/AzureScaleSetPassword.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 class AzureScaleSetPassword(BaseResourceCheck):
     def __init__(self):
         name = "Ensure Azure linux scale set does not use basic authentication(Use SSH Key Instead)"
-        id = "CKV_AZURE_48"
+        id = "CKV_AZURE_49"
         supported_resources = ['azurerm_linux_virtual_machine_scale_set']
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
