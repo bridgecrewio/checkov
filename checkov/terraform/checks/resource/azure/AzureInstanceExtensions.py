65384b2e25dab081c250b32c5d72da5541be2bea
Nimrod Kor <nimrodkor@gmail.com>
2021-02-23 10:42:00 +0200

    Add Azure Disk Encryption Check

--- checkov/terraform/checks/resource/azure/AzureInstanceExtensions.py
+++ checkov/terraform/checks/resource/azure/AzureInstanceExtensions.py
@@ -1,29 +1,21 @@
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class AzureInstanceExtensions(BaseResourceCheck):
+class AzureInstanceExtensions(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure Virtual Machine Extensions are not Installed"
         id = "CKV_AZURE_50"
         supported_resources = ['azurerm_virtual_machine', 'azurerm_linux_virtual_machine']
         categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED)
 
-    def scan_resource_conf(self, conf):
-        """
-            Virtual Machine Extensions Installed at azure_instance:
-            https://www.terraform.io/docs/providers/azure/r/instance.html
-        :param conf: azure_instance configuration
-        :return: <CheckResult>
-        """
-        if 'allow_extension_operations' in conf.keys():
-            config = conf['allow_extension_operations'][0]
-            if config:
-                return CheckResult.FAILED
-            else:
-                return CheckResult.PASSED 
-        return CheckResult.PASSED
+    def get_inspected_key(self):
+        return 'allow_extension_operations'
+
+    def get_expected_value(self):
+        return False
 
 
 check = AzureInstanceExtensions()
