c8f0bf625ed7c2d9df6318d0ced865258249cffc
Carlo Bongiovanni <cbongiovanni@ebay.com>
2021-03-06 00:48:10 +0100

    fixes #899 (#936)
    * when logging on a GCP bucket is disabled there is no logging key in the TF hcl
    
    * fix typo
    
    Co-authored-by: Carlo Bongiovanni <cbongiovanni@eclassifiedsgroup.com>

--- checkov/terraform/checks/resource/gcp/CloudStorageSelfLogging.py
+++ checkov/terraform/checks/resource/gcp/CloudStorageSelfLogging.py
@@ -12,9 +12,9 @@ class CloudStorageSelfLogging(BaseResourceCheck):
 
     def scan_resource_conf(self, conf):
         bucket_name = conf['name']
-        #check fot logging
-        if 'logging' in conf.keys():
-            if conf['logging'][0]['log_bucket']:
+        #check for logging
+        if 'logging' in conf:
+            if conf['logging'][0]:
                 log_bucket_name = conf['logging'][0]['log_bucket']
                 if log_bucket_name != bucket_name:
                     return CheckResult.PASSED
