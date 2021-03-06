3793b4d5ade5375057cf6584d3c1e4a6dffd3cb3
Rob Eden <rob@robeden.com>
2021-03-06 08:06:43 -0600

    Additional comment (really, just to re-kick build)

--- tests/terraform/parser/resources/parser_scenarios/module_output_reference/main.tf
+++ tests/terraform/parser/resources/parser_scenarios/module_output_reference/main.tf
@@ -3,5 +3,5 @@ module "common" {
 }
 module "bucket" {
   source = "./bucket"
-  tags = module.common.tags
+  tags = module.common.tags   # <-- reference to other module, must be resolved in second pass
 }
