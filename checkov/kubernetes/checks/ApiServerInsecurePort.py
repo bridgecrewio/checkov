5511fc6e2b9c0cde5fe40cc7ecdb8a4216af1fe7
Nir Adler <niradler@users.noreply.github.com>
2021-03-09 17:57:43 +0200

    CKV_K8S_88 (#28)
    * pull
    
    * CKV_K8S_86
    
    * CKV_K8S_88
    
    * CKV_K8S_88
    
    * CKV_K8S_88
    
    Co-authored-by: Nir Adler <nir@bridgecrew.io>

---
+++ checkov/kubernetes/checks/ApiServerInsecurePort.py
@@ -0,0 +1,24 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.kubernetes.base_spec_check import BaseK8Check
+
+class ApiServerInsecurePort(BaseK8Check):
+    def __init__(self):
+        # CIS-1.6 1.2.19
+        id = "CKV_K8S_88"
+        name = "Ensure that the --insecure-port argument is set to 0"
+        categories = [CheckCategories.KUBERNETES]
+        supported_kind = ['containers']
+        super().__init__(name=name, id=id, categories=categories, supported_entities=supported_kind)
+
+    def get_resource_id(self, conf):
+        return f'{conf["parent"]} - {conf["name"]}'
+
+    def scan_spec_conf(self, conf):
+        if "command" in conf:
+            if "kube-apiserver" in conf["command"]:
+                if "--insecure-port=0" not in conf["command"]:
+                    return CheckResult.FAILED
+
+        return CheckResult.PASSED
+
+check = ApiServerInsecurePort()
