6247bdf75fb2a20ae8d974730ca94d31b99eb7b2
Or Evron <20145882+orevron@users.noreply.github.com>
2021-03-09 17:00:44 +0200

    added checks CKV_AWS_123, CKV_AWS_122, CKV_AWS_87 (#21)
    * fix redshift not accessible test
    
    * segaMaker check
    
    * typo
    
    * add test

--- checkov/terraform/checks/resource/aws/VPCEndpointAcceptanceConfigured.py
+++ checkov/terraform/checks/resource/aws/VPCEndpointAcceptanceConfigured.py
@@ -2,19 +2,16 @@ from checkov.common.models.enums import CheckCategories
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class RedshiftClusterPubliclyAccessible(BaseResourceValueCheck):
+class VPCEndpointAcceptanceConfigured(BaseResourceValueCheck):
     def __init__(self):
-        name = "Redshift cluster should not be publicly accessible"
-        id = "CKV_AWS_87"
-        supported_resources = ['aws_redshift_cluster']
+        name = "Ensure that VPC Endpoint Service is configured for Manual Acceptance"
+        id = "CKV_AWS_123"
+        supported_resources = ['aws_vpc_endpoint_service']
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
-        return 'publicly_accessible'
-
-    def get_expected_value(self):
-        return False
+        return 'acceptance_required'
 
 
-check = RedshiftClusterPubliclyAccessible()
+check = VPCEndpointAcceptanceConfigured()
