803f4dcb3006f264cf63845eb3aece00a685d519
avivgu <guetaaviv@gmail.com>
2021-03-10 16:28:47 +0200

    CKV_AWS_114 (#43)
    * added EMRClusterKerberosAttributes check and test
    
    * fix the check

---
+++ tests/terraform/checks/resource/aws/test_EMRClusterKerberosAttributes.py
@@ -0,0 +1,73 @@
+import unittest
+
+from checkov.common.models.enums import CheckResult
+from checkov.terraform.checks.resource.aws.EMRClusterKerberosAttributes import check
+import hcl2
+
+
+class TestEMRClusterKerberosAttributes(unittest.TestCase):
+
+    def test_success_no_kerberos(self):
+        hcl_res = hcl2.loads("""
+resource "aws_emr_cluster" "test" {
+  name          = "emr-test-arn"
+  release_label = "emr-4.6.0"
+  applications  = ["Spark"]
+
+  termination_protection            = false
+  keep_job_flow_alive_when_no_steps = true
+
+  ec2_attributes {
+    subnet_id                         = aws_subnet.main.id
+    emr_managed_master_security_group = aws_security_group.sg.id
+    emr_managed_slave_security_group  = aws_security_group.sg.id
+    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+  }
+}
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_emr_cluster']['test']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success(self):
+        hcl_res = hcl2.loads("""
+resource "aws_emr_cluster" "test" {
+  name          = "emr-test-arn"
+  release_label = "emr-4.6.0"
+  applications  = ["Spark"]
+
+  termination_protection            = false
+  keep_job_flow_alive_when_no_steps = true
+
+  kerberos_attributes {
+    kdc_admin_password                = "somePassword"
+    realm                             = "EC2.INTERNAL"
+    }
+}
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_emr_cluster']['test']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+        
+    def test_failure(self):
+        hcl_res = hcl2.loads("""
+resource "aws_emr_cluster" "test" {
+  name          = "emr-test-arn"
+  release_label = "emr-4.6.0"
+  applications  = ["Spark"]
+
+  termination_protection            = false
+  keep_job_flow_alive_when_no_steps = true
+
+  kerberos_attributes {
+    kdc_admin_password                = "somePassword"
+    }
+}
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_emr_cluster']['test']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)           
+
+
+if __name__ == '__main__':
+    unittest.main()
