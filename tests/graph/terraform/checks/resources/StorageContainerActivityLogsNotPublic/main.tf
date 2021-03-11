5153251aae37ac86ccf11d61af312ecb3dd08dc1
nofarbelo <nofarbelo@gmail.com>
2021-03-11 14:20:40 +0200

    add check CKV2_AZURE_8

---
+++ tests/graph/terraform/checks/resources/StorageContainerActivityLogsNotPublic/main.tf
@@ -0,0 +1,70 @@
+resource "azurerm_storage_container" "ok_container" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.ok_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "not_ok_container" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.not_ok_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_account" "ok_account" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_account" "not_ok_account" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_monitor_activity_log_alert" "ok_monitor_activity_log_alert" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.main.name
+  scopes              = [azurerm_resource_group.main.id]
+  description         = "This alert will monitor a specific storage account updates."
+
+  criteria {
+    resource_id    = azurerm_storage_account.ok_account.id
+    operation_name = "Microsoft.Storage/storageAccounts/write"
+    category       = "Recommendation"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+
+    webhook_properties = {
+      from = "terraform"
+    }
+  }
+}
+
+resource "azurerm_monitor_activity_log_alert" "not_ok_monitor_activity_log_alert" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.main.name
+  scopes              = [azurerm_resource_group.main.id]
+  description         = "This alert will monitor a specific storage account updates."
+  enabled             = false
+
+  criteria {
+    resource_id    = azurerm_storage_account.not_ok_account.id
+    operation_name = "Microsoft.Storage/storageAccounts/write"
+    category       = "Recommendation"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+
+    webhook_properties = {
+      from = "terraform"
+    }
+  }
+}
