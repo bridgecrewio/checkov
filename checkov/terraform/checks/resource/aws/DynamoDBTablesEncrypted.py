5549774bfaaf31479cb679e508f627dcb139707f
Or Evron <20145882+orevron@users.noreply.github.com>
2021-03-15 11:28:31 +0200

    CKV_AWS_111 (#50)
    * add test
    
    * fix test

---
+++ checkov/terraform/checks/resource/aws/DynamoDBTablesEncrypted.py
@@ -0,0 +1,19 @@
+from abc import ABC
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+
+
+class DynamoDBTablesEncrypted(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure DynamoDB Tables are encrypted"
+        id = "CKV_AWS_111"
+        supported_resources = ['aws_dynamodb_table']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "server_side_encryption/[0]/enabled"
+
+
+check = DynamoDBTablesEncrypted()
---
+++ tests/terraform/checks/resource/aws/test_DynamoDBTablesEncrypted.py
@@ -0,0 +1,80 @@
+import unittest
+import hcl2
+
+from checkov.terraform.checks.resource.aws.DynamoDBTablesEncrypted import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestELBAccessLogs(unittest.TestCase):
+
+    def test_failure(self):
+        hcl_res = hcl2.loads("""
+            resource "aws_dynamodb_table" "basic-dynamodb-table" {
+              name           = "GameScores"
+              billing_mode   = "PROVISIONED"
+              read_capacity  = 20
+              write_capacity = 20
+              hash_key       = "UserId"
+              range_key      = "UserId"
+            
+              attribute {
+                name = "UserId"
+                type = "S"
+              }
+           
+              server_side_encryption {
+                  enabled = false
+              }
+            } 
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
+        scan_result = check.scan_resource_conf(conf=resource_conf, entity_type=None)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_failure2(self):
+        hcl_res = hcl2.loads("""
+            resource "aws_dynamodb_table" "basic-dynamodb-table" {
+              name           = "GameScores"
+              billing_mode   = "PROVISIONED"
+              read_capacity  = 20
+              write_capacity = 20
+              hash_key       = "UserId"
+              range_key      = "UserId"
+            
+              attribute {
+                name = "UserId"
+                type = "S"
+              }
+            } 
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
+        scan_result = check.scan_resource_conf(conf=resource_conf, entity_type=None)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success(self):
+        hcl_res = hcl2.loads("""
+            resource "aws_dynamodb_table" "basic-dynamodb-table" {
+              name           = "GameScores"
+              billing_mode   = "PROVISIONED"
+              read_capacity  = 20
+              write_capacity = 20
+              hash_key       = "UserId"
+              range_key      = "UserId"
+            
+              attribute {
+                name = "UserId"
+                type = "S"
+              }
+           
+              server_side_encryption {
+                  enabled = true
+              }
+            } 
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
+        scan_result = check.scan_resource_conf(conf=resource_conf, entity_type=None)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
