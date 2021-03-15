ab03ab906c354a2d39bdf7e294740d808ece7352
Anton Grübel <anton.gruebel@gmail.com>
2021-03-15 22:55:53 +0100

    Ignore TLS check for TCP, UDP and TCP_UDP protocol (#975)

--- tests/terraform/checks/resource/aws/test_AppLoadBalancerTLS12.py
+++ tests/terraform/checks/resource/aws/test_AppLoadBalancerTLS12.py
@@ -1,79 +1,47 @@
+import os
 import unittest
 
-import hcl2
-
-from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.AppLoadBalancerTLS12 import check
+from checkov.terraform.runner import Runner
+
 
 class TestAppLoadBalancerTLS12(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    def test_failure(self):
-        resource_conf =  {'load_balancer_arn': ['${aws_lb.examplea.arn}'], 'port': ['443'], 'protocol': ['HTTPS'], 'ssl_policy': ["ELBSecurityPolicy-2016-08"],
-                         'default_action': [{'type': ['forward'], 'target_group_arn': ['${aws_lb_target_group.examplea.arn}'] }]}
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
- 
-    def test_success(self):
-        resource_conf =  {
-            'load_balancer_arn': [
-                '${aws_lb.examplea.arn}'
-                ], 
-            'port': ['443'], 
-            'protocol': ['HTTPS'], 
-            'ssl_policy': ["ELBSecurityPolicy-TLS-1-2-2017-01"],
-            'default_action': [
-                {
-                    'type': ['forward'], 
-                    'target_group_arn': [
-                        '${aws_lb_target_group.examplea.arn}'
-                ]
-            }
-        ]
+        test_files_dir = current_dir + "/example_AppLoadBalancerTLS12"
+        report = runner.run(
+            root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id])
+        )
+        summary = report.get_summary()
+
+        passing_resources = {
+            "aws_lb_listener.http_redirect",
+            "aws_lb_listener.tcp",
+            "aws_lb_listener.udp",
+            "aws_lb_listener.tcp_udp",
+            "aws_lb_listener.tls_fs_1_2",
+            "aws_lb_listener.https_fs_1_2",
         }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
-
-    def test_nlb_tls_success(self):
-        resource_conf = {
-            'load_balancer_arn': [
-                '${aws_lb.example.arn}'
-            ],
-            'port': ['443'],
-            'protocol': ['TLS'],
-            'ssl_policy': ["ELBSecurityPolicy-FS-1-2-Res-2019-08"],
-            'default_action': [
-                {
-                    'type': ['forward'],
-                    'target_group_arn': [
-                        '${aws_lb_target_group.example.arn}'
-                    ]
-                }
-            ]
+        failing_resources = {
+            "aws_lb_listener.http",
+            "aws_lb_listener.https_2016",
+            "aws_lb_listener.tls_fs_1_1",
         }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
 
-    def test_redirect(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_lb_listener" "http" {
-              load_balancer_arn = aws_lb.public.arn
-              port              = "80"
-              protocol          = "HTTP" 
-            
-              default_action {
-                redirect {
-                  port        = "443"
-                  protocol    = "HTTPS"
-                  status_code = "HTTP_301"
-                }
-                type = "redirect"
-              }
-            }
-            """)
-        resource_conf = hcl_res['resource'][0]['aws_lb_listener']['http']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
 
-if __name__ == '__main__':
+        self.assertEqual(summary["passed"], 6)
+        self.assertEqual(summary["failed"], 3)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == "__main__":
     unittest.main()
-
