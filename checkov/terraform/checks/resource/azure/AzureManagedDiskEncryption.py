764655851a5443b961cd52ebe56ceb2dafcf09d5
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-03-16 02:59:06 -0500

    Remove duplicate azure disk encryption and fix default fail (#983)
    * remove duplicate and fix false positive
    
    * add skipped check ID in test
    
    * add pr # to skipped check comment
    
    * fix check title

--- checkov/terraform/checks/resource/azure/AzureManagedDiskEncryption.py
+++ checkov/terraform/checks/resource/azure/AzureManagedDiskEncryption.py
@@ -1,11 +1,11 @@
-from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
-class AzureDiskEncryption(BaseResourceCheck):
+class AzureManagedDiskEncryption(BaseResourceCheck):
     def __init__(self):
-        name = "Ensure Disks are encrypted at rest"
-        id = "CKV_AZURE_51"
+        name = "Ensure Azure managed disk has encryption enabled"
+        id = "CKV_AZURE_2"
         supported_resources = ['azurerm_managed_disk']
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
@@ -16,6 +16,7 @@ class AzureDiskEncryption(BaseResourceCheck):
         if 'encryption_settings' in conf:
             if isinstance(conf['encryption_settings'][0], dict):
                 return CheckResult.PASSED if conf['encryption_settings'][0]['enabled'][0] else CheckResult.FAILED
-        return CheckResult.FAILED
+        return CheckResult.PASSED  # enabled by default
 
-check = AzureDiskEncryption()
+
+check = AzureManagedDiskEncryption()
--- tests/terraform/checks/resource/azure/test_AzureManagedDiscEncryption.py
+++ tests/terraform/checks/resource/azure/test_AzureManagedDiscEncryption.py
@@ -2,7 +2,7 @@ import unittest
 import hcl2
 
 from checkov.common.models.enums import CheckResult
-from checkov.terraform.checks.resource.azure.AzureManagedDiscEncryption import check
+from checkov.terraform.checks.resource.azure.AzureManagedDiskEncryption import check
 
 
 class TestAzureManagedDiscEncryption(unittest.TestCase):
@@ -26,7 +26,7 @@ class TestAzureManagedDiscEncryption(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
-    def testmissing_failure(self):
+    def test_default_success(self):
         hcl_res = hcl2.loads("""
             resource "azurerm_managed_disk" "example" {
                 name                 = var.disk_name
@@ -40,7 +40,7 @@ class TestAzureManagedDiscEncryption(unittest.TestCase):
                 """)
         resource_conf = hcl_res['resource'][0]['azurerm_managed_disk']['example']
         scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        self.assertEqual(CheckResult.PASSED, scan_result)
 
     def test_success(self):
         hcl_res = hcl2.loads("""
@@ -58,6 +58,26 @@ class TestAzureManagedDiscEncryption(unittest.TestCase):
             }
                 """)
         resource_conf = hcl_res['resource'][0]['azurerm_managed_disk']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_encryption_set_success(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_managed_disk" "source" {
+              name                 = "acctestmd1"
+              location             = "West US 2"
+              resource_group_name  = azurerm_resource_group.example.name
+              storage_account_type = "Standard_LRS"
+              create_option        = "Empty"
+              disk_size_gb         = "1"
+              disk_encryption_set_id = var.encryption_set_id
+            
+              tags = {
+                environment = "staging"
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_managed_disk']['source']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
