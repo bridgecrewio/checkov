3602fa5afafa5fb127ec50e37fdac1229bf7de99
Nick <njgibbon@outlook.com>
2021-03-16 11:55:35 +0000

    CF - Check - Glue Encryption (#984)
    * cf - check - es logging
    
    * check to match with the terraform one
    
    * formatting
    
    * unix eol
    
    * cflint feedback

---
+++ checkov/cloudformation/checks/resource/aws/GlobalAcceleratorAcceleratorFlowLogs.py
@@ -0,0 +1,4 @@
+####################################################################################################################
+## CloudFormation (Mar 2021) doesn't support GlobalAccelerator Attributes Configuration                           ##
+## https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-globalaccelerator-accelerator.html ##
+####################################################################################################################
