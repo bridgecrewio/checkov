c300f5840d8d83acc8391a4d3ca11e7a4d760867
Guy Sulema <48174831+guy-suli@users.noreply.github.com>
2021-03-16 11:53:21 +0200

    Add check CKV_AZURE_88 - Ensure that app services use Azure Files (#117)

---
+++ tests/terraform/checks/resource/azure/test_AppServiceUsedAzureFiles.py
@@ -0,0 +1,72 @@
+import unittest
+
+import hcl2
+
+from checkov.terraform.checks.resource.azure.AppServiceUsedAzureFiles import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestAppServicePythonVersion(unittest.TestCase):
+
+    def test_failure(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_app_service" "example" {
+              name                = "example-app-service"
+              location            = azurerm_resource_group.example.location
+              resource_group_name = azurerm_resource_group.example.name
+              app_service_plan_id = azurerm_app_service_plan.example.id
+              https_only          = true
+              storage_account {
+                name = "test_name"
+                type = "AzureBlob"
+                account_name ="test_account_name"
+                share_name = "test_share_name"
+                access_key = "test_access_key"
+                }
+              }
+            """)
+        resource_conf = hcl_res['resource'][0]['azurerm_app_service']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_app_service" "example" {
+               name                = "example-app-service"
+              location            = azurerm_resource_group.example.location
+              resource_group_name = azurerm_resource_group.example.name
+              app_service_plan_id = azurerm_app_service_plan.example.id
+              https_only          = true
+              storage_account {
+                name = "test_name"
+                type = "AzureFiles"
+                account_name ="test_account_name"
+                share_name = "test_share_name"
+                access_key = "test_access_key"
+                }
+              }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_app_service']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_failed_no_storage_account(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_app_service" "example" {
+              name                = "example-app-service"
+              location            = azurerm_resource_group.example.location
+              resource_group_name = azurerm_resource_group.example.name
+              app_service_plan_id = azurerm_app_service_plan.example.id
+              https_only          = true
+              site_config {
+                scm_type                 = "someValue"
+                }
+              }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_app_service']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
