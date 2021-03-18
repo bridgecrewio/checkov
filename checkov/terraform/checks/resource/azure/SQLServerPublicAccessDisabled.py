bf88a2374bcfce8629f6d2d7deca58c44667f8c8
Mor Kadosh <kadoshms@gmail.com>
2021-03-18 07:26:20 +0200

    Simplified checks and removed duplicate check (#186)
    * implementation of CKV_AZURE_131
    
    * implementation of CKV_AZURE_131 - CKV_AZURE_133
    
    * fixed test class name
    
    * rmoeved already implemented check, simplified others
    
    * removed SQLServerHasPublicAccessDisabled
    
    * simplified CKV_AZURE_131

--- checkov/terraform/checks/resource/azure/SQLServerPublicAccessDisabled.py
+++ checkov/terraform/checks/resource/azure/SQLServerPublicAccessDisabled.py
@@ -8,16 +8,14 @@ class SQLServerHasPublicAccessDisabled(BaseResourceValueCheck):
         id = "CKV_AZURE_131"
         supported_resources = ['azurerm_mssql_server']
         categories = [CheckCategories.NETWORKING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.FAILED)
 
     def get_inspected_key(self):
         return 'public_network_access_enabled'
 
-    def scan_resource_conf(self, conf):
-        public_access = conf.get('public_network_access_enabled', [True])
-        if public_access[0]:
-            return CheckResult.FAILED
-        return CheckResult.PASSED
+    def get_expected_value(self):
+        return False
 
 
 check = SQLServerHasPublicAccessDisabled()
