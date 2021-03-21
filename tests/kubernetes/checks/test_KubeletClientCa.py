d4b3d054f788685e597a4f56b9fbd5bf55679f04
Nir Adler <niradler@users.noreply.github.com>
2021-03-21 12:41:56 +0200

    CKV_K8S_14x (#183)
    * 4x
    
    * CKV_K8S_143
    
    * CKV_K8S_144
    
    * CKV_K8S_145
    
    * repeted code
    
    Co-authored-by: Nir Adler <nir@bridgecrew.io>

--- tests/kubernetes/checks/test_KubeletClientCa.py
+++ tests/kubernetes/checks/test_KubeletClientCa.py
@@ -1,35 +1,38 @@
+
 import os
 import unittest
 
-from checkov.kubernetes.checks.KubeletCryptographicCiphers import check
+from checkov.kubernetes.checks.KubeletClientCa import check
 from checkov.kubernetes.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestKubeletCryptographicCiphers(unittest.TestCase):
+class TestKubeletClientCa(unittest.TestCase):
 
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_KubeletCryptographicCiphers"
-        report = runner.run(root_folder=test_files_dir,
-                            runner_filter=RunnerFilter(checks=[check.id]))
+        test_files_dir = current_dir + "/example_KubeletClientCa"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
         self.assertEqual(summary['passed'], 1)
         self.assertEqual(summary['failed'], 1)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
-
+        
+        
         for record in report.failed_checks:
             self.assertTrue("FAILED" in record.file_path)
             self.assertTrue(record.check_id in [check.id])
-
+            
         for record in report.passed_checks:
             self.assertTrue("PASSED" in record.file_path)
-            self.assertTrue(record.check_id in [check.id])
+            self.assertTrue(record.check_id in [check.id])              
 
 
 if __name__ == '__main__':
     unittest.main()
+        
+    
