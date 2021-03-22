d2964e2834775babe84417cdceaca33eb5ab70a2
ismail yenigul <ismailyenigul@gmail.com>
2021-03-22 19:17:16 +0300

    check Lambda variable only for AWS resources The following regex  hits aws kms key id and creates FP
    _secrets_regexes = {
        'azure': [
                "(\"|')?([0-9A-Fa-f]{4}-){4}[0-9A-Fa-f]{12}(\"|')?",  # client_secret
                "(\"|')?[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}(\"|')?", # client_id and many other forms of IDs
    
    environment": [
                  {
                 "variables": {
                  "kms_key_id": "arn:aws:kms:eu-west-1:123456789:key/c0baad75-d0d3-24e3-95d1-1e4e38a44c4a"

--- checkov/cloudformation/checks/resource/aws/LambdaEnvironmentCredentials.py
+++ checkov/cloudformation/checks/resource/aws/LambdaEnvironmentCredentials.py
@@ -18,7 +18,7 @@ class LambdaEnvironmentCredentials(BaseResourceCheck):
                 if 'Variables' in environment.keys():
                     variables = environment['Variables']
                     for value in variables.values():
-                        if string_has_secrets(str(value)):
+                        if string_has_secrets(str(value), AWS):
                             return CheckResult.FAILED
 
         return CheckResult.PASSED
