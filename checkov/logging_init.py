89dc4cf0f25434d55ef47c0f7f6c9755a1006b54
ismail yenigul <ismailyenigul@gmail.com>
2021-03-22 20:22:26 +0300

    set root logger

--- checkov/logging_init.py
+++ checkov/logging_init.py
@@ -13,8 +13,8 @@ def init():
     consoleHandler.setFormatter(logFormatter)
     consoleHandler.setLevel(LOG_LEVEL)
     rootLogger.addHandler(consoleHandler)
+    logging.getLogger().setLevel(LOG_LEVEL)
     logging.getLogger("urllib3").setLevel(logging.ERROR)
     logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
     logging.getLogger("urllib3.connectionpool").propagate = False
     logging.getLogger("urllib3").propagate = False
-    logging.getLogger().setLevel(LOG_LEVEL)
