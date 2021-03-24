7005219453f04cc550d01ec71f1804bb6d0f4d1c
Nofar Belo <nofarbelo@gmail.com>
2021-03-24 17:10:11 +0200

    CKV2_AWS_16 (#208)
    * add check CKV2_AWS_16
    
    * add test
    
    Co-authored-by: Nimrod Kor <nimrodkor@gmail.com>

--- tests/graph/terraform/checks/test_yaml_policies.py
+++ tests/graph/terraform/checks/test_yaml_policies.py
@@ -147,6 +147,9 @@ class TestYamlPolicies(unittest.TestCase):
     def test_EncryptedEBSVolumeOnlyConnectedToEC2s(self):
         self.go("EncryptedEBSVolumeOnlyConnectedToEC2s")
 
+    def test_AutoScalingEnableOnDynamoDBTables(self):
+        self.go("AutoScalingEnableOnDynamoDBTables")
+
     def test_EIPAllocatedToVPCAttachedEC2(self):
         self.go("EIPAllocatedToVPCAttachedEC2")
 
