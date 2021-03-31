61c3ee874923193cf21c7b93352b0628e5ba984c
Rotem Avni <52502521+rotemavni@users.noreply.github.com>
2021-03-31 17:33:33 +0300

    Solve policies with resources that have multiple connections (#226)
    * try to test ALBConnectedToHTTPS
    
    * fixed condition for excluded vertices
    
    (cherry picked from commit 986e40f85ff71879f62d8ef52f5e4d886b79d4f7)

--- tests/graph/terraform/checks_infra/connection_solvers/and_connection_solver/test_solver.py
+++ tests/graph/terraform/checks_infra/connection_solvers/and_connection_solver/test_solver.py
@@ -17,3 +17,12 @@ class ConnectionSolver(TestBaseSolver):
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_multiple_connections(self):
+        root_folder = '../../../resources/lb'
+        check_id = "ALBConnectedToHTTPS"
+        should_pass = []
+        should_fail = ["aws_lb.lb_bad_1"]
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
