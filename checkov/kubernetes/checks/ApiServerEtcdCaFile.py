9cc8a0f64eaf293f86cfc2e87fec6d3fa1aa7a48
Nir Adler <niradler@users.noreply.github.com>
2021-04-01 15:51:34 +0300

    Ckv k8 s 102 (#228)
    * CKV_K8S_102
    
    * CKV_K8S_102
    
    * CKV_K8S_102
    
    Co-authored-by: Nir Adler <nir@bridgecrew.io>
    (cherry picked from commit 431c1d63b59a1997048540db3fe126c380cecf45)

--- checkov/kubernetes/checks/ApiServerEtcdCaFile.py
+++ checkov/kubernetes/checks/ApiServerEtcdCaFile.py
@@ -3,10 +3,10 @@ from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.kubernetes.base_spec_check import BaseK8Check
 
 
-class ApiServerEncryptionProviders(BaseK8Check):
+class ApiServerEtcdCaFile(BaseK8Check):
     def __init__(self):
-        id = "CKV_K8S_104"
-        name = "Ensure that encryption providers are appropriately configured"
+        id = "CKV_K8S_102"
+        name = "Ensure that the --etcd-ca-file argument is set as appropriate"
         categories = [CheckCategories.KUBERNETES]
         supported_entities = ['containers']
         super().__init__(name=name, id=id, categories=categories, supported_entities=supported_entities)
@@ -27,12 +27,12 @@ class ApiServerEncryptionProviders(BaseK8Check):
                 else:
                     keys.append(cmd)
                     values.append(None)
+
         if "kube-apiserver" in keys:
-            if "--encryption-provider-config" not in keys:
-                        return CheckResult.FAILED
-        
+            if '--etcd-ca-file' not in keys:
+                return CheckResult.FAILED
 
         return CheckResult.PASSED
 
 
-check =  ApiServerEncryptionProviders()
+check =  ApiServerEtcdCaFile()
