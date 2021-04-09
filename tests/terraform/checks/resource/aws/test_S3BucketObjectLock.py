08cc171aa6bf7959ac35a17d548bd5bf0a5c0942
Harry Papadopoulos <harry.papadopo>
2021-04-09 17:03:42 +0100

    CKV_AWS_143 - allow the check to handle dynamic values

--- tests/terraform/checks/resource/aws/test_S3BucketObjectLock.py
+++ tests/terraform/checks/resource/aws/test_S3BucketObjectLock.py
@@ -12,7 +12,6 @@ class TestS3BucketObjectLock(unittest.TestCase):
                 resource "aws_s3_bucket" "test" {
                       bucket = "my-tf-test-bucket"
                       acl    = "private"
-                    
                       tags = {
                         Name        = "My bucket"
                         Environment = "Dev"
@@ -32,7 +31,6 @@ class TestS3BucketObjectLock(unittest.TestCase):
                 resource "aws_s3_bucket" "test" {
                       bucket = "my-tf-test-bucket"
                       acl    = "private"
-                    
                       tags = {
                         Name        = "My bucket"
                         Environment = "Dev"
@@ -51,7 +49,6 @@ class TestS3BucketObjectLock(unittest.TestCase):
                 resource "aws_s3_bucket" "test" {
                       bucket = "my-tf-test-bucket"
                       acl    = "private"
-                    
                       tags = {
                         Name        = "My bucket"
                         Environment = "Dev"
@@ -62,6 +59,34 @@ class TestS3BucketObjectLock(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
+    def test_dynamic_value(self):
+        hcl_res = hcl2.loads("""
+            resource "aws_s3_bucket" "test" {
+              count         = local.enabled ? 1 : 0
+              bucket        = module.this.id
+              acl           = "private"
+              tags          = module.this.tags
+
+              dynamic "object_lock_configuration" {
+                for_each = var.object_lock_enabled == true ? [local.object_lock_params] : []
+
+                content {
+                    object_lock_enabled = "Enabled"
+
+                    rule {
+                    default_retention {
+                      mode  = object_lock_configuration.value.retention_mode
+                      days  = object_lock_configuration.value.retention_period_days
+                      years = object_lock_configuration.value.retention_period_years
+                    }
+                  }
+                }
+              }
+            }
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_s3_bucket']['test']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
 
 if __name__ == '__main__':
     unittest.main()
