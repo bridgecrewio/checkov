ab62de8424cc9ebcd26839273aeb8417f2ee8f7b
elaygl <elaygl@gmail.com>
2021-04-13 00:37:45 +0300

    Checkov download twistlock cli directly from platform

--- checkov/common/bridgecrew/image_scanning/image_scanner.py
+++ checkov/common/bridgecrew/image_scanning/image_scanner.py
@@ -1,9 +1,5 @@
 import logging
-import platform
 import subprocess  # nosec
-import os
-import stat
-import urllib.request
 
 from checkov.common.bridgecrew.image_scanning.twistlock_integration import twistlock_integration
 
@@ -12,14 +8,7 @@ TWISTLOCK_CLI_FILE_NAME = 'twistcli'
 
 class ImageScanner:
     def scan(self, docker_image_id):
-        os_type = platform.system().lower()
-        twistlock_download_link = twistlock_integration.get_download_link(os_type)
-        logging.debug(f'TwistLock CLI download link: {twistlock_download_link}')
-
-        urllib.request.urlretrieve(twistlock_download_link, TWISTLOCK_CLI_FILE_NAME)  # nosec - validated the URL in the integration
-        st = os.stat(TWISTLOCK_CLI_FILE_NAME)
-        os.chmod(TWISTLOCK_CLI_FILE_NAME, st.st_mode | stat.S_IEXEC)
-        logging.debug('TwistLock CLI downloaded and set execute permission successfully')
+        twistlock_integration.download_cli(TWISTLOCK_CLI_FILE_NAME)
 
         command_args = f"./twistcli images scan --address {twistlock_integration.get_proxy_address()} --token {twistlock_integration.get_bc_api_key()} --details --output-file results.json {docker_image_id}".split()
         subprocess.run(command_args, stdout=subprocess.DEVNULL)  # nosec
--- checkov/common/bridgecrew/image_scanning/twistlock_integration.py
+++ checkov/common/bridgecrew/image_scanning/twistlock_integration.py
@@ -1,17 +1,17 @@
-import json
 import logging
-
+import os
+import platform
+import stat
 import requests
 
-from checkov.common.bridgecrew.integration_features.base_integration_feature import BC_API_URL
 from checkov.common.bridgecrew.platform_integration import bc_integration
+from checkov.common.bridgecrew.integration_features.base_integration_feature import BC_API_URL
 from checkov.common.util.dict_utils import merge_dicts
-from checkov.common.util.http_utils import get_auth_header, extract_error_message, get_default_get_headers
+from checkov.common.util.http_utils import get_auth_header, get_default_get_headers
 
 
 class TwistLockIntegration:
-    # twistlock_base_url = f"{BC_API_URL}/vulnerabilities/twistlock"
-    twistlock_base_url = "http://localhost:3009/api/v1/vulnerabilities/twistlock"
+    twistlock_base_url = f"{BC_API_URL}/vulnerabilities/twistlock"
 
     def get_bc_api_key(self):
         return bc_integration.bc_api_key
@@ -19,25 +19,16 @@ class TwistLockIntegration:
     def get_proxy_address(self):
         return f"{self.twistlock_base_url}/proxy"
 
-    def get_download_link(self, os_type):
+    def download_cli(self, twistlock_cli_file_name):
+        os_type = platform.system().lower()
         headers = merge_dicts(
             get_default_get_headers(bc_integration.bc_source, bc_integration.bc_source_version),
             get_auth_header(bc_integration.bc_api_key)
         )
         response = requests.request('GET', f"{self.twistlock_base_url}/download-link?os={os_type}", headers=headers)
-
-        if response.status_code != 200:
-            error_message = extract_error_message(response)
-            raise Exception(f'Get TwistLock download link request failed with response code {response.status_code}: {error_message}')
-
-        logging.debug(f'Response from TwistLock download link endpoint: {response.content}')
-
-        download_link_result = json.loads(response.content) if response.content else None
-        download_link = download_link_result['data']
-
-        if not download_link.startswith('https://'):
-            raise Exception(f'Invalid URL received from TwistLock download endpoint {download_link}')
-
-        return download_link
+        open(twistlock_cli_file_name, 'wb').write(response.content)
+        st = os.stat(twistlock_cli_file_name)
+        os.chmod(twistlock_cli_file_name, st.st_mode | stat.S_IEXEC)
+        logging.debug(f'TwistLock CLI downloaded and has execute permission')
 
 twistlock_integration = TwistLockIntegration()
