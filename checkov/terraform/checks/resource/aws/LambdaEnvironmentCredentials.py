6148f7980a32913f13634e52f060245938408ee1
ismail yenigul <ismailyenigul@gmail.com>
2021-04-13 16:58:31 +0300

    import  secret GENERAL

--- checkov/terraform/checks/resource/aws/LambdaEnvironmentCredentials.py
+++ checkov/terraform/checks/resource/aws/LambdaEnvironmentCredentials.py
@@ -1,6 +1,6 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.util.secrets import string_has_secrets, AWS
+from checkov.common.util.secrets import string_has_secrets, AWS, GENERAL
 from checkov.common.util.type_forcers import force_list
 
 
