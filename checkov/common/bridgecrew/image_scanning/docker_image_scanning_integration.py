087bdd3abb9966b19e4a32747157bfe007ae71b4
elaygl <elaygl@gmail.com>
2021-04-13 11:16:54 +0300

    Renamed twistlock cli related classes to more general name

--- checkov/common/bridgecrew/image_scanning/docker_image_scanning_integration.py
+++ checkov/common/bridgecrew/image_scanning/docker_image_scanning_integration.py
@@ -10,25 +10,26 @@ from checkov.common.util.dict_utils import merge_dicts
 from checkov.common.util.http_utils import get_auth_header, get_default_get_headers
 
 
-class TwistLockIntegration:
-    twistlock_base_url = f"{BC_API_URL}/vulnerabilities/twistlock"
+class DockerImageScanningIntegration:
+    docker_image_scanning_base_url = f"{BC_API_URL}/vulnerabilities/docker-images"
 
     def get_bc_api_key(self):
         return bc_integration.bc_api_key
 
     def get_proxy_address(self):
-        return f"{self.twistlock_base_url}/proxy"
+        return f"{self.docker_image_scanning_base_url}/twistcli-proxy"
 
-    def download_cli(self, twistlock_cli_file_name):
+    def download_twistcli(self, cli_file_name):
         os_type = platform.system().lower()
         headers = merge_dicts(
             get_default_get_headers(bc_integration.bc_source, bc_integration.bc_source_version),
             get_auth_header(bc_integration.bc_api_key)
         )
-        response = requests.request('GET', f"{self.twistlock_base_url}/download-link?os={os_type}", headers=headers)
-        open(twistlock_cli_file_name, 'wb').write(response.content)
-        st = os.stat(twistlock_cli_file_name)
-        os.chmod(twistlock_cli_file_name, st.st_mode | stat.S_IEXEC)
-        logging.debug(f'TwistLock CLI downloaded and has execute permission')
+        response = requests.request('GET', f"{self.docker_image_scanning_base_url}/download-twistcli?os={os_type}", headers=headers)
+        open(cli_file_name, 'wb').write(response.content)
+        st = os.stat(cli_file_name)
+        os.chmod(cli_file_name, st.st_mode | stat.S_IEXEC)
+        logging.debug(f'TwistCLI downloaded and has execute permission')
 
-twistlock_integration = TwistLockIntegration()
+
+docker_image_scanning_integration = DockerImageScanningIntegration()
--- checkov/common/bridgecrew/image_scanning/image_scanner.py
+++ checkov/common/bridgecrew/image_scanning/image_scanner.py
@@ -1,18 +1,18 @@
 import logging
 import subprocess  # nosec
 
-from checkov.common.bridgecrew.image_scanning.twistlock_integration import twistlock_integration
+from checkov.common.bridgecrew.image_scanning.docker_image_scanning_integration import docker_image_scanning_integration
 
-TWISTLOCK_CLI_FILE_NAME = 'twistcli'
+TWISTCLI_FILE_NAME = 'twistcli'
 
 
 class ImageScanner:
     def scan(self, docker_image_id):
-        twistlock_integration.download_cli(TWISTLOCK_CLI_FILE_NAME)
+        docker_image_scanning_integration.download_twistcli(TWISTCLI_FILE_NAME)
 
-        command_args = f"./twistcli images scan --address {twistlock_integration.get_proxy_address()} --token {twistlock_integration.get_bc_api_key()} --details --output-file results.json {docker_image_id}".split()
+        command_args = f"./{TWISTCLI_FILE_NAME} images scan --address {docker_image_scanning_integration.get_proxy_address()} --token {docker_image_scanning_integration.get_bc_api_key()} --details --output-file results.json {docker_image_id}".split()
         subprocess.run(command_args, stdout=subprocess.DEVNULL)  # nosec
-        logging.debug(f'TwistLock CLI ran successfully on image {docker_image_id}')
+        logging.debug(f'TwistCLI ran successfully on image {docker_image_id}')
 
 
 image_scanner = ImageScanner()
