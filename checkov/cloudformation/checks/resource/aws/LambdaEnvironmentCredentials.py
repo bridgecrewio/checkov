48c90faaf77091e0b4efab041a1f06e90db33265
ismail yenigul <ismailyenigul@gmail.com>
2021-04-13 22:14:17 +0300

    check  AWS secrets in Lambda env variable only for AWS and GENERAL regex (#1008)
    * check Lambda variable only for AWS resources
    The following regex  hits aws kms key id
    and creates FP
    
    _secrets_regexes = {
        'azure': [
                "(\"|')?([0-9A-Fa-f]{4}-){4}[0-9A-Fa-f]{12}(\"|')?",  # client_secret
                "(\"|')?[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}(\"|')?", # client_id and many other forms of IDs
    
    environment": [
                  {
                 "variables": {
                  "kms_key_id": "arn:aws:kms:eu-west-1:123456789:key/c0baad75-d0d3-24e3-95d1-1e4e38a44c4a"
    
    * add General to string_has_secrets function
    
    * use GENERAL instead of General
    
    * add GENERAL category to string_has_secrets
    
    * Update LambdaEnvironmentCredentials.py
    
    * import GENERAL
    
    * import  secret GENERAL

--- checkov/cloudformation/checks/resource/aws/LambdaEnvironmentCredentials.py
+++ checkov/cloudformation/checks/resource/aws/LambdaEnvironmentCredentials.py
@@ -1,6 +1,6 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.util.secrets import string_has_secrets, AWS
+from checkov.common.util.secrets import string_has_secrets, AWS, GENERAL
 
 class LambdaEnvironmentCredentials(BaseResourceCheck):
     def __init__(self):
@@ -18,7 +18,7 @@ class LambdaEnvironmentCredentials(BaseResourceCheck):
                 if 'Variables' in environment.keys():
                     variables = environment['Variables']
                     for value in variables.values():
-                        if string_has_secrets(str(value)):
+                        if string_has_secrets(str(value),AWS,GENERAL):
                             return CheckResult.FAILED
 
         return CheckResult.PASSED
