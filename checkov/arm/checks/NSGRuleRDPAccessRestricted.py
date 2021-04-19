d94f9070a25ac7d012ec5500da216c3bbb21a994
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-04-19 12:00:49 -0500

    Add arm param check (#1091)
    * refactor arm checks to arm resource checks
    
    * refactor multiple registry types
    
    * add parameter check
    
    * add test for arm param check
    
    * update arm registry tests for param checks
    
    * update check ID
    
    * combine loops to eliminate duplicate parsing

--- checkov/arm/checks/NSGRuleRDPAccessRestricted.py
+++
@@ -1,9 +0,0 @@
-from checkov.arm.checks.NSGRulePortAccessRestricted import NSGRulePortAccessRestricted
-
-
-class NSGRuleRDPAccessRestricted(NSGRulePortAccessRestricted):
-    def __init__(self):
-        super().__init__(name="Ensure that RDP access is restricted from the internet", check_id="CKV_AZURE_9", port=3389)
-
-
-check = NSGRuleRDPAccessRestricted()
--- tests/arm/test_scanner_registry.py
+++ tests/arm/test_scanner_registry.py
@@ -1,20 +1,30 @@
 import unittest
 
-from checkov.arm.registry import arm_registry
+from checkov.arm.registry import arm_resource_registry, arm_parameter_registry
 
 
 class TestScannerRegistry(unittest.TestCase):
 
     def test_num_of_scanners(self):
-        scanners_counter = 0
-        for key in list(arm_registry.checks.keys()):
-            scanners_counter += len(arm_registry.checks[key])
+        resource_scanners_counter = 0
+        for key in list(arm_resource_registry.checks.keys()):
+            resource_scanners_counter += len(arm_resource_registry.checks[key])
 
-        self.assertGreater(scanners_counter, 0)
+        self.assertGreater(resource_scanners_counter, 0)
+
+        parameter_scanners_counter = 0
+        for key in list(arm_parameter_registry.checks.keys()):
+            parameter_scanners_counter += len(arm_parameter_registry.checks[key])
+
+        self.assertGreater(parameter_scanners_counter, 0)
 
     def test_non_colliding_check_ids(self):
         check_id_check_class_map = {}
-        for (resource_type, checks) in arm_registry.checks.items():
+        for (resource_type, checks) in arm_resource_registry.checks.items():
+            for check in checks:
+                check_id_check_class_map.setdefault(check.id, []).append(check)
+
+        for (resource_type, checks) in arm_parameter_registry.checks.items():
             for check in checks:
                 check_id_check_class_map.setdefault(check.id, []).append(check)
 
