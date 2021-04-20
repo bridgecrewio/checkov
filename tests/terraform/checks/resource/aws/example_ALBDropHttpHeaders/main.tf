a859abe8aa0fc0ff6eab00f780722cef66fd23c1
gruebel <anton.gruebel@gmail.com>
2021-04-20 22:59:08 +0200

    Fix CKV_AWS_131 check for network load balancers

---
+++ tests/terraform/checks/resource/aws/example_ALBDropHttpHeaders/main.tf
@@ -0,0 +1,62 @@
+# pass
+
+resource "aws_lb" "enabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  drop_invalid_header_fields = true
+}
+
+resource "aws_alb" "enabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  drop_invalid_header_fields = true
+}
+
+# failure
+
+resource "aws_lb" "default" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_alb" "default" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_lb" "disabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  drop_invalid_header_fields = false
+}
+
+resource "aws_alb" "disabled" {
+  internal           = false
+  load_balancer_type = "application"
+  name               = "alb"
+  subnets            = var.public_subnet_ids
+
+  drop_invalid_header_fields = false
+}
+
+# unknown
+
+resource "aws_lb" "default" {
+  internal           = false
+  load_balancer_type = "network"
+  name               = "nlb"
+  subnets            = var.public_subnet_ids
+}
