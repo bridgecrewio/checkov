38d8f76f337d3af59e2601ed11f9a549e44e5c9b
Anton Grübel <anton.gruebel@gmail.com>
2021-04-21 06:13:46 +0200

    Fix CKV_AWS_131 check for network load balancers (#1106)

--- checkov/terraform/checks/resource/aws/ALBDropHttpHeaders.py
+++ checkov/terraform/checks/resource/aws/ALBDropHttpHeaders.py
@@ -3,21 +3,20 @@ from checkov.common.models.enums import CheckCategories, CheckResult
 
 
 class ALBDropHttpHeaders(BaseResourceValueCheck):
-
     def __init__(self):
         name = "Ensure that ALB drops HTTP headers"
         id = "CKV_AWS_131"
-        supported_resources = ['aws_lb', 'aws_alb']
+        supported_resources = ["aws_lb", "aws_alb"]
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if conf.get('load_balancer_type') == 'network':
+        if conf.get("load_balancer_type") == ["network"]:
             return CheckResult.UNKNOWN
         return super().scan_resource_conf(conf)
 
     def get_inspected_key(self):
-        return 'drop_invalid_header_fields'
+        return "drop_invalid_header_fields"
 
 
 check = ALBDropHttpHeaders()
