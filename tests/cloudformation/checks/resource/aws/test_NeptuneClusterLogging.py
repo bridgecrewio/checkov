ea425f3346ed9a8189df07ee4cb8cef01f92c627
gruebel <anton.gruebel@gmail.com>
2021-05-02 11:40:00 +0200

    Add CKV_AWS_101 check to cloudformation

---
+++ tests/cloudformation/checks/resource/aws/test_NeptuneClusterLogging.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+from checkov.cloudformation.checks.resource.aws.NeptuneClusterLogging import check
+from checkov.cloudformation.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+
+class TestNeptuneClusterLogging(unittest.TestCase):
+    def test_summary(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_NeptuneClusterLogging"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
