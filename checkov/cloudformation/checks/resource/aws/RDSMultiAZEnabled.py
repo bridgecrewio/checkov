2eff1543ef91a21a2bf3c7273b0adeebf4c70e6a
Kartik Pande <kpande479@gmail.com>
2021-05-02 17:21:21 -0400

    Added CKV_AWS_154 check for TF and CFN.

---
+++ checkov/cloudformation/checks/resource/aws/RDSMultiAZEnabled.py
@@ -0,0 +1,19 @@
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+
+class RDSMultiAZEnabled(BaseResourceValueCheck):
+
+    def __init__(self):
+        name = "Ensure that RDS instances have multi_az enabled"
+        id = "CKV_AWS_154"
+        supported_resources = ['AWS::RDS::DBInstance']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED)
+
+    def get_inspected_key(self):
+        return 'Properties/MultiAZ'
+
+
+check = RDSMultiAZEnabled()
