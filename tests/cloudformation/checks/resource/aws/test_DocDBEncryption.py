c54822b34d5ce10a18fbbdd2cbfe743d55219e1e
gruebel <anton.gruebel@gmail.com>
2021-05-02 17:23:14 +0200

    Add test for passed/failed resources in other DocDB check tests

--- tests/cloudformation/checks/resource/aws/test_DocDBEncryption.py
+++ tests/cloudformation/checks/resource/aws/test_DocDBEncryption.py
@@ -7,20 +7,33 @@ from checkov.runner_filter import RunnerFilter
 
 
 class TestDocDBEncryption(unittest.TestCase):
-
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
         test_files_dir = current_dir + "/example_DocDBEncryption"
-        report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
-        self.assertEqual(summary['passed'], 1)
-        self.assertEqual(summary['failed'], 1)
-        self.assertEqual(summary['skipped'], 0)
-        self.assertEqual(summary['parsing_errors'], 0)
+        passing_resources = {
+            "AWS::DocDB::DBCluster.DocDBEnabled",
+        }
+        failing_resources = {
+            "AWS::DocDB::DBCluster.DocDBDefault",
+            "AWS::DocDB::DBCluster.DocDBDisabled",
+        }
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
