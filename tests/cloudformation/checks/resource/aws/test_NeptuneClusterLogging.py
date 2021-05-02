72c87a472ecd839875ed7cc6bb6f81b59351b232
gruebel <anton.gruebel@gmail.com>
2021-05-02 16:31:27 +0200

    Test also for passed/failed resources

--- tests/cloudformation/checks/resource/aws/test_NeptuneClusterLogging.py
+++ tests/cloudformation/checks/resource/aws/test_NeptuneClusterLogging.py
@@ -15,10 +15,24 @@ class TestNeptuneClusterLogging(unittest.TestCase):
         report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
+        passing_resources = {
+            "AWS::Neptune::DBCluster.NeptuneDBClusterEnabled",
+        }
+        failing_resources = {
+            "AWS::Neptune::DBCluster.NeptuneDBClusterDefault",
+            "AWS::Neptune::DBCluster.NeptuneDBClusterEmpty",
+        }
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
         self.assertEqual(summary["passed"], 1)
         self.assertEqual(summary["failed"], 2)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == "__main__":
