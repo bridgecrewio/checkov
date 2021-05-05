7c592dd426c42e7a85f9fda8ef52b24ec145e6d2
gruebel <anton.gruebel@gmail.com>
2021-05-06 00:11:27 +0200

    Add missing references to the Terraform plan parsing

--- tests/terraform/runner/test_plan_runner.py
+++ tests/terraform/runner/test_plan_runner.py
@@ -6,14 +6,17 @@ from checkov.terraform.plan_runner import Runner
 
 
 class TestRunnerValid(unittest.TestCase):
-
     def test_runner_two_checks_only(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan/tfplan.json"
         runner = Runner()
-        checks_allowlist = ['CKV_AWS_21']
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all', checks=checks_allowlist))
+        checks_allowlist = ["CKV_AWS_21"]
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all", checks=checks_allowlist),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -30,8 +33,12 @@ class TestRunnerValid(unittest.TestCase):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_with_child_modules/tfplan.json"
         runner = Runner()
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all"),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -46,8 +53,12 @@ class TestRunnerValid(unittest.TestCase):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_nested_child_modules/tfplan.json"
         runner = Runner()
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all"),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -73,8 +84,12 @@ class TestRunnerValid(unittest.TestCase):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_root_module_resources_no_values/tfplan.json"
         runner = Runner()
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all"),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -110,12 +125,16 @@ class TestRunnerValid(unittest.TestCase):
         # This test verifies that such a circumstance stops occurring
         # There is a EKS Managed Resource and a EKS Data Resource
         # The EKS Managed Resource should have 4 failures corresponding with EKS checks.
-        # The EKS Data Resource should not be scanned. Previously this would cause 8 failures. 
+        # The EKS Data Resource should not be scanned. Previously this would cause 8 failures.
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_data_resource_partial_values/tfplan.json"
         runner = Runner()
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all"),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -141,8 +160,9 @@ class TestRunnerValid(unittest.TestCase):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         root_dir = current_dir + "/resources"
         runner = Runner()
-        report = runner.run(root_folder=root_dir, files=None, external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=root_dir, files=None, external_checks_dir=None, runner_filter=RunnerFilter(framework="all")
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -168,9 +188,12 @@ class TestRunnerValid(unittest.TestCase):
         dir_rel_path = os.path.relpath(scan_dir_path)
 
         runner = Runner()
-        checks_allowlist = ['CKV_AWS_20']
-        report = runner.run(root_folder=dir_rel_path, external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='terraform', checks=checks_allowlist))
+        checks_allowlist = ["CKV_AWS_20"]
+        report = runner.run(
+            root_folder=dir_rel_path,
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="terraform", checks=checks_allowlist),
+        )
 
         all_checks = report.failed_checks + report.passed_checks
         for record in all_checks:
@@ -188,9 +211,12 @@ class TestRunnerValid(unittest.TestCase):
         dir_abs_path = os.path.abspath(scan_dir_path)
 
         runner = Runner()
-        checks_allowlist = ['CKV_AWS_20']
-        report = runner.run(root_folder=dir_abs_path, external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='terraform', checks=checks_allowlist))
+        checks_allowlist = ["CKV_AWS_20"]
+        report = runner.run(
+            root_folder=dir_abs_path,
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="terraform", checks=checks_allowlist),
+        )
 
         all_checks = report.failed_checks + report.passed_checks
         for record in all_checks:
@@ -209,9 +235,13 @@ class TestRunnerValid(unittest.TestCase):
         file_rel_path = os.path.relpath(scan_file_path)
 
         runner = Runner()
-        checks_allowlist = ['CKV_AWS_20']
-        report = runner.run(root_folder=None, external_checks_dir=None, files=[file_rel_path],
-                            runner_filter=RunnerFilter(framework='terraform', checks=checks_allowlist))
+        checks_allowlist = ["CKV_AWS_20"]
+        report = runner.run(
+            root_folder=None,
+            external_checks_dir=None,
+            files=[file_rel_path],
+            runner_filter=RunnerFilter(framework="terraform", checks=checks_allowlist),
+        )
 
         all_checks = report.failed_checks + report.passed_checks
         for record in all_checks:
@@ -229,22 +259,29 @@ class TestRunnerValid(unittest.TestCase):
         file_abs_path = os.path.abspath(scan_file_path)
 
         runner = Runner()
-        checks_allowlist = ['CKV_AWS_20']
-        report = runner.run(root_folder=None, external_checks_dir=None, files=[file_abs_path],
-                            runner_filter=RunnerFilter(framework='terraform', checks=checks_allowlist))
+        checks_allowlist = ["CKV_AWS_20"]
+        report = runner.run(
+            root_folder=None,
+            external_checks_dir=None,
+            files=[file_abs_path],
+            runner_filter=RunnerFilter(framework="terraform", checks=checks_allowlist),
+        )
 
         all_checks = report.failed_checks + report.passed_checks
         for record in all_checks:
             # The plan runner sets file_path to be relative from the CWD already, so this is easy
             self.assertEqual(record.repo_file_path, record.file_path)
 
-
     def test_runner_unexpected_eks_node_group_remote_access(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/unexpected/eks_node_group_remote_access.json"
         runner = Runner()
-        report = runner.run(root_folder=None, files=[valid_plan_path], external_checks_dir=None,
-                            runner_filter=RunnerFilter(framework='all'))
+        report = runner.run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all"),
+        )
         report_json = report.get_json()
         self.assertTrue(isinstance(report_json, str))
         self.assertIsNotNone(report_json)
@@ -255,5 +292,28 @@ class TestRunnerValid(unittest.TestCase):
         self.assertEqual(report.get_summary()["failed"], 0)
         self.assertEqual(report.get_summary()["passed"], 1)
 
-if __name__ == '__main__':
+    def test_runner_with_resource_reference(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        valid_plan_path = current_dir + "/resources/plan_with_resource_reference/tfplan.json"
+        allowed_checks = ["CKV_AWS_84"]
+
+        report = Runner().run(
+            root_folder=None,
+            files=[valid_plan_path],
+            external_checks_dir=None,
+            runner_filter=RunnerFilter(framework="all", checks=allowed_checks),
+        )
+
+        report_json = report.get_json()
+        self.assertTrue(isinstance(report_json, str))
+        self.assertIsNotNone(report_json)
+        self.assertIsNotNone(report.get_test_suites())
+        self.assertEqual(report.get_exit_code(soft_fail=False), 0)
+        self.assertEqual(report.get_exit_code(soft_fail=True), 0)
+
+        self.assertEqual(report.get_summary()["failed"], 0)
+        self.assertEqual(report.get_summary()["passed"], 1)
+
+
+if __name__ == "__main__":
     unittest.main()
