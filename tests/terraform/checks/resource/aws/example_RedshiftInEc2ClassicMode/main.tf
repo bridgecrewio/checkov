99028b5c35474e6cadb07be4cb24a7b8cb8c74b1
James Woolfenden <james.woolfenden@gmail.com>
2021-05-05 20:09:51 +0100

    add check reshift should be in vpc

---
+++ tests/terraform/checks/resource/aws/example_RedshiftInEc2ClassicMode/main.tf
@@ -0,0 +1,16 @@
+resource "aws_redshift_cluster" "fail" {
+  cluster_identifier = "redshift-defaults-only"
+  node_type = "dc2.large"
+  master_password = "Test1234"
+  master_username = "test"
+  skip_final_snapshot = true
+}
+
+resource "aws_redshift_cluster" "pass" {
+  cluster_identifier = "redshift-defaults-only"
+  node_type = "dc2.large"
+  master_password = "Test1234"
+  master_username = "test"
+  skip_final_snapshot = true
+  cluster_subnet_group_name="subnet-ebd9cead"
+}
