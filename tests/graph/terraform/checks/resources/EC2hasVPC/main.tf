a218465f25e32c1374b4442709f9ee952e81dee1
Anton Grübel <anton.gruebel@gmail.com>
2021-05-13 16:53:06 +0200

    Add different possibilities to check CKV2_AWS_17 (#1209)

--- tests/graph/terraform/checks/resources/EC2hasVPC/main.tf
+++ tests/graph/terraform/checks/resources/EC2hasVPC/main.tf
@@ -16,7 +16,9 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_network_interface" "network_interface_ok" {
+# pass
+
+resource "aws_network_interface" "pass_instance_network" {
   subnet_id   = aws_subnet.my_subnet.id
   private_ips = ["172.16.10.100"]
 
@@ -25,38 +27,41 @@ resource "aws_network_interface" "network_interface_ok" {
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+resource "aws_instance" "pass_instance_network" {
+  ami           = "ami-005e54dee72cc1d00"
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.network_interface_ok.id
+    network_interface_id = aws_network_interface.pass_instance_network.id
     device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
   }
 }
 
-resource "aws_network_interface" "network_interface_not_ok" {
+resource "aws_instance" "pass_instance_subnet" {
+  ami           = "ami-005e54dee72cc1d00"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+}
+
+resource "aws_network_interface" "pass_attachment" {
+  subnet_id   = aws_subnet.my_subnet.id
   private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
 }
 
-resource "aws_instance" "not_ok" {
+resource "aws_instance" "pass_attachment" {
+  ami           = "ami-005e54dee72cc1d00"
+  instance_type = "t3.micro"
+}
+
+resource "aws_network_interface_attachment" "pass_attachment" {
+  instance_id          = aws_instance.pass_attachment.id
+  network_interface_id = aws_network_interface.pass_attachment.id
+  device_index         = 0
+}
+
+# fail
+
+resource "aws_instance" "fail" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.network_interface_not_ok.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
 }
