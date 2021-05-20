e4ee4b419a5f7b4eccb36d58739d783eaa8a9266
nimrodkor <nimrodkor@gmail.com>
2021-05-20 12:15:07 +0300

    Fail if there is an SG rule connected to default SG

--- tests/graph/terraform/checks/resources/VPCHasRestrictedSG/main.tf
+++ tests/graph/terraform/checks/resources/VPCHasRestrictedSG/main.tf
@@ -6,6 +6,10 @@ resource "aws_vpc" "not_ok_vpc_2" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_vpc" "not_ok_vpc_3" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_vpc" "ok_vpc" {
   cidr_block = "10.0.0.0/16"
 }
@@ -30,4 +34,16 @@ resource "aws_default_security_group" "default_2" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_default_security_group" "default_3" {
+  vpc_id = aws_vpc.not_ok_vpc_3.id
+}
+
+resource "aws_security_group_rule" "default_sg_rule" {
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  type              = "-1"
+  security_group_id = aws_default_security_group.default_3.id
 }
