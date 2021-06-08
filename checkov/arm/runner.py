7639df19217802f1dd92ff98163a11ca87216c6a
tronxd <tron47@gmail.com>
2021-06-08 12:53:49 +0300

    fix relative path calculation in plan runner

--- checkov/arm/runner.py
+++ checkov/arm/runner.py
@@ -122,7 +122,7 @@ class Runner(BaseRunner):
                                                 code_block=entity_code_lines, file_path=arm_file,
                                                 file_line_range=entity_lines_range,
                                                 resource=resource_id, evaluations=variable_evaluations,
-                                                check_class=check.__class__.__module__, file_abs_path=file_abs_path,repo_file_path=arm_file)
+                                                check_class=check.__class__.__module__, file_abs_path=file_abs_path)
                                 report.add_record(record=record)
 
         return report
