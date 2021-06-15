c5f1ad093fc0a0f38101ec2e4110531982d8f36a
Mike Urbanski <mike@bridgecrew.io>
2021-06-15 17:47:57 -0500

    use default encoding logic for a list

--- checkov/common/util/json_utils.py
+++ checkov/common/util/json_utils.py
@@ -4,6 +4,6 @@ import json
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, set):
-            return json.JSONEncoder.default(self, list(o))
+            return json.dumps(list(o))
         else:
             return json.JSONEncoder.default(self, o)
