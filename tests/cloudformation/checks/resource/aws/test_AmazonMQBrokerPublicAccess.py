b118ae8e706e795e9b751ba2ba6a31e1f06464d2
root <root@ip-10-2-20-10.ap-southeast-1.compute.internal>
2021-06-16 19:06:33 +0000

    Add new check - CF - CKV_AWS_166 (AmazonMQ Public Access)

--- tests/cloudformation/checks/resource/aws/test_AmazonMQBrokerPublicAccess.py
+++ tests/cloudformation/checks/resource/aws/test_AmazonMQBrokerPublicAccess.py
@@ -1,33 +1,34 @@
 import os
 import unittest
 
-from checkov.cloudformation.checks.resource.aws.DynamodbGlobalTableRecovery import check
+from checkov.cloudformation.checks.resource.aws.AmazonMQBrokerPublicAccess import check
 from checkov.cloudformation.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestDynamodbGlobalTableRecovery(unittest.TestCase):
+class TestAmazonMQBrokerPublicAccess(unittest.TestCase):
 
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_DynamodbGlobalTableRecovery"
+        test_files_dir = current_dir + "/example_AmazonMQBrokerPublicAccess"
         report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
         passing_resources = {
-            "AWS::DynamoDB::GlobalTable.MyGlobalTableRecoveryEnabled",
+            "AWS::AmazonMQ::Broker.PrivateBroker0",
+            "AWS::AmazonMQ::Broker.PrivateBroker1",
         }
         failing_resources = {
-            "AWS::DynamoDB::GlobalTable.MyGlobalTableRecoveryDisabled",
-            "AWS::DynamoDB::GlobalTable.MyGlobalTableRecoveryDefault",
+            "AWS::AmazonMQ::Broker.PublicBroker0",
+            "AWS::AmazonMQ::Broker.PublicBroker1",
         }
 
         passed_check_resources = set([c.resource for c in report.passed_checks])
         failed_check_resources = set([c.resource for c in report.failed_checks])
 
-        self.assertEqual(summary['passed'], 1)
+        self.assertEqual(summary['passed'], 2)
         self.assertEqual(summary['failed'], 2)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
