90b4a4d515ca2201a09c680a01c9ade7d7145165
root <root@ip-10-2-20-10.ap-southeast-1.compute.internal>
2021-06-17 05:18:34 +0000

    CKV_AWS_69: enhancement CF Amazon Broker

--- checkov/cloudformation/checks/resource/aws/AmazonMQBrokerPublicAccess.py
+++ checkov/cloudformation/checks/resource/aws/AmazonMQBrokerPublicAccess.py
@@ -1,28 +1,25 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
-class AmazonMQBrokerPublicAccess(BaseResourceCheck):
+class AmazonMQBrokerPublicAccess(BaseResourceValueCheck):
 
     def __init__(self):
         name = "Ensure Amazon MQ Broker should not have public access"
-        id = "CKV_AWS_166"
+        id = "CKV_AWS_69"
         supported_resources = ['AWS::AmazonMQ::Broker']
         categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources, missing_block_result=CheckResult.FAILED)
 
-    def scan_resource_conf(self, conf):
+    def get_expected_value(self):
+        return False
+
+    def get_inspected_key(self):
         """
             validates Amazon MQ Broker should not have public access
             https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-amazonmq-broker.html
         :return: <CheckResult>
         """
+        return 'Properties/PubliclyAccessible'
 
-        if 'Properties' in conf.keys():
-            if 'PubliclyAccessible' in conf['Properties'].keys():
-                if conf['Properties']['PubliclyAccessible'] is True:
-                    return CheckResult.FAILED
-                else:
-                    return CheckResult.PASSED
-        return CheckResult.PASSED
 
 check = AmazonMQBrokerPublicAccess()
