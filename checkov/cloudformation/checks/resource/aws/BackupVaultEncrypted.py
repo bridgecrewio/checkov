c83152c4af01fc25b0a4b97e7ff3b739b75838b8
njgibbon <njgibbon@outlook.com>
2021-06-25 17:27:25 +0100

    new check

---
+++ checkov/cloudformation/checks/resource/aws/BackupVaultEncrypted.py
@@ -0,0 +1,17 @@
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class BackupVaultEncrypted(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure Backup Vault is encrypted at rest using KMS CMK"
+        id = "CKV_AWS_166"
+        supported_resources = ['AWS::Backup::BackupVault']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'Properties/EncryptionKeyArn'
+
+
+check = BackupVaultEncrypted()
