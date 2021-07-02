a81482b7da3733c4cdf0b6f0715f85fb26022f24
Brad Dausses <brad.dausses@gmail.com>
2021-07-02 16:04:13 -0400

    Updated test file per format suggested by mikeurbanski1
    Signed-off-by: Brad Dausses <brad.dausses@gmail.com>

--- tests/terraform/checks/provider/aws/test_defaulttags.py
+++ tests/terraform/checks/provider/aws/test_defaulttags.py
@@ -1,4 +1,5 @@
 import unittest
+import hcl2
 
 from checkov.terraform.checks.provider.aws.defaulttags import check
 from checkov.common.models.enums import CheckResult
@@ -6,15 +7,46 @@ from checkov.common.models.enums import CheckResult
 
 class TestDefaultTags(unittest.TestCase):
 
-    def test_success(self):
-        provider_conf = {'region': ['us-west-2'], 'default_tags': {'tags': {'Environment': 'Test', 'Owner': 'TestOwner'}}}
+    def test_failure_no_default_tags(self):
+        hcl_res = hcl2.loads("""
+          provider "aws" {
+            region     = "${var.region}"
+          }
+        """)
+        provider_conf = hcl_res['provider'][0]['aws']
+        scan_result = check.scan_provider_conf(conf=provider_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_failure_empty_map(self):
+        hcl_res = hcl2.loads("""
+          provider "aws" {
+            region     = "${var.region}"
+            default_tags {
+              tags = {
+              }
+            }
+          }
+        """)
+        provider_conf = hcl_res['provider'][0]['aws']
+        scan_result = check.scan_provider_conf(conf=provider_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success_default_tags(self):
+        hcl_res = hcl2.loads("""
+          provider "aws" {
+            region     = "${var.region}"
+            default_tags {
+              tags = {
+                Environment = "Test"
+                Project     = "Test"
+              }
+            }
+          }
+        """)
+        provider_conf = hcl_res['provider'][0]['aws']
         scan_result = check.scan_provider_conf(conf=provider_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
-    def test_failure(self):
-        provider_conf = {'region': ['us-west-2']}
-        scan_result = check.scan_provider_conf(conf=provider_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
 
 if __name__ == '__main__':
     unittest.main()
