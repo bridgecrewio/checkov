232a7601181e400e544719933733e2480e0a6c22
tronxd <tron47@gmail.com>
2021-07-04 18:57:02 +0300

    handle a case where interpolation syntax is given as the policy content

--- checkov/secrets/plugins/entropy_keyword_combinator.py
+++ checkov/secrets/plugins/entropy_keyword_combinator.py
@@ -1,7 +1,9 @@
 from abc import ABCMeta
 
 from detect_secrets.plugins.base import BasePlugin
+from detect_secrets.plugins.high_entropy_strings import HighEntropyStringsPlugin
+from detect_secrets.plugins.keyword import KeywordDetector
 
 
-class EntropyKeywordCombinator(BasePlugin, metaclass=ABCMeta):
+class EntropyKeywordCombinator(BasePlugin, HighEntropyStringsPlugin, KeywordDetector, metaclass=ABCMeta):
     secret_type = 'Scans for High entropy strings and secret-sounding variable names'
