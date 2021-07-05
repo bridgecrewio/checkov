3152e88ee5c3f5f6a5a8aba51a92b312a3064003
tronxd <tron47@gmail.com>
2021-07-05 14:26:36 +0300

    plugin definition

--- checkov/secrets/plugins/entropy_keyword_combinator.py
+++ checkov/secrets/plugins/entropy_keyword_combinator.py
@@ -10,8 +10,6 @@ class EntropyKeywordCombinator(BasePlugin):
     def __init__(self, limit: float) -> None:
         self.high_entropy_scanner = (Base64HighEntropyString(limit=limit), HexHighEntropyString(limit=limit))
         self.keyword_scanner = KeywordDetector()
-        print(1)
-        # TODO
 
     def analyze_string(self, string: str) -> Generator[str, None, None]:
         return iter(["1"])
