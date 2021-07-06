76364b93b78550fac115e080a02b1d7cb92d3629
tronxd <tron47@gmail.com>
2021-07-06 10:45:27 +0300

    provide abspath

--- integration_tests/test_checkov_json_report.py
+++ integration_tests/test_checkov_json_report.py
@@ -8,19 +8,19 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 class TestCheckovJsonReport(unittest.TestCase):
 
     def test_terragoat_report(self):
-        report_path = current_dir + "/../checkov_report_terragoat.json"
+        report_path = os.path.abspath(current_dir + "/../checkov_report_terragoat.json")
         self.validate_report(os.path.abspath(report_path))
 
     def test_cfngoat_report(self):
-        report_path = current_dir + "/../checkov_report_cfngoat.json"
+        report_path = os.path.abspath(current_dir + "/../checkov_report_cfngoat.json")
         self.validate_report(os.path.abspath(report_path))
 
     def test_k8goat_report(self):
-        report_path = current_dir + "/../checkov_report_kubernetes-goat.json"
+        report_path = os.path.abspath(current_dir + "/../checkov_report_kubernetes-goat.json")
         self.validate_report(os.path.abspath(report_path))
 
     def test_k8goat_report(self):
-        report_path = current_dir + "/../checkov_report_kubernetes-goat-helm.json"
+        report_path = os.path.abspath(current_dir + "/../checkov_report_kubernetes-goat-helm.json")
         self.validate_report(os.path.abspath(report_path))
 
     def test_checkov_report_terragoat_with_skip(self):
