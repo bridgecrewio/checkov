e60274e57ea3d84c69acba512ab7b4946fbfcbcc
njgibbon <njgibbon@outlook.com>
2021-07-11 18:00:04 +0100

    simplify old test cf

--- tests/cloudformation/checks/resource/aws/test_CloudwatchLogGroupRetention.py
+++ tests/cloudformation/checks/resource/aws/test_CloudwatchLogGroupRetention.py
@@ -6,18 +6,18 @@ from checkov.cloudformation.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestcloudwatchLogGroupRetention(unittest.TestCase):
+class TestCloudWatchLogGroupRetention(unittest.TestCase):
 
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_cloudwatchLogGroupRetention"
+        test_files_dir = current_dir + "/example_CloudWatchLogGroupRetention"
         report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
-        self.assertEqual(summary['passed'], 2)
-        self.assertEqual(summary['failed'], 2)
+        self.assertEqual(summary['passed'], 1)
+        self.assertEqual(summary['failed'], 1)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
 
