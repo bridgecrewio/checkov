ced031aa88b3eace4c6c0073abe4ddf18fd3209d
rotemavni <rotemavn@gmail.com>
2021-07-11 14:18:20 +0300

    moved UTs

---
+++ tests/terraform/graph/checks_infra/attribute_solvers/not_contains_solver/test_solver.py
@@ -0,0 +1,38 @@
+import os
+
+from tests.terraform.graph.checks_infra.test_base import TestBaseSolver
+
+TEST_DIRNAME = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestNotContainsSolver(TestBaseSolver):
+    def setUp(self):
+        self.checks_dir = TEST_DIRNAME
+        super(TestNotContainsSolver, self).setUp()
+
+    def test_public_virtual_machines(self):
+        root_folder = '../../../resources/public_virtual_machines'
+        check_id = "PublicVMs"
+        should_pass = ['aws_default_security_group.default_security_group_closed']
+        should_fail = ['aws_default_security_group.default_security_group_open']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_list_cidr_blocks(self):
+        root_folder = '../../../resources/security_group_list_cidr_blocks'
+        check_id = "PublicSG"
+        should_pass = ['aws_security_group.passed_cidr_block', 'aws_security_group.failed_cidr_blocks']
+        should_fail = []
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_list_cidr_blocks_specific(self):
+        root_folder = '../../../resources/security_group_list_cidr_blocks'
+        check_id = "SpecificBlockSG"
+        should_pass = ['aws_security_group.failed_cidr_blocks']
+        should_fail = ['aws_security_group.passed_cidr_block']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
---
+++ tests/terraform/graph/checks_infra/complex_solvers/and_solver/test_solver.py
@@ -0,0 +1,18 @@
+from tests.terraform.graph.checks_infra.test_base import TestBaseSolver
+import os
+TEST_DIRNAME = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestAndQuery(TestBaseSolver):
+    def setUp(self):
+        self.checks_dir = TEST_DIRNAME
+        super(TestAndQuery, self).setUp()
+
+    def test_buckets_with_option_env_tag(self):
+        root_folder = '../../../resources/s3_bucket_2'
+        check_id = "BucketsWithDevEnvAndPrivateACL"
+        should_pass = ['aws_s3_bucket.private']
+        should_fail = ['aws_s3_bucket.public', 'aws_s3_bucket.non_tag']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
