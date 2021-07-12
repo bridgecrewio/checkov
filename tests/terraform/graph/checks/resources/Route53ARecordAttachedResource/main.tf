b395264c62997e1c13e0f0aa76a93d4223c56c9c
James Woolfenden <james.woolfenden@gmail.com>
2021-07-12 15:24:28 +0100

    also ignore modules

--- tests/terraform/graph/checks/resources/Route53ARecordAttachedResource/main.tf
+++ tests/terraform/graph/checks/resources/Route53ARecordAttachedResource/main.tf
@@ -44,3 +44,14 @@ resource "aws_route53_record" "ignore" {
   ttl     = 60
   records = [module.controller.loadbalancer_dns_name]
 }
+
+resource "aws_route53_record" "pass3" {
+  zone_id = var.zone_id
+  name = "test.example.com"
+  type = "A"
+  alias {
+    name = module.alb.lb_dns_name
+    zone_id = module.alb.lb_zone_id
+    evaluate_target_health = true
+  }
+}
