c145889b76fa2dcbc9d656225c76ac6ae2e70787
nimrodkor <nimrodkor@gmail.com>
2021-07-14 13:48:57 +0300

    Fix entropy-keyword plugin to use analyze_line

--- checkov/secrets/plugins/entropy_keyword_combinator.py
+++ checkov/secrets/plugins/entropy_keyword_combinator.py
@@ -1,8 +1,8 @@
+from detect_secrets.core.potential_secret import PotentialSecret
 from detect_secrets.plugins.high_entropy_strings import Base64HighEntropyString, HexHighEntropyString
 from detect_secrets.plugins.keyword import KeywordDetector
 from detect_secrets.plugins.base import    BasePlugin
-from typing import Generator
-from checkov.common.util.data_structures_utils import generator_reader_wrapper
+from typing import Generator, Any, Set
 
 
 class EntropyKeywordCombinator(BasePlugin):
@@ -12,18 +12,24 @@ class EntropyKeywordCombinator(BasePlugin):
         self.high_entropy_scanners = (Base64HighEntropyString(limit=limit), HexHighEntropyString(limit=limit))
         self.keyword_scanner = KeywordDetector()
 
+    def analyze_line(
+            self,
+            filename: str,
+            line: str,
+            line_number: int = 0,
+            **kwargs: Any
+    ) -> Set[PotentialSecret]:
+        """
+        This method first runs the keyword plugin. If it finds a match - it runs the entropy scanners, and if
+        one of the entropy scanners find a match (on a line which was already matched by keyword plugin) - it is returned.
+        """
+        keyword_matches = self.keyword_scanner.analyze_line(filename, line, line_number, **kwargs)
+        if len(keyword_matches):
+            for entropy_scanner in self.high_entropy_scanners:
+                matches = entropy_scanner.analyze_line(filename, line, line_number, **kwargs)
+                if len(matches) > 0:
+                    return matches
+        return set([])
+
     def analyze_string(self, string: str) -> Generator[str, None, None]:
-        keyword_secrets_generator = self.keyword_scanner.analyze_string(string)
-        potential_entropy_secrets = []
-        potential_kw_secret = generator_reader_wrapper(keyword_secrets_generator)
-        if not potential_kw_secret:
-            return
-        for entropy_scanner in self.high_entropy_scanners:
-            potential_entropy_secret = generator_reader_wrapper(entropy_scanner.analyze_string(string))
-            if potential_entropy_secret:
-                self.secret_type = entropy_scanner.secret_type
-                potential_entropy_secrets.append(potential_entropy_secret)
-        if potential_kw_secret and len(potential_entropy_secrets) > 0:
-            yield string
-
-
+        raise NotImplementedError()
