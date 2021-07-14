332ca55cd4dbf5867868cf6dd964fa785e14437e
Vimukthi Perera <VimukthiPerera@users.noreply.github.com>
2021-07-14 14:20:57 +0530

    Update checkov/kubernetes/checks/MinimizeCapabilities.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/kubernetes/checks/MinimizeCapabilities.py
+++ checkov/kubernetes/checks/MinimizeCapabilities.py
@@ -21,7 +21,7 @@ class MinimizeCapabilities(BaseK8Check):
             if conf["securityContext"].get("capabilities"):
                 if conf["securityContext"]["capabilities"].get("drop"):
                     for d in conf["securityContext"]["capabilities"]["drop"]:
-                        if "ALL" in d or "all" in d:
+                        if any(cap in d for cap in ("ALL", "all")):
                             return CheckResult.PASSED
         return CheckResult.FAILED
 
