c419f6ed8bb7614ec47d7b78edd8bfc633e9b99a
Mike Urbanski <mike@bridgecrew.io>
2021-07-15 10:15:51 -0500

    make source type Optional

--- checkov/common/bridgecrew/bc_source.py
+++ checkov/common/bridgecrew/bc_source.py
@@ -1,10 +1,10 @@
-
+from typing import Optional
 
 # Helper methods for determining behavior based on different BC_SOURCE values. Python enums are limiting here, and
 # using a class is overkill
 
 
-def should_upload_results(source: str) -> bool:
+def should_upload_results(source: Optional[str]) -> bool:
     """Whether scan results should be uploaded to the platform. Default for unknown sources is False."""
     if source == 'vscode':
         return False
