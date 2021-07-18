b359a515b3456c76b7d42e58e615fc3eabce36ee
rotemavni <rotemavn@gmail.com>
2021-07-18 14:55:39 +0300

    fixed expected number of failed checks

--- tests/terraform/runner/test_runner.py
+++ tests/terraform/runner/test_runner.py
@@ -84,7 +84,7 @@ class TestRunnerValid(unittest.TestCase):
         self.assertEqual(report.get_exit_code(False), 1)
         summary = report.get_summary()
         self.assertGreaterEqual(summary['passed'], 1)
-        self.assertEqual(7, summary['failed'])
+        self.assertEqual(3, summary['failed'])
         self.assertEqual(1, summary['skipped'])
         self.assertEqual(0, summary["parsing_errors"])
 
@@ -159,7 +159,7 @@ class TestRunnerValid(unittest.TestCase):
         # self.assertEqual(report.get_exit_code(), 0)
         summary = report.get_summary()
         self.assertGreaterEqual(summary['passed'], 1)
-        self.assertEqual(4, summary['failed'])
+        self.assertEqual(2, summary['failed'])
         self.assertEqual(0, summary["parsing_errors"])
 
     def test_check_ids_dont_collide(self):
