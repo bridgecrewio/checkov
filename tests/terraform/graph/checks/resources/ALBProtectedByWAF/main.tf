ae00741ebbc207363f1af01f864ccc1fca4818c8
James Woolfenden <james.woolfenden@gmail.com>
2021-07-28 10:32:15 +0100

    Public ALB must be protected by WAF

--- tests/terraform/graph/checks/resources/ALBProtectedByWAF/main.tf
+++ tests/terraform/graph/checks/resources/ALBProtectedByWAF/main.tf
@@ -2,22 +2,23 @@ resource "aws_lb" "lb_good_1" {
   internal= false
 }
 
-resource "aws_lb" "lb_good_2" {
-}
 
 resource "aws_wafregional_web_acl_association" "foo" {
-  resource_arn = aws_alb.lb_good_1.arn
+  resource_arn = aws_lb.lb_good_1.arn
   web_acl_id = aws_wafregional_web_acl.foo.id
 }
 
-resource "aws_wafregional_web_acl_association" "bar" {
-  resource_arn = aws_alb.lb_good_2.arn
-  web_acl_id = aws_wafregional_web_acl.foo.id
-}
-
+//public no WAF
 resource "aws_lb" "lb_bad_1" {
-  internal=true
+  internal=false
 }
 
+//internal should ignore
+resource "aws_lb" "ignore" {
+  internal= true
+}
 
+//public internal not set (takes default - public)
+resource "aws_lb" "lb_bad_2" {
+}
 
