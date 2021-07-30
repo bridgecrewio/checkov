4bf16a6a5c719bf3854aa803d35c1efa2885113c
James Woolfenden <james.woolfenden@gmail.com>
2021-07-30 15:38:29 +0100

    finally?

--- tests/terraform/graph/checks/resources/APIProtectedByWAF/main.tf
+++ tests/terraform/graph/checks/resources/APIProtectedByWAF/main.tf
@@ -1,14 +1,3 @@
-resource "aws_api_gateway_stage" "pass" {
-  deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.pass.id
-  stage_name    = "example"
-}
-
-resource "aws_wafregional_web_acl_association" "pass" {
-  resource_arn = aws_api_gateway_stage.example.arn
-  web_acl_id   = aws_wafregional_web_acl.foo.id
-}
-
 resource "aws_api_gateway_rest_api" "pass" {
   name = var.name
 
@@ -53,4 +42,27 @@ resource "aws_api_gateway_stage" "no_assoc" {
   deployment_id = aws_api_gateway_deployment.example.id
   rest_api_id   = aws_api_gateway_rest_api.no_assoc.id
   stage_name    = "example"
+}
+
+resource "aws_api_gateway_stage" "private" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.private.id
+  stage_name    = "example"
+}
+
+resource "aws_api_gateway_stage" "no_api" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.no_api.id
+  stage_name    = "example"
+}
+
+resource "aws_api_gateway_stage" "pass" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.pass.id
+  stage_name    = "example"
+}
+
+resource "aws_wafregional_web_acl_association" "pass" {
+  resource_arn = aws_api_gateway_stage.pass.arn
+  web_acl_id   = aws_wafregional_web_acl.foo.id
 }
