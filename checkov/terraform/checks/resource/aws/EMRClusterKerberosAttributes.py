eb8aa1c388022d6a4162e6872d66f674a240fbde
gruebel <anton.gruebel@gmail.com>
2021-07-31 23:25:32 +0900

    fix default response for CVK_AWS_114

--- checkov/terraform/checks/resource/aws/EMRClusterKerberosAttributes.py
+++ checkov/terraform/checks/resource/aws/EMRClusterKerberosAttributes.py
@@ -1,23 +1,25 @@
+from typing import Dict, List, Any
+
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
 class EMRClusterKerberosAttributes(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure that EMR clusters with Kerberos have Kerberos Realm set"
         id = "CKV_AWS_114"
-        supported_resources = ['aws_emr_cluster']
+        supported_resources = ["aws_emr_cluster"]
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        if 'kerberos_attributes' in conf:
-            kerberos_attributes = conf['kerberos_attributes'][0]
-            if len(kerberos_attributes) > 0 and 'realm' in kerberos_attributes:
+    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+        if "kerberos_attributes" in conf:
+            kerberos_attributes = conf["kerberos_attributes"][0]
+            if kerberos_attributes and "realm" in kerberos_attributes:
                 return CheckResult.PASSED
             else:
                 return CheckResult.FAILED
-        return CheckResult.SKIPPED
+        return CheckResult.UNKNOWN
 
 
 check = EMRClusterKerberosAttributes()
