59b738ea26d73c0b0fafd5370e600af993ff4a8a
kartikp10 <kpande479@gmail.com>
2021-08-03 17:32:34 -0400

    Use md5 hash of source for windows folder name

--- checkov/terraform/module_loading/loaders/git_loader.py
+++ checkov/terraform/module_loading/loaders/git_loader.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 from checkov.common.goget.github.get_git import GitGetter
 from checkov.terraform.module_loading.content import ModuleContent
@@ -14,9 +15,6 @@ class GenericGitLoader(ModuleLoader):
     def _load_module(self) -> ModuleContent:
         try:
             module_source = self.module_source.replace('git::', '')
-            if os.name == 'nt':
-                self.logger.info(f'Operating System: {os.name}')
-                self._create_valid_windows_dest_dir()
             git_getter = GitGetter(module_source, create_clone_and_result_dirs=False)
             git_getter.temp_dir = self.dest_dir
             git_getter.do_get()
@@ -27,13 +25,6 @@ class GenericGitLoader(ModuleLoader):
         except Exception as e:
             self.logger.error(f'failed to get {self.module_source} because of {e}')
             return ModuleContent(dir=None, failed_url=self.module_source)
-
-    def _create_valid_windows_dest_dir(self):
-        # https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
-        reserved_windows_chars = ['<', '>', ':', '"', '|', '?', '*']
-        self.logger.info(f'External module will be cloned to: {self.dest_dir}')
-        for char in reserved_windows_chars:
-            self.dest_dir = self.dest_dir.replace(char, '')
 
 
 loader = GenericGitLoader()
