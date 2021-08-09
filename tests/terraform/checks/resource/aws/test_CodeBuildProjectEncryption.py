7ef4f231e83649a5e5045da79041789ce628409e
James Woolfenden <james.woolfenden@gmail.com>
2021-08-09 12:36:00 +0100

    refactored test to use new method

--- tests/terraform/checks/resource/aws/test_CodeBuildProjectEncryption.py
+++ tests/terraform/checks/resource/aws/test_CodeBuildProjectEncryption.py
@@ -1,62 +1,39 @@
+import os
 import unittest
 
-from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.CodeBuildProjectEncryption import check
+from checkov.terraform.runner import Runner
 
 
 class TestCodeBuildProjectEncryption(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    def test_failure(self):
-        resource_conf = {
-            "name": "test-project",
-            "artifacts": [
-                {
-                    "type": "S3",
-                    "encryption_disabled": True,
-                }
-            ],
+        test_files_dir = current_dir + "/example_CodeBuildProjectEncryption"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            "aws_codebuild_project.success_no_encryption_disabled",
+            "aws_codebuild_project.success"
         }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
-
-    def test_success_type_no_artifacts_encryption_ignored(self):
-        resource_conf = {
-            "name": "test-project",
-            "artifacts": [
-                {
-                    "type": "NO_ARTIFACTS",
-                    "encryption_disabled": True,
-                }
-            ],
+        failing_resources = {
+            "aws_codebuild_project.fail",
         }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
 
-    def test_success_no_encryption_disabled(self):
-        resource_conf = {
-            "name": "test-project",
-            "artifacts": [
-                {
-                    "type": "S3",
-                }
-            ],
-        }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
-
-    def test_success(self):
-        resource_conf = {
-            "name": "test-project",
-            "artifacts": [
-                {
-                    "type": "S3",
-                    "encryption_disabled": False,
-                }
-            ],
-        }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
 
 
-if __name__ == '__main__':
+        self.assertEqual(summary["passed"], 2)
+        self.assertEqual(summary["failed"], 1)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == "__main__":
     unittest.main()
