4702212313e1b74de8332d9f12ed1ffb8903f08a
njgibbon <njgibbon@outlook.com>
2021-08-13 01:10:44 +0100

    cmk encrypted dynamo db 4 tf

--- checkov/terraform/checks/resource/aws/DynamoDBTablesEncrypted.py
+++ checkov/terraform/checks/resource/aws/DynamoDBTablesEncrypted.py
@@ -1,17 +1,25 @@
+from typing import Dict, List, Any
+
 from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
 class DynamoDBTablesEncrypted(BaseResourceValueCheck):
     def __init__(self):
-        name = "Ensure DynamoDB Tables are encrypted using KMS"
+        name = "Ensure DynamoDB Tables are encrypted using a KMS Customer Managed CMK"
         id = "CKV_AWS_119"
         supported_resources = ["aws_dynamodb_table"]
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return "server_side_encryption/[0]/enabled"
+    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+        sse = conf.get("server_side_encryption")
+        enabled = sse.get("enabled")
+        kms_key_arn = sse.get("kms_key_arn")
+        if enabled is True and kms_key_arn != None:
+            return CheckResult.PASSED
+        else:
+            return CheckResult.FAILED
 
 
 check = DynamoDBTablesEncrypted()
--- tests/terraform/checks/resource/aws/test_DynamoDBTablesEncrypted.py
+++ tests/terraform/checks/resource/aws/test_DynamoDBTablesEncrypted.py
@@ -1,79 +1,39 @@
+import os
 import unittest
-import hcl2
 
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.DynamoDBTablesEncrypted import check
-from checkov.common.models.enums import CheckResult
+from checkov.terraform.runner import Runner
 
 
-class TestELBAccessLogs(unittest.TestCase):
+class TestDynamoDBTablesEncrypted(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    def test_failure(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_dynamodb_table" "basic-dynamodb-table" {
-              name           = "GameScores"
-              billing_mode   = "PROVISIONED"
-              read_capacity  = 20
-              write_capacity = 20
-              hash_key       = "UserId"
-              range_key      = "UserId"
-            
-              attribute {
-                name = "UserId"
-                type = "S"
-              }
-           
-              server_side_encryption {
-                  enabled = false
-              }
-            } 
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        test_files_dir = current_dir + "/example_DynamoDBTableEncrypted"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
 
-    def test_failure2(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_dynamodb_table" "basic-dynamodb-table" {
-              name           = "GameScores"
-              billing_mode   = "PROVISIONED"
-              read_capacity  = 20
-              write_capacity = 20
-              hash_key       = "UserId"
-              range_key      = "UserId"
-            
-              attribute {
-                name = "UserId"
-                type = "S"
-              }
-            } 
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        passing_resources = {
+            "aws_dynamodb_table.cmk",
+        }
+        failing_resources = {
+            "aws_dynamodb_table.default",
+            "aws_dynamodb_table.encrypted_false",
+            "aws_dynamodb_table.encrypted_no_cmk",
+        }
 
-    def test_success(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_dynamodb_table" "basic-dynamodb-table" {
-              name           = "GameScores"
-              billing_mode   = "PROVISIONED"
-              read_capacity  = 20
-              write_capacity = 20
-              hash_key       = "UserId"
-              range_key      = "UserId"
-            
-              attribute {
-                name = "UserId"
-                type = "S"
-              }
-           
-              server_side_encryption {
-                  enabled = true
-              }
-            } 
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_dynamodb_table']['basic-dynamodb-table']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["failed"], 3)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == '__main__':
