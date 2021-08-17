623600210b5a8392e62580e80986d4c430427b56
Mike Urbanski <mike@bridgecrew.io>
2021-08-17 15:47:25 -0700

    pass record BC ID to super

--- checkov/common/output/graph_record.py
+++ checkov/common/output/graph_record.py
@@ -8,6 +8,6 @@ class GraphRecord(Record):
         super().__init__(record.check_id, record.check_name, record.check_result, record.code_block, record.file_path,
                          record.file_line_range, record.resource, record.evaluations, record.check_class,
                          record.file_abs_path, record.entity_tags, record.caller_file_path,
-                         record.caller_file_line_range)
+                         record.caller_file_line_range, bc_check_id=record.bc_check_id)
         self.fixed_definition = record.fixed_definition
         self.breadcrumbs = breadcrumbs
