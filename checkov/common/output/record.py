6a2e373615d7d1186e5c8cdf0bbe2729c700f1cb
Mike Urbanski <mike@bridgecrew.io>
2021-08-20 15:43:50 -0500

    make the path used for suppressions a unix path always

--- checkov/common/output/record.py
+++ checkov/common/output/record.py
@@ -40,7 +40,7 @@ class Record:
         self.code_block = code_block
         self.file_path = file_path
         self.file_abs_path = file_abs_path
-        self.repo_file_path = f'/{os.path.relpath(file_abs_path)}' # matches file paths given in the BC platform.
+        self.repo_file_path = f'/{os.path.relpath(file_abs_path)}'.replace('\\', '/')  # matches file paths given in the BC platform and should always be a unix path
         self.file_line_range = file_line_range
         self.resource = resource
         self.evaluations = evaluations
