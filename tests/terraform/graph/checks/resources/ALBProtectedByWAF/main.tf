7bdfcc53a419f65ae3c05149ae2c45fb1bdcfdf9
hegge.nu <hegge.nu@gmail.com>
2021-08-23 15:04:05 +0200

    Fix/Improvement to CKV2_AWS_28 (#1519)
    * Add support for aws_wafv2_web_acl_association
    
    * Add support for aws_wafv2_web_acl_association
    
    Updated main.tf for testing support for aws_wafv2_web_acl_association, added additional resources.
    
    * Add support for aws_wafv2_web_acl_association
    
    Updated expected test results for aws_wafv2_web_acl_association alb resources.

--- tests/terraform/graph/checks/resources/ALBProtectedByWAF/main.tf
+++ tests/terraform/graph/checks/resources/ALBProtectedByWAF/main.tf
@@ -2,10 +2,18 @@ resource "aws_lb" "lb_good_1" {
   internal= false
 }
 
+resource "aws_lb" "lb_good_2" {
+  internal= false
+}
 
 resource "aws_wafregional_web_acl_association" "foo" {
   resource_arn = aws_lb.lb_good_1.arn
   web_acl_id = aws_wafregional_web_acl.foo.id
+}
+
+resource "aws_wafv2_web_acl_association" "bar" {
+  resource_arn = aws_lb.lb_good_2.arn
+  web_acl_arn = aws_wafv2_web_acl.bar.arn
 }
 
 //public no WAF
