883d74ae904cf5be6df40d41cbd07e7f0c63f06f
Mike Urbanski <mike@bridgecrew.io>
2021-08-26 18:03:17 -0500

    add var without file test

--- tests/terraform/parser/resources/parser_scenarios/tfvars/main.tf
+++ tests/terraform/parser/resources/parser_scenarios/tfvars/main.tf
@@ -1,6 +1,9 @@
 variable "foo" {}
 variable "list_data" {}
 variable "map_data" {}
+variable "only_here" {
+  default = "hello"
+}
 variable "other_var_1" {
   default = "abc"
 }
@@ -9,5 +12,5 @@ variable "other_var_2" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${var.foo}-${var.list_data[0]}-${var.map_data[stage]}-${var.other_var_1}-${var.other_var_2}"
+  bucket = "${var.only_here}-${var.foo}-${var.list_data[0]}-${var.map_data[stage]}-${var.other_var_1}-${var.other_var_2}"
 }
