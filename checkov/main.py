cc25fd49fedc7de00d49438953ef5245f2e8fe97
Mike Urbanski <mike@bridgecrew.io>
2021-08-30 11:58:32 -0500

    add extra help text for --var-file

--- checkov/main.py
+++ checkov/main.py
@@ -302,7 +302,7 @@ def add_parser_args(parser):
     parser.add('--var-file', action='append',
                help='Variable files to load in addition to the default files (see '
                     'https://www.terraform.io/docs/language/values/variables.html#variable-definitions-tfvars-files).'
-                    'Currently only supported for source Terraform (.tf file) scans.')
+                    'Currently only supported for source Terraform (.tf file) scans. Requires using --directory, not --file.')
     parser.add('--external-modules-download-path',
                help="set the path for the download external terraform modules",
                default=DEFAULT_EXTERNAL_MODULES_DIR, env_var='EXTERNAL_MODULES_DIR')
