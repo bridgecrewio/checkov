42583b840219d159ffc921b8085200cd9101e970
Megha Jindal <mjindal@squareup.com>
2021-09-07 09:26:43 -0700

    Updated checks location to common/checks

--- tests/common/checks/test_graph_checks.py
+++ tests/common/checks/test_graph_checks.py
@@ -34,12 +34,11 @@ class TestGraphChecks(unittest.TestCase):
     def test_external_checks_and_graph_checks_load(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        tf_file_repo = current_dir + "/example_custom_policy_dir"
 
         # without external yaml checks the external graph registry checks should be 0
         extra_checks_dir_path = [current_dir + "/extra_checks"]
         runner_filter = RunnerFilter(framework='terraform')
-        runner.run(root_folder=tf_file_repo, external_checks_dir=extra_checks_dir_path,
+        runner.run(root_folder=current_dir, external_checks_dir=extra_checks_dir_path,
                    runner_filter=runner_filter)
         external_graph_checks = 0
         for check in runner.graph_registry.checks:
@@ -49,7 +48,7 @@ class TestGraphChecks(unittest.TestCase):
 
         # with external yaml checks external graph registry checks count should be equal to the external graph checks
         extra_checks_dir_path = [current_dir + "/extra_checks", current_dir + "/extra_yaml_checks"]
-        runner.run(root_folder=tf_file_repo, external_checks_dir=extra_checks_dir_path,
+        runner.run(root_folder=current_dir, external_checks_dir=extra_checks_dir_path,
                    runner_filter=runner_filter)
         for check in runner.graph_registry.checks:
             if runner_filter.is_external_check(check.id):
