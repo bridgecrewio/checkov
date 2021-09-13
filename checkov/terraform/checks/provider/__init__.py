cc10307abf08a849c98f164e8353f0c7b6cd813a
James Woolfenden <james.woolfenden@gmail.com>
2021-09-13 17:21:38 +0100

    new provider

--- checkov/terraform/checks/provider/__init__.py
+++ checkov/terraform/checks/provider/__init__.py
@@ -1,2 +1,3 @@
 from checkov.terraform.checks.provider.aws import *
 from checkov.terraform.checks.provider.linode import *
+from checkov.terraform.checks.provider.bridgecrew import *
---
+++ tests/terraform/checks/provider/bridgecrew/test_credentials.py
@@ -0,0 +1,21 @@
+import unittest
+
+from checkov.terraform.checks.provider.bridgecrew.credentials import check
+from checkov.common.models.enums import CheckResult
+
+
+class TestCredentials(unittest.TestCase):
+
+    def test_success(self):
+        provider_conf = {}
+
+        scan_result = check.scan_provider_conf(conf=provider_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_failure(self):
+        provider_conf = {'token' :['80e54890-f282-3595-ab3d-45f9bd874987']}
+        scan_result = check.scan_provider_conf(conf=provider_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+if __name__ == '__main__':
+    unittest.main()
