2c5ee4d1c8f9005e1498d1ae5fd78f5d6646bd63
njgibbon <njgibbon@outlook.com>
2021-09-15 00:51:42 +0100

    split testing and data into more logical groupings

--- tests/common/checks_infra/test_registry.py
+++ tests/common/checks_infra/test_registry.py
@@ -9,5 +9,11 @@ class TestRegistry(unittest.TestCase):
     def test_invalid_check_yaml_does_not_throw_exception(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         test_files_dir = current_dir + "/test-registry-data/invalid-yaml"
+        r = Registry(checks_dir=test_files_dir)
+        r.load_checks()
+
+    def test_valid_yaml_but_invalid_check_does_not_throw_exception(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        test_files_dir = current_dir + "/test-registry-data/valid-yaml-invalid-check"
         r = Registry(checks_dir=test_files_dir, parser=NXGraphCheckParser())
         r.load_checks()
