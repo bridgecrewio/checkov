110c374df02767c931e651767580ab0a4a10f461
Mike Urbanski <mike@bridgecrew.io>
2021-09-14 16:40:13 -0600

    handle bc ids in --check, etc

--- checkov/secrets/runner.py
+++ checkov/secrets/runner.py
@@ -144,7 +144,7 @@ class Runner(BaseRunner):
                 bc_check_id = bc_integration.ckv_to_bc_id_mapping.get(check_id) if bc_integration.ckv_to_bc_id_mapping else None
                 if not check_id:
                     continue
-                if runner_filter.checks and check_id not in runner_filter.checks:
+                if runner_filter.checks and (check_id not in runner_filter.checks and bc_check_id not in runner_filter.checks):
                     continue
                 result: _CheckResult = {'result': CheckResult.FAILED}
                 line_text = linecache.getline(secret.filename, secret.line_number)
@@ -152,6 +152,7 @@ class Runner(BaseRunner):
                     continue
                 result = self.search_for_suppression(
                     check_id=check_id,
+                    bc_check_id=bc_check_id,
                     secret=secret,
                     skipped_checks=runner_filter.skip_checks,
                 ) or result
@@ -174,19 +175,21 @@ class Runner(BaseRunner):
     @staticmethod
     def search_for_suppression(
         check_id: str,
+        bc_check_id: str,
         secret: PotentialSecret,
-        skipped_checks: List[str]
+        skipped_checks: List[str],
+        output_bc_ids: bool = False
     ) -> Optional[_CheckResult]:
-        if check_id in skipped_checks and check_id in CHECK_ID_TO_SECRET_TYPE.keys():
+        if (check_id in skipped_checks or bc_check_id in skipped_checks) and check_id in CHECK_ID_TO_SECRET_TYPE.keys():
             return {
                 "result": CheckResult.SKIPPED,
-                "suppress_comment": f"Secret scan {check_id} is skipped"
+                "suppress_comment": f"Secret scan {bc_check_id if output_bc_ids else check_id} is skipped"
             }
         # Check for suppression comment in the line before, the line of, and the line after the secret
         for line_number in [secret.line_number, secret.line_number - 1, secret.line_number + 1]:
             lt = linecache.getline(secret.filename, line_number)
             skip_search = re.search(COMMENT_REGEX, lt)
-            if skip_search and skip_search.group(2) == check_id:
+            if skip_search and (skip_search.group(2) == check_id or skip_search.group(2) == bc_check_id):
                 return {
                     "result": CheckResult.SKIPPED,
                     "suppress_comment": skip_search.group(3)[1:] if skip_search.group(3) else "No comment provided"
