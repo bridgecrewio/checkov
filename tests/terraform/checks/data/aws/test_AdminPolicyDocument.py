8f0faed8ad604a10ed880e1665e3107818dba4ee
Anton Grübel <anton.gruebel@gmail.com>
2021-09-19 16:11:27 +0900

    Improve cloudsplaining usage (#1628)
    * add cache for cloudsplaining checks
    
    * update data check tests to new syntax

--- tests/terraform/checks/data/aws/test_AdminPolicyDocument.py
+++ tests/terraform/checks/data/aws/test_AdminPolicyDocument.py
@@ -1,53 +1,38 @@
 import unittest
+from pathlib import Path
 
-import hcl2
-
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.data.aws.AdminPolicyDocument import check
-from checkov.common.models.enums import CheckResult
+from checkov.terraform.runner import Runner
 
 
 class TestAdminPolicyDocument(unittest.TestCase):
+    def test(self):
+        test_files_dir = Path(__file__).parent / "example_AdminPolicyDocument"
 
-    def test_success(self):
-        resource_conf = {'version': ['2012-10-17'], 'statement': [{'actions': [['s3:Describe*']], 'resources': [['*']], 'effect': ['Allow']}]}
-        scan_result = check.scan_data_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
 
-    def test_failure(self):
-        resource_conf = {'version': ['2012-10-17'], 'statement': [{'actions': [['*']], 'resources': [['*']], 'effect': ['Allow']}]}
-        scan_result = check.scan_data_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        passing_resources = {
+            "aws_iam_policy_document.list",
+            "aws_iam_policy_document.pass",
+        }
+        failing_resources = {
+            "aws_iam_policy_document.fail",
+            "aws_iam_policy_document.no_effect",
+        }
 
-    def test_failure_no_effect(self):
-        resource_conf = {'version': ['2012-10-17'], 'statement': [{'actions': [['*']], 'resources': [['*']]}]}
-        scan_result = check.scan_data_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
 
-    def test_passed_list_statement(self):
-        hcl_res = hcl2.loads("""
-            data "aws_iam_policy_document" "default" {
-              statement = [{
-                actions = ["s3:GetObject"]
-            
-                resources = ["${aws_s3_bucket.default.arn}/*"]
-            
-                principals {
-                  type        = "AWS"
-                  identifiers = ["*"]
-                }
-              }]
-            
-              # Support replication ARNs
-              statement = ["${flatten(data.aws_iam_policy_document.replication.*.statement)}"]
-            
-              # Support deployment ARNs
-              statement = ["${flatten(data.aws_iam_policy_document.deployment.*.statement)}"]
-            }
-        """)
-        resource_conf = hcl_res['data'][0]['aws_iam_policy_document']['default']
-        scan_result = check.scan_data_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(summary["passed"], 2)
+        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
