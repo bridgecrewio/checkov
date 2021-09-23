5f5dc192f1ff057373b4ba9d8ec40cc4f535d843
James Woolfenden <james.woolfenden@gmail.com>
2021-09-23 17:01:28 +0100

    Add SSO inline

--- checkov/terraform/checks/resource/aws/IAMAdminPolicyDocument.py
+++ checkov/terraform/checks/resource/aws/IAMAdminPolicyDocument.py
@@ -8,14 +8,19 @@ class IAMAdminPolicyDocument(BaseResourceCheck):
     def __init__(self):
         name = "Ensure IAM policies that allow full \"*-*\" administrative privileges are not created"
         id = "CKV_AWS_62"
-        supported_resources = ['aws_iam_role_policy', 'aws_iam_user_policy', 'aws_iam_group_policy', 'aws_iam_policy']
+        supported_resources = ['aws_iam_role_policy', 'aws_iam_user_policy', 'aws_iam_group_policy', 'aws_iam_policy','aws_ssoadmin_permission_set_inline_policy']
         categories = [CheckCategories.IAM]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
         if 'policy' in conf.keys():
+            policyStr='policy'
+        elif 'inline_policy' in conf.keys():
+            policyStr='inline_policy'
+
+        if "policyStr" in locals():
             try:
-                policy_block = extract_policy_dict(conf['policy'][0])
+                policy_block = extract_policy_dict(conf[policyStr][0])
                 if policy_block and 'Statement' in policy_block.keys():
                     for statement in force_list(policy_block['Statement']):
                         if 'Action' in statement:
@@ -26,6 +31,7 @@ class IAMAdminPolicyDocument(BaseResourceCheck):
                                 return CheckResult.FAILED
             except:  # nosec
                 pass
+
         return CheckResult.PASSED
 
 
--- tests/terraform/checks/resource/aws/example_IAMAdminPolicyDocument/iam.tf
+++ tests/terraform/checks/resource/aws/example_IAMAdminPolicyDocument/iam.tf
@@ -133,3 +133,62 @@ resource "aws_iam_policy" "pass2" {
 }
 POLICY
 }
+
+
+resource "aws_ssoadmin_permission_set_inline_policy" "pass1" {
+  instance_arn       = aws_ssoadmin_permission_set.example.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.example.arn
+  inline_policy = <<POLICY
+{
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListBucket*",
+        "s3:HeadBucket",
+        "s3:Get*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:s3:::b1",
+        "arn:aws:s3:::b1/*",
+        "arn:aws:s3:::b2",
+        "arn:aws:s3:::b2/*"
+      ],
+      "Sid": ""
+    },
+    {
+      "Action": "s3:PutObject*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::b1/*",
+      "Sid": ""
+    }
+  ],
+  "Version": "2012-10-17"
+}
+POLICY
+}
+
+resource "aws_ssoadmin_permission_set_inline_policy" "fail1" {
+  instance_arn       = aws_ssoadmin_permission_set.example.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.example.arn
+  inline_policy = <<POLICY
+{
+  "Statement": [
+    {
+      "Action": [
+        "s3:HeadBucket",
+        "*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:s3:::b1",
+        "arn:aws:s3:::b1/*",
+        "*"
+      ],
+      "Sid": ""
+    }
+  ],
+  "Version": "2012-10-17"
+}
+POLICY
+}
