816b061e6aa533ca95ceb56e4eb5e0bed25ec352
James Woolfenden <james.woolfenden@gmail.com>
2021-09-24 21:51:26 +0100

    add support for checking sso inline policy for CKV_AWS_63 (#1646)
    * add support for checking sso inline policy
    
    * merge policy eval logic
    
    Co-authored-by: Mike Urbanski <mike@bridgecrew.io>

--- checkov/terraform/checks/resource/aws/IAMStarActionPolicyDocument.py
+++ checkov/terraform/checks/resource/aws/IAMStarActionPolicyDocument.py
@@ -8,22 +8,25 @@ class IAMStarActionPolicyDocument(BaseResourceCheck):
     def __init__(self):
         name = "Ensure no IAM policies documents allow \"*\" as a statement's actions"
         id = "CKV_AWS_63"
-        supported_resources = ['aws_iam_role_policy', 'aws_iam_user_policy', 'aws_iam_group_policy', 'aws_iam_policy']
+        supported_resources = ['aws_iam_role_policy', 'aws_iam_user_policy', 'aws_iam_group_policy', 'aws_iam_policy','aws_ssoadmin_permission_set_inline_policy']
         categories = [CheckCategories.IAM]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if 'policy' in conf.keys():
-            try:
+        try:
+            policy_block = None
+            if 'policy' in conf.keys():
                 policy_block = extract_policy_dict(conf['policy'][0])
-                if policy_block and 'Statement' in policy_block.keys():
-                    for statement in force_list(policy_block['Statement']):
-                        if 'Action' in statement and \
-                                statement.get('Effect', ['Allow']) == 'Allow' and \
-                                '*' in force_list(statement['Action']):
-                            return CheckResult.FAILED
-            except:  # nosec
-                pass
+            elif 'inline_policy' in conf.keys():
+                policy_block = extract_policy_dict(conf['inline_policy'][0])
+            if policy_block and 'Statement' in policy_block.keys():
+                for statement in force_list(policy_block['Statement']):
+                    if 'Action' in statement and \
+                            statement.get('Effect', ['Allow']) == 'Allow' and \
+                            '*' in force_list(statement['Action']):
+                        return CheckResult.FAILED
+        except:  # nosec
+            pass
         return CheckResult.PASSED
 
 
