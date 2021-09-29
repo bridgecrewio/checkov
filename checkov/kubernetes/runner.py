00359fae8cbf20c3c2ffd7e7b1790ef54180caa3
nimrodkor <nimrodkor@gmail.com>
2021-09-29 16:21:57 +0300

    Handle non-relative path in helm runner

--- checkov/kubernetes/runner.py
+++ checkov/kubernetes/runner.py
@@ -49,7 +49,12 @@ class Runner(BaseRunner):
                             files_list.append(full_path)
 
             for file in files_list:
-                relative_file_path = f'/{os.path.relpath(file, os.path.commonprefix((root_folder, file)))}'
+                relative_file_path = file
+                try:
+                    relative_file_path = os.path.relpath(file, os.path.commonprefix((root_folder, file)))
+                except Exception as e:
+                    logging.debug(f'File path is not relative to origin file, returning the abs path of the temp file')
+                    logging.debug(f'Error: {e}')
                 try:
                     parse_result = parse(file)
                     if parse_result:
