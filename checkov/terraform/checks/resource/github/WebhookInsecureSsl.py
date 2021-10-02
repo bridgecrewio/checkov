8ed7bd91c225db0714b987d62e55b65504b49328
Brandon Bailey <brandondanielbailey@gmail.com>
2021-10-01 22:01:24 -0400

    Update WebhookInsecureSsl.py

--- checkov/terraform/checks/resource/github/WebhookInsecureSsl.py
+++ checkov/terraform/checks/resource/github/WebhookInsecureSsl.py
@@ -1,22 +1,22 @@
 from typing import Dict, List, Any
 
 from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
-
-class WebhookInsecureSsl(BaseResourceCheck):
+class WebhookInsecureSsl(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure Repository Webhook uses secure Ssl"
         id = "CKV_GIT_2"
         supported_resources = ["github_repository_webhook"]
         categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED)
 
-    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        configuration = conf.get("configuration")
-        ssl_insecure = configuration.get("insecure_ssl")
-        if ssl_insecure == [False]:
-            return CheckResult.PASSED
-        return CheckResult.FAILED
+    def get_inspected_key(self):
+        return "configuration/[0]/insecure_ssl/[0]"
+
+    def get_expected_value(self):
+        return False
+
 
 check = WebhookInsecureSsl()
