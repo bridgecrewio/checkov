4361481b6157d523a027b15a593e7dda71fddfdc
c0d3x27 <banginpadr@aol.com>
2021-10-04 04:56:48 +0200

    Incomplete URL substring sanitization

--- checkov/terraform/module_loading/loaders/bitbucket_loader.py
+++ checkov/terraform/module_loading/loaders/bitbucket_loader.py
@@ -1,16 +1,14 @@
 from checkov.terraform.module_loading.loaders.git_loader import GenericGitLoader
+from flask import Flask, request, redirect
 
 
 class BitbucketLoader(GenericGitLoader):
-    def _is_matching_loader(self):
+    def _is_matching_loader(resquest):
+        allowlist = ["bitbucket.org/product", "bitbucket.org/login"]
         # https://www.terraform.io/docs/modules/sources.html#bitbucket
-        if 'bitbucket.org' in self.module_source:
-            if not self.module_source.startswith('https://') or not self.module_source.startswith('http://'):
-                self.module_source = 'https://' + self.module_source
-            if not self.module_source.endswith('.git'):
-                self.module_source = self.module_source + '.git'
-            return True
-        return False
+        target = request.args.get('target', '')
+        if target in allowlist:
+            return redirect(target)
 
 
 loader = BitbucketLoader()
