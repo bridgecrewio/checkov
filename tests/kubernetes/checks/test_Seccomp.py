46acf2401fa0e737ac2b926f048290d7c07e67eb
gruebel <anton.gruebel@gmail.com>
2021-10-10 14:04:52 +0200

    Fix CKV_K8S_31 check

--- tests/kubernetes/checks/test_Seccomp.py
+++ tests/kubernetes/checks/test_Seccomp.py
@@ -1,26 +1,47 @@
 import os
 import unittest
+from pathlib import Path
 
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
 from checkov.kubernetes.checks.Seccomp import check
 from checkov.kubernetes.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
 class TestSeccomp(unittest.TestCase):
-
     def test_summary(self):
-        runner = Runner()
-        current_dir = os.path.dirname(os.path.realpath(__file__))
+        # given
+        test_files_dir = Path(__file__).parent / "example_Seccomp"
 
-        test_files_dir = current_dir + "/example_Seccomp"
-        report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        # then
         summary = report.get_summary()
+        passed_resources = [check.resource for check in report.passed_checks]
+        failed_resources = [check.resource for check in report.failed_checks]
 
-        self.assertEqual(summary['passed'], 6)
-        self.assertEqual(summary['failed'], 2)
-        self.assertEqual(summary['skipped'], 0)
-        self.assertEqual(summary['parsing_errors'], 0)
+        self.assertEqual(summary["passed"], 7)
+        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        expected_passed_resources = [
+            "CronJob.cronjob-passed.default",
+            "Deployment.seccomp-passed-deployment.default",
+            "Deployment.seccomp-passed-metadata-annotations.default",
+            "Pod.seccomp-passed-metadata-annotations-docker.default",
+            "Pod.seccomp-passed-metadata-annotations-runtime.default",
+            "Pod.seccomp-passed-security-context.default",
+            "StatefulSet.RELEASE-NAME.default",
+        ]
+        expected_failed_resources = [
+            "Deployment.app-cert-manager.infra",
+            "Pod.seccomp-failed.default",
+        ]
+        self.assertCountEqual(expected_passed_resources, passed_resources)
+        self.assertCountEqual(expected_failed_resources, failed_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
