f4729402cc65b4e481312dabcee89c41870fea7d
Rotem Avni <rotemavn@gmail.com>
2021-10-19 16:56:48 +0300

    restored RDSPubliclyAccessible

--- checkov/terraform/checks/resource/aws/RDSPubliclyAccessible.py
+++ checkov/terraform/checks/resource/aws/RDSPubliclyAccessible.py
@@ -1,8 +1,9 @@
+from checkov.common.models.consts import ANY_VALUE
 from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class RDSPubliclyAccessible(BaseResourceValueCheck):
+class RDSPubliclyAccessible(BaseResourceNegativeValueCheck):
     def __init__(self):
         name = "Ensure all data stored in RDS is not publicly accessible"
         id = "CKV_AWS_17"
@@ -13,8 +14,8 @@ class RDSPubliclyAccessible(BaseResourceValueCheck):
     def get_inspected_key(self):
         return 'publicly_accessible'
 
-    def get_expected_value(self):
-        return False
+    def get_forbidden_values(self):
+        return [True]
 
 
 check = RDSPubliclyAccessible()
