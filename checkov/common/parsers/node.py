db709f465b374e30d3cbe3c4ebc45d7ff70edfb9
YaaraVerner <86768411+YaaraVerner@users.noreply.github.com>
2021-10-20 18:04:39 +0300

    Update checkov/common/parsers/node.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/common/parsers/node.py
+++ checkov/common/parsers/node.py
@@ -159,7 +159,7 @@ class ListNode(list):
     def __deepcopy__(self, memo):
         result = ListNode([], self.start_mark, self.end_mark)
         memo[id(self)] = result
-        for _, v in enumerate(self):
+        for v in self:
             result.append(deepcopy(v, memo))
 
         return result
