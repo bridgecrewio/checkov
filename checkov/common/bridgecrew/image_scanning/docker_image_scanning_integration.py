c681bf112a89035d9aa540b4b7118aef6c65552e
gruebel <anton.gruebel@gmail.com>
2021-10-22 10:41:03 +0200

    only download twist cli, if not exist

--- checkov/common/bridgecrew/image_scanning/docker_image_scanning_integration.py
+++ checkov/common/bridgecrew/image_scanning/docker_image_scanning_integration.py
@@ -2,6 +2,8 @@ import logging
 import os
 import platform
 import stat
+from typing import Union, Dict, Any
+
 import requests
 from datetime import datetime, timedelta
 
@@ -13,25 +15,34 @@ from checkov.common.util.http_utils import get_default_get_headers, get_default_
 class DockerImageScanningIntegration:
     docker_image_scanning_base_url = f"{bc_integration.api_url}/api/v1/vulnerabilities/docker-images"
 
-    def get_bc_api_key(self):
+    def get_bc_api_key(self) -> str:
         return bc_integration.get_auth_token()
 
-    def get_proxy_address(self):
+    def get_proxy_address(self) -> str:
         return f"{self.docker_image_scanning_base_url}/twistcli/proxy"
 
-    def download_twistcli(self, cli_file_name):
+    def download_twistcli(self, cli_file_name: Union[str, "os.PathLike[str]"]) -> None:
         os_type = platform.system().lower()
         headers = merge_dicts(
             get_default_get_headers(bc_integration.bc_source, bc_integration.bc_source_version),
             {'Authorization': self.get_bc_api_key()}
         )
         response = requests.request('GET', f"{self.docker_image_scanning_base_url}/twistcli/download?os={os_type}", headers=headers)
-        open(cli_file_name, 'wb').write(response.content)
+        response.raise_for_status()
+
+        with open(cli_file_name, 'wb') as fb:
+            fb.write(response.content)
         st = os.stat(cli_file_name)
         os.chmod(cli_file_name, st.st_mode | stat.S_IEXEC)
         logging.debug(f'TwistCLI downloaded and has execute permission')
 
-    def report_results(self, docker_image_name, dockerfile_path, dockerfile_content, twistcli_scan_result):
+    def report_results(
+        self,
+        docker_image_name: str,
+        dockerfile_path: str,
+        dockerfile_content: str,
+        twistcli_scan_result: Dict[str, Any],
+    ) -> None:
         headers = merge_dicts(
             get_default_post_headers(bc_integration.bc_source, bc_integration.bc_source_version),
             {'Authorization': self.get_bc_api_key()}
