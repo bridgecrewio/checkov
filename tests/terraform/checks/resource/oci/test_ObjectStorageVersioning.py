b64fa4bb1f40b198ac52c8a10bc84d34d4512953
James Woolfenden <james.woolfenden@gmail.com>
2021-10-25 10:58:34 +0100

    add default case

--- tests/terraform/checks/resource/oci/test_ObjectStorageVersioning.py
+++ tests/terraform/checks/resource/oci/test_ObjectStorageVersioning.py
@@ -20,13 +20,14 @@ class TestObjectStorageVersioning(unittest.TestCase):
         }
         failing_resources = {
             "oci_objectstorage_bucket.fail",
+            "oci_objectstorage_bucket.default",
         }
 
         passed_check_resources = set([c.resource for c in report.passed_checks])
         failed_check_resources = set([c.resource for c in report.failed_checks])
 
         self.assertEqual(summary["passed"], 1)
-        self.assertEqual(summary["failed"], 1)
+        self.assertEqual(summary["failed"], 2)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
