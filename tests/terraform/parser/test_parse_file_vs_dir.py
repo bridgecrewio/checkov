87a121a58ec2b0fdd7d31c875006668a886cadb1
Kartikeya Pande <kpande479@gmail.com>
2021-10-25 09:18:44 -0400

    Fix splat exp parsing (#1814)
    * Serialize definitions for file parsing
    
    * Add tests for comparing file parsing and dir parsing
    
    * Fix tests
    
    * Rename test class

---
+++ tests/terraform/parser/test_parse_file_vs_dir.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+from checkov.terraform.parser import Parser
+
+
+class TestFileVsDirParser(unittest.TestCase):
+
+    def test_file_dir_parser_results_match(self):
+        parser = Parser()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = current_dir + '/resources/parse_file_vs_dir/main.tf'
+        dir_path = current_dir + '/resources/parse_file_vs_dir'
+        tf_definitions_file = parser.parse_file(file_path)
+        _, tf_definitions_dir = parser.parse_hcl_module(dir_path, 'terraform')
+        self.assertDictEqual(tf_definitions_file, tf_definitions_dir.get(list(tf_definitions_dir.keys())[0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
