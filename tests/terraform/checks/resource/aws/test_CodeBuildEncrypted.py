ab71470749359ef6738264dddc0cc00f7131ac0e
gruebel <anton.gruebel@gmail.com>
2021-10-25 18:31:09 +0200

    adjust CKV_AWS_147 check to incorporate NO_ARTIFACTS setups

--- tests/terraform/checks/resource/aws/test_CodeBuildEncrypted.py
+++ tests/terraform/checks/resource/aws/test_CodeBuildEncrypted.py
@@ -1,39 +1,40 @@
 import unittest
-import hcl2
+from pathlib import Path
 
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.CodeBuildEncrypted import check
-from checkov.common.models.enums import CheckResult
+from checkov.terraform.runner import Runner
 
 
 class TestCodeBuildEncrypted(unittest.TestCase):
-    def test_failure(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_codebuild_project" "example" {
-              name          = "test-project"
-              description   = "test_codebuild_project"
-              build_timeout = "5"
-              service_role  = aws_iam_role.example.arn
-            }
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_codebuild_project']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+    def test(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_CodeBuildProjectEncrypted"
 
-    def test_success(self):
-        hcl_res = hcl2.loads("""
-            resource "aws_codebuild_project" "example" {
-              name          = "test-project"
-              description   = "test_codebuild_project"
-              build_timeout = "5"
-              service_role  = aws_iam_role.example.arn
-              
-              encryption_key = "AWS_Key_Management_Service_example"
-            }
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_codebuild_project']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        # then
+        summary = report.get_summary()
+
+        passing_resources = {
+            "aws_codebuild_project.enabled",
+        }
+        failing_resources = {
+            "aws_codebuild_project.default",
+        }
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["failed"], 1)
+        self.assertEqual(summary["parsing_errors"], 0)
+        self.assertEqual(summary["resource_count"], 3)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
