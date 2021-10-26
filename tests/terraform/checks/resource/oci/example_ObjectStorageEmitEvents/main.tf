f71bba05a5ef6305fef0ad48759e45f06041c073
James Woolfenden <james.woolfenden@gmail.com>
2021-10-26 09:32:42 +0100

    Implement OCI - OCI Object Storage bucket does not emit object events (#1824)
    * Onject events
    
    * Update checkov/terraform/checks/resource/oci/ObjectStorageEmitEvents.py
    
    Absolutely
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

---
+++ tests/terraform/checks/resource/oci/example_ObjectStorageEmitEvents/main.tf
@@ -0,0 +1,52 @@
+resource "oci_objectstorage_bucket" "pass" {
+  compartment_id = var.compartment_id
+  name           = var.bucket_name
+  namespace      = var.namespace
+
+  access_type           = var.bucket_access_type
+  defined_tags          = var.defined_tags
+  freeform_tags         = var.freeform_tags
+  kms_key_id            = var.oci_kms_key.id
+  metadata              = var.metadata
+  storage_tier          = var.bucket_storage_tier
+  object_events_enabled = true
+
+  retention_rules {
+    display_name = var.retention_rule_display_name
+
+    duration {
+      time_amount = var.retention_rule_duration_time_amount
+      time_unit   = var.retention_rule_duration_time_unit
+    }
+    time_rule_locked = var.retention_rule_time_rule_locked
+  }
+
+  versioning = var.bucket_versioning
+}
+
+
+resource "oci_objectstorage_bucket" "fail" {
+  compartment_id = var.compartment_id
+  name           = var.bucket_name
+  namespace      = var.namespace
+
+  access_type           = var.bucket_access_type
+  defined_tags          = var.defined_tags
+  freeform_tags         = var.freeform_tags
+  kms_key_id            = var.oci_kms_key.id
+  metadata              = var.metadata
+  storage_tier          = var.bucket_storage_tier
+  object_events_enabled = false
+
+  retention_rules {
+    display_name = var.retention_rule_display_name
+
+    duration {
+      time_amount = var.retention_rule_duration_time_amount
+      time_unit   = var.retention_rule_duration_time_unit
+    }
+    time_rule_locked = var.retention_rule_time_rule_locked
+  }
+
+  versioning = var.bucket_versioning
+}
