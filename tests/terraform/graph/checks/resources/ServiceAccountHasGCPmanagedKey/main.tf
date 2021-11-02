95d406d3cf1102663a621f75078d680b3916a0f8
mzwennes <m.zwennes@felyx.com>
2021-11-02 21:26:30 +0100

    Reverse test logic for GCP service account key check

--- tests/terraform/graph/checks/resources/ServiceAccountHasGCPmanagedKey/main.tf
+++ tests/terraform/graph/checks/resources/ServiceAccountHasGCPmanagedKey/main.tf
@@ -2,10 +2,10 @@ resource "google_service_account" "account_ok" {
   account_id = "dev-foo-account"
 }
 
-resource "google_service_account_key" "ok_key" {
-  service_account_id = google_service_account.account_ok.name
-}
-
 resource "google_service_account" "account_bad" {
   account_id = "dev-foo-account"
+}
+
+resource "google_service_account_key" "account_bad" {
+  service_account_id = google_service_account.account_bad.name
 }
