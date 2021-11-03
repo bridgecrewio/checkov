68e69674b56039db736b72f7ee4c7b05c4c58ca2
James Woolfenden <james.woolfenden@gmail.com>
2021-11-03 16:49:46 +0000

    ensure WAF2 has logs

--- tests/terraform/graph/checks/test_yaml_policies.py
+++ tests/terraform/graph/checks/test_yaml_policies.py
@@ -191,6 +191,9 @@ class TestYamlPolicies(unittest.TestCase):
     def test_SQLServerAuditingEnabled(self):
         self.go("SQLServerAuditingEnabled")
 
+    def test_WAF2HasLogs(self):
+        self.go("WAF2HasLogs")
+
     def test_registry_load(self):
         registry = Registry(parser=NXGraphCheckParser(), checks_dir=str(
             Path(__file__).parent.parent.parent.parent.parent / "checkov" / "terraform" / "checks" / "graph_checks"))
