2a03d47acd097c289e06d912d92327966e121ffc
mzwennes <m.zwennes@felyx.com>
2021-11-09 22:03:19 +0100

    Add check that ignore Cloud SQL replica instances

--- checkov/terraform/checks/resource/gcp/GoogleCloudSqlBackupConfiguration.py
+++ checkov/terraform/checks/resource/gcp/GoogleCloudSqlBackupConfiguration.py
@@ -1,8 +1,8 @@
-from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
 
 
-class GoogleCloudSqlBackupConfiguration(BaseResourceValueCheck):
+class GoogleCloudSqlBackupConfiguration(BaseResourceCheck):
     def __init__(self):
         name = "Ensure all Cloud SQL database instance have backup configuration enabled"
         id = "CKV_GCP_14"
@@ -10,8 +10,19 @@ class GoogleCloudSqlBackupConfiguration(BaseResourceValueCheck):
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return 'settings/[0]/backup_configuration/[0]/enabled'
+    def scan_resource_conf(self, conf):
+        if 'master_instance_name' in conf:
+           return CheckResult.PASSED
+        
+        if 'settings' in conf:
+            settings = conf["settings"][0]
+            if isinstance(settings, dict) and 'backup_configuration' in settings:
+                backup_configuration = settings["backup_configuration"][0]
+                if backup_configuration.get("enabled", None) == [True]:
+                    return CheckResult.PASSED
 
+                return CheckResult.FAILED
+            return CheckResult.FAILED
+        return CheckResult.FAILED
 
 check = GoogleCloudSqlBackupConfiguration()
--- tests/terraform/checks/resource/gcp/test_GoogleCloudSqlBackupConfiguration.py
+++ tests/terraform/checks/resource/gcp/test_GoogleCloudSqlBackupConfiguration.py
@@ -22,6 +22,12 @@ class GoogleCloudSqlDatabaseBackupConfiguration(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
+    def test_replica_success(self):
+        resource_conf = {'name': ['google_cluster'], 'monitoring_service': ['none'], 'master_instance_name': 'foo'}
+
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
 
 if __name__ == '__main__':
     unittest.main()
