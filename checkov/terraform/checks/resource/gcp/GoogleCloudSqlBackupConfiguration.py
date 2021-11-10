b5b42ca643433d26966f783909a5b3ed52df2f72
mzwennes <m.zwennes@felyx.com>
2021-11-10 10:42:03 +0100

    Clean up check logic by using default super().scan_resource_conf(conf)

--- checkov/terraform/checks/resource/gcp/GoogleCloudSqlBackupConfiguration.py
+++ checkov/terraform/checks/resource/gcp/GoogleCloudSqlBackupConfiguration.py
@@ -1,28 +1,21 @@
 from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class GoogleCloudSqlBackupConfiguration(BaseResourceCheck):
+class GoogleCloudSqlBackupConfiguration(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure all Cloud SQL database instance have backup configuration enabled"
         id = "CKV_GCP_14"
         supported_resources = ['google_sql_database_instance']
-        categories = [CheckCategories.NETWORKING]
+        categories = [CheckCategories.BACKUP_AND_RECOVERY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
         if 'master_instance_name' in conf:
-           return CheckResult.PASSED
-        
-        if 'settings' in conf:
-            settings = conf["settings"][0]
-            if isinstance(settings, dict) and 'backup_configuration' in settings:
-                backup_configuration = settings["backup_configuration"][0]
-                if backup_configuration.get("enabled", None) == [True]:
-                    return CheckResult.PASSED
+            return CheckResult.UNKNOWN
+        return super().scan_resource_conf(conf)
 
-                return CheckResult.FAILED
-            return CheckResult.FAILED
-        return CheckResult.FAILED
+    def get_inspected_key(self):
+        return "settings/[0]/backup_configuration/[0]/enabled"
 
 check = GoogleCloudSqlBackupConfiguration()
