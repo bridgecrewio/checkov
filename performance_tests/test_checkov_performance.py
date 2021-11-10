7df9e7aee8f2afdda6e18d31c8e0de8da8b2cb9a
tronxd <tron47@gmail.com>
2021-11-10 19:31:49 +0200

    added cfn and k8 repos, configruation per IAC

--- performance_tests/test_checkov_performance.py
+++ performance_tests/test_checkov_performance.py
@@ -1,10 +1,6 @@
-import math
 import os
-
 import pytest
 import time
-
-from pytest_benchmark.plugin import benchmark
 
 from checkov.cloudformation.runner import Runner as cfn_runner
 from checkov.common.runners.runner_registry import RunnerRegistry
@@ -13,22 +9,95 @@ from checkov.kubernetes.runner import Runner as k8_runner
 from checkov.runner_filter import RunnerFilter
 from checkov.terraform.runner import Runner as tf_runner
 
+# Ensure repo_name is a cloned repository into performance_tests directory.
+# Thresholds are in ms, and are set to the current maximum duration
+performance_configurations = {
+    'terraform': {
+        'repo_name': 'terraform-aws-components',
+        'threshold': 32.6493
+    },
+    'cloudformation': {
+        'repo_name': 'terraform-aws-components',
+        'threshold': 66.5699
+    },
+    'kubernetes': {
+        'repo_name': 'kubernetes-yaml-templates',
+        'threshold': 673.8789
+    }
+}
+
+deviation_percent = 10
+
+
 @pytest.mark.benchmark(
-    group="performance-tests",
-    min_time=0,
-    max_time=120,
-    min_rounds=1,
-    timer=time.time,
+    group="terraform-performance-tests",
     disable_gc=True,
-    warmup=False
+    min_time=0.1,
+    max_time=0.5,
+    min_rounds=5,
+    timer=time.time,
+    warmup=False,
 )
-def test_performance(benchmark):
-    def test_terragoat():
+def test_terraform_performance(benchmark):
+    repo_name = performance_configurations['terraform']['repo_name']
+    repo_threshold = performance_configurations['terraform']['threshold']
+
+    def run_terraform_scan():
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        test_files_dir = "/Users/tronxd/clones/tronxd/terragoat"  # TODO run on forked data
+        test_files_dir = os.path.join(current_dir, repo_name)
         runner_filter = RunnerFilter()
         runner_registry = RunnerRegistry(banner, runner_filter, tf_runner())
         reports = runner_registry.run(root_folder=test_files_dir)
         assert len(reports) > 0
 
-    benchmark(test_terragoat)
+    benchmark(run_terraform_scan)
+    assert benchmark.stats.stats.mean <= repo_threshold + (deviation_percent / 100.0) * repo_threshold
+
+
+@pytest.mark.benchmark(
+    group="cloudformation-performance-tests",
+    disable_gc=True,
+    min_time=0.1,
+    max_time=0.5,
+    min_rounds=5,
+    timer=time.time,
+    warmup=False
+)
+def test_cloudformation_performance(benchmark):
+    repo_name = performance_configurations['cloudformation']['repo_name']
+    repo_threshold = performance_configurations['cloudformation']['threshold']
+
+    def run_cloudformation_scan():
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        test_files_dir = os.path.join(current_dir, repo_name)
+        runner_filter = RunnerFilter()
+        runner_registry = RunnerRegistry(banner, runner_filter, cfn_runner())
+        reports = runner_registry.run(root_folder=test_files_dir)
+        assert len(reports) > 0
+
+    benchmark(run_cloudformation_scan)
+    assert benchmark.stats.stats.mean <= repo_threshold + (deviation_percent / 100) * repo_threshold
+
+@pytest.mark.benchmark(
+    group="kubernetes-performance-tests",
+    disable_gc=True,
+    min_time=0.1,
+    max_time=0.5,
+    min_rounds=5,
+    timer=time.time,
+    warmup=False
+)
+def test_k8_performance(benchmark):
+    repo_name = performance_configurations['kubernetes']['repo_name']
+    repo_threshold = performance_configurations['kubernetes']['threshold']
+
+    def run_kubernetes_scan():
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        test_files_dir = os.path.join(current_dir, repo_name)
+        runner_filter = RunnerFilter()
+        runner_registry = RunnerRegistry(banner, runner_filter, k8_runner())
+        reports = runner_registry.run(root_folder=test_files_dir)
+        assert len(reports) > 0
+
+    benchmark(run_kubernetes_scan)
+    assert benchmark.stats.stats.mean <= repo_threshold + (deviation_percent / 100) * repo_threshold
