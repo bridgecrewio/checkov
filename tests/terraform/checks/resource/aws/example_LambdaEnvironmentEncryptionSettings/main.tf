9378fc598e219b087f8e4c7cb8da43880c566da8
James Woolfenden <james.woolfenden@gmail.com>
2021-11-20 13:41:33 +0000

    fix for empty string

--- tests/terraform/checks/resource/aws/example_LambdaEnvironmentEncryptionSettings/main.tf
+++ tests/terraform/checks/resource/aws/example_LambdaEnvironmentEncryptionSettings/main.tf
@@ -57,3 +57,17 @@ resource "aws_lambda_function" "pass" {
     }
    kms_key_arn = aws_kms_key.anyoldguff.arn
 }
+
+resource "aws_lambda_function" "failasempty" {
+   function_name                  = var.function_name
+   role                           = aws_iam_role.lambda-messageprocessor.arn
+   runtime                        = "python3.6"
+   handler                        = "handler.lambda_handler"
+   filename                       = data.archive_file.notify.output_path
+   source_code_hash               = data.archive_file.notify.output_base64sha256
+   reserved_concurrent_executions = var.concurrency
+   tracing_config {
+       mode = "PassThrough"
+    }
+   kms_key_arn = ""
+}
