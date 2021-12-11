df3a32e3b6d9669866f8a28e92dfe4fb5358d791
Nick Gibbon <njgibbon@outlook.com>
2021-12-11 22:45:00 +0000

    Combine small changes (#2088)
    * setup.py: "Programming Language :: Python :: 3.10",
    
    * typos
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Security",
         "Topic :: Software Development :: Build Tools",
     ],
