fc208acb91e942306440b63f9eba54cf1a0a7162
Anton Grübel <anton.gruebel@gmail.com>
2021-12-13 16:44:36 +0100

    fix CKV_AWS_192 for Terraform plan runs (#2095)

--- tests/terraform/checks/resource/aws/example_WAFACLCVE202144228/main.tf
+++ tests/terraform/checks/resource/aws/example_WAFACLCVE202144228/main.tf
@@ -48,6 +48,88 @@ resource "aws_wafv2_web_acl" "pass" {
   }
 }
 
+resource "aws_wafv2_web_acl" "multi_rules" {
+  name        = "managed-rule-example"
+  description = "Example of a managed rule."
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "aws-managed-known-bad-inputs"
+    priority = 1
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "aws-managed-known-bad-inputs"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-common-rule-set"
+    priority = 2
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        excluded_rule {
+          name = "SizeRestrictions_BODY"
+        }
+        excluded_rule {
+          name = "CrossSiteScripting_COOKIE"
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "aws-managed-common-rule-set"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "rate-limit-ip"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "rate-limit-ip"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
 # fail
 
 resource "aws_wafv2_web_acl" "no_rule" {
