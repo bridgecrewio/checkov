af520abe4544b0bfdd0a5c6d4088e4761743d4c5
kartikp10 <kpande479@gmail.com>
2021-12-15 11:38:41 -0500

    Update CKV_GCP_34

--- tests/terraform/checks/resource/gcp/test_GoogleComputeInstanceOSLogin.py
+++ tests/terraform/checks/resource/gcp/test_GoogleComputeInstanceOSLogin.py
@@ -98,7 +98,7 @@ class TestGoogleComputeInstanceOSLogin(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
-    def test_success_4(self):
+    def test_unknown_1(self):
         hcl_res = hcl2.loads("""
             resource "google_compute_instance_from_template" "default" {
               name         = "test"
@@ -107,7 +107,21 @@ class TestGoogleComputeInstanceOSLogin(unittest.TestCase):
                 """)
         resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
         scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
+
+    def test_unknown_2(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name         = "test"
+              source_instance_template = google_compute_instance_template.default.id
+              metadata = {
+                 foo = "bar"
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
 
 
 if __name__ == '__main__':
