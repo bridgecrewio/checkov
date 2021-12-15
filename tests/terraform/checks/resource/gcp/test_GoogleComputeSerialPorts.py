347a38866f6901793450a3f8c4326cf6ee6cecf7
kartikp10 <kpande479@gmail.com>
2021-12-15 11:48:33 -0500

    Update CKV_GCP_35

--- tests/terraform/checks/resource/gcp/test_GoogleComputeSerialPorts.py
+++ tests/terraform/checks/resource/gcp/test_GoogleComputeSerialPorts.py
@@ -53,6 +53,31 @@ class TestGoogleComputeSerialPorts(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_unknown_1(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name         = "test"
+              source_instance_template = google_compute_instance_template.default.id
+              metadata = {
+                 foo = true
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
+
+    def test_unknown_2(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name         = "test"
+              source_instance_template = google_compute_instance_template.default.id
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
+
     def test_success_1(self):
         hcl_res = hcl2.loads("""
             resource "google_compute_instance" "default" {
@@ -92,17 +117,6 @@ class TestGoogleComputeSerialPorts(unittest.TestCase):
             }
                 """)
         resource_conf = hcl_res['resource'][0]['google_compute_instance_template']['default']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
-
-    def test_success_4(self):
-        hcl_res = hcl2.loads("""
-            resource "google_compute_instance_from_template" "default" {
-              name         = "test"
-              source_instance_template = google_compute_instance_template.default.id
-            }
-                """)
-        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
