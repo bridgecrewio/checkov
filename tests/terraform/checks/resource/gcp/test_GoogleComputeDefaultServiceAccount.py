8008e714a0202270650ea7401bcaf86fccfb9b35
Kartikeya Pande <kpande479@gmail.com>
2021-12-16 10:27:11 -0500

    Add instance_template and instance_from_template to supported resources (#2100)
    * Add instance_template and instance_from_template to supported resources
    
    * Apply suggestions from code review
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>
    
    * Fix GoogleComputeDefaultServiceAccount for google_compute_instance_from_template
    
    * Add tests
    
    * Update CKV_GCP_32
    
    * Update CKV_GCP_40
    
    * Update CKV_GCP_36
    
    * Update CKV_GCP_34
    
    * Update CKV_GCP_35
    
    * Update CKV_GCP_39
    
    * Update CKV_GCP_31
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- tests/terraform/checks/resource/gcp/test_GoogleComputeDefaultServiceAccount.py
+++ tests/terraform/checks/resource/gcp/test_GoogleComputeDefaultServiceAccount.py
@@ -36,6 +36,32 @@ class TestGoogleComputeDefaultServiceAccount(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_failure_3(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name                     = "instance_from_template"
+              source_instance_template = google_compute_instance_template.default.id
+              service_account {
+                scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+                email  =  "123456789-compute@developer.gserviceaccount.com"
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_unknown(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name                     = "instance_from_template"
+              source_instance_template = google_compute_instance_template.default.id
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
+
     def test_success_1(self):
         hcl_res = hcl2.loads("""
             resource "google_compute_instance" "default" {
@@ -65,6 +91,37 @@ class TestGoogleComputeDefaultServiceAccount(unittest.TestCase):
             }
                 """)
         resource_conf = hcl_res['resource'][0]['google_compute_instance']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_3(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_template" "default" {
+              name         = "account"
+              machine_type = "n1-standard-1"
+              zone         = "us-central1-a"
+              service_account {
+                scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+                email  = "example@email.com"
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_4(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name                     = "instance_from_template"
+              source_instance_template = google_compute_instance_template.default.id
+              service_account {
+                scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+                email  = "example@email.com"
+              }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
--- tests/terraform/checks/resource/gcp/test_GoogleComputeExternalIP.py
+++ tests/terraform/checks/resource/gcp/test_GoogleComputeExternalIP.py
@@ -24,6 +24,36 @@ class TestGoogleComputeExternalIP(unittest.TestCase):
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
+    def test_failure_1(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_template" "default" {
+              name         = "test"
+              machine_type = "n1-standard-1"
+              zone         = "us-central1-a"
+              boot_disk {}
+              access_config {
+                network_tier = "STANDARD"
+                }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_failure_2(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name         = "test"
+              source_instance_template = google_compute_instance_template.default.id
+              access_config {
+                network_tier = "STANDARD"
+                }
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
     def test_success(self):
         hcl_res = hcl2.loads("""
             resource "google_compute_instance" "default" {
@@ -36,6 +66,30 @@ class TestGoogleComputeExternalIP(unittest.TestCase):
         resource_conf = hcl_res['resource'][0]['google_compute_instance']['default']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success_1(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_template" "default" {
+              name         = "test"
+              machine_type = "n1-standard-1"
+              zone         = "us-central1-a"
+              boot_disk {}
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_unknown(self):
+        hcl_res = hcl2.loads("""
+            resource "google_compute_instance_from_template" "default" {
+              name         = "test"
+              source_instance_template = google_compute_instance_template.default.id
+            }
+                """)
+        resource_conf = hcl_res['resource'][0]['google_compute_instance_from_template']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.UNKNOWN, scan_result)
 
 
 if __name__ == '__main__':
