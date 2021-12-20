f2455f5d799b210724f06bbe1c6d78438a3df443
gruebel <anton.gruebel@gmail.com>
2021-12-21 00:32:34 +0100

    add CKV_GCP_73 to check for log4shell prevention in Cloud Armor

---
+++ checkov/terraform/checks/resource/gcp/CloudArmorWAFACLCVE202144228.py
@@ -0,0 +1,39 @@
+from typing import List, Dict, Any
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.common.util.type_forcers import force_list
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class CloudArmorWAFACLCVE202144228(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure Cloud Armor prevents message lookup in Log4j2. See CVE-2021-44228 aka log4jshell"
+        id = "CKV_GCP_73"
+        supported_resources = ("google_compute_security_policy",)
+        categories = (CheckCategories.APPLICATION_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+        self.evaluated_keys = ["rule"]
+        rules = conf.get("rule") or []
+        for idx_rule, rule in enumerate(force_list(rules)):
+            self.evaluated_keys = [
+                f"rule/[{idx_rule}]/action",
+                f"rule/[{idx_rule}]/preview",
+                f"rule/[{idx_rule}]/match/[0]/expr/[0]/expression",
+            ]
+            match = rule.get("match")
+            if match:
+                expr = match[0].get("expr")
+                if expr and expr[0].get("expression") == ["evaluatePreconfiguredExpr('cve-canary')"]:
+                    if rule.get("action") == ["allow"]:
+                        return CheckResult.FAILED
+                    if rule.get("preview") == [True]:
+                        return CheckResult.FAILED
+
+                    return CheckResult.PASSED
+
+        return CheckResult.FAILED
+
+
+check = CloudArmorWAFACLCVE202144228()
---
+++ tests/terraform/checks/resource/gcp/example_CloudArmorWAFACLCVE202144228/main.tf
@@ -0,0 +1,74 @@
+# pass
+
+resource "google_compute_security_policy" "enabled_deny_403" {
+  name = "example"
+
+  rule {
+    action   = "deny(403)"
+    priority = 1
+    match {
+      expr {
+        expression = "evaluatePreconfiguredExpr('cve-canary')"
+      }
+    }
+  }
+}
+
+resource "google_compute_security_policy" "enabled_deny_404" {
+  name = "example"
+
+  rule {
+    action   = "deny(404)"
+    priority = 1
+    match {
+      expr {
+        expression = "evaluatePreconfiguredExpr('cve-canary')"
+      }
+    }
+  }
+}
+
+# fail
+
+resource "google_compute_security_policy" "allow" {
+  name = "example"
+
+  rule {
+    action   = "allow"
+    priority = 1
+    match {
+      expr {
+        expression = "evaluatePreconfiguredExpr('cve-canary')"
+      }
+    }
+  }
+}
+
+resource "google_compute_security_policy" "preview" {
+  name = "example"
+
+  rule {
+    action   = "deny(403)"
+    priority = 1
+    match {
+      expr {
+        expression = "evaluatePreconfiguredExpr('cve-canary')"
+      }
+    }
+    preview = true
+  }
+}
+
+resource "google_compute_security_policy" "different_expr" {
+  name = "example"
+
+  rule {
+    action   = "deny(403)"
+    priority = 1
+    match {
+      expr {
+        expression = "evaluatePreconfiguredExpr('xss-canary')"
+      }
+    }
+  }
+}
