04a1b6070947f6907854353f47efb5273b5f611c
Rahul Lokurte <81438015+rahulmlokurte@users.noreply.github.com>
2021-12-28 15:54:13 +0530

    Fix issue 2063 - CKV_AWS_174 incorrect reporting (#2154)
    * Fix issue 2063 - CKV_AWS_174 incorrect reporting
    
    * Reformating duplicate code and Adding Tests for CKV_AWS_174 incorrect reporting

--- tests/terraform/checks/resource/aws/test_CloudFrontTLS12.py
+++ tests/terraform/checks/resource/aws/test_CloudFrontTLS12.py
@@ -17,6 +17,7 @@ class TestCloudFrontTLS12(unittest.TestCase):
 
         passing_resources = {
             "aws_cloudfront_distribution.pass",
+            "aws_cloudfront_distribution.pass1",
         }
         failing_resources = {
             "aws_cloudfront_distribution.fail",
@@ -27,7 +28,7 @@ class TestCloudFrontTLS12(unittest.TestCase):
         passed_check_resources = set([c.resource for c in report.passed_checks])
         failed_check_resources = set([c.resource for c in report.failed_checks])
 
-        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["passed"], 2)
         self.assertEqual(summary["failed"], 3)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
