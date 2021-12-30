c03c14971c4b825dee5a314ed686c9cb2c83ecae
nimrodkor <nimrodkor@gmail.com>
2021-12-30 21:00:35 +0200

    Fix 2 CodeQL findings

--- checkov/terraform/module_loading/loaders/bitbucket_loader.py
+++ checkov/terraform/module_loading/loaders/bitbucket_loader.py
@@ -1,10 +1,13 @@
+from urllib.parse import urlparse
+
 from checkov.terraform.module_loading.loaders.git_loader import GenericGitLoader
 
 
 class BitbucketLoader(GenericGitLoader):
     def _is_matching_loader(self) -> bool:
         # https://www.terraform.io/docs/modules/sources.html#bitbucket
-        if self.module_source.startswith("bitbucket.org"):
+        module_host = urlparse(self.module_source).hostname
+        if module_host == "bitbucket.org":
             self.module_source = f"git::https://{self.module_source}"
             return True
         return False
--- checkov/terraform/module_loading/loaders/github_loader.py
+++ checkov/terraform/module_loading/loaders/github_loader.py
@@ -1,10 +1,13 @@
+from urllib.parse import urlparse
+
 from checkov.terraform.module_loading.loaders.git_loader import GenericGitLoader
 
 
 class GithubLoader(GenericGitLoader):
     def _is_matching_loader(self) -> bool:
         # https://www.terraform.io/docs/modules/sources.html#github
-        if self.module_source.startswith("github.com"):
+        module_host = urlparse(self.module_source).hostname
+        if module_host == "github.com":
             self.module_source = f"git::https://{self.module_source}"
             return True
         if self.module_source.startswith("git@github.com"):
