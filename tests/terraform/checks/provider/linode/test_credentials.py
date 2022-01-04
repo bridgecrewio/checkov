2e9616600970309b0947bdec1259a73ece6c8a05
Anton Grübel <anton.gruebel@gmail.com>
2022-01-04 09:42:43 +0100

    adjust provider tests and docs (#2166)
    * adjust provider tests and docs
    
    * print IP

--- tests/terraform/checks/provider/linode/test_credentials.py
+++ tests/terraform/checks/provider/linode/test_credentials.py
@@ -1,21 +1,34 @@
 import unittest
 
+import hcl2
+
 from checkov.terraform.checks.provider.linode.credentials import check
 from checkov.common.models.enums import CheckResult
 
 
 class TestCredentials(unittest.TestCase):
-
     def test_success(self):
-        provider_conf = {}
-
+        hcl_res = hcl2.loads(
+            """
+            provider "linode" {}
+            """
+        )
+        provider_conf = hcl_res["provider"][0]["linode"]
         scan_result = check.scan_provider_conf(conf=provider_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
     def test_failure(self):
-        provider_conf = {'token' :['c7680462065ee80d0fef2940784b1af6826f6e0b18586194c5f67c4b40fa7f09']}
+        hcl_res = hcl2.loads(
+            """
+            provider "linode" {
+                token = "c7680462065ee80d0fef2940784b1af6826f6e0b18586194c5f67c4b40fa7f09"
+            }
+            """
+        )
+        provider_conf = hcl_res["provider"][0]["linode"]
         scan_result = check.scan_provider_conf(conf=provider_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
