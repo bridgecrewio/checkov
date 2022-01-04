e8f77db73c999a7bcf6d82008bebfbf129ff6d5a
James Holland <jholland@paloaltonetworks.com>
2022-01-04 11:44:57 +0000

    Initial panos terraform support (#2135)
    * Initial support for PAN-OS Terraform provider
    
    * Updated to use hcl2 per new guidance and docs

--- checkov/terraform/checks/provider/__init__.py
+++ checkov/terraform/checks/provider/__init__.py
@@ -3,3 +3,4 @@ from checkov.terraform.checks.provider.linode import *  # noqa
 from checkov.terraform.checks.provider.bridgecrew import *  # noqa
 from checkov.terraform.checks.provider.oci import *  # noqa
 from checkov.terraform.checks.provider.openstack import *  # noqa
+from checkov.terraform.checks.provider.panos import *  # noqa
---
+++ checkov/terraform/checks/provider/panos/credentials.py
@@ -0,0 +1,31 @@
+import re
+from typing import Dict, List, Any
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.provider.base_check import BaseProviderCheck
+from checkov.common.models.consts import panos_api_key_pattern
+
+
+class PanosCredentials(BaseProviderCheck):
+    def __init__(self) -> None:
+        name = "Ensure no hard coded PAN-OS API keys exist in provider"
+        id = "CKV_PAN_1"
+        supported_provider = ["panos"]
+        categories = [CheckCategories.SECRETS]
+        super().__init__(name=name, id=id, categories=categories, supported_provider=supported_provider)
+
+    def scan_provider_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+        if self.secret_found(conf, "api_key", panos_api_key_pattern):
+            return CheckResult.FAILED
+        return CheckResult.PASSED
+
+    @staticmethod
+    def secret_found(conf: Dict[str, List[Any]], field: str, pattern: str) -> bool:
+        if field in conf.keys():
+            value = conf[field][0]
+            if re.match(pattern, value) is not None:
+                return True
+        return False
+
+
+check = PanosCredentials()
