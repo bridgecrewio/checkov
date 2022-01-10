9a996bf6d7673682fc16df0cb308029c1c5be1f0
Barak Schoster Goihman <schosterbarak@gmail.com>
2022-01-10 09:29:23 +0200

    Update checkov/github/base_github_configuration_check.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/github/base_github_configuration_check.py
+++ checkov/github/base_github_configuration_check.py
@@ -4,13 +4,14 @@ from checkov.github.registry import registry
 
 
 class BaseGithubCheck(BaseCheck):
-    def __init__(self, name, id, categories, supported_entities, block_type, path=None):
+    def __init__(self, name, id, categories, supported_entities, block_type, path=None, guideline=None):
         super().__init__(
             name=name,
             id=id,
             categories=categories,
             supported_entities=supported_entities,
             block_type=block_type,
+            guideline=guideline,
         )
         self.path = path
         registry.register(self)
