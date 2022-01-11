bbf818290c4d36bc31602e87606406eaf6bc6ba5
Saar Ettinger <saarettinger@gmail.com>
2022-01-11 14:37:03 +0200

    fixed oci ingress stateless check to support list syntax

--- checkov/terraform/checks/resource/oci/SecurityListIngressStateless.py
+++ checkov/terraform/checks/resource/oci/SecurityListIngressStateless.py
@@ -14,10 +14,17 @@ class SecurityListIngressStateless(BaseResourceCheck):
         if 'ingress_security_rules' in conf.keys():
             self.evaluated_keys=['ingress_security_rules']
             rules = conf.get("ingress_security_rules")
+            is_list_syntax = False
+            if isinstance(rules[0], list):
+                # Old terraform oci provider version syntax for ingress_security_rules was a list instead of a block
+                rules = rules[0]
+                is_list_syntax = True
             for idx, rule in enumerate(rules):
                 if 'stateless' in rule.keys():
-                    if rule.get("stateless") != [True]:
-                        self.evaluated_keys = [f'ingress_security_rules/[{idx}]/stateless']
+                    stateless = rule.get("stateless")
+                    if stateless is not True and stateless != [True]:
+                        self.evaluated_keys = [f'ingress_security_rules/[{idx}]/stateless'] if is_list_syntax \
+                            else [f'ingress_security_rules/[0]/[{idx}]/stateless']
                         return CheckResult.FAILED
             return CheckResult.PASSED
 
