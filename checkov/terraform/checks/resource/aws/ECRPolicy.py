9f3c44c91bb7dc7871799a11c4f13ff20ab1cb52
James Woolfenden <james.woolfenden@gmail.com>
2022-01-18 10:39:31 +0000

    check types

--- checkov/terraform/checks/resource/aws/ECRPolicy.py
+++ checkov/terraform/checks/resource/aws/ECRPolicy.py
@@ -21,13 +21,14 @@ class ECRPolicy(BaseResourceCheck):
         """
         if "policy" in conf.keys():
             policy = conf["policy"][0]
-            if policy == '':
+            if type(policy) is str:
                 return CheckResult.PASSED
-            if policy['Statement'][0]:
+            if policy['Statement'][0] and type(policy['Statement'][0]) is dict:
                 statement = policy['Statement'][0]
-                if statement['Principal']:
+                if statement['Principal'] and type(statement['Principal']) is str:
                     principal = statement['Principal']
                     if principal == "*":
+                        self.evaluated_keys = ["policy/Statement/Principal"]
                         return CheckResult.FAILED
         return CheckResult.PASSED
 
