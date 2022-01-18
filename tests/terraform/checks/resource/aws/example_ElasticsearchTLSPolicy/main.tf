cb9a5b3e0a15dfd774cd3e71ddbcfcca20cf578d
James Woolfenden <james.woolfenden@gmail.com>
2022-01-18 18:36:44 +0000

    doesnt fail locally

--- tests/terraform/checks/resource/aws/example_ElasticsearchTLSPolicy/main.tf
+++ tests/terraform/checks/resource/aws/example_ElasticsearchTLSPolicy/main.tf
@@ -1,12 +1,16 @@
+provider "aws" {
+  region="eu-west-2"
+}
+
 resource "aws_elasticsearch_domain" "fail" {
-  domain_name = "node_to_node_encryption_disabled"
+  domain_name = "nodetonode"
   domain_endpoint_options {
     enforce_https       = false
     tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
   }
 
   cluster_config {
-    instance_count = 2 // a value > 1
+    instance_count = 2
   }
 
   encrypt_at_rest {
@@ -19,7 +23,7 @@ resource "aws_elasticsearch_domain" "fail" {
 }
 
 resource "aws_elasticsearch_domain" "notset" {
-  domain_name = "node_to_node_encryption_disabled"
+  domain_name = "notset"
 
   cluster_config {
     instance_count = 2 // a value > 1
@@ -35,14 +39,15 @@ resource "aws_elasticsearch_domain" "notset" {
 }
 
 resource "aws_elasticsearch_domain" "pass" {
-  domain_name = "node_to_node_encryption_disabled"
+  domain_name = "pass"
+
   domain_endpoint_options {
     enforce_https       = false
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   cluster_config {
-    instance_count = 2 // a value > 1
+    instance_count = 2
   }
 
   encrypt_at_rest {
