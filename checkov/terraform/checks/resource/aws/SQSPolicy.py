125e9f8c0f315afe53072206453560da34bbfd19
James Woolfenden <james.woolfenden@gmail.com>
2022-01-19 14:44:15 +0000

    Policy did not work -does now

--- checkov/terraform/checks/resource/aws/SQSPolicy.py
+++ checkov/terraform/checks/resource/aws/SQSPolicy.py
@@ -1,7 +1,5 @@
-import json
 
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.common.util.type_forcers import is_json
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 from typing import List
 
@@ -21,9 +19,9 @@ class SQSPolicy(BaseResourceCheck):
         :param conf: aws_sqs_queue_policy configuration
         :return: <CheckResult>
         """
-        if "policy" in conf.keys() and is_json(conf["policy"][0]) \
-                and json.loads(conf["policy"][0])['Statement'][0]['Action'] == '*':
-            return CheckResult.FAILED
+        if "policy" in conf.keys() and type(conf["policy"][0]) is dict:
+            if (conf["policy"][0])['Statement'][0]['Action'] == '*':
+                return CheckResult.FAILED
         return CheckResult.PASSED
 
     def get_evaluated_keys(self) -> List[str]:
