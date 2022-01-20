0ed3c066b0c4c38d41a499246e012d1a4b850146
nimrodkor <nimrodkor@gmail.com>
2022-01-20 09:35:42 +0200

    Fix test dir name

--- tests/terraform/graph/checks/test_yaml_policies.py
+++ tests/terraform/graph/checks/test_yaml_policies.py
@@ -201,7 +201,7 @@ class TestYamlPolicies(unittest.TestCase):
         self.go("SQLServerAuditingRetention90Days")
 
     def test_AWSSSMParameterShouldBeEncrypted(self):
-        self.go("AWSSSMParameterShouldBeEncrypted")
+        self.go("AWSSSMParameterShouldBeEncrypted", "AWSSSMParametershouldbeEncrypted")
 
     def test_registry_load(self):
         registry = Registry(parser=NXGraphCheckParser(), checks_dir=str(
