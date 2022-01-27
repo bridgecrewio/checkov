683d95ddd2415344319dd43510405601a650c887
James Woolfenden <james.woolfenden@gmail.com>
2022-01-27 09:08:18 +0000

    ensure amis are made encrypted

--- checkov/terraform/checks/resource/aws/ImagebuilderDistributionConfigurationEncryptedWithCMK.py
+++ checkov/terraform/checks/resource/aws/ImagebuilderDistributionConfigurationEncryptedWithCMK.py
@@ -3,19 +3,20 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 from checkov.common.models.enums import CheckCategories
 
 
-class ImagebuilderComponentEncryptedWithCMK(BaseResourceValueCheck):
+class ImagebuilderDistributionConfigurationEncryptedWithCMK(BaseResourceValueCheck):
     def __init__(self) -> None:
-        name = "Ensure Image Builder component is encrypted by KMS using a customer managed Key (CMK)"
-        id = "CKV_AWS_180"
-        supported_resources = ['aws_imagebuilder_component']
+        name = "Ensure Image Builder Distribution Configuration encrypts AMI's " \
+               "using KMS - a customer managed Key (CMK)"
+        id = "CKV_AWS_199"
+        supported_resources = ['aws_imagebuilder_distribution_configuration']
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
-        return "kms_key_id"
+        return "distribution/[0]/ami_distribution_configuration/[0]/kms_key_id"
 
     def get_expected_value(self) -> str:
         return ANY_VALUE
 
 
-check = ImagebuilderComponentEncryptedWithCMK()
+check = ImagebuilderDistributionConfigurationEncryptedWithCMK()
