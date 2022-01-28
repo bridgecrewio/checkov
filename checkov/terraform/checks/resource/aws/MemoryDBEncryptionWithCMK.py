bb212198fe36d2ef18f73657987544d711be381b
James Woolfenden <james.woolfenden@gmail.com>
2022-01-28 15:14:40 +0000

    Update checkov/terraform/checks/resource/aws/MemoryDBEncryptionWithCMK.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/terraform/checks/resource/aws/MemoryDBEncryptionWithCMK.py
+++ checkov/terraform/checks/resource/aws/MemoryDBEncryptionWithCMK.py
@@ -8,7 +8,7 @@ class MemoryDBEncryptionWithCMK(BaseResourceValueCheck):
         name = "Ensure MemoryDB is encrypted at rest using KMS CMKs"
         id = "CKV_AWS_201"
         supported_resources = ['aws_memorydb_cluster']
-        categories = [CheckCategories.LOGGING]
+        categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
