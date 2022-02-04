3bc9c4e7f2273cfdce1334360401d3b86a00adc6
James Woolfenden <james.woolfenden@gmail.com>
2022-02-04 12:07:12 +0000

    Resource use should be restricted

---
+++ tests/terraform/checks/resource/aws/test_AMILaunchIsShared.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+
+from checkov.runner_filter import RunnerFilter
+from checkov.terraform.checks.resource.aws.AMILaunchIsShared import check
+from checkov.terraform.runner import Runner
+
+
+class TestAMILaunchIsShared(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_AMILaunchIsShared"
+        report = runner.run(
+            root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id])
+        )
+        summary = report.get_summary()
+
+        failing_resources = {
+            "aws_ami_launch_permission.fail"
+        }
+
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary["failed"], 1)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == "__main__":
+    unittest.main()
