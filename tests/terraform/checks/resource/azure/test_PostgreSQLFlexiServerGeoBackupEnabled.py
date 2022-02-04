5d9fa14d8462db0bd9e7869ad20ead9a8bd2b761
Aaditya Nandeshwar <aaditya.nandeshwar@cldcvr.com>
2022-02-04 11:36:10 +0530

    Added tests for real terraform code

--- tests/terraform/checks/resource/azure/test_PostgreSQLFlexiServerGeoBackupEnabled.py
+++ tests/terraform/checks/resource/azure/test_PostgreSQLFlexiServerGeoBackupEnabled.py
@@ -1,86 +1,42 @@
 import unittest
+from pathlib import Path
 
-import hcl2
-
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.azure.PostgreSQLFlexiServerGeoBackupEnabled import check
-from checkov.common.models.enums import CheckResult
+from checkov.terraform.runner import Runner
 
 
 class TestPostgreSQLFlexiServerGeoBackupEnabled(unittest.TestCase):
 
-    def test_failure_1(self):
-        hcl_res = hcl2.loads("""
-            resource "azurerm_postgresql_flexible_server" "example" {
-              name                   = "example-psqlflexibleserver"
-              resource_group_name    = azurerm_resource_group.example.name
-              location               = azurerm_resource_group.example.location
-              version                = "12"
-              delegated_subnet_id    = azurerm_subnet.example.id
-              private_dns_zone_id    = azurerm_private_dns_zone.example.id
-              administrator_login    = "psqladmin"
-              administrator_password = "H@Sh1CoR3!"
-              zone                   = "1"
-            
-              storage_mb = 32768
-              geo_redundant_backup_enabled = false
-            
-              sku_name   = "GP_Standard_D4s_v3"
-              depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
-            
-            }
-        """)
-        resource_conf = hcl_res['resource'][0]['azurerm_postgresql_flexible_server']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+    def test(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_PostgreSQLFlexiServerGeoBackupEnabled"
 
-    def test_failure_2(self):
-        hcl_res = hcl2.loads("""
-            resource "azurerm_postgresql_flexible_server" "example" {
-              name                   = "example-psqlflexibleserver"
-              resource_group_name    = azurerm_resource_group.example.name
-              location               = azurerm_resource_group.example.location
-              version                = "12"
-              delegated_subnet_id    = azurerm_subnet.example.id
-              private_dns_zone_id    = azurerm_private_dns_zone.example.id
-              administrator_login    = "psqladmin"
-              administrator_password = "H@Sh1CoR3!"
-              zone                   = "1"
-            
-              storage_mb = 32768
-            
-              sku_name   = "GP_Standard_D4s_v3"
-              depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
-            
-            }
-                """)
-        resource_conf = hcl_res['resource'][0]['azurerm_postgresql_flexible_server']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
 
-    def test_success(self):
-        hcl_res = hcl2.loads("""
-            resource "azurerm_postgresql_flexible_server" "example" {
-              name                   = "example-psqlflexibleserver"
-              resource_group_name    = azurerm_resource_group.example.name
-              location               = azurerm_resource_group.example.location
-              version                = "12"
-              delegated_subnet_id    = azurerm_subnet.example.id
-              private_dns_zone_id    = azurerm_private_dns_zone.example.id
-              administrator_login    = "psqladmin"
-              administrator_password = "H@Sh1CoR3!"
-              zone                   = "1"
-            
-              storage_mb = 32768
-              geo_redundant_backup_enabled = true
-            
-              sku_name   = "GP_Standard_D4s_v3"
-              depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
-            
-            }
-                """)
-        resource_conf = hcl_res['resource'][0]['azurerm_postgresql_flexible_server']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        # then
+        summary = report.get_summary()
+
+        passing_resources = {
+            "azurerm_postgresql_flexible_server.pass",
+        }
+        failing_resources = {
+            "azurerm_postgresql_flexible_server.fail1",
+            "azurerm_postgresql_flexible_server.fail2",
+
+        }
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary["passed"], 1)
+        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == '__main__':
