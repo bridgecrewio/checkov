6313033244ab0c14e3e3686ce5f16076c395a40e
Adrian Berger <43774417+adberger@users.noreply.github.com>
2022-02-10 13:25:13 +0100

    Update test_UserExists.py

--- tests/dockerfile/checks/test_UserExists.py
+++ tests/dockerfile/checks/test_UserExists.py
@@ -29,7 +29,7 @@ class TestUserExists(unittest.TestCase):
         failed_check_resources = set([c.resource for c in report.failed_checks])
 
         self.assertEqual(summary["passed"], 2)
-        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["failed"], 3)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
