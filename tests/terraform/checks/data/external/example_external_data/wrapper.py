0a8f34a6bc185a13afd5c672338b4b3fcbf64255
barak <schosterbarak@gmail.com>
2022-02-13 15:14:34 +0200

    skip test file

--- tests/terraform/checks/data/external/example_external_data/wrapper.py
+++ tests/terraform/checks/data/external/example_external_data/wrapper.py
@@ -1,8 +1,10 @@
+# pylint: skip-file
+# skip this file from pylint since it vulnerable by design
 import json
 import os
 
 stream = os.popen('id')
 output = stream.read()
-result = { "result" : output }
+result = {"result": output}
 
 print(json.dumps(result))
