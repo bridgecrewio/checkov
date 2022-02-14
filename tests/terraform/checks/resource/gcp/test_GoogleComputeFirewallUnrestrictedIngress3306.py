68f37bada0a8c67eb15a6909147d0432e9088a21
Jason Dyke <jasonadyke@gmail.com>
2022-02-14 14:40:54 -0500

    migrate to external file test cases

--- tests/terraform/checks/resource/gcp/test_GoogleComputeFirewallUnrestrictedIngress3306.py
+++ tests/terraform/checks/resource/gcp/test_GoogleComputeFirewallUnrestrictedIngress3306.py
@@ -1,71 +1,46 @@
 import unittest
-import hcl2
+from pathlib import Path
 
-from checkov.terraform.checks.resource.gcp.GoogleComputeFirewallUnrestrictedIngress3306 import check, PORT
-from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
+from checkov.terraform.checks.resource.gcp.GoogleComputeFirewallUnrestrictedIngress3306 import check
+from checkov.terraform.runner import Runner
 
 
 class TestGoogleComputeFirewallUnrestrictedIngress3306(unittest.TestCase):
+    def test(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_GoogleComputeFirewallUnrestrictedIngress3306"
 
-    def test_failure(self):
-        resource_conf = {
-            'name': ['mysql-database-public'],
-            'network': ['${google_compute_network.mysql-network.name}'],
-            'allow': [
-                {
-                    'protocol': ['tcp'],
-                    'ports': [[str(PORT)]]
-                }
-            ],
-            'source_ranges': [['0.0.0.0/0']]
-            }
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
 
-        # This fails because we are allowing unrestricted mysql access
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        # then
+        summary = report.get_summary()
 
-    def test_success_1(self):
-        resource_conf = {
-            'name': ['mysql-database-public'],
-            'network': ['${google_compute_network.mysql-network.name}'],
-            'allow': [
-                {
-                    'protocol': ['tcp'],
-                    'ports': [[str(PORT)]]
-                }
-            ],
-            'source_ranges': [['172.1.2.3/32']]  # Non-public CIDR
-            }
+        passing_resources = {
+            "google_compute_firewall.restricted",
+            "google_compute_firewall.allow_different_int",
+            "google_compute_firewall.allow_null",
+        }
 
-        # This passes b/c we specify a non-public CIDR/IP
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        failing_resources = {
+            "google_compute_firewall.allow_multiple",
+            "google_compute_firewall.allow_mysql_int",
+            "google_compute_firewall.allow_all",
+        }
 
-    def test_success_2(self):
-        hcl_res = hcl2.loads(
-            """
-                resource "google_compute_firewall" "deny-all-egress" {
-                    name        = "deny-all-egress"
-                    description = "Prevent all egress traffic by default"
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
 
-                    network        = google_compute_network.vpc_network.name
-                    enable_logging = true
+        self.assertEqual(summary["passed"], 3)
+        self.assertEqual(summary["failed"], 3)
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+        self.assertEqual(summary["resource_count"], 7)  # 1 unknown
 
-                    priority           = 65534
-                    direction          = "EGRESS"
-                    destination_ranges = ["0.0.0.0/0"]
-                    deny { protocol = "all" }
-                }
-            """
-            )
-        resource_conf = hcl_res['resource'][0]['google_compute_firewall']['deny-all-egress']
-
-        # This passes b/c we always pass if "allow" is not found
-        # We tested "deny { protocol = "all" }" so there is no "allow" rule
-        # Check AbsGoogleComputeFirewallUnrestrictedIngress.py file for logic
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
