2e4ad19342bf3b92740af7b35d72287105f1406f
James Woolfenden <james.woolfenden@gmail.com>
2022-02-14 16:07:21 +0000

    fix test data

--- tests/terraform/checks/resource/gcp/example_DataflowJobEncryptedWithCMK/main.tf
+++ tests/terraform/checks/resource/gcp/example_DataflowJobEncryptedWithCMK/main.tf
@@ -9,7 +9,7 @@ resource "google_dataflow_job" "fail" {
   #   kms_key_name =
 }
 
-resource "google_dataflow_job" "fail" {
+resource "google_dataflow_job" "pass" {
   name              = "dataflow-job"
   template_gcs_path = "gs://my-bucket/templates/template_file"
   temp_gcs_location = "gs://my-bucket/tmp_dir"
