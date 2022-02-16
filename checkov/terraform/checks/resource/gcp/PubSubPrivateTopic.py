4c5969c0857445712172fcdbb9efb600a6b7c189
Jason Dyke <jasonadyke@gmail.com>
2022-02-16 16:37:24 -0500

    remove unused library

--- checkov/terraform/checks/resource/gcp/PubSubPrivateTopic.py
+++ checkov/terraform/checks/resource/gcp/PubSubPrivateTopic.py
@@ -2,7 +2,6 @@
 
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from typing import List
 
 
 class PubSubPrivateTopic(BaseResourceCheck):
