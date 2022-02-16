b97ed06f2b39c34ccc734181488b688df693e4a7
c0mmad0r3 <c0mmad0r3@gmail.com>
2022-02-16 12:03:37 -0500

    Modification of CosmosDBLocalAuthDisabled.py and addition of new check for Machine Learning Public access

--- checkov/terraform/checks/resource/azure/MLDisablePublicAccess.py
+++ checkov/terraform/checks/resource/azure/MLDisablePublicAccess.py
@@ -1,26 +1,26 @@
-from checkov.common.models.enums import CheckCategories
+from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class CosmosDBLocalAuthDisabled(BaseResourceValueCheck):
+class MLDisablePublicAccess(BaseResourceValueCheck):
     def __init__(self):
         # This is the full description of your check
-        description = "Ensure that Local Authentication is disabled on CosmosDB"
+        description = "Ensure that Public Access is disabled for Machine Learning Workspace"
 
         # This is the Unique ID for your check
-        id = "CKV_AZURE_140"
+        id = "CKV_AZURE_144"
 
         # These are the terraform objects supported by this check (ex: aws_iam_policy_document)
-        supported_resources = ['azurerm_cosmosdb_account']
+        supported_resources = ['azurerm_machine_learning_workspace']
 
         # Valid CheckCategories are defined in checkov/common/models/enums.py
-        categories = [CheckCategories.IAM]
+        categories = [CheckCategories.NETWORKING]
         super().__init__(name=description, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
-        return "local_authentication_disabled"
-        
-    def get_expected_value(self):
-        return True
+        return "public_network_access_enabled"
 
-check = CosmosDBLocalAuthDisabled()
+    def get_expected_value(self):
+        return False
+
+check = MLDisablePublicAccess()
