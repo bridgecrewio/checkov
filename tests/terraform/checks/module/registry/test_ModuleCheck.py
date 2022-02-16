3fd4f7583fb4bc6373a3b294fd2bf43fdd63d563
barak <schosterbarak@gmail.com>
2022-02-16 11:53:53 +0200

    fix tests

--- tests/terraform/checks/module/registry/test_ModuleCheck.py
+++ tests/terraform/checks/module/registry/test_ModuleCheck.py
@@ -2,19 +2,20 @@ import unittest
 from pathlib import Path
 
 from checkov.runner_filter import RunnerFilter
-from checkov.terraform.checks.data.aws.IAMWriteAccess import check
+from checkov.terraform.checks.module.registry import module_registry
 from checkov.terraform.runner import Runner
 
 
 class TestModuleCheck(unittest.TestCase):
 
     def test_module_version(self):
-        external_checks = Path.joinpath(Path(__file__).parent , "example_external_dir_with_module_version_check/extra_checks").as_posix()
+        external_checks = Path.joinpath(Path(__file__).parent,
+                                        "example_external_dir_with_module_version_check/extra_checks").as_posix()
         test_files_dir = Path(__file__).parent / "resources"
 
-        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=["CKV_TF_MODULE_1"]),external_checks_dir=[external_checks])
+        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=["CKV_TF_MODULE_1"]),
+                              external_checks_dir=[external_checks])
         summary = report.get_summary()
-
 
         self.assertEqual(summary["passed"], 2)
         self.assertEqual(summary["failed"], 3)
@@ -25,12 +26,13 @@ class TestModuleCheck(unittest.TestCase):
         module_registry.checks["module"].remove(check)
 
     def test_immutable_module(self):
-        external_checks = Path.joinpath(Path(__file__).parent , "example_external_dir_with_module_version_check/extra_checks").as_posix()
+        external_checks = Path.joinpath(Path(__file__).parent,
+                                        "example_external_dir_with_module_version_check/extra_checks").as_posix()
         test_files_dir = Path(__file__).parent / "resources"
 
-        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=["CKV_TF_MODULE_2"]),external_checks_dir=[external_checks])
+        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=["CKV_TF_MODULE_2"]),
+                              external_checks_dir=[external_checks])
         summary = report.get_summary()
-
 
         self.assertEqual(summary["passed"], 1)
         self.assertEqual(summary["failed"], 4)
@@ -39,5 +41,7 @@ class TestModuleCheck(unittest.TestCase):
 
         check = next(c for c in module_registry.checks["module"] if c.id == "CKV_TF_MODULE_2")
         module_registry.checks["module"].remove(check)
+
+
 if __name__ == "__main__":
     unittest.main()
