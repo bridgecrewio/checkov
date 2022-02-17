0fbda7879ce583c5ca9dcd301e00d9628314dbf7
James Woolfenden <james.woolfenden@gmail.com>
2022-02-17 17:15:53 +0000

    Where art CKV_AZURE_142

--- checkov/terraform/checks/resource/azure/FunctionAppMinTLSVersion.py
+++ checkov/terraform/checks/resource/azure/FunctionAppMinTLSVersion.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 class FunctionAppMinTLSVersion(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure Function app is using the latest version of TLS encryption"
-        id = "CKV_AZURE_142"
+        id = "CKV_AZURE_143"
         supported_resources = ['azurerm_function_app']
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
