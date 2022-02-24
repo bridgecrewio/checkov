cde05a60b435cada5c5f93dab3de87650fc1d764
Rotem Avni <rotemavn@gmail.com>
2022-02-24 15:26:51 +0200

    compare only ints

--- checkov/terraform/checks/resource/aws/AbsNACLUnrestrictedIngress.py
+++ checkov/terraform/checks/resource/aws/AbsNACLUnrestrictedIngress.py
@@ -48,11 +48,11 @@ class AbsNACLUnrestrictedIngress(BaseResourceCheck):
         if rule.get('cidr_block'):
             if rule.get('cidr_block') == ["0.0.0.0/0"]:
                 if rule.get('action') == ["allow"] or rule.get('rule_action') == ["allow"]:
-                    if rule.get('from_port')[0] <= self.port <= rule.get('to_port')[0]:
+                    if int(rule.get('from_port')[0]) <= self.port <= int(rule.get('to_port')[0]):
                         return False
         if rule.get('ipv6_cidr_block'):
             if rule.get('ipv6_cidr_block') == ["::/0"]:
                 if rule.get('action') == ["allow"] or rule.get('rule_action') == ["allow"]:
-                    if rule.get('from_port')[0] <= self.port <= rule.get('to_port')[0]:
+                    if int(rule.get('from_port')[0]) <= self.port <= int(rule.get('to_port')[0]):
                         return False
         return True
