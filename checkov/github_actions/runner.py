2ceddf99f4bbcbc4e9d49b3011154c212db15ca5
barak <schosterbarak@gmail.com>
2022-03-01 17:32:27 +0200

    fix by review

--- checkov/github_actions/runner.py
+++ checkov/github_actions/runner.py
@@ -2,7 +2,6 @@ import os
 
 from checkov.common.output.report import CheckType
 from checkov.github_actions.checks.job_registry import registry as job_registry
-from checkov.runner_filter import RunnerFilter
 from checkov.yaml_doc.runner import Runner as YamlRunner
 
 
@@ -14,13 +13,6 @@ class Runner(YamlRunner):
 
     def __init__(self):
         super().__init__()
-
-    def run(self, root_folder=None, external_checks_dir=None, files=None,
-            runner_filter=RunnerFilter(), collect_skip_comments=True):
-        report = super().run(root_folder=root_folder, external_checks_dir=external_checks_dir,
-                             files=files,
-                             runner_filter=runner_filter, collect_skip_comments=collect_skip_comments)
-        return report
 
     def require_external_checks(self):
         return False
