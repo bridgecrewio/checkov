d0295a6ea5467bde3974c1efb76d0cf69613d568
Barak Schoster Goihman <schosterbarak@gmail.com>
2022-03-01 17:30:33 +0200

    Update tests/github_actions/test_runner.py
    Co-authored-by: Nimrod Kor <nimrodkor@gmail.com>

--- tests/github_actions/test_runner.py
+++ tests/github_actions/test_runner.py
@@ -14,7 +14,7 @@ class TestRunnerValid(unittest.TestCase):
         checks = ["CKV_GHA_1", "CKV_GHA_2"]
         report = runner.run(
             root_folder=valid_dir_path,
-            runner_filter=RunnerFilter(framework='github_action', checks=checks)
+            runner_filter=RunnerFilter(framework='github_actions', checks=checks)
         )
         self.assertEqual(len(report.failed_checks), 2)
         self.assertEqual(report.parsing_errors, [])
