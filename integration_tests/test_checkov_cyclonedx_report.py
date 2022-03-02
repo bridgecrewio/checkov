699bfd374728af583c5ae2a47e2cd0698a057a34
barak <schosterbarak@gmail.com>
2022-03-02 14:28:34 +0200

    update integration tests

--- integration_tests/test_checkov_cyclonedx_report.py
+++ integration_tests/test_checkov_cyclonedx_report.py
@@ -20,7 +20,7 @@ class TestCheckovCyclonedxReport(unittest.TestCase):
         vulnrability_file = \
             report.getElementsByTagName('components')[0].getElementsByTagName('component')[0].getElementsByTagName(
                 'name')[0].firstChild.nodeValue
-        self.assertEquals(vulnrability_file, '/aws/db-app.tf')
+        self.assertTrue('db-app.tf' in vulnrability_file)
 
 
 if __name__ == '__main__':
