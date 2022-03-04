c6972c23aa7b2dec5a361aaf1b800965979b4b7a
James Woolfenden <james.woolfenden@gmail.com>
2022-03-04 10:10:17 +0000

    prefer apt-get over apt

---
+++ checkov/dockerfile/checks/RunUsingAPT.py
@@ -0,0 +1,25 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.dockerfile.base_dockerfile_check import BaseDockerfileCheck
+
+
+class RunUsingAPT(BaseDockerfileCheck):
+    def __init__(self):
+        """
+        Apt interface is less stable than apt-get and so this preferred
+        """
+        name = "Ensure that APT isn't used"
+        id = "CKV_DOCKER_9"
+        supported_instructions = ["*"]
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_instructions=supported_instructions)
+
+    def scan_entity_conf(self, conf):
+        for instruction, content in conf.items():
+            if instruction == "RUN":
+                line = content[0]["content"]
+                if " apt " in line:
+                    return CheckResult.FAILED, conf[instruction][0]
+        return CheckResult.PASSED, None
+
+
+check = RunUsingAPT()
