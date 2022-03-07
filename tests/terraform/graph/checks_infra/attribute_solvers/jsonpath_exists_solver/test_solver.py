64900d50954cc2f8873216bea0215b75518090f3
Matan Shati <>
2022-03-07 15:20:08 +0200

    add test

--- tests/terraform/graph/checks_infra/attribute_solvers/jsonpath_exists_solver/test_solver.py
+++ tests/terraform/graph/checks_infra/attribute_solvers/jsonpath_exists_solver/test_solver.py
@@ -36,3 +36,12 @@ class TestJsonpathExistsSolver(TestBaseSolver):
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_jsonpath_exists_example(self):
+        root_folder = './'
+        check_id = "example"
+        should_pass = ['xyz.pass']
+        should_fail = ['xyz.fail']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
