aafdaff1a4a94b471ed97c22dfdb67c468d012ca
JonZeolla <Zeolla@gmail.com>
2022-03-11 04:59:33 -0500

    Allow references to aws_lightsail_instance resources in CKV2_AWS_23 (#2580)
    * Allow references to aws_lightsail_instance resources in CKV2_AWS_23
    
    * Add test
    
    * Add aws_lightsail_instance for CKV2_AWS_23 test

--- tests/terraform/graph/checks/resources/Route53ARecordAttachedResource/main.tf
+++ tests/terraform/graph/checks/resources/Route53ARecordAttachedResource/main.tf
@@ -271,3 +271,20 @@ resource "aws_elastic_beanstalk_environment" "pass_eb" {
   application = aws_elastic_beanstalk_application.example.name
   name        = "example"
 }
+
+# Lightsail
+
+resource "aws_route53_record" "pass_lightsail" {
+  zone_id  = data.aws_route53_zone.dns_zone.zone_id
+  name     = var.sub_domain
+  type     = "A"
+  ttl      = "300"
+  records  = [aws_lightsail_instance.example.public_ip_address]
+}
+
+resource "aws_lightsail_instance" "example" {
+  name              = "example_lightsail_instance"
+  availability_zone = "us-east-1f"
+  blueprint_id      = "ubuntu_20_04"
+  bundle_id         = "medium_2_0"
+}
