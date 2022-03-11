f5aa631a822e31a357105b3144f9e77b8b6e89cd
James Woolfenden <james.woolfenden@gmail.com>
2022-03-11 09:49:25 +0000

    k8s: tf: Dont use privileged containers (#2595)
    * Dont use privileged containers
    
    * Update checkov/terraform/checks/resource/kubernetes/PrivilegedContainerPSP.py
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

---
+++ checkov/terraform/checks/resource/kubernetes/PrivilegedContainerPSP.py
@@ -0,0 +1,29 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class PrivilegedContainersPSP(BaseResourceCheck):
+
+    def __init__(self):
+        # CIS-1.3 1.7.1
+        # CIS-1.5 5.2.1
+        name = "Do not admit privileged containers"
+        id = "CKV_K8S_2"
+
+        supported_resources = ['kubernetes_pod']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf) -> CheckResult:
+        spec = conf['spec'][0]
+        containers = spec.get("container")
+        for idx, container in enumerate(containers):
+            if container.get("security_context"):
+                context = container.get("security_context")[0]
+                if context.get("privileged") == [True]:
+                    self.evaluated_keys = [f'spec/[0]/container/[{idx}]/security_context/[0]/privileged']
+                    return CheckResult.FAILED
+        return CheckResult.PASSED
+
+
+check = PrivilegedContainersPSP()
