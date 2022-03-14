cdf0c94e07cfc4aa2318d22f38a7dd3bc1c3fdf5
ipeleg <ipeleg@paloaltonetworks.com>
2022-03-14 12:42:00 +0200

    BCE-5356 typo

--- tests/dockerfile/test_utils.py
+++ tests/dockerfile/test_utils.py
@@ -1,11 +1,11 @@
 from checkov.dockerfile.utils import is_docker_file
 
-VALID_DOCKER_FILE_NAME = ["Dockerfile", "dockerfile", "Dockerfile.prod", "Dockerfile.Product1", "dev.Dockerfile",
+VALID_DOCKER_FILE_NAMES = ["Dockerfile", "dockerfile", "Dockerfile.prod", "Dockerfile.Product1", "dev.Dockerfile",
                            "team1.product.dockerfile"]
-INVALID_DOCKER_FILE_NAME = ["package.json", "dockerfil", "dockerfilee", ".dockerfile", "ddockerfile",
+INVALID_DOCKER_FILE_NAMES = ["package.json", "dockerfil", "dockerfilee", ".dockerfile", "ddockerfile",
                              "ockerfile", "docker-file", "dockerfile1"]
 
 
 def test_is_docker_file():
-    assert all(is_docker_file(curr_name) for curr_name in VALID_DOCKER_FILE_NAME)
-    assert all(not is_docker_file(curr_name) for curr_name in INVALID_DOCKER_FILE_NAME)
+    assert all(is_docker_file(curr_name) for curr_name in VALID_DOCKER_FILE_NAMES)
+    assert all(not is_docker_file(curr_name) for curr_name in INVALID_DOCKER_FILE_NAMES)
