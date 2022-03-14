49ecabea48b5104ed99aa95b620265ec964b16ad
Mike Urbanski <mike@bridgecrew.io>
2022-03-14 17:56:26 -0500

    handle tf plan parsing error

--- checkov/common/parsers/json/__init__.py
+++ checkov/common/parsers/json/__init__.py
@@ -4,6 +4,7 @@ SPDX-License-Identifier: MIT-0
 """
 import json
 import logging
+from typing import Dict
 
 from checkov.common.parsers.json.decoder import Decoder
 from checkov.common.parsers.json.errors import DecodeError
@@ -27,20 +28,27 @@ def load(filename, allow_nulls=True):
     return (json.loads(content, cls=Decoder, allow_nulls=allow_nulls), file_lines)
 
 
-def parse(filename, allow_nulls=True):
+def parse(filename, out_parsing_errors: Dict[str, str], allow_nulls=True):
     template = None
     template_lines = None
+    error = None
     try:
         (template, template_lines) = load(filename, allow_nulls)
-    except DecodeError:
-        pass
-    except json.JSONDecodeError:
+    except DecodeError as e:
+        logging.debug(f'Got DecodeError parsing file {filename}', exc_info=True)
+        error = e
+    except json.JSONDecodeError as e:
         # Most parsing errors will get caught by the exception above. But, if the file
         # is totally empty, and perhaps in other specific cases, the json library will
         # not even begin parsing with our custom logic that throws the exception above,
         # and will fail with this exception instead.
-        pass
-    except UnicodeDecodeError:
-        pass
+        logging.debug(f'Got JSONDecodeError parsing file {filename}', exc_info=True)
+        error = e
+    except UnicodeDecodeError as e:
+        logging.debug(f'Got UnicodeDecodeError parsing file {filename}', exc_info=True)
+        error = e
+
+    if error:
+        out_parsing_errors[filename] = str(error)
 
     return (template, template_lines)
