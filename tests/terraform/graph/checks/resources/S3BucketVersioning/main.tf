c1b6172778bbf3521c5e466413e82d545bb942f7
achiar99 <34912231+achiar99@users.noreply.github.com>
2022-03-16 17:19:25 +0200

    AWS S3 graph checks for Terraform V4 provider (#2638)
    * add s3BucketSSE yaml check
    
    * add test
    
    * CKV2_AWS_37 check - S3 versioning check with TFv4 support
    
    * fixes
    
    * add s3 logging graph check
    
    * add test to bucket logging
    
    * revert
    
    * eof
    
    * CKV2_AWS_38 check - S3 public ACL check with TFv4 support
    
    * CKV2_AWS_39 check - S3 public ACL write access check with TFv4 support
    
    * support a case where ACL object does not exist for the bucket
    
    * rename check description to match the legacy encryption check
    
    * Fix UTs according to new checks
    
    * Fix UTs according to new checks
    
    * Update checkov/terraform/checks/graph_checks/aws/S3BucketEncryption.yaml
    
    Use within operator
    
    Co-authored-by: Rotem Avni <52502521+rotemavni@users.noreply.github.com>
    
    * use within operator
    
    Co-authored-by: tronxd <tron47@gmail.com>
    Co-authored-by: naorda <tronxd@users.noreply.github.com>
    Co-authored-by: Rotem Avni <52502521+rotemavni@users.noreply.github.com>

---
+++ tests/terraform/graph/checks/resources/S3BucketVersioning/main.tf
@@ -0,0 +1,74 @@
+# pass
+
+resource "aws_s3_bucket" "enabled" {
+  bucket = "example"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket" "enabled_var" {
+  bucket = "example"
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+}
+
+resource "aws_s3_bucket" "unknown_var" {
+  bucket = "example"
+
+  versioning {
+    enabled = var.unknown_var
+  }
+}
+
+variable "unknown_var" {
+  description = "unknown value"
+}
+
+
+variable "versioning_enabled" {
+  default = true
+}
+
+# fail
+
+resource "aws_s3_bucket" "default" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket" "disabled" {
+  bucket = "example"
+
+  versioning {
+    enabled = false
+  }
+}
+
+# provider version 4
+
+resource "aws_s3_bucket" "enabled_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_versioning" "enabled_v4" {
+  bucket = aws_s3_bucket.enabled_v4.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "disabled_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_versioning" "disabled_v4" {
+  bucket = aws_s3_bucket.disabled_v4.id
+
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
---
+++ tests/terraform/graph/checks/resources/S3PublicACLRead/main.tf
@@ -0,0 +1,93 @@
+# pass
+
+resource "aws_s3_bucket" "private_acl" {
+  bucket = "example"
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket" "no_acl" {
+  bucket = "example_no_acl"
+}
+
+resource "aws_s3_bucket" "unknown_var" {
+  bucket = "example"
+
+  acl = var.unknown_var
+}
+
+variable "unknown_var" {
+  description = "unknown value"
+}
+
+# fail
+
+resource "aws_s3_bucket" "public_read" {
+  bucket = "example"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket" "public_read_write" {
+  bucket = "example"
+  acl    = "public-read-write"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "example"
+  acl    = "website"
+}
+
+resource "aws_s3_bucket" "authenticated_read" {
+  bucket = "example"
+  acl    = "authenticated-read"
+}
+
+# provider version 4
+
+# pass
+resource "aws_s3_bucket" "private_acl_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_acl" "private_acl_v4" {
+  bucket = aws_s3_bucket.private_acl_v4.id
+  acl    = "private"
+}
+
+# fail
+resource "aws_s3_bucket" "public_read_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_acl" "public_read_v4" {
+  bucket = aws_s3_bucket.public_read_v4.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket" "public_read_write_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_acl" "public_read_write_v4" {
+  bucket = aws_s3_bucket.public_read_write_v4.id
+  acl    = "public-read-write"
+}
+
+
+resource "aws_s3_bucket" "website_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_acl" "website_v4" {
+  bucket = aws_s3_bucket.website_v4.id
+  acl    = "website"
+}
+
+resource "aws_s3_bucket" "authenticated_read_v4" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_acl" "authenticated_read_v4" {
+  bucket = aws_s3_bucket.authenticated_read_v4.id
+  acl    = "authenticated-read"
+}
