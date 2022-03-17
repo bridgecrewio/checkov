4b4bdca09971ee81d6123c718a53387044d25928
James Woolfenden <james.woolfenden@gmail.com>
2022-03-17 16:41:12 +0000

    Enforce client certificates for api management ckv azure 152 (#2642)
    * CKV_K8S_22 for terraform
    
    * Update checkov/terraform/checks/resource/kubernetes/ReadonlyRootFilesystem.py
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>
    
    * use is instance
    
    * enforce client certs
    
    * purge
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

---
+++ tests/terraform/checks/resource/azure/example_APIManagementCertsEnforced/main.tf
@@ -0,0 +1,130 @@
+resource "azurerm_api_management" "ignore" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Developer_1"
+
+  policy {
+    xml_content = <<XML
+                    <policies>
+                      <inbound />
+                      <backend />
+                      <outbound />
+                      <on-error />
+                    </policies>
+                XML
+
+  }
+  security {
+    enable_frontend_tls10 = false
+    enable_frontend_tls11 = false
+    enable_frontend_ssl30 = false
+  }
+
+  identity {
+
+  }
+}
+resource "azurerm_api_management" "faulty" {
+}
+
+#not set
+resource "azurerm_api_management" "fail" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Consumption"
+
+  policy {
+    xml_content = <<XML
+                    <policies>
+                      <inbound />
+                      <backend />
+                      <outbound />
+                      <on-error />
+                    </policies>
+                XML
+
+  }
+  security {
+    enable_frontend_tls10 = false
+    enable_frontend_tls11 = false
+    enable_frontend_ssl30 = false
+  }
+
+  identity {
+
+  }
+}
+
+#false
+resource "azurerm_api_management" "fail2" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Consumption"
+  client_certificate_enabled = false
+
+  policy {
+    xml_content = <<XML
+                    <policies>
+                      <inbound />
+                      <backend />
+                      <outbound />
+                      <on-error />
+                    </policies>
+                XML
+
+  }
+  security {
+    enable_frontend_tls10 = false
+    enable_frontend_tls11 = false
+    enable_frontend_ssl30 = false
+  }
+
+  identity {
+
+  }
+}
+
+
+resource "azurerm_api_management" "pass" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Consumption"
+  client_certificate_enabled = true
+
+  policy {
+    xml_content = <<XML
+                    <policies>
+                      <inbound />
+                      <backend />
+                      <outbound />
+                      <on-error />
+                    </policies>
+                XML
+
+  }
+  security {
+    enable_frontend_tls10 = false
+    enable_frontend_tls11 = false
+    enable_frontend_ssl30 = false
+  }
+
+  identity {
+
+  }
+}
