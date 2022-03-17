4192f96073ba6bf2926a5d0370373cb3a8bacabe
Igor Beliakov <demtis.register@gmail.com>
2022-03-17 14:44:32 +0100

    feat: add support for terraform AKS role_based_access_control_enabled
    Signed-off-by: Igor Beliakov <demtis.register@gmail.com>

--- checkov/terraform/checks/resource/azure/AKSRbacEnabled.py
+++ checkov/terraform/checks/resource/azure/AKSRbacEnabled.py
@@ -1,17 +1,37 @@
-from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+import dpath.util
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
-class AKSRbacEnabled(BaseResourceValueCheck):
+class AKSRbacEnabled(BaseResourceCheck):
     def __init__(self):
         name = "Ensure RBAC is enabled on AKS clusters"
         id = "CKV_AZURE_5"
-        supported_resources = ['azurerm_kubernetes_cluster']
+        supported_resources = ["azurerm_kubernetes_cluster"]
         categories = [CheckCategories.KUBERNETES]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+        )
 
-    def get_inspected_key(self):
-        return 'role_based_access_control/[0]/enabled'
+    def scan_resource_conf(self, conf):
+        # azurerm < 2.99.0
+        inspected_key1 = "role_based_access_control/[0]/enabled"
+        # azurerm >= 2.99.0
+        inspected_key2 = "role_based_access_control_enabled"
+        rbac_enabled = False
+
+        if dpath.search(conf, inspected_key1) != {}:
+            rbac_enabled = dpath.get(conf, inspected_key1)[0]
+        elif dpath.search(conf, inspected_key2) != {}:
+            rbac_enabled = dpath.get(conf, inspected_key2)[0]
+
+        if rbac_enabled:
+            return CheckResult.PASSED
+
+        return CheckResult.FAILED
 
 
 check = AKSRbacEnabled()
