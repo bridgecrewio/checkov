871a00e25688f0a7e924372506c786ff4faf0851
achiagit <achiar99@gmail.com>
2022-03-24 20:21:55 +0200

    rerun

--- checkov/common/checks_infra/solvers/connections_solvers/or_connection_solver.py
+++ checkov/common/checks_infra/solvers/connections_solvers/or_connection_solver.py
@@ -1,7 +1,7 @@
 from typing import Optional, List, Tuple, Dict, Any
 
 from networkx.classes.digraph import DiGraph
-from checkov.common.graph.checks_infra.enums import SolverType, Operators
+from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.graph.checks_infra.solvers.base_solver import BaseSolver
 from checkov.common.checks_infra.solvers.connections_solvers.base_connection_solver import BaseConnectionSolver
 from checkov.common.checks_infra.solvers.connections_solvers.complex_connection_solver import ComplexConnectionSolver
