20097bb8b5c237bfb6c5628b891ad60ca0449870
Saar Ettinger <saarettinger@gmail.com>
2022-03-31 11:14:12 +0300

    Added types

--- checkov/openapi/runner.py
+++ checkov/openapi/runner.py
@@ -1,20 +1,20 @@
+from checkov.common.checks.base_check_registry import BaseCheckRegistry
 from checkov.common.output.report import CheckType, Report
 from checkov.common.runners.object_runner import Runner as ObjectRunner
-from checkov.runner_filter import RunnerFilter
 
 
 class Runner(ObjectRunner):
     check_type = CheckType.OPENAPI
 
-    def import_registry(self):
+    def import_registry(self) -> BaseCheckRegistry:
         from checkov.openapi.checks.registry import openapi_registry
         return openapi_registry
 
-    def _parse_file(self, f):
+    def _parse_file(self, f) -> None:
         raise Exception("parser should be implemented")
 
-    def get_start_end_lines(self, end, result_config, start):
+    def get_start_end_lines(self, end, result_config, start) -> None:
         raise Exception("get_start_end_lines should be implemented")
 
-    def require_external_checks(self):
+    def require_external_checks(self) -> bool:
         return False
