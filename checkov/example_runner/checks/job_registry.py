ffd1670c8447a0075cffb5bdd157bb18220a628e
eurogig <eurogig@gmail.com>
2022-03-31 11:16:00 +0100

    first draft of example IaC runner code

---
+++ checkov/example_runner/checks/job_registry.py
@@ -0,0 +1,7 @@
+#
+# No change required normally except to possibly switch it to json
+# eg. from checkov.json_doc.base_registry import Registry
+# 
+from checkov.yaml_doc.base_registry import Registry
+
+registry = Registry()
