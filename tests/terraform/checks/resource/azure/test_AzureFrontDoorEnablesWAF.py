25ec76378763f5692b8fdbcf1fc15b70f94eb051
Taylor <28880387+tsmithv11@users.noreply.github.com>
2022-04-01 17:33:52 -0700

    Update tests/terraform/checks/resource/azure/test_AzureFrontDoorEnablesWAF.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- tests/terraform/checks/resource/azure/test_AzureFrontDoorEnablesWAF.py
+++ tests/terraform/checks/resource/azure/test_AzureFrontDoorEnablesWAF.py
@@ -6,7 +6,7 @@ from checkov.terraform.runner import Runner
 from checkov.terraform.checks.resource.azure.AzureFrontDoorEnablesWAF import check
 
 
-class TestOSSBucketAccessLogs(unittest.TestCase):
+class TestAzureFrontDoorEnablesWAF(unittest.TestCase):
 
     def test(self):
         runner = Runner()
