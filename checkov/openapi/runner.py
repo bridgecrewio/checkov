f619935583caacf4c67c5b4893b5cb507c8d9d3e
ajbara <ajbara@paloaltonetworks.com>
2022-04-03 12:34:35 +0300

    Added parser

--- checkov/openapi/runner.py
+++ checkov/openapi/runner.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Dict, Union, Any
 
 from checkov.common.checks.base_check_registry import BaseCheckRegistry
@@ -6,6 +7,7 @@ from checkov.yaml_doc.runner import Runner as YamlRunner
 from checkov.json_doc.runner import Runner as JsonRunner
 
 
+logger = logging.getLogger(__name__)
 class Runner(YamlRunner, JsonRunner):
     check_type = CheckType.OPENAPI
 
@@ -14,9 +16,16 @@ class Runner(YamlRunner, JsonRunner):
         return openapi_registry
 
     def _parse_file(self, f: str) -> None:
-        raise Exception("parser should be implemented")
+        if f.endswith(".json"):
+            return JsonRunner._parse_file(self, f)
+        elif f.endswith(".yml") or f.endswith(".yaml"):
+            return YamlRunner._parse_file(self, f)
+        else:
+            logger.warn(f'file {f} is not json or yaml.')
 
-    def get_start_end_lines(self, end: int, result_config: Union[List[Dict[str, Any]], List[Dict[str, Any]]], start: int) -> None:
+
+    def get_start_end_lines(self, end: int, result_config: Union[List[Dict[str, Any]], List[Dict[str, Any]]],
+                            start: int) -> None:
         raise Exception("get_start_end_lines should be implemented")
 
     def require_external_checks(self) -> bool:
