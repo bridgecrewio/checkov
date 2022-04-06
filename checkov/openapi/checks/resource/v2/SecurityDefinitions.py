539b89954ec5617b04c37e133c2c6a9a6dd23768
ajbara <ajbara@paloaltonetworks.com>
2022-04-06 13:24:17 +0300

    Fixed SecurityDefinitions.py format.

--- checkov/openapi/checks/resource/v2/SecurityDefinitions.py
+++ checkov/openapi/checks/resource/v2/SecurityDefinitions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Dict, Any
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.common.checks.enums import BlockType
@@ -7,15 +9,20 @@ from checkov.openapi.checks.base_openapi_check import BaseOpenapiCheck
 class SecurityDefinitions(BaseOpenapiCheck):
     def __init__(self) -> None:
         id = "CKV_OPENAPI_1"
-        name = "Ensure that securityDefinitions has defined and not empty."
+        name = "Ensure that securityDefinitions is defined and not empty."
         categories = [CheckCategories.NETWORKING]
         supported_resources = ['securityDefinitions']
         super().__init__(name=name, id=id, categories=categories, supported_entities=supported_resources,
                          block_type=BlockType.DOCUMENT)
 
-    def scan_entity_conf(self, conf: Dict[str, Any], entity_type: str) -> CheckResult:
-        self.evaluated_container_keys = ["securityDefinitions"]
+    def scan_entity_conf(self, conf: Dict[str, Any]) -> tuple[CheckResult, dict[str, Any] | None]:
+        self.evaluated_keys = ["securityDefinitions"]
+        if "securityDefinitions" not in conf:
+            return CheckResult.FAILED, conf
 
-        return CheckResult.FAILED if "securityDefinitions" not in conf or not conf["securityDefinitions"] else CheckResult.PASSED
+        content = conf["securityDefinitions"]
+        if not content or ('__startline__' in content and len(content) <= 2):
+            return CheckResult.FAILED, content
+        return CheckResult.PASSED, content
 
 check = SecurityDefinitions()
