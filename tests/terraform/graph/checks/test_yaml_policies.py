e7165259eb3b1ad47fcb3dbd8b6b37c8eb2151b5
nazoulay <noazoulay1@gmail.com>
2022-04-10 13:34:26 +0300

    new solver - connection_one_exists

--- tests/terraform/graph/checks/test_yaml_policies.py
+++ tests/terraform/graph/checks/test_yaml_policies.py
@@ -24,6 +24,9 @@ class TestYamlPolicies(unittest.TestCase):
     def test_VPCHasFlowLog(self):
         self.go("VPCHasFlowLog")
 
+    def test_VPCHasOneOfWantedFlowLogs(self):
+        self.go("VPCHasOneOfWantedFlowLogs")
+
     def test_VPCHasRestrictedSG(self):
         self.go("VPCHasRestrictedSG")
 
