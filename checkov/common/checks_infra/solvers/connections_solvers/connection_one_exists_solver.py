43ce7a34d9e4fe06dec6f3c6d30d56db9ff6019e
Noa Azoulay <noazoulay1@gmail.com>
2022-04-10 12:49:46 +0300

    Update connection_one_exists_solver.py

--- checkov/common/checks_infra/solvers/connections_solvers/connection_one_exists_solver.py
+++ checkov/common/checks_infra/solvers/connections_solvers/connection_one_exists_solver.py
@@ -24,4 +24,4 @@ class ConnectionOneExistsSolver(ConnectionExistsSolver):
         passed, failed = super().get_operation(graph_connector)
         failed = [f for f in failed if f not in passed]
         return passed, failed
-
+    
