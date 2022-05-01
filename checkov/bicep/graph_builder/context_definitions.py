1afdf54b7537b3ccb974e9ad4418d8cddb543b74
pelegli <peleg.litvak@gmail.com>
2022-05-01 09:58:25 +0300

    fixed PR comments

--- checkov/bicep/graph_builder/context_definitions.py
+++ checkov/bicep/graph_builder/context_definitions.py
@@ -5,14 +5,12 @@ from checkov.common.util.suppression import collect_suppressions_for_report
 
 from pycep.typing import BicepJson
 
-
 BICEP_COMMENT = "//"
 DEFINITIONS_KEYS_TO_PARSE = {"parameters": "parameters", "resources": "resources"}
 
 
-def build_definitions_context(
-        definitions: Dict[Path, BicepJson], definitions_raw: Dict[Path, List[Tuple[int, str]]]
-) -> Dict[str, Dict[str, Any]]:
+def build_definitions_context(definitions: Dict[Path, BicepJson], definitions_raw: Dict[Path, List[Tuple[int, str]]]
+                              ) -> Dict[str, Dict[str, Any]]:
     definitions_context: Dict[str, Dict[str, Any]] = {}
     for file_path_object, file_path_definitions in definitions.items():
         file_path = str(file_path_object)
@@ -22,9 +20,8 @@ def build_definitions_context(
                 continue
             definitions_context[file_path][definition_attribute] = {}
             # ignore mypy mismatched type warning since it can't resolve this type correctly
-            for resource_key, resource_attributes in resources.items():  # type: ignore
-                definition_resource = {"start_line": resource_attributes["__start_line__"],
-                                       "end_line": resource_attributes["__end_line__"]}
+            for resource_key, resource_attributes in resources.items():  # type:ignore[attr-defined]
+                definition_resource = {"start_line": resource_attributes["__start_line__"], "end_line": resource_attributes["__end_line__"]}
 
                 if definition_attribute == DEFINITIONS_KEYS_TO_PARSE["resources"]:
                     definition_key = f"{resource_attributes['type']}.{resource_key}"
@@ -32,33 +29,24 @@ def build_definitions_context(
                     int_end_line = cast(int, definition_resource["end_line"])
                     code_lines_for_suppressions_check = definitions_raw[file_path_object][int_start_line: int_end_line]
                     definition_resource['skipped_checks'] = collect_suppressions_for_report(code_lines=code_lines_for_suppressions_check)
-                if definition_attribute == DEFINITIONS_KEYS_TO_PARSE["parameters"]:
+                elif definition_attribute == DEFINITIONS_KEYS_TO_PARSE["parameters"]:
                     definition_key = resource_key
                     definition_resource["type"] = resource_attributes['type']
 
                 start_line = resource_attributes["__start_line__"]
-                end_line = min(resource_attributes["__end_line__"], len(definitions_raw[file_path_object]))
-                first_line_index = 0
-                # skip empty lines
-                while not str.strip(definitions_raw[file_path_object][first_line_index][1]):
-                    first_line_index += 1
+                end_line = resource_attributes["__end_line__"]
 
-                # check if the file is a json file
-                if str.strip(definitions_raw[file_path_object][first_line_index][1])[0] == "{":
-                    start_line += 1
-                    end_line += 1
-                else:
-                    # add resource comments to definition lines
+                # add resource comments to definition lines
+                current_line = str.strip(definitions_raw[file_path_object][start_line - 1][1])
+                while not current_line or current_line[0] == BICEP_COMMENT:
+                    start_line -= 1
                     current_line = str.strip(definitions_raw[file_path_object][start_line - 1][1])
-                    while not current_line or current_line[0] == BICEP_COMMENT:
-                        start_line -= 1
-                        current_line = str.strip(definitions_raw[file_path_object][start_line - 1][1])
 
-                    # remove next resource comments from definition lines
+                # remove next resource comments from definition lines
+                current_line = str.strip(definitions_raw[file_path_object][end_line - 1][1])
+                while not current_line or current_line[0] == BICEP_COMMENT:
+                    end_line -= 1
                     current_line = str.strip(definitions_raw[file_path_object][end_line - 1][1])
-                    while not current_line or current_line[0] == BICEP_COMMENT:
-                        end_line -= 1
-                        current_line = str.strip(definitions_raw[file_path_object][end_line - 1][1])
 
                 definition_resource["code_lines"] = definitions_raw[file_path_object][start_line - 1: end_line]
                 definitions_context[file_path][definition_attribute][definition_key] = definition_resource
