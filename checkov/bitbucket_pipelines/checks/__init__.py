1291b76218b97384d0c292dd856f60e505f7bbd5
barak <schosterbarak@gmail.com>
2022-05-03 09:53:28 +0300

    fix tests

--- checkov/bitbucket_pipelines/checks/__init__.py
+++ checkov/bitbucket_pipelines/checks/__init__.py
@@ -1,1 +1,4 @@
-from checkov.bitbucket_pipelines.checks.job import *  # noqa
+from pathlib import Path
+
+modules = Path(__file__).parent.glob("*.py")
+__all__ = [f.stem for f in modules if f.is_file() and not f.stem == "__init__"]
