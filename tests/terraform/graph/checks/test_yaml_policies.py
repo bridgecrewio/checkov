0532fd015349d417f887c058c57423371dc2b99f
James Woolfenden <james.woolfenden@gmail.com>
2022-05-05 10:01:19 +0100

    Min 2 approving reviews for a PR

--- tests/terraform/graph/checks/test_yaml_policies.py
+++ tests/terraform/graph/checks/test_yaml_policies.py
@@ -23,6 +23,9 @@ class TestYamlPolicies(unittest.TestCase):
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+    def test_ADORepositoryHasMinTwoReviewers(self):
+        self.go("ADORepositoryHasMinTwoReviewers")
+
     def test_RepositoryHasBranchProtection(self):
         self.go("RepositoryHasBranchProtection")
 
