c42a0218c759cced2bc144809f840c271d64e0b3
James Woolfenden <james.woolfenden@gmail.com>
2022-05-05 16:26:06 +0100

    Reject unsigned commits (#2932)

---
+++ tests/terraform/checks/resource/gitlab/example_RejectUnsignedCommits/main.tf
@@ -0,0 +1,34 @@
+resource "gitlab_project" "fail" {
+  name = "example-two"
+
+  push_rules {
+    author_email_regex     = "@example\\.com$"
+    commit_committer_check = true
+    member_check           = true
+    prevent_secrets        = true
+  }
+}
+
+resource "gitlab_project" "fail2" {
+  name = "example-two"
+
+  push_rules {
+    author_email_regex     = "@example\\.com$"
+    commit_committer_check = true
+    member_check           = true
+    prevent_secrets        = true
+    reject_unsigned_commits = false
+  }
+}
+
+resource "gitlab_project" "pass" {
+  name = "example-two"
+
+  push_rules {
+    author_email_regex     = "@example\\.com$"
+    commit_committer_check = true
+    member_check           = true
+    prevent_secrets        = true
+    reject_unsigned_commits = true
+  }
+}
