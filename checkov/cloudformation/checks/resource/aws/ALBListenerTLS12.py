b9b4b36290756eb78744a4f1a55ace1f718964d4
Andreas Kohn (Framer) <andreas.kohn@framer.com>
2022-05-05 20:05:09 +0200

    Allow using TLS 1.3 policies with NLBs
    Fixes: #2928
    Signed-off-by: Andreas Kohn (Framer) <andreas.kohn@framer.com>

--- checkov/cloudformation/checks/resource/aws/ALBListenerTLS12.py
+++ checkov/cloudformation/checks/resource/aws/ALBListenerTLS12.py
@@ -3,11 +3,17 @@ from checkov.cloudformation.checks.resource.base_resource_check import BaseResou
 from checkov.common.util.type_forcers import force_list
 from checkov.cloudformation.parser.cfn_keywords import ConditionFunctions, IntrinsicFunctions
 
+supported_policy_prefixes = {
+    # ALBs support TLS v1.2
+    'HTTPS': ("ELBSecurityPolicy-FS-1-2", "ELBSecurityPolicy-TLS-1-2"),
+    # NLBs support TLS v1.2 and 1.3
+    'TLS': ("ELBSecurityPolicy-TLS13-1-3-2021-06", "ELBSecurityPolicy-TLS13-1-2", "ELBSecurityPolicy-FS-1-2", "ELBSecurityPolicy-TLS-1-2")
+}
 
 class ALBListenerTLS12(BaseResourceCheck):
 
     def __init__(self):
-        name = "Ensure that Application Load Balancer Listener is using TLS v1.2"
+        name = "Ensure that Load Balancer Listener is using at least TLS v1.2"
         id = "CKV_AWS_103"
         supported_resources = ['AWS::ElasticLoadBalancingV2::Listener']
         categories = [CheckCategories.GENERAL_SECURITY]
@@ -15,7 +21,7 @@ class ALBListenerTLS12(BaseResourceCheck):
 
     def scan_resource_conf(self, conf):
         """
-            validates that ALB Listener is using TLS v1.2
+            validates that ElasticLoadBalancing V2 Listener is using at least TLS v1.2
             https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-elasticloadbalancingv2-listener.html
         :param conf: aws_alb_listener configuration
         :return: <CheckResult>
@@ -23,11 +29,12 @@ class ALBListenerTLS12(BaseResourceCheck):
 
         if 'Properties' in conf.keys():
             if 'Protocol' in conf['Properties'].keys():
-                # Check SslPolicy only if protocol is HTTPS or TLS.
-                # Other protocols are not intresting within the context of this check.
-                if conf['Properties']['Protocol'] in ('HTTPS', 'TLS'):
+                # Check SslPolicy only if protocol is HTTPS (ALB) or TLS (NLB).
+                # Other protocols are not interesting within the context of this check.
+                protocol = conf['Properties']['Protocol']
+                if protocol in ('HTTPS', 'TLS'):
                     if 'SslPolicy' in conf['Properties'].keys():
-                        if conf['Properties']['SslPolicy'].startswith(("ELBSecurityPolicy-FS-1-2", "ELBSecurityPolicy-TLS-1-2")):
+                        if conf['Properties']['SslPolicy'].startswith(supported_policy_prefixes[protocol]):
                             return CheckResult.PASSED
                     return CheckResult.FAILED
                 elif conf['Properties']['Protocol'] in ('TCP', 'UDP', 'TCP_UDP'):
