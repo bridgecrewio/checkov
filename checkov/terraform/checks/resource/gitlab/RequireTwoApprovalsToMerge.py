9b9bb0d9c293841291f33ed83f46b45cab929714
James Woolfenden <james.woolfenden@gmail.com>
2022-05-05 11:42:03 +0100

    git lab project require 2 approvers to merge

---
+++ checkov/terraform/checks/resource/gitlab/RequireTwoApprovalsToMerge.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+
+
+class RequireTwoApprovalsToMerge(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure at least two approving reviews to merge"
+        id = "CKV_GLB_1"
+        supported_resources = ["gitlab_project"]
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        if conf.get("approvals_before_merge") and isinstance(conf.get("approvals_before_merge"), list):
+            if conf.get("approvals_before_merge")[0] >= 2:
+                return CheckResult.PASSED
+        self.evaluated_keys = ["approvals_before_merge"]
+        return CheckResult.FAILED
+
+
+check = RequireTwoApprovalsToMerge()
