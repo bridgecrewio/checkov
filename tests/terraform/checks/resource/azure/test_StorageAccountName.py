9ebb95269cd095b5511b453e907d624d7e1a383d
Derek Moore <derek.moore@leaven.co.nz>
2022-05-06 15:15:40 +1200

    Add test for random_*

--- tests/terraform/checks/resource/azure/test_StorageAccountName.py
+++ tests/terraform/checks/resource/azure/test_StorageAccountName.py
@@ -34,7 +34,7 @@ class TestStorageAccountName(unittest.TestCase):
         self.assertEqual(summary["failed"], 3)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
-        self.assertEqual(summary["resource_count"], 8)  # 3 unknown
+        self.assertEqual(summary["resource_count"], 12)  # 7 unknown
 
         self.assertEqual(passing_resources, passed_check_resources)
         self.assertEqual(failing_resources, failed_check_resources)
