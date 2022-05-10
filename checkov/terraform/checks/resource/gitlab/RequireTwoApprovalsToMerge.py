203bdc5725ca2c72be5fe26005492261b7f713aa
tronxd <tron47@gmail.com>
2022-05-10 16:41:34 +0300

    use force_int on checks that compare numbers, protect against empty list K8 check

--- checkov/terraform/checks/resource/gitlab/RequireTwoApprovalsToMerge.py
+++ checkov/terraform/checks/resource/gitlab/RequireTwoApprovalsToMerge.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.common.util.type_forcers import force_int
 
 
 class RequireTwoApprovalsToMerge(BaseResourceCheck):
@@ -17,7 +18,7 @@ class RequireTwoApprovalsToMerge(BaseResourceCheck):
     def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
         approvals = conf.get("approvals_before_merge")
         if approvals and isinstance(approvals, list):
-            if approvals[0] >= 2:
+            if force_int(approvals[0]) >= 2:
                 return CheckResult.PASSED
         self.evaluated_keys = ["approvals_before_merge"]
         return CheckResult.FAILED
