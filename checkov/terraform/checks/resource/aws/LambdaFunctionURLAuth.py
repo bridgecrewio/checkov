a94f8d5791383987a60ec8f8047ef369a2d05b0e
ChanochShayner <57212002+ChanochShayner@users.noreply.github.com>
2022-05-23 17:12:18 +0300

    Apply suggestions from code review
    CR Fixes
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/terraform/checks/resource/aws/LambdaFunctionURLAuth.py
+++ checkov/terraform/checks/resource/aws/LambdaFunctionURLAuth.py
@@ -5,10 +5,10 @@ from checkov.common.models.enums import CheckCategories
 class LambdaFunctionURLAuth(BaseResourceNegativeValueCheck):
 
     def __init__(self):
-        name = "Ensure that lambda URL functions AuthType is not None"
+        name = "Ensure that Lambda function URLs AuthType is not None"
         id = "CKV_AWS_258"
         supported_resources = ['aws_lambda_function_url']
-        categories = [CheckCategories.ENCRYPTION]
+        categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
