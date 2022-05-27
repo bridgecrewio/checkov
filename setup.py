b9e697a854566ec57f251461612d16177c5d3997
gruebel <anton.gruebel@gmail.com>
2022-05-27 23:28:13 +0200

    support Python 3.11

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        'Programming Language :: Python :: 3.11',
         "Topic :: Security",
         "Topic :: Software Development :: Build Tools",
         "Typing :: Typed",
