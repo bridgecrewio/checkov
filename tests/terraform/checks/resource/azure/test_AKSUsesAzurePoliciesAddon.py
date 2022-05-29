958f2e0f9fdf9cfeda88eae9200d81eb45e83761
Thomas Enderle <thomas.enderle@posteo.de>
2022-05-29 21:46:51 +0200

    fix CKV_AZURE_116 to reflect changes in v2.97.0 of Terraform Azure provider

--- tests/terraform/checks/resource/azure/test_AKSUsesAzurePoliciesAddon.py
+++ tests/terraform/checks/resource/azure/test_AKSUsesAzurePoliciesAddon.py
@@ -43,18 +43,84 @@ class TestAKSUsesAzurePoliciesAddon(unittest.TestCase):
                   resource_group_name = azurerm_resource_group.example.name
                   dns_prefix          = "exampleaks1"
 
-                  azure_policy_enabled = false
-                  
+                  addon_profile {
+                    azure_policy {
+                      enabled = false
+                    }
+                  }
+
                   default_node_pool {
                     name       = "default"
                     node_count = 1
                     vm_size    = "Standard_D2_v2"
                   }
-                
+
                   identity {
                     type = "SystemAssigned"
                   }
-                
+
+                  tags = {
+                    Environment = "Production"
+                  }
+                }
+        """)
+        resource_conf = hcl_res['resource'][0]['azurerm_kubernetes_cluster']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+
+    def test_failure3(self):
+        hcl_res = hcl2.loads("""
+                resource "azurerm_kubernetes_cluster" "example" {
+                  name                = "example-aks1"
+                  location            = azurerm_resource_group.example.location
+                  resource_group_name = azurerm_resource_group.example.name
+                  dns_prefix          = "exampleaks1"
+
+                  azure_policy {
+                    enabled = true
+                  }
+
+                  default_node_pool {
+                    name       = "default"
+                    node_count = 1
+                    vm_size    = "Standard_D2_v2"
+                  }
+
+                  identity {
+                    type = "SystemAssigned"
+                  }
+
+                  tags = {
+                    Environment = "Production"
+                  }
+                }
+        """)
+        resource_conf = hcl_res['resource'][0]['azurerm_kubernetes_cluster']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+
+    def test_failure4(self):
+        hcl_res = hcl2.loads("""
+                resource "azurerm_kubernetes_cluster" "example" {
+                  name                = "example-aks1"
+                  location            = azurerm_resource_group.example.location
+                  resource_group_name = azurerm_resource_group.example.name
+                  dns_prefix          = "exampleaks1"
+
+                  azure_policy_enabled = false
+
+                  default_node_pool {
+                    name       = "default"
+                    node_count = 1
+                    vm_size    = "Standard_D2_v2"
+                  }
+
+                  identity {
+                    type = "SystemAssigned"
+                  }
+
                   tags = {
                     Environment = "Production"
                   }
@@ -66,6 +132,40 @@ class TestAKSUsesAzurePoliciesAddon(unittest.TestCase):
 
 
     def test_success(self):
+        hcl_res = hcl2.loads("""
+                resource "azurerm_kubernetes_cluster" "example" {
+                  name                = "example-aks1"
+                  location            = azurerm_resource_group.example.location
+                  resource_group_name = azurerm_resource_group.example.name
+                  dns_prefix          = "exampleaks1"
+                  
+                  addon_profile {
+                    azure_policy {
+                      enabled = true
+                    }
+                  }
+
+                  default_node_pool {
+                    name       = "default"
+                    node_count = 1
+                    vm_size    = "Standard_D2_v2"
+                  }
+
+                  identity {
+                    type = "SystemAssigned"
+                  }
+
+                  tags = {
+                    Environment = "Production"
+                  }
+                }
+        """)
+        resource_conf = hcl_res['resource'][0]['azurerm_kubernetes_cluster']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+
+    def test_success2(self):
         hcl_res = hcl2.loads("""
                 resource "azurerm_kubernetes_cluster" "example" {
                   name                = "example-aks1"
