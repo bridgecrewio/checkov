ba03db6e83311d1af15c82e8bda66ac7dda0e996
YaaraVerner <86768411+YaaraVerner@users.noreply.github.com>
2022-05-31 15:54:32 +0300

    refactor terraform plan runner (#3032)
    * refactor terraform plan runner
    
    * fix test
    
    * add graph initializers
    
    * refactor to one runner
    
    * add plan_utils and create definitions context function
    
    * change plan_parser to return the parsed definition

--- tests/terraform/parser/test_plan_parser.py
+++ tests/terraform/parser/test_plan_parser.py
@@ -8,8 +8,8 @@ class TestPlanFileParser(unittest.TestCase):
     def test_tags_values_are_flattened(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_tags/tfplan.json"
-        tf_definitions, _ = parse_tf_plan(valid_plan_path, {})
-        file_resource_definition = next(iter(tf_definitions.values()))['resource'][0]
+        tf_definition, _ = parse_tf_plan(valid_plan_path, {})
+        file_resource_definition = tf_definition['resource'][0]
         resource_definition = next(iter(file_resource_definition.values()))
         resource_attributes = next(iter(resource_definition.values()))
         resource_tags = resource_attributes['tags'][0]
@@ -20,7 +20,7 @@ class TestPlanFileParser(unittest.TestCase):
     def test_more_tags_values_are_flattened(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_tags_variety/tfplan.json"
-        tf_definitions, _ = parse_tf_plan(valid_plan_path, {})
+        tf_definition, _ = parse_tf_plan(valid_plan_path, {})
         # TODO: this should also verify the flattening but at least shows it parses now.
         assert True
 
@@ -29,8 +29,8 @@ class TestPlanFileParser(unittest.TestCase):
     def test_simple_type_booleans_clean(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         valid_plan_path = current_dir + "/resources/plan_booleans/tfplan.json"
-        tf_definitions, _ = parse_tf_plan(valid_plan_path, {})
-        file_resource_definition = next(iter(tf_definitions.values()))['resource'][0]
+        tf_definition, _ = parse_tf_plan(valid_plan_path, {})
+        file_resource_definition = tf_definition['resource'][0]
         resource_definition = next(iter(file_resource_definition.values()))
         resource_attributes = next(iter(resource_definition.values()))
         self.assertTrue(resource_attributes['metadata'][0]['a'][0])
