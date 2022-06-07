c2c7c0991a12f69fa1ca734e2416c7ea0b661fc2
Girondel Etienne <egirondel@oui.sncf>
2022-06-07 16:45:51 +0200

    Add proxy authentication headers

--- checkov/common/bridgecrew/platform_integration.py
+++ checkov/common/bridgecrew/platform_integration.py
@@ -157,13 +157,17 @@ class BcPlatformIntegration:
         if ca_certificate:
             os.environ['REQUESTS_CA_BUNDLE'] = ca_certificate
             try:
+                parsed_url = urllib3.util.parse_url(os.environ['https_proxy'])
                 self.http = urllib3.ProxyManager(os.environ['https_proxy'], cert_reqs='REQUIRED',
-                                                 ca_certs=ca_certificate)
+                                                 ca_certs=ca_certificate,
+                                                 proxy_headers=urllib3.make_headers(proxy_basic_auth=parsed_url.auth))
             except KeyError:
                 self.http = urllib3.PoolManager(cert_reqs='REQUIRED', ca_certs=ca_certificate)
         else:
             try:
-                self.http = urllib3.ProxyManager(os.environ['https_proxy'])
+                parsed_url = urllib3.util.parse_url(os.environ['https_proxy'])
+                self.http = urllib3.ProxyManager(os.environ['https_proxy'],
+                                                 proxy_headers=urllib3.make_headers(proxy_basic_auth=parsed_url.auth))
             except KeyError:
                 self.http = urllib3.PoolManager()
 
