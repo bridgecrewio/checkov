9e5fc56f1bf32be657c644f35323f8eca6d3b632
Mike Urbanski <mike@bridgecrew.io>
2022-06-15 13:48:25 -0500

    add tests for unrendered vars

--- tests/terraform/graph/checks_infra/attribute_solvers/greater_than_solver/test_solver.py
+++ tests/terraform/graph/checks_infra/attribute_solvers/greater_than_solver/test_solver.py
@@ -57,6 +57,42 @@ class TestGreaterThanLessThanSolvers(TestBaseSolver):
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
 
+    def test_greater_than_solver_unrendered(self):
+        root_folder = '../../../resources/variable_rendering/unrendered'
+        check_id = "GT"
+        should_pass = ['aws_s3_bucket.pass1', 'aws_s3_bucket.pass2', 'aws_s3_bucket.pass3']
+        should_fail = []
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_less_than_solver_unrendered(self):
+        root_folder = '../../../resources/variable_rendering/unrendered'
+        check_id = "LT"
+        should_pass = ['aws_s3_bucket.pass1', 'aws_s3_bucket.pass2', 'aws_s3_bucket.pass3']
+        should_fail = []
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_greater_than_or_equal_solver_unrendered(self):
+        root_folder = '../../../resources/variable_rendering/unrendered'
+        check_id = "GTE"
+        should_pass = ['aws_s3_bucket.pass1', 'aws_s3_bucket.pass2', 'aws_s3_bucket.pass3']
+        should_fail = []
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_less_than_or_equal_solver_unrendered(self):
+        root_folder = '../../../resources/variable_rendering/unrendered'
+        check_id = "LTE"
+        should_pass = ['aws_s3_bucket.pass1', 'aws_s3_bucket.pass2', 'aws_s3_bucket.pass3']
+        should_fail = []
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
     def test_gt_combinations(self):
         cls = GreaterThanAttributeSolver
 
