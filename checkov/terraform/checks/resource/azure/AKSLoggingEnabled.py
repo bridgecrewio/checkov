d0fb8b253d1c88b5b8fffb2f1f1bf41062bee137
lonegunmanb <lonegunmanb@hotmail.com>
2022-07-04 20:51:53 +0800

    Make `CKV_AZURE_4` check work for AzureRM 3.0 syntax to fix #3159 (#3160)
    * Make `CKV_AZURE_4` check work for AzureRM 3.0 syntax to fix #3159
    
    * add checked path to self.evaluated_keys
    
    * Revert "add checked path to self.evaluated_keys"
    
    This reverts commit 08eb3d4e08d404491fd2134997df464119df85f0.
    
    * Make `CKV_AZURE_4` check work for AzureRM 3.0 syntax to fix #3159
    
    * rename variable due to the review comment.
    
    Co-authored-by: zjhe <hezijie@microsoft.com>

--- checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
+++ checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
@@ -1,8 +1,10 @@
-from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+import dpath
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
 
 
-class AKSLoggingEnabled(BaseResourceValueCheck):
+class AKSLoggingEnabled(BaseResourceCheck):
     def __init__(self):
         name = "Ensure AKS logging to Azure Monitoring is Configured"
         id = "CKV_AZURE_4"
@@ -10,8 +12,17 @@ class AKSLoggingEnabled(BaseResourceValueCheck):
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return "addon_profile/[0]/oms_agent/[0]/enabled"
+    def scan_resource_conf(self, conf):
+        provider_version_2_path = "addon_profile/[0]/oms_agent/[0]/enabled"
+        provider_version_3_path = "oms_agent/[0]"
+        if dpath.search(conf, provider_version_2_path) and dpath.get(conf, provider_version_2_path)[0]:
+            self.evaluated_keys = [provider_version_2_path]
+            return CheckResult.PASSED
+        elif dpath.search(conf, provider_version_3_path):
+            self.evaluated_keys = [provider_version_3_path]
+            return CheckResult.PASSED
+
+        return CheckResult.FAILED
 
 
 check = AKSLoggingEnabled()
