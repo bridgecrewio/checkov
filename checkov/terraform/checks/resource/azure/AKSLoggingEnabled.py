ef2d0325af7ceccb4bc681d2b2024ca72b17a6a5
hezijie <lonegunmanb@hotmail.com>
2022-07-04 14:44:24 +0800

    Make `CKV_AZURE_4` check work for AzureRM 3.0 syntax to fix #3159

--- checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
+++ checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
@@ -1,8 +1,10 @@
-from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+import dpath
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
 
 
-class AKSLoggingEnabled(BaseResourceValueCheck):
+class AKSLoggingEnabled(BaseResourceCheck):
     def __init__(self):
         name = "Ensure AKS logging to Azure Monitoring is Configured"
         id = "CKV_AZURE_4"
@@ -10,8 +12,15 @@ class AKSLoggingEnabled(BaseResourceValueCheck):
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return "addon_profile/[0]/oms_agent/[0]/enabled"
+    def scan_resource_conf(self, conf):
+        two_point_o_path = "addon_profile/[0]/oms_agent/[0]/enabled"
+        three_point_o_path = "oms_agent/[0]"
+        if dpath.search(conf, two_point_o_path) and dpath.get(conf, two_point_o_path)[0]:
+            return CheckResult.PASSED
+        elif dpath.search(conf, three_point_o_path):
+            return CheckResult.PASSED
+
+        return CheckResult.FAILED
 
 
 check = AKSLoggingEnabled()
