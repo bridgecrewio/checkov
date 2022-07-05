95f133ba62455c2633c57248e261e863afe35611
tronxd <tron47@gmail.com>
2022-07-05 13:30:44 +0300

    support AKSLoggingEnabled to use base resource value check in case the azure provider is version 2.x

--- checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
+++ checkov/terraform/checks/resource/azure/AKSLoggingEnabled.py
@@ -1,28 +1,31 @@
 import dpath
 
 from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class AKSLoggingEnabled(BaseResourceCheck):
+class AKSLoggingEnabled(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure AKS logging to Azure Monitoring is Configured"
         id = "CKV_AZURE_4"
         supported_resources = ['azurerm_kubernetes_cluster']
         categories = [CheckCategories.KUBERNETES]
+        self.provider_version_2_path = "addon_profile/[0]/oms_agent/[0]/enabled"
+        self.provider_version_3_path = "oms_agent/[0]"
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        provider_version_2_path = "addon_profile/[0]/oms_agent/[0]/enabled"
-        provider_version_3_path = "oms_agent/[0]"
-        if dpath.search(conf, provider_version_2_path) and dpath.get(conf, provider_version_2_path)[0]:
-            self.evaluated_keys = [provider_version_2_path]
-            return CheckResult.PASSED
-        elif dpath.search(conf, provider_version_3_path):
-            self.evaluated_keys = [provider_version_3_path]
+        if dpath.search(conf, self.provider_version_2_path):
+            self.evaluated_keys = [self.provider_version_2_path]
+            return super().scan_resource_conf(conf)
+        elif dpath.search(conf, self.provider_version_3_path):
+            self.evaluated_keys = [self.provider_version_3_path]
             return CheckResult.PASSED
 
         return CheckResult.FAILED
+
+    def get_inspected_key(self) -> str:
+        return self.provider_version_2_path
 
 
 check = AKSLoggingEnabled()
