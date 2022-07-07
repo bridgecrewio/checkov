b0b9274543aa6e5a7f669c0d689d97a8a422fb19
Mike Urbanski <mike@bridgecrew.io>
2022-07-07 12:48:18 -0500

    do not populate BC ID on local graph checks

--- checkov/common/checks_infra/checks_parser.py
+++ checkov/common/checks_infra/checks_parser.py
@@ -116,7 +116,6 @@ class NXGraphCheckParser(BaseGraphCheckParser):
         policy_definition = raw_check.get("definition", {})
         check = self._parse_raw_check(policy_definition, kwargs.get("resources_types"))
         check.id = raw_check.get("metadata", {}).get("id", "")
-        check.bc_id = raw_check.get("metadata", {}).get("id", "")
         check.name = raw_check.get("metadata", {}).get("name", "")
         check.category = raw_check.get("metadata", {}).get("category", "")
         check.frameworks = raw_check.get("metadata", {}).get("frameworks", [])
