c3cee5f1d7f3ddc9e0333ae26acaef3f06c3ca1b
James Woolfenden <james.woolfenden@gmail.com>
2022-07-08 14:31:20 +0100

    ACL should not allow 0.0.0.0/0

--- checkov/terraform/checks/resource/alicloud/ALBACLIsUnrestricted.py
+++ checkov/terraform/checks/resource/alicloud/ALBACLIsUnrestricted.py
@@ -2,19 +2,19 @@ from checkov.common.models.enums import CheckCategories
 from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class OSSBucketPublic(BaseResourceNegativeValueCheck):
+class ALBACLIsUnrestricted(BaseResourceNegativeValueCheck):
     def __init__(self):
-        name = "Alibaba Cloud OSS bucket accessible to public"
-        id = "CKV_ALI_1"
-        supported_resources = ['alicloud_oss_bucket']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        name = "Alibaba ALB ACL does not restrict Access"
+        id = "CKV_ALI_29"
+        supported_resources = ['alicloud_alb_acl_entry_attachment']
+        categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
-        return 'acl'
+        return 'entry'
 
     def get_forbidden_values(self):
-        return ["public-read", "public-read-write"]
+        return ["0.0.0.0/0"]
 
 
-check = OSSBucketPublic()
+check = ALBACLIsUnrestricted()
