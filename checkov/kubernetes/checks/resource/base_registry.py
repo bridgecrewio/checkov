bfe89b27c4831ab8120ca23598b232ea3e0b322e
Mike Urbanski <mike@bridgecrew.io>
2022-07-14 15:46:45 -0500

    fix k8s enforcement rule filtering

--- checkov/kubernetes/checks/resource/base_registry.py
+++ checkov/kubernetes/checks/resource/base_registry.py
@@ -3,8 +3,8 @@ from checkov.common.checks.base_check_registry import BaseCheckRegistry
 
 
 class Registry(BaseCheckRegistry):
-    def __init__(self):
-        super().__init__(CheckType.KUBERNETES)
+    def __init__(self, report_type: str):
+        super().__init__(report_type)
 
     def extract_entity_details(self, entity):
         kind = entity["kind"]
@@ -21,7 +21,7 @@ class Registry(BaseCheckRegistry):
                 if check.id in [x['id'] for x in skipped_checks]:
                     skip_info = [x for x in skipped_checks if x['id'] == check.id][0]
 
-            if self._should_run_scan(check, entity_configuration, runner_filter):
+            if self._should_run_scan(check, entity_configuration, runner_filter, self.report_type):
                 self.logger.debug("Running check: {} on file {}".format(check.name, scanned_file))
 
                 result = check.run(scanned_file=scanned_file, entity_configuration=entity_configuration,
@@ -30,7 +30,7 @@ class Registry(BaseCheckRegistry):
         return results
 
     @staticmethod
-    def _should_run_scan(check, entity_configuration, runner_filter):
+    def _should_run_scan(check, entity_configuration, runner_filter, report_type):
         check_id_allowlist = runner_filter.checks
         check_id_denylist = runner_filter.skip_checks
         if check_id_allowlist or runner_filter.check_threshold:
@@ -67,7 +67,7 @@ class Registry(BaseCheckRegistry):
                         run_check = True
             if run_check:
                 return True
-        elif check_id_denylist or runner_filter.skip_check_threshold:
+        elif check_id_denylist or runner_filter.skip_check_threshold or runner_filter.use_enforcement_rules:
             namespace_skip = False
             if "metadata" in entity_configuration and "namespace" in entity_configuration["metadata"]:
                 if entity_configuration["metadata"]["namespace"] in check_id_denylist:
@@ -78,7 +78,7 @@ class Registry(BaseCheckRegistry):
             else:
                 if "default" in check_id_denylist:
                     namespace_skip = True
-            if runner_filter.should_run_check(check=check) and not namespace_skip:
+            if runner_filter.should_run_check(check=check, report_type=report_type) and not namespace_skip:
                 return True
         else:
             return True
