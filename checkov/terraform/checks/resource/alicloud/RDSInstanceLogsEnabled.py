e4a697951ae00ab90a831b9fec084308d33a2562
James Woolfenden <james.woolfenden@gmail.com>
2022-07-15 11:12:16 +0100

    Use own class

--- checkov/terraform/checks/resource/alicloud/RDSInstanceLogsEnabled.py
+++ checkov/terraform/checks/resource/alicloud/RDSInstanceLogsEnabled.py
@@ -1,22 +1,9 @@
-from checkov.common.models.enums import CheckCategories,CheckResult
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.alicloud.AbsRDSParameter import AbsRDSParameter
 
 
-class RDSInstanceLogsEnabled(BaseResourceCheck):
+class RDSInstanceLogsEnabled(AbsRDSParameter):
     def __init__(self):
-        name = "Ensure RDS Instance Parameters are set to enable logging"
-        id = "CKV_ALI_35"
-        supported_resources = ['alicloud_db_instance']
-        categories = [CheckCategories.LOGGING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-
-    def scan_resource_conf(self, conf) -> CheckResult:
-        if conf.get("parameters") and isinstance(conf.get("parameters"), list):
-            params = conf.get("parameters")
-            for param in params:
-                if param['name'][0] == "log_duration" and param['value'][0] == 'ON':
-                    return CheckResult.PASSED
-        return CheckResult.FAILED
+        super().__init__(check_id="CKV_ALI_35", parameter="log_duration")
 
 
 check = RDSInstanceLogsEnabled()
