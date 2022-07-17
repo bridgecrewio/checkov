90d5689b99a6603b2471e6a92bafb71a93c9417a
YaaraVerner <86768411+YaaraVerner@users.noreply.github.com>
2022-07-17 15:05:44 +0300

    fix edge case in LBTargetGroupDefinesHealthCheck (#3230)

--- tests/terraform/checks/resource/aws/example_LBTargetGroupDefinesHealthcheck/main.tf
+++ tests/terraform/checks/resource/aws/example_LBTargetGroupDefinesHealthcheck/main.tf
@@ -16,6 +16,13 @@ resource "aws_lb_target_group" "fail2" {
   vpc_id   = aws_vpc.main.id
 }
 
+resource "aws_lb_target_group" "fail3" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check = [{}]
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
