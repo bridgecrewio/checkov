b5e4f581ff51c56a8b1004c7d28446f6d5e1bb77
pelegli <peleg.litvak@gmail.com>
2022-07-24 17:51:24 +0300

    added some uts

--- tests/terraform/checks/resource/kubernetes/test_MemoryRequests.py
+++ tests/terraform/checks/resource/kubernetes/test_MemoryRequests.py
@@ -31,8 +31,8 @@ class TestMemoryRequests(unittest.TestCase):
         passed_check_resources = {c.resource for c in report.passed_checks}
         failed_check_resources = {c.resource for c in report.failed_checks}
 
-        self.assertEqual(summary["passed"], 1)
-        self.assertEqual(summary["failed"], 4)
+        self.assertEqual(summary["passed"], 2)
+        self.assertEqual(summary["failed"], 8)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
