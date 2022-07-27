acd1ec2905c3ff9d8bde0b4dc0cfd06e3d66c6c2
Tasha Marreiros <tasha.marreiros@appsbroker.com>
2022-07-27 17:05:52 +0100

    add in the additional condition and test

--- tests/terraform/graph/checks/resources/GCPAuditLogsConfiguredForAllServicesAndUsers/main.tf
+++ tests/terraform/graph/checks/resources/GCPAuditLogsConfiguredForAllServicesAndUsers/main.tf
@@ -18,13 +18,26 @@ resource "google_project" "project_bad_3" {
   project_id = "123456"
 }
 
-resource "google_project_iam_audit_config" "project_good_audit" {
+resource "google_project_iam_audit_config" "project_good_audit_1" {
   project = google_project.project_good.id
   service = "allServices"
   audit_log_config {
     log_type = "ADMIN_READ"
   }
   audit_log_config {
+    log_type = "DATA_READ"
+  }
+}
+
+resource "google_project_iam_audit_config" "project_good_audit_2" {
+  project = google_project.project_good.id
+  service = "allServices"
+  audit_log_config {
+    exempted_members = []
+    log_type = "ADMIN_READ"
+  }
+  audit_log_config {
+    exempted_members = []
     log_type = "DATA_READ"
   }
 }
