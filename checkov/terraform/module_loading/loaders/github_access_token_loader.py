ce0c66bee62f91e6f1b7660f04999dec3c1e9420
Kartik Pande <kpande@m-c02f6cz4md6m.paloaltonetworks.local>
2022-07-29 11:52:49 -0400

    support ssh sources when github token is present

--- checkov/terraform/module_loading/loaders/github_access_token_loader.py
+++ checkov/terraform/module_loading/loaders/github_access_token_loader.py
@@ -11,16 +11,20 @@ class GithubAccessTokenLoader(GenericGitLoader):
 
     def _is_matching_loader(self) -> bool:
         if self.token:
+            # if GITHUB_PAT is set and previous loaders failed, convert source (ssh or http or generic git)
+            # to use the token in generic format git::https://x-access-token:<token>@github.com/org/repo.git
             self.logger.debug("GITHUB_PAT found. Attempting to clone module using HTTP basic authentication.")
+            # if module_source = github.com/org/repo
             if self.module_source.startswith(self.module_source_prefix):
                 self.module_source = f"git::https://{self.username}:{self.token}@{self.module_source}"
                 return True
-            if self.module_source.startswith(self.module_source_prefix):
-                source = self.module_source.replace(":", "/")
-                self.module_source = f"git::https://{self.username}:{self.token}@{source}"
-                return True
+            # if module_source = git@github.com:org/repo.git
             if self.module_source.startswith(f"git::https://{self.module_source_prefix}"):
                 self.module_source = f"git::https://{self.username}:{self.token}@{self.module_source.split('git::https://')[1]}"
+                return True
+            # if module_source = git@github.com:org/repo.git
+            if self.module_source.startswith(f"git@{self.module_source_prefix}:"):
+                self.module_source = f"git::https://{self.username}:{self.token}@{self.module_source.split('git@')[1].replace(':', '/')}"
                 return True
 
         return False
