00204636fa0484d2e4ed5e637e00b7dca180f0ce
arielk <ar.kruszyn@gmail.com>
2022-08-02 23:50:54 +0300

    fixes

--- checkov/common/output/cyclonedx.py
+++ checkov/common/output/cyclonedx.py
@@ -6,7 +6,7 @@ import os
 import sys
 from datetime import datetime
 from pathlib import Path
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, cast, Dict, Any
 
 from cyclonedx.model import XsUri, ExternalReference, ExternalReferenceType, sha1sum, HashAlgorithm, HashType
 from cyclonedx.model.bom import Bom, Tool
@@ -65,7 +65,7 @@ class CycloneDX:
 
             if report.check_type == CheckType.SCA_IMAGE:
                 image_record = next(itertools.chain(report.failed_checks, report.passed_checks, report.skipped_checks))
-                image_id = cast(dict, image_record.vulnerability_details).get('image_details', ImageDetails).image_id
+                image_id = cast(Dict[str, Any], image_record.vulnerability_details).get('image_details', ImageDetails).image_id
                 file_path = image_record.file_path.split(' ')[0]
                 image_purl = PackageURL(
                     type='oci',
@@ -114,6 +114,7 @@ class CycloneDX:
 
     def create_component(self, check_type: str, resource: Record | ExtraResource) -> Component:
         """Creates a component"""
+        # purl structure conventions: https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst
 
         if check_type in SCA_CHECKTYPES:
             component = self.create_library_component(check_type=check_type, resource=resource)
