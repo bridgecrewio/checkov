0edd7d28a35c00f9c03c62a409ae000415e3c3e2
arielk <ar.kruszyn@gmail.com>
2022-08-02 18:33:50 +0300

    linting

--- checkov/sca_package/output.py
+++ checkov/sca_package/output.py
@@ -287,8 +287,8 @@ def create_cve_summary_table_part(table_width: int, column_width: int, cve_count
     # hack to make multiple tables look like one
     cve_table_bottom_line = (
         cve_table_lines[-1]
-            .replace(cve_table.bottom_left_junction_char, cve_table.left_junction_char)
-            .replace(cve_table.bottom_right_junction_char, cve_table.right_junction_char)
+        .replace(cve_table.bottom_left_junction_char, cve_table.left_junction_char)
+        .replace(cve_table.bottom_right_junction_char, cve_table.right_junction_char)
     )
     cve_table_lines[-1] = cve_table_bottom_line
 
