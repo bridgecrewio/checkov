e7a611c015bf5dd363ee8b3b39a8e9d89926c159
gruebel <anton.gruebel@gmail.com>
2022-08-05 15:36:15 +0200

    clean up

--- checkov/github_actions/checks/job/CosignArtifacts.py
+++ checkov/github_actions/checks/job/CosignArtifacts.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckResult
 from checkov.common.util.consts import START_LINE
 from checkov.github_actions.checks.base_github_action_check import BaseGithubActionsCheck
@@ -7,7 +11,7 @@ from checkov.yaml_doc.enums import BlockType
 
 
 class CosignSignPresent(BaseGithubActionsCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Found artifact build without evidence of cosign sign execution in pipeline"
         id = "CKV_GHA_5"
         super().__init__(
@@ -17,30 +21,27 @@ class CosignSignPresent(BaseGithubActionsCheck):
             supported_entities=['jobs']
         )
 
-    def scan_entity_conf(self, conf):
-        buildfound = False
+    def scan_entity_conf(self, conf: dict[str, Any]) -> tuple[CheckResult, dict[str, Any]]:
+        build_found = False
         for jobname, jobdetail in conf.items():
             if jobname == START_LINE:
                 return CheckResult.PASSED, conf
             steps = jobdetail.get("steps")
             if steps:
                 for step in steps:
-                    if buildfound is False:
-                        uses = step.get("uses")
-                        if uses is not None:
-                            for action in buildactions:
-                                if action in uses:
-                                    buildfound = True                    
-                        run = step.get("run")
-                        if run is not None:
-                            for build in buildcmds:
-                                if build in run:
-                                    buildfound = True
-                    else: 
-                        run = step.get("run","")            
+                    if build_found:
+                        run = step.get("run", "")
                         if "cosign sign" in run:
                             return CheckResult.PASSED, step
-        if buildfound:
+                    else:
+                        uses = step.get("uses")
+                        if uses is not None and any(action in uses for action in buildactions):
+                            build_found = True
+                        run = step.get("run")
+                        if run is not None and any(build in run for build in buildcmds):
+                            build_found = True
+
+        if build_found:
             return CheckResult.FAILED, conf            
         return CheckResult.PASSED, conf
 
