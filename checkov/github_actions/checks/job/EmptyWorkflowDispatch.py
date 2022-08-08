32db5facdd88e66d9bfb6b45ecf1772a485c683a
tronxd <tron47@gmail.com>
2022-08-08 17:21:52 +0300

    handle list in EmptyWorkflowDispatch

--- checkov/github_actions/checks/job/EmptyWorkflowDispatch.py
+++ checkov/github_actions/checks/job/EmptyWorkflowDispatch.py
@@ -20,12 +20,17 @@ class EmptyWorkflowDispatch(BaseGithubActionsCheck):
         )
 
     def scan_entity_conf(self, conf: dict[str, Any]) -> tuple[CheckResult, dict[str, Any]]:
+        if isinstance(conf, list):
+            for sub_conf in conf:
+                if sub_conf == "workflow_dispatch":
+                    return CheckResult.PASSED, sub_conf
+            return CheckResult.UNKNOWN, {}
+
         if isinstance(conf, str):
             if conf == "workflow_dispatch":
                 return CheckResult.PASSED, conf
             else:
                 return CheckResult.UNKNOWN, conf
-
         workflow_dispatch = conf.get("workflow_dispatch")
         if isinstance(workflow_dispatch, dict):
             workflow_dispatch_inputs = workflow_dispatch.get("inputs", {})
