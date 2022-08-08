3637dbdc3c71e2671770d063dc5cd5a247ec52c4
nijdarshan <darshannij@gmail.com>
2022-08-08 15:11:13 +0530

    Update --client-ca-file in KubeletClientCa check (CKV_K8S_140)

--- checkov/kubernetes/checks/resource/k8s/KubeletClientCa.py
+++ checkov/kubernetes/checks/resource/k8s/KubeletClientCa.py
@@ -14,15 +14,13 @@ class KubeletClientCa(BaseK8sContainerCheck):
     def scan_container_conf(self, metadata: Dict[str, Any], conf: Dict[str, Any]) -> CheckResult:
         self.evaluated_container_keys = ["command"]
         if conf.get("command"):
-            if "kubelet" in conf["command"]:
+             if "kubelet" in conf["command"]:
+                hasClientCaFile = False
                 for command in conf["command"]:
-                    if command.startswith("--root-ca-file"):
-                        file_name = command.split("=")[1]
-                        extension = file_name.split(".")[1]
-                        if extension == "pem":
-                            return CheckResult.PASSED
-                        else:
-                            return CheckResult.FAILED
+                    if command.startswith("--client-ca-file"):
+                        if len(command.split("=")) == 2:
+                            hasClientCaFile = True
+                return CheckResult.PASSED if hasClientCaFile else CheckResult.FAILED
 
         return CheckResult.PASSED
 
