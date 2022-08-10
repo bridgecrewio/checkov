5a964b110b6554180b99e04814bb4f26f635cbc9
nazoulay <noazoulay1@gmail.com>
2022-08-10 13:23:40 +0300

    fix gha checks

--- checkov/github_actions/checks/job/CosignArtifacts.py
+++ checkov/github_actions/checks/job/CosignArtifacts.py
@@ -22,6 +22,9 @@ class CosignSignPresent(BaseGithubActionsCheck):
         )
 
     def scan_entity_conf(self, conf: dict[str, Any]) -> tuple[CheckResult, dict[str, Any]]:
+        if not conf:
+            return CheckResult.UNKNOWN, conf
+
         build_found = False
         for jobname, jobdetail in conf.items():
             if jobname == START_LINE:
