5bbedaf6ed669b77e9a2e2b31f6497e303d58c23
yaaraverner <yaara@vernermail.com>
2022-08-18 10:22:56 +0300

    remove redundant conditions

--- checkov/kubernetes/checks/resource/k8s/DockerSocketVolume.py
+++ checkov/kubernetes/checks/resource/k8s/DockerSocketVolume.py
@@ -24,13 +24,10 @@ class DockerSocketVolume(BaseK8Check):
             if "spec" in conf:
                 spec = conf["spec"]
         elif conf['kind'] == 'CronJob':
-            if "spec" in conf:
-                if "jobTemplate" in conf["spec"]:
-                    job_template_spec = conf["spec"]["jobTemplate"].get("spec")
-                    if job_template_spec:
-                        if "template" in job_template_spec:
-                            if "spec" in job_template_spec["template"]:
-                                spec = job_template_spec["template"]["spec"]
+            if "spec" in conf and "jobTemplate" in conf["spec"]:
+                job_template_spec = conf["spec"]["jobTemplate"].get("spec")
+                if job_template_spec and "template" in job_template_spec and "spec" in job_template_spec["template"]:
+                    spec = job_template_spec["template"]["spec"]
         else:
             inner_spec = self.get_inner_entry(conf, "spec")
             spec = inner_spec if inner_spec else spec
