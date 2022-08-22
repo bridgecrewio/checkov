ed13d418d40db3a57941d44db01400b19749a3ec
achiagit <achiar99@gmail.com>
2022-08-22 10:08:22 +0300

    mypy

--- checkov/runner_filter.py
+++ checkov/runner_filter.py
@@ -203,7 +203,7 @@ class RunnerFilter(object):
             return True
         return check_id in self.filtered_policy_ids
     
-    def to_dict(self) -> Dict[str, any]:
+    def to_dict(self):
         result = {}
         result['framework'] = self.framework
         result['checks'] = self.checks
