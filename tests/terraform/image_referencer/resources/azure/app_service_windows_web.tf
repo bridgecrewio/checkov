fd84e75440622875abece3479b08287a7081b087
Anton Grübel <anton.gruebel@gmail.com>
2022-08-24 10:40:13 +0200

    feat(terraform): support Azure resources for Image Referencer (#3383)
    * support Azure TF resources for Image Referencer
    
    * leverage a function map and create a base class
    
    * add missing __init__ file

---
+++ tests/terraform/image_referencer/resources/azure/app_service_windows_web.tf
@@ -0,0 +1,39 @@
+resource "azurerm_windows_web_app" "test" {
+  name                = "acctestWA-%d"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  service_plan_id     = azurerm_service_plan.test.id
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = ""
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = ""
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    application_stack {
+      docker_container_name = "hello-world"
+      docker_container_tag  = "latest"
+    }
+  }
+}
+
+resource "azurerm_windows_web_app_slot" "test" {
+  name           = "acctestWAS-%d"
+  app_service_id = azurerm_linux_web_app.test.id
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = ""
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = ""
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    application_stack {
+      docker_container_name = "busybox"
+      docker_container_tag  = "latest"
+    }
+  }
+}
