89a7ab2419fc7a128c0f7fb794e11a6fd8b267a9
Akintayo Akinbade <yakinbade@paloaltonetworks.com>
2022-08-29 07:29:44 -0700

    updated testing for 2 YAMl policies

--- tests/terraform/graph/checks/resources/Route53ZoneEnableDNSSECSigning/main.tf
+++ tests/terraform/graph/checks/resources/Route53ZoneEnableDNSSECSigning/main.tf
@@ -2,14 +2,30 @@
 resource "aws_route53_zone" "pass" {
   name = "pass"
 }
-resource "aws_route53_query_log" "pass" {
-  depends_on = [aws_cloudwatch_log_resource_policy.route53-query-logging-policy]
+resource "aws_route53_key_signing_key" "pass" {
+  hosted_zone_id             = aws_route53_zone.pass.id
+  key_management_service_arn = aws_kms_key.pass.arn
+  name                       = "pass"
+}
 
-  cloudwatch_log_group_arn = aws_cloudwatch_log_group.aws_route53_pass.arn
-  zone_id                  = aws_route53_zone.pass.zone_id
+resource "aws_route53_hosted_zone_dnssec" "pass" {
+  depends_on = [
+    aws_route53_key_signing_key.pass
+  ]
+  hosted_zone_id = aws_route53_key_signing_key.pass.hosted_zone_id
 }
 
 #fail
 resource "aws_route53_zone" "fail" {
   name = "fail"
+}
+resource "aws_route53_key_signing_key" "fail" {
+  hosted_zone_id             = aws_route53_zone.fail.id
+  key_management_service_arn = aws_kms_key.fail.arn
+  name                       = "pass"
+}
+
+#fail2
+resource "aws_route53_zone" "fail2" {
+  name = "fail2"
 }
