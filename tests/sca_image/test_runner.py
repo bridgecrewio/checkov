1557ebfb270ef15706c7ef4e0fe51814047e9d8b
Mike Urbanski <murbanski@paloaltonetworks.com>
2022-09-08 09:02:39 -0500

    fix license policy test

--- tests/sca_image/test_runner.py
+++ tests/sca_image/test_runner.py
@@ -113,13 +113,66 @@ def test_runner_honors_enforcement_rules(mock_bc_integration, image_name, cached
 
     # when
     image_runner = Runner()
+    filter = RunnerFilter(framework=['sca_image'], use_enforcement_rules=True)
+    # this is not quite a true test, because the checks don't have severities. However, this shows that the check registry
+    # passes the report type properly to RunnerFilter.should_run_check, and we have tests for that method
+    filter.enforcement_rule_configs = {CheckType.SCA_IMAGE: Severities[BcSeverities.OFF]}
+    image_runner.image_referencers = [GHA_Runner()]
+    report = image_runner.run(root_folder=WORKFLOW_EXAMPLES_DIR, runner_filter=filter)
+
+    summary = report.get_summary()
+    # then
+    assert summary["passed"] == 0
+    assert summary["failed"] == 0
+    assert summary["skipped"] == 5
+    assert summary["parsing_errors"] == 0
+
+
+def test_run_license_policy(mock_bc_integration, image_name, cached_scan_result):
+    # given
+    image_id_encoded = quote_plus(f"image:{image_name}")
+
+    response_json = {
+        "violations": [
+            {
+                "name": "readline",
+                "version": "8.1.2-r0",
+                "license": "Apache-2.0",
+                "policy": "BC_LIC_1",
+                "status": "OPEN"
+            },
+            {
+                "name": "libnsl",
+                "version": "2.0.0-r0",
+                "license": "Apache-2.0",
+                "policy": "BC_LIC_1",
+                "status": "COMPLIANT"
+            },
+        ]
+    }
+
+    responses.add(
+        method=responses.POST,
+        url=mock_bc_integration.bc_api_url + "/api/v1/vulnerabilities/packages/get-licenses-violations",
+        json=response_json,
+        status=200
+    )
+    responses.add(
+        method=responses.GET,
+        url=mock_bc_integration.bc_api_url + f"/api/v1/vulnerabilities/scan-results/{image_id_encoded}",
+        json=cached_scan_result,
+        status=200,
+    )
+
+    # when
+    image_runner = Runner()
     filter = RunnerFilter(framework=['sca_image'], checks=['BC_LIC_1'])
     image_runner.image_referencers = [GHA_Runner()]
     report = image_runner.run(root_folder=WORKFLOW_EXAMPLES_DIR, runner_filter=filter)
 
     # then
     assert not [c for c in report.passed_checks + report.failed_checks if c.check_id.startswith('CKV_CVE')]
-
+    
 
 def test_run(sca_image_report):
     # given
