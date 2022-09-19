67f4c62b05e8e019841414198ab5490167346fff
Aleksandar Stojanov <2626216+losisin@users.noreply.github.com>
2022-09-19 09:08:54 +0200

    Add `log_hostname` check for GCP PostgreSQL (CIS 1.3 6.2.5)

---
+++ checkov/terraform/checks/resource/gcp/GoogleCloudPostgreSqlLogHostname.py
@@ -0,0 +1,44 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class GoogleCloudPostgreSqlLogHostname(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure PostgreSQL database 'log_hostname' flag is set to 'on'"
+        check_id = "CKV_GCP_108"
+        supported_resources = ['google_sql_database_instance']
+        categories = [CheckCategories.LOGGING]
+        super().__init__(name=name, id=check_id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Looks for google_sql_database_instance which allows log log_hostname on PostgreSQL DBs::
+            :param
+            conf: google_sql_database_instance
+            configuration
+            :return: < CheckResult >
+        """
+        if 'database_version' in conf.keys() and isinstance(conf['database_version'][0], str) and 'POSTGRES' in conf['database_version'][0]:
+            if 'settings' in conf.keys():
+                self.evaluated_keys = ['database_version/[0]/POSTGRES', 'settings']
+                flags = conf['settings'][0].get('database_flags')
+                if flags:
+                    evaluated_keys_prefix = 'settings/[0]/database_flags'
+                    if isinstance(flags[0], list):  # treating use cases of the following database_flags parsing (list of list of dictionaries with strings):'database_flags': [[{'name': '<key>', 'value': '<value>'}, {'name': '<key>', 'value': '<value>'}]]
+                        flags = flags[0]
+                        evaluated_keys_prefix += '/[0]'
+                    else:  # treating use cases of the following database_flags parsing (list of dictionaries with arrays): 'database_flags': [{'name': ['<key>'], 'value': ['<value>']},{'name': ['<key>'], 'value': ['<value>']}]
+                        flags = [{key: flag[key][0] for key in flag} for flag in flags]
+                    for flag in flags:
+                        if (flag['name'] == 'log_hostname') and (flag['value'] == 'on'):  # Must be explicitly set for check to pass
+                            self.evaluated_keys = ['database_version/[0]/POSTGRES',
+                                                   f'{evaluated_keys_prefix}/[{flags.index(flag)}]/name',
+                                                   f'{evaluated_keys_prefix}/[{flags.index(flag)}]/value']
+                            return CheckResult.PASSED
+                    self.evaluated_keys = ['database_version/[0]/POSTGRES', 'settings/[0]/database_flags']
+
+            return CheckResult.FAILED
+        return CheckResult.UNKNOWN
+
+
+check = GoogleCloudPostgreSqlLogHostname()
