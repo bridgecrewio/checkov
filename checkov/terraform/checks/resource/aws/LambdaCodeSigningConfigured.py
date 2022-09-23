db7db9937a2a59ae272c3f6d3861a87267afe027
AdamDev <AdamVarsan@gmail.com>
2022-09-23 08:17:28 +0300

    Update checkov/terraform/checks/resource/aws/LambdaCodeSigningConfigured.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/terraform/checks/resource/aws/LambdaCodeSigningConfigured.py
+++ checkov/terraform/checks/resource/aws/LambdaCodeSigningConfigured.py
@@ -5,7 +5,7 @@ from checkov.common.models.enums import CheckCategories
 
 class LambdaCodeSigningConfigured(BaseResourceValueCheck):
     def __init__(self):
-        name = "Ensure that AWS Lambda function is configured for a code-signing with signing profiles, which define the trusted publishers for this function"
+        name = "Ensure AWS Lambda function is configured to validate code-signing"
         id = "CKV_AWS_272"
         supported_resources = ['aws_lambda_function']
         categories = [CheckCategories.GENERAL_SECURITY]
