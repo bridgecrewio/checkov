a95620196cf8574c769948deb36366cf37dda75f
floodnut <gsniper777@gmail.com>
2022-10-09 14:46:06 +0900

    [22.10.09][refactor] modify branch for ncp tf 6

--- checkov/terraform/checks/resource/ncp/AccessControlGroupInboundRulePort3389.py
+++
@@ -1,9 +0,0 @@
-from checkov.terraform.checks.resource.ncp.AccessControlGroupInboundRule import AccessControlGroupInboundRule
-
-
-class AccessControlGroupGRuleInboundPort3389(AccessControlGroupInboundRule):
-    def __init__(self):
-        super().__init__(check_id="CUSTOM_NCP_ACG_005", port=3389)
-
-
-check = AccessControlGroupGRuleInboundPort3389()
