3417ea3d8304031fafdf5fefcef6434e6cd1815c
Anton Grübel <anton.gruebel@gmail.com>
2022-10-10 21:15:26 +0200

    fix supported_resources value

--- checkov/terraform/checks/resource/aws/IAMUserNotUsedForAccess.py
+++ checkov/terraform/checks/resource/aws/IAMUserNotUsedForAccess.py
@@ -11,7 +11,7 @@ class IAMUserNotUsedForAccess(BaseResourceNegativeValueCheck):
                "AWS Access should be controlled by roles via an SSO, inline with AWS security best practices " \
                "Require human users to use federation with an identity provider to access AWS using temporary credentials "
         id = "CKV_AWS_273"
-        supported_resources = ('aws_iam_user')
+        supported_resources = ('aws_iam_user',)
         categories = (CheckCategories.IAM,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
