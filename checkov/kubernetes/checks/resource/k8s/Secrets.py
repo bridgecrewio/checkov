2e9b5689b3a9f2cb6fc4b150c79cd451c99193b3
Peleg Litvak <peleg.litvak@gmail.com>
2022-10-11 14:58:22 +0300

    Update checkov/kubernetes/checks/resource/k8s/Secrets.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/kubernetes/checks/resource/k8s/Secrets.py
+++ checkov/kubernetes/checks/resource/k8s/Secrets.py
@@ -16,10 +16,10 @@ class Secrets(BaseK8sContainerCheck):
         self.evaluated_container_keys = ["env", "envFrom"]
         if conf.get("env"):
             for idx, e in enumerate(conf["env"]):
-                if "valueFrom" in e and e["valueFrom"] is not None:
-                    if "secretKeyRef" in e["valueFrom"]:
-                        self.evaluated_container_keys = [f"env/[{idx}]/valueFrom/secretKeyRef"]
-                        return CheckResult.FAILED
+                value_from = e.get("valueFrom")
+                if value_from and "secretKeyRef" in value_from:
+                    self.evaluated_container_keys = [f"env/[{idx}]/valueFrom/secretKeyRef"]
+                    return CheckResult.FAILED
         if conf.get("envFrom"):
             for idx, ef in enumerate(conf["envFrom"]):
                 if "secretRef" in ef:
