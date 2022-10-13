ab9f1cf967731515d5092af1b0c2e19d531cfd89
Ulrich Grave <ugrave@users.noreply.github.com>
2022-10-13 09:32:05 +0200

    Add versioned kubernetes resources to terraform kubernetes checks (Part 2/5)

--- tests/terraform/checks/resource/kubernetes/example_HostPort/main.tf
+++ tests/terraform/checks/resource/kubernetes/example_HostPort/main.tf
@@ -5,6 +5,13 @@ resource "kubernetes_pod" "fail2" {
   }
 }
 
+# fails no spec
+resource "kubernetes_pod_v1" "fail2" {
+  metadata {
+    name = "terraform-example"
+  }
+}
+
 # fails no resource
 resource "kubernetes_pod" "fail" {
   metadata {
@@ -49,7 +56,101 @@ resource "kubernetes_pod" "fail" {
   }
 }
 
+# fails no resource
+resource "kubernetes_pod_v1" "fail" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    host_ipc = true
+    host_pid = true
+
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 8080
+        host_port = 8080
+      }
+    }
+
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
+    }
+
+    dns_policy = "None"
+  }
+}
+
 resource "kubernetes_pod" "pass" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    host_ipc = true
+    host_pid = true
+
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 8080
+      }
+
+      resources {
+        limits = {
+          cpu = "500m"
+        }
+
+      }
+
+    }
+
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
+    }
+
+    dns_policy = "None"
+  }
+}
+
+resource "kubernetes_pod_v1" "pass" {
   metadata {
     name = "terraform-example"
   }
