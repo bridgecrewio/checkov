8c3953d43d97332b918648ddb98bd922f617300b
gruebel <anton.gruebel@gmail.com>
2022-10-17 16:30:40 +0200

    fix Python 3.11 issue

--- checkov/github_actions/graph_builder/graph_components/resource_types.py
+++ checkov/github_actions/graph_builder/graph_components/resource_types.py
@@ -4,3 +4,7 @@ from enum import Enum
 class ResourceType(str, Enum):
     JOBS = "jobs"
     STEPS = "steps"
+
+    def __str__(self) -> str:
+        # needed, because of a Python 3.11 change
+        return self.value
