b926c61ac58eaacd8e1ad9de1083fd9234714687
ipeleg <ipeleg@paloaltonetworks.com>
2022-10-17 20:35:05 +0300

    fix flake8

--- checkov/sca_image/runner.py
+++ checkov/sca_image/runner.py
@@ -94,7 +94,7 @@ class Runner(PackageRunner):
         except UnicodeDecodeError:
             logging.error("error was caught when trying to decode the \'stdout\' from twistcli.\n"
                           f"file content is:\n{image_scanner.dockerfile_content}.\n"
-                          f"twistcli command is \'{command}\'",exc_info=True)
+                          f"twistcli command is \'{command}\'", exc_info=True)
 
         exit_code = await process.wait()
 
