a19b72dff2cb52a38ad1ab3c49866b5f4ddb71b1
floodnut <gsniper777@gmail.com>
2022-10-19 20:09:14 +0900

    [22.10.19][refactor] adjust ncp rule-3

--- checkov/terraform/checks/resource/ncp/AccessControlGroupOutboundRule.py
+++ checkov/terraform/checks/resource/ncp/AccessControlGroupOutboundRule.py
@@ -6,18 +6,17 @@ class AccessControlGroupOutboundRule(BaseResourceCheck):
     def __init__(self):
         name = "Ensure no security group rules allow outbound traffic to 0.0.0.0/0"
         id = "CKV_NCP_3"
-        supported_resources = ['ncloud_access_control_group_rule']
+        supported_resources = ('ncloud_access_control_group_rule',)
 
-        categories = [CheckCategories.NETWORKING]
+        categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories,
                          supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if 'outbound' in conf.keys():
-            for outbound in conf['outbound']:
-                ip = outbound.get('ip_block')
-                if ip == ['0.0.0.0/0'] or ip == ['::/0']:
-                    return CheckResult.FAILED
+        for outbound in conf.get('outbound', []):
+            ip = outbound.get('ip_block')
+            if ip == ['0.0.0.0/0'] or ip == ['::/0']:
+                return CheckResult.FAILED
         return CheckResult.PASSED
 
 
