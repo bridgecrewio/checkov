04a2bd48fa35a2aa6113ed6131e21a8e30bc92b5
yaaraverner <yaara@vernermail.com>
2022-10-20 12:23:29 +0100

    fix lint

--- checkov/common/checks_infra/solvers/filter_solvers/within_filter_solver.py
+++ checkov/common/checks_infra/solvers/filter_solvers/within_filter_solver.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Dict
 
 from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.checks_infra.solvers.filter_solvers.base_filter_solver import BaseFilterSolver
@@ -14,7 +14,7 @@ class WithinFilterSolver(BaseFilterSolver):
         return self._get_operation()(*args)
 
     def _get_operation(self, *args: Any, **kwargs: Any) -> Callable[..., bool]:
-        def op(check: dict[str, Any]) -> bool:
+        def op(check: Dict[str, Any]) -> bool:
             if not self.attribute:
                 return False
 
