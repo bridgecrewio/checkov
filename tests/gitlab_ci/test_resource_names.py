e086250fde34480f3227fb9edcb1a6647f0251b4
Eliran Turgeman <elturgeman@paloaltonetworks.com>
2022-10-20 12:37:56 +0300

    fix tests

--- tests/gitlab_ci/test_resource_names.py
+++ tests/gitlab_ci/test_resource_names.py
@@ -9,7 +9,7 @@ from checkov.gitlab_ci.runner import Runner
         ('*.script[].*.script[].CKV_GITLABCI_1[19:19]', '/checkov/tests/gitlab_ci/resources/images/.gitlab-ci.yml',
          'test.script'),
         ('*.rules.*.rules.CKV_GITLABCI_2[7:9]', '/checkov/tests/gitlab_ci/resources/two/.gitlab-ci.yml',
-         'planOnlySubset.rules'),
+         'planOnlySubset'),
     ],
 )
 def test_get_resource(key, file_path, expected_key, definitions):
