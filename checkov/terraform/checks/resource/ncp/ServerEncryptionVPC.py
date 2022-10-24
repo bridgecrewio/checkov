48c2fdd1625cc1c3be5c1a5bd0a079ff02dbe6f5
floodnut <gsniper777@gmail.com>
2022-10-24 15:17:32 +0900

    [22.10.24][refactor] adjust change

--- checkov/terraform/checks/resource/ncp/ServerEncryptionVPC.py
+++ checkov/terraform/checks/resource/ncp/ServerEncryptionVPC.py
@@ -1,26 +1,18 @@
 
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class ServerEncryptionVPC(BaseResourceCheck):
+class ServerEncryptionVPC(BaseResourceValueCheck):
     def __init__(self):
-        name = "You can set whether to encrypt basic block storage if server image is RHV. Default false." \
-               " Only support VPC environment."
+        name = "Ensure Server instance is encrypted."
         id = "CKV_NCP_6"
         supported_resources = ('ncloud_server',)
         categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-
-    def scan_resource_conf(self, conf):
-        if 'is_encrypted_base_block_storage_volume' in conf.keys():
-            is_encrypted_base_block_storage_volume = conf['is_encrypted_base_block_storage_volume']
-            if is_encrypted_base_block_storage_volume == [True]:
-                return CheckResult.PASSED
-            else:
-                return CheckResult.FAILED
-        else:
-            return CheckResult.FAILED
+    
+    def get_inspected_key(self):
+        return "is_encrypted_base_block_storage_volume"
 
 
 check = ServerEncryptionVPC()
