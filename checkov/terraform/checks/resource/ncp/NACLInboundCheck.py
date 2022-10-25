d102d99a810cb967b45f42e2f9081659a7ec4d2e
floodnut <gsniper777@gmail.com>
2022-10-25 20:01:22 +0900

    [22.10.25][refactor] adjust change

--- checkov/terraform/checks/resource/ncp/NACLInboundCheck.py
+++ checkov/terraform/checks/resource/ncp/NACLInboundCheck.py
@@ -4,25 +4,24 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 
 class NACLInboundCheck(BaseResourceCheck):
     def __init__(self, check_id, port):
-        name = "Ensure no NACL allow inbound from 0.0.0.0:0 to port %d" % port
+        name = f"Ensure no NACL allow inbound from 0.0.0.0:0 to port {port}"
         id = check_id
         supported_resources = ('ncloud_network_acl_rule',)
         categories = (CheckCategories.NETWORKING,)
-        guideline = "You should Ensure no NACL allow inbound from 0.0.0.0:0 to port %d" % port
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources, guideline=guideline)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
         self.port = port
 
     def scan_resource_conf(self, conf):
         for inbound in conf.get('inbound', []):
             if inbound['rule_action'] == ["ALLOW"]:
-                ip = inbound.get('ip_block', '0.0.0.0/0')
+                ip = inbound.get('ip_block', ['0.0.0.0/0'])
                 if ip == ['0.0.0.0/0'] or ip == ['::/0']:
                     port = inbound.get('port_range', str(self.port))[0]
                     if port == str(self.port):
                         return CheckResult.FAILED
                     elif port.find('-'):
-                        portRange = list(map(int, port.split("-")))
-                        if portRange[0] <= self.port <= portRange[-1]:
+                        port_range = list(map(int, port.split("-")))
+                        if port_range[0] <= self.port <= port_range[-1]:
                             return CheckResult.FAILED
 
         return CheckResult.PASSED
