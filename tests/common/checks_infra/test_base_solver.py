7b414561f2524823dcc40d66f6cef2f7f92ce289
ChanochShayner <chsh32340@gmail.com>
2022-10-25 13:17:48 +0300

    remove unrelated file

--- tests/common/checks_infra/test_base_solver.py
+++
@@ -1,5 +0,0 @@
-from checkov.common.checks_infra.solvers.complex_solvers.base_complex_solver import BaseComplexSolver
-
-
-def test_run_complex_solver():
-    base_complex_solver = BaseComplexSolver()
