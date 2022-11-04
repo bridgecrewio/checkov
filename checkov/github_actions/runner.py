995cadda6221ed070b9a8e775aa9d440fa78f677
gruebel <anton.gruebel@gmail.com>
2022-11-04 09:42:17 +0100

    fix linting

--- checkov/github_actions/runner.py
+++ checkov/github_actions/runner.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any
 
 import yaml
 
