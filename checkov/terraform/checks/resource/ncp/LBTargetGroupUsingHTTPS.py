77b362b9aed783efe3ebc8f33443b5dac3767807
pj991207 <pj991207@gmail.com>
2022-11-05 15:49:16 +0900

    [22.11.05][add]NCP_LBTargetGroupUsingHTTPS

---
+++ checkov/terraform/checks/resource/ncp/LBTargetGroupUsingHTTPS.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class LBTargetGroupUsingHTTPS(BaseResourceCheck):
+
+    def __init__(self):
+        name = "Ensure LB TargetGroup Using HTTPS"
+        id = "CKV_NCP_15"
+        supported_resources = ("ncloud_lb_target_group",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if "protocol" in conf.keys():
+            if conf.get("protocol")[0] == 'HTTPS':
+                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = LBTargetGroupUsingHTTPS()
