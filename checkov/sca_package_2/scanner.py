0a5369e0bb02f9757337b505692c3507455e9e44
ajbara <ajbara@paloaltonetworks.com>
2022-11-08 15:17:10 +0200

    updated

--- checkov/sca_package_2/scanner.py
+++ checkov/sca_package_2/scanner.py
@@ -19,6 +19,7 @@ MAX_SLEEP_DURATION = 60
 class Scanner:
     def __init__(self, pbar: ProgressBar | None = None, root_folder: str | Path | None = None) -> None:
         self._base_url = bc_integration.api_url
+        self.bc_cli_scan_api_url = f"{self._base_url}/api/v1/vulnerabilities/cli/scan"
         if pbar:
             self.pbar = pbar
         else:
@@ -26,12 +27,14 @@ class Scanner:
             self.pbar.turn_off_progress_bar()
         self.root_folder = root_folder
 
-    def scan(self) -> Sequence[dict[str, Any]]:
+    def scan(self) -> Sequence[dict[str, Any]] | None:
         """run SCA package scan and poll scan results"""
-        self.run_scan()
+        if not self.run_scan():
+            return None
+        self.poll_scan_result()
         return []
 
-    def run_scan(self) -> dict[str, Any]:
+    def run_scan(self) -> bool:
         logging.info("Start to scan package files.")
 
         request_body = {
@@ -44,7 +47,7 @@ class Scanner:
         }
 
         response = request_wrapper(
-            "POST", f"{self._base_url}/api/v1/vulnerabilities/cli/scan",
+            "POST", self.bc_cli_scan_api_url,
             headers=bc_integration.get_default_headers("POST"),
             json=request_body,
             should_call_raise_for_status=True
@@ -54,9 +57,9 @@ class Scanner:
 
         if not response_json["startedSuccessfully"]:
             logging.info("Failed to run package scanning.")
-            return dict()
+            return False
+        return True
 
-        return self.poll_scan_result()
 
     def poll_scan_result(self) -> dict[str, Any]:
         pass
