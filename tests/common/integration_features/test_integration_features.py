9002af0da927086e93582bec39e1a17346e78654
Mike Urbanski <murbanski@paloaltonetworks.com>
2022-11-08 17:09:16 -0600

    add integration order test

--- tests/common/integration_features/test_integration_features.py
+++ tests/common/integration_features/test_integration_features.py
@@ -5,12 +5,15 @@ from checkov.common.bridgecrew.integration_features.features.custom_policies_int
 from checkov.common.bridgecrew.integration_features.features.fixes_integration import integration as fixes_integration
 from checkov.common.bridgecrew.integration_features.features.repo_config_integration import integration as repo_config_integration
 from checkov.common.bridgecrew.integration_features.features.suppressions_integration import integration as suppressions_integration
+from checkov.common.bridgecrew.integration_features.features.attribute_resource_types_integration import integration as attribute_resource_types_integration
 
 
 class TestSuppressionsIntegration(unittest.TestCase):
     def test_feature_order(self):
         self.assertGreater(fixes_integration.order, max([i.order for i in [policy_metadata_integration, custom_policies_integration, repo_config_integration, suppressions_integration]]))
         self.assertGreater(custom_policies_integration.order, policy_metadata_integration.order)
+        self.assertGreater(attribute_resource_types_integration.order, custom_policies_integration.order)
+        self.assertGreater(suppressions_integration.order, policy_metadata_integration.order)
         self.assertGreater(suppressions_integration.order, policy_metadata_integration.order)
 
 
