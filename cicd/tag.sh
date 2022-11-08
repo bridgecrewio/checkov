f329e07b829a1976c9b909559380d9a84a846437
Pipeline <checkov@flybuys.com.au>
2022-11-08 13:35:02 +1100

    trigger

--- cicd/tag.sh
+++ cicd/tag.sh
@@ -9,8 +9,8 @@ git remote remove origin
 git remote add origin https://oauth2:glpat-8ffy-yAs9mKSkZAZGz6Q@gitlab.com/flybuys/secops/iac/checkov.git
 git config --global user.email "checkov@flybuys.com.au"
 git config --global user.name "Pipeline"
-git pull origin main
-git checkout main
+git pull origin master
+git checkout master
 pip install pycalver
 pycalver bump  --release final
 Version=$(pycalver  show | grep Current | cut -d ":" -f 2 | cut -d " " -f 2)
