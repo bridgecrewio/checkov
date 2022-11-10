f948ccf4677ef8989e42d2359cdac98e4b131b8e
pj991207 <pj991207@gmail.com>
2022-11-10 19:53:15 +0900

    [22.11.10][fix]CKV_NCP_20

--- checkov/terraform/checks/resource/ncp/RouteTableNATGatewayDefault.py
+++ checkov/terraform/checks/resource/ncp/RouteTableNATGatewayDefault.py
@@ -6,15 +6,15 @@ class RouteTableNATGatewayDefault(BaseResourceCheck):
     def __init__(self):
         name = "Ensure Routing Table associated with Web tier subnet have the default route (0.0.0.0/0) defined to " \
                "allow connectivity "
-        id = "CKV_NCP_32"
+        id = "CKV_NCP_20"
         supported_resources = ('ncloud_route',)
         categories = (CheckCategories.KUBERNETES,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
         if "destination_cidr_block" in conf.keys() and "target_type" in conf.keys():
-            if conf.get("destination_cidr_block") in (["0.0.0.0/0"],) \
-                    and conf.get("target_type") in (["NATGW"],):
+            if conf.get("destination_cidr_block") == ["0.0.0.0/0"] \
+                    and conf.get("target_type") == ["NATGW"]:
                 return CheckResult.PASSED
         return CheckResult.FAILED
 
