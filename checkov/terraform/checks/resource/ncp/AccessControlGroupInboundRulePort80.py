ece13ea3661b1ecbfa222b8e9ae01fe03f860f9d
pj991207 <pj991207@gmail.com>
2022-11-12 00:32:12 +0900

    [22.11.12][fix]CKV_NCP_25

--- checkov/terraform/checks/resource/ncp/AccessControlGroupInboundRulePort80.py
+++ checkov/terraform/checks/resource/ncp/AccessControlGroupInboundRulePort80.py
@@ -3,7 +3,7 @@ from checkov.terraform.checks.resource.ncp.AccessControlGroupInboundRule import 
 
 class AccessControlGroupRuleInboundPort80(AccessControlGroupInboundRule):
     def __init__(self):
-        super().__init__(check_id="CKV_NCP_36", port=80)
+        super().__init__(check_id="CKV_NCP_25", port=80)
 
 
 check = AccessControlGroupRuleInboundPort80()
