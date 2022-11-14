206391b81c4dd2035714051fb5bc905b98f7a785
Adam Varsano <avarsano@paloaltonetworks.com>
2022-11-14 13:12:27 +0200

    fix pr comments

--- checkov/common/output/common.py
+++ checkov/common/output/common.py
@@ -18,11 +18,10 @@ class ImageDetails(SCADetails):
 
 
 def is_raw_formatted(licenses: str) -> bool:
-    return licenses.__contains__('","')
+    return '","' in licenses
 
 
 def format_licenses_to_string(licenses_lst: list[str]) -> str:
-    print('format_licenses_to_string - start', licenses_lst)
     if isinstance(licenses_lst, list):
         if len(licenses_lst) > 1:
             joined_str = '","'.join(licenses_lst)
@@ -33,7 +32,6 @@ def format_licenses_to_string(licenses_lst: list[str]) -> str:
 
 
 def format_string_to_licenses(licenses_str: str) -> list[str]:
-    print('format_string_to_licenses - start', licenses_str)
     if licenses_str == 'Unknown':
         return [licenses_str]
     elif licenses_str:
