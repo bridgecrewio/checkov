ce9ec4d9b69dfcc27ec28b46552fe10e9c6fb0b3
Mor Kadosh <kadoshms@gmail.com>
2022-11-14 16:13:12 +0200

    fix(general): Fixed operator name for number_of_words_derivaties (#3875)
    fix: fixed operator names for number_of_words derivaties
    
    Co-authored-by: Mor Kadosh <mkadosh@paloaltonetworks.com>

--- checkov/common/checks_infra/solvers/attribute_solvers/number_of_words_greater_than_or_equal_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/number_of_words_greater_than_or_equal_attribute_solver.py
@@ -6,7 +6,7 @@ from checkov.common.graph.checks_infra.enums import Operators
 
 
 class NumberOfWordsGreaterThanOrEqualAttributeSolver(BaseNumberOfWordsAttributeSolver):
-    operator = Operators.NUMBER_OF_WORDS_GREATER_THAN  # noqa: CCE003  # a static attribute
+    operator = Operators.NUMBER_OF_WORDS_GREATER_THAN_OR_EQUAL  # noqa: CCE003  # a static attribute
 
     def _get_operation(self, vertex: Dict[str, Any], attribute: Optional[str]) -> bool:
         attr = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
