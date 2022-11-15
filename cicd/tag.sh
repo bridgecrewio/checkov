95329e6ed440e0e8007ce0f4c365f4abf0ba5dd8
Pipeline <checkov@flybuys.com.au>
2022-11-16 01:35:50 +1100

    fix(terraform): download external modules with enrichment

--- cicd/tag.sh
+++ cicd/tag.sh
@@ -6,7 +6,7 @@ VERSION="v20221108.0226"
 set -e
 git --version
 git remote remove origin
-git remote add origin https://oauth2:glpat-8ffy-yAs9mKSkZAZGz6Q@gitlab.com/flybuys/secops/iac/checkov.git
+git remote add origin https://oauth2:glpat-5cP_VtxbtdpAgXnkx-Ua@gitlab.com/flybuys/secops/iac/checkov.git
 git config --global user.email "checkov@flybuys.com.au"
 git config --global user.name "Pipeline"
 git pull origin master
