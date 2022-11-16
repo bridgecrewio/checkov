9152d6117230c5bc1b40033fe347c455dd510cc9
Anton Grübel <anton.gruebel@gmail.com>
2022-11-16 11:08:54 +0100

    chore: fix flake8 error W292 (#3852)
    * fix flake8 error W292
    
    * fix resource_types

--- checkov/arm/base_registry.py
+++ checkov/arm/base_registry.py
@@ -1,33 +1,16 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.bridgecrew.check_type import CheckType
 from checkov.common.checks.base_check_registry import BaseCheckRegistry
 
 
 class Registry(BaseCheckRegistry):
-
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(report_type=CheckType.ARM)
 
-    def extract_entity_details(self, entity):
+    def extract_entity_details(self, entity: dict[str, Any]) -> tuple[str, str, dict[str, Any]]:
         resource_name, resource = next(iter(entity.items()))
-        resource_type = str(resource.get('type', ''))    # entity['type'] ??
+        resource_type = str(resource.get("type", ""))  # entity['type'] ??
         return resource_type, resource_name, resource
-
-
-'''
-    def scan(self, scanned_file, entity, skipped_checks, runner_filter=None):
-        entity_type, entity_configuration = self.extract_entity_details(entity)
-        results = {}
-        checks = self.get_checks(entity_type)
-        for check in checks:
-            skip_info = {}
-            if skipped_checks:
-                if check.id in [x['id'] for x in skipped_checks]:
-                    skip_info = [x for x in skipped_checks if x['id'] == check.id][0]
-                    
-            if runner_filter.should_run_check(check.id):
-                self.logger.debug("Running check: {} on file {}".format(check.name, scanned_file))
-                result = check.run(scanned_file=scanned_file, entity_configuration=entity_configuration,
-                                   entity_name=entity_type, entity_type=entity_type, skip_info=skip_info)
-                results[check] = result
-        return results
-'''
