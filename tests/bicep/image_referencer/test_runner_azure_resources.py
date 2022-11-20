f99872786bf396bba83c774fb9bea3c90c4d78d7
gruebel <anton.gruebel@gmail.com>
2022-11-20 15:49:17 +0100

    test for image_cached_results existence

--- tests/bicep/image_referencer/test_runner_azure_resources.py
+++ tests/bicep/image_referencer/test_runner_azure_resources.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from pytest_mock import MockerFixture
 
+from checkov.common.bridgecrew.bc_source import get_source_type
 from checkov.common.output.report import CheckType
 from checkov.runner_filter import RunnerFilter
 from checkov.bicep.runner import Runner
@@ -9,8 +10,12 @@ from checkov.bicep.runner import Runner
 RESOURCES_PATH = Path(__file__).parent / "resources/azure"
 
 
-def test_batch_resources(mocker: MockerFixture, image_cached_result):
+def test_batch_resources(mocker: MockerFixture, image_cached_result, license_statuses_result):
     # given
+    from checkov.common.bridgecrew.platform_integration import bc_integration
+
+    bc_integration.bc_source = get_source_type("disabled")
+
     file_name = "batch.bicep"
     image_name = "centos7"
     code_lines = "1-26"
@@ -23,7 +28,7 @@ def test_batch_resources(mocker: MockerFixture, image_cached_result):
     )
     mocker.patch(
         "checkov.common.images.image_referencer.get_license_statuses",
-        return_value=[],
+        return_value=license_statuses_result,
     )
 
     # when
@@ -41,12 +46,25 @@ def test_batch_resources(mocker: MockerFixture, image_cached_result):
     assert len(bicep_report.skipped_checks) == 0
     assert len(bicep_report.parsing_errors) == 0
 
-    assert len(sca_image_report.resources) == 1
-    assert sca_image_report.resources == {f"{file_name} ({image_name} lines:{code_lines} (sha256:f9b91f78b0)).zlib"}
-    assert len(sca_image_report.passed_checks) == 0
-    assert len(sca_image_report.failed_checks) == 1
+    assert len(sca_image_report.resources) == 3
+    assert sca_image_report.resources == {
+        f"{file_name} ({image_name} lines:{code_lines} (sha256:f9b91f78b0)).musl",
+        f"{file_name} ({image_name} lines:{code_lines} (sha256:f9b91f78b0)).openssl",
+        f"{file_name} ({image_name} lines:{code_lines} (sha256:f9b91f78b0)).zlib",
+    }
+    assert len(sca_image_report.passed_checks) == 1
+    assert len(sca_image_report.failed_checks) == 2
     assert len(sca_image_report.skipped_checks) == 0
     assert len(sca_image_report.parsing_errors) == 0
+    assert len(sca_image_report.image_cached_results) == 1
+
+    assert sca_image_report.image_cached_results[0]["dockerImageName"] == "centos7"
+    assert sca_image_report.image_cached_results[0]["relatedResourceId"].endswith(
+        "bicep/image_referencer/resources/azure/batch.bicep:Microsoft.Batch/batchAccounts/pools.pool"
+    )
+    assert sca_image_report.image_cached_results[0]["packages"] == [
+        {"type": "os", "name": "zlib", "version": "1.2.12-r1", "licenses": ["Zlib"]}
+    ]
 
 
 def test_container_instance_resources(mocker: MockerFixture, image_cached_result):
