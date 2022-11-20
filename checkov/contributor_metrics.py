c7d58a4111152749ca6b36a3ed4732bfe132c1b8
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2022-11-20 11:51:10 +0200

    add logs of source to report_contributor_metrics

--- checkov/contributor_metrics.py
+++ checkov/contributor_metrics.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def report_contributor_metrics(repository: str, source: str, bc_integration: BcPlatformIntegration) -> None:  # ignore: type
-    logging.debug(f"Attempting to get log history for repository {repository}")
+    logging.debug(f"Attempting to get log history for repository {repository} under source {source}")
     request_body = parse_gitlog(repository, source)
     if request_body:
         response = request_wrapper(
