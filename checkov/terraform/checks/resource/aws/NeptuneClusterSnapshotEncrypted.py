e6f75c84a379546f2d1c38599178629719459464
gruebel <anton.gruebel@gmail.com>
2022-11-20 22:34:18 +0100

    add type hints

--- checkov/terraform/checks/resource/aws/NeptuneClusterSnapshotEncrypted.py
+++ checkov/terraform/checks/resource/aws/NeptuneClusterSnapshotEncrypted.py
@@ -3,14 +3,14 @@ from checkov.common.models.enums import CheckCategories
 
 
 class NeptuneClusterSnapshotEncrypted(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure Neptune snapshot is securely encrypted"
         id = "CKV_AWS_279"
-        supported_resources = ['aws_neptune_cluster_snapshot']
-        categories = [CheckCategories.ENCRYPTION]
+        supported_resources = ("aws_neptune_cluster_snapshot",)
+        categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "storage_encrypted"
 
 
