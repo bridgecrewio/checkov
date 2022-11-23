4e1afb2c20ca822bf4e4654ff45fd1af44a30f1f
James Woolfenden <james.woolfenden@gmail.com>
2022-11-23 14:28:35 +0000

    check for rention policy

---
+++ checkov/terraform/checks/resource/azure/ACREnableRetentionPolicy.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+
+
+class ACREnableRetentionPolicy(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure a retention policy is set to cleanup untagged manifests."
+        id = "CKV_AZURE_167"
+        supported_resources = ['azurerm_container_registry']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self) -> str:
+        return "retention_policy/enabled"
+
+    def get_expected_value(self):
+        return True
+
+
+check = ACREnableRetentionPolicy()
