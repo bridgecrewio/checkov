169f73f04dda0567027d05d8e3194bc17e550926
Anton Grübel <anton.gruebel@gmail.com>
2022-11-27 00:03:03 +0100

    remove unneeded get_expected_value

--- checkov/terraform/checks/resource/azure/ACREnableImageQuarantine.py
+++ checkov/terraform/checks/resource/azure/ACREnableImageQuarantine.py
@@ -3,18 +3,15 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 
 class ACREnableImageQuarantine(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure container image quarantine, scan, and mark images verified"
         id = "CKV_AZURE_166"
-        supported_resources = ['azurerm_container_registry']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("azurerm_container_registry",)
+        categories = (CheckCategories.SUPPLY_CHAIN,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
         return "quarantine_policy_enabled"
-
-    def get_expected_value(self):
-        return True
 
 
 check = ACREnableImageQuarantine()
