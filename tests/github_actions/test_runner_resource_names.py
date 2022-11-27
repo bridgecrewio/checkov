a5d5d3cd3dba80fda09d25c5941bdd26f0cfd31c
marynaKK <kabirmaryna@gmail.com>
2022-11-27 09:37:50 +0200

    added test to gha if there is no step name

--- tests/github_actions/test_runner_resource_names.py
+++ tests/github_actions/test_runner_resource_names.py
@@ -1,6 +1,5 @@
 import pytest
 from checkov.github_actions.runner import Runner
-from checkov.yaml_doc.runner import resolve_sub_name
 
 
 @pytest.mark.parametrize(
@@ -14,7 +13,7 @@ from checkov.yaml_doc.runner import resolve_sub_name
     ],
 )
 def test_resolve_job_name(start_line, end_line, expected_job_name, definition):
-    job_name = resolve_sub_name(definition, start_line, end_line, tag='jobs')
+    job_name = Runner.resolve_sub_name(definition, start_line, end_line, tag='jobs')
 
     assert job_name == expected_job_name
 
@@ -26,6 +25,8 @@ def test_resolve_job_name(start_line, end_line, expected_job_name, definition):
          ('jobs', 'jobs.*.steps[]'), 7, 23),
         ('jobs.*.steps[].jobs.*.steps[].CKV_GHA_3[18:23]', "jobs(container-test-job).steps[1](Check for dockerenv file)",
          ('jobs', 'jobs.*.steps[]'), 18, 23),
+        ('jobs.*.steps[].jobs.*.steps[].CKV_GHA_3[31:35]', "jobs(no_step_name_job).steps[1]",
+        ('jobs', 'jobs.*.steps[]'), 31, 35),
     ],
 )
 def test_get_resource(key, supported_entities, expected_key, start_line, end_line, definition):
