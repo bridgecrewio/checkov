492a9f92d497846bdc421369866055dee02f3fec
pj991207 <pj991207@gmail.com>
2022-11-29 15:35:01 +0900

    [22.11.29][fix]CKV_NCP_23
    BaseResourceCheck -> BaseResourceNegativeValueCheck

--- checkov/terraform/checks/resource/ncp/ServerPublicIP.py
+++ checkov/terraform/checks/resource/ncp/ServerPublicIP.py
@@ -1,8 +1,9 @@
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.consts import ANY_VALUE
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class ServerPublicIP(BaseResourceCheck):
+class ServerPublicIP(BaseResourceNegativeValueCheck):
     def __init__(self):
         name = "Ensure Server instance should not have public IP."
         id = "CKV_NCP_23"
@@ -10,10 +11,11 @@ class ServerPublicIP(BaseResourceCheck):
         categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resource)
 
-    def scan_resource_conf(self, conf):
-        if 'server_instance_no' in conf.keys():
-            return CheckResult.FAILED
-        return CheckResult.PASSED
+    def get_inspected_key(self):
+        return 'server_instance_no'
+
+    def get_forbidden_values(self):
+        return [ANY_VALUE]
 
 
 check = ServerPublicIP()
