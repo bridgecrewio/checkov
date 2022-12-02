483dfeb6987dde7d1a59bfbeb3e652b18fce8eff
Anton Grübel <anton.gruebel@gmail.com>
2022-12-02 22:12:17 +0100

    slight adjustments

--- checkov/terraform/checks/resource/azure/APIManagementMinTLS12.py
+++ checkov/terraform/checks/resource/azure/APIManagementMinTLS12.py
@@ -1,19 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
 class APIManagementMinTLS12(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure API management uses at least TLS 1.2"
         id = "CKV_AZURE_173"
-        supported_resources = ['azurerm_api_management']
-        categories = [CheckCategories.ENCRYPTION]
+        supported_resources = ("azurerm_api_management",)
+        categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
         if conf.get('security'):
-            security = conf.get('security')[0]
+            security = conf['security'][0]
             if 'enable_back_end_ssl30' in security:
                 if security['enable_back_end_ssl30'][0]:
                     return CheckResult.FAILED
