0c966b77e29c8426982aeeaf57ae7cbe067d7ba6
James Woolfenden <james.woolfenden@gmail.com>
2022-12-06 13:13:20 +0000

    fix(terraform): azurerm kusto cluster encryption - wrong attribute tested for (#3972)
    wrong attribute tested for

--- tests/terraform/checks/resource/azure/test_DataExplorerUsesDiskEncryption.py
+++ tests/terraform/checks/resource/azure/test_DataExplorerUsesDiskEncryption.py
@@ -1,77 +1,37 @@
 import unittest
+from pathlib import Path
 
-import hcl2
-
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.azure.DataExplorerUsesDiskEncryption import check
-from checkov.common.models.enums import CheckResult
+from checkov.terraform.runner import Runner
 
 
 class TestDataExplorerUsesDiskEncryption(unittest.TestCase):
+    def test(self):
+        test_files_dir = Path(__file__).parent / "example_DataExplorerUsesDiskEncryption"
 
-    def test_failure(self):
-        hcl_res = hcl2.loads("""
-                resource "azurerm_kusto_cluster" "example" {
-                  name                = "kustocluster"
-                  location            = azurerm_resource_group.rg.location
-                  resource_group_name = azurerm_resource_group.rg.name
-                
-                  sku {
-                    name     = "Standard_D13_v2"
-                    capacity = 2
-                  }
-                
-                  tags = {
-                    Environment = "Production"
-                  }
-                }
-                """)
-        resource_conf = hcl_res['resource'][0]['azurerm_kusto_cluster']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        report = Runner().run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
 
-    def test_failure2(self):
-        hcl_res = hcl2.loads("""
-                resource "azurerm_kusto_cluster" "example" {
-                  name                = "kustocluster"
-                  location            = azurerm_resource_group.rg.location
-                  resource_group_name = azurerm_resource_group.rg.name
-                
-                  sku {
-                    name     = "Standard_D13_v2"
-                    capacity = 2
-                  }
-                
-                  tags = {
-                    Environment = "Production"
-                  }
-                  enable_disk_encryption = false
-                }
-                """)
-        resource_conf = hcl_res['resource'][0]['azurerm_kusto_cluster']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        passing_resources = {
+            "azurerm_kusto_cluster.pass",
+        }
+        failing_resources = {
+            "azurerm_kusto_cluster.fail",
+            "azurerm_kusto_cluster.fail2",
+        }
 
-    def test_success(self):
-        hcl_res = hcl2.loads("""
-                resource "azurerm_kusto_cluster" "example" {
-                  name                = "kustocluster"
-                  location            = azurerm_resource_group.rg.location
-                  resource_group_name = azurerm_resource_group.rg.name
-                
-                  sku {
-                    name     = "Standard_D13_v2"
-                    capacity = 2
-                  }
-                  tags = {
-                    Environment = "Production"
-                  }
-                  enable_disk_encryption = true
-                }
-                """)
-        resource_conf = hcl_res['resource'][0]['azurerm_kusto_cluster']['example']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
