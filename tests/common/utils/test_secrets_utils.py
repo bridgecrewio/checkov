1b4f8b407461101cae5573f52b5876664cc17e55
arielk <ar.kruszyn@gmail.com>
2022-12-07 00:42:54 +0000

    Revert "handle omitting multiline secrets by attribute"
    This reverts commit 03111cbab2c7ed751a1e542aed5260184695931f.

--- tests/common/utils/test_secrets_utils.py
+++ tests/common/utils/test_secrets_utils.py
@@ -16,18 +16,6 @@ def test_omit_secret_value_from_checks_by_attribute(tfplan_resource_lines_with_s
                                          tfplan_resource_config_with_secrets, resource_attributes_to_omit
                                          ) == tfplan_resource_lines_without_secrets
 
-def test_omit_multiline_secret_value_from_checks_by_attribute(tfplan_resource_lines_with_multiline_secret,
-                                                    tfplan_resource_config_with_multiline_secret,
-                                                    tfplan_resource_lines_without_multiline_secret):
-    check = SecretExpirationDate()
-    check.entity_type = 'azurerm_key_vault_secret'
-    check_result = {'result': CheckResult.FAILED}
-    resource_attributes_to_omit = {'azurerm_key_vault_secret': 'value'}
-
-    assert omit_secret_value_from_checks(check, check_result, tfplan_resource_lines_with_multiline_secret,
-                                         tfplan_resource_config_with_multiline_secret, resource_attributes_to_omit
-                                         ) == tfplan_resource_lines_without_multiline_secret
-
 
 def test_omit_secret_value_from_checks_by_secret(aws_provider_lines_with_secrets, aws_provider_config_with_secrets,
                                                  aws_provider_lines_without_secrets):
