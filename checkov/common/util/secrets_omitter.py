90854c45fb90fca30c9fc4c31d09d915e45cee44
Eliran Turgeman <elturgeman@paloaltonetworks.com>
2022-12-07 15:55:44 +0200

    lint

--- checkov/common/util/secrets_omitter.py
+++ checkov/common/util/secrets_omitter.py
@@ -83,7 +83,7 @@ class SecretsOmitter:
                     continue
 
                 for secret_line_index, omitted_line in \
-                        zip(list(range(secret_check_line_range[0], secret_check_line_range[1] + 1)),
+                        zip(list(range(secret_check_line_range[0], secret_check_line_range[1] + 1)),  # noqa: B905
                             secrets_check_lines):
                     for entry_index, (line_index, _) in enumerate(check.code_block):
                         if secret_line_index == line_index:
