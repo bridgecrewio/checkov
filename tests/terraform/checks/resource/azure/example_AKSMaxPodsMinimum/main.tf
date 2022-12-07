35a8d6f6888cf18bdfa99f71205a6252673eed51
ChanochShayner <57212002+ChanochShayner@users.noreply.github.com>
2022-12-07 16:25:04 +0200

    fix(terraform): Fix CKV_AZURE_168 check (#4000)
    * Fix CKV_AZURE_168 check
    
    * Update checkov/terraform/checks/resource/azure/AKSMaxPodsMinimum.py
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- tests/terraform/checks/resource/azure/example_AKSMaxPodsMinimum/main.tf
+++ tests/terraform/checks/resource/azure/example_AKSMaxPodsMinimum/main.tf
@@ -21,6 +21,29 @@ resource "azurerm_kubernetes_cluster" "pass" {
   private_cluster_enabled = var.private_cluster
 }
 
+resource "azurerm_kubernetes_cluster" "failed_empty_max_pods" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = var.default_node_pool.name
+    node_count = var.default_node_pool.node_count
+    vm_size    = var.default_node_pool.vm_size
+    max_pods   = []
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  tags = var.tags
+  local_account_disabled = var.local_account_disabled
+  private_cluster_enabled = var.private_cluster
+}
+
 resource "azurerm_kubernetes_cluster" "fail" {
   name                = "example-aks1"
   location            = azurerm_resource_group.example.location
