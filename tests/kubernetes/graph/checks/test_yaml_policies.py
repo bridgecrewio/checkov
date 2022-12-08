aa7af87a514b6916ca9409d80999c0c952d00a81
Barak Fatal <35402131+bo156@users.noreply.github.com>
2022-12-08 14:53:59 +0200

    feat(kubernetes): Added CKV2_K8S_1 to ensure RoleBinding do not allow privilege escalation to a ServiceAccount/Node (#4004)
    * Added check only to tests to make sure a ClusterRoleBinding does not contain a ClusterRole which allows configuring other ClusterRoleBinding
    
    * Moved real check to checkov instead of tests dir
    
    * Added more tests, did not adjust the yaml to make it work
    
    * Made the second failing cluster fail by using a  attribute solver and use all available resources/verbs
    
    * Made RoleBinding fail as well by adding it to the resource types
    
    * added  attribute solver
    
    * added a test case for not using a Node/ServiceAccount
    
    * renamed to RoleBindingPE
    
    * Added handling for no connection case
    
    * Filled in a checkov id for the check
    
    * Added option to set K8sGraphFlags for tests
    
    * Added teardown to kubernetes graph registry inside TestCustomPolicyIntegration
    
    * Removed new attribute solver, used not on within condition instead

--- tests/kubernetes/graph/checks/test_yaml_policies.py
+++ tests/kubernetes/graph/checks/test_yaml_policies.py
@@ -1,4 +1,6 @@
+import contextlib
 import copy
+import os
 import os.path
 
 from checkov.common.graph.db_connectors.networkx.networkx_db_connector import NetworkxConnector
@@ -7,7 +9,19 @@ from checkov.common.models.enums import CheckResult
 from checkov.common.output.record import Record
 from checkov.common.output.report import Report
 from checkov.kubernetes.graph_manager import KubernetesGraphManager
+from checkov.kubernetes.kubernetes_graph_flags import CREATE_COMPLEX_VERTICES, CREATE_EDGES
 from tests.common.graph.checks.test_yaml_policies_base import TestYamlPoliciesBase
+
+
+@contextlib.contextmanager
+def with_k8s_graph_flags():
+    create_complex_vertices_env_var = os.environ.get('CREATE_COMPLEX_VERTICES')
+    create_edges_env_var = os.environ.get('CREATE_EDGES')
+    os.environ[CREATE_COMPLEX_VERTICES] = 'True'
+    os.environ[CREATE_EDGES] = 'True'
+    yield
+    os.environ[CREATE_COMPLEX_VERTICES] = create_complex_vertices_env_var or ''
+    os.environ[CREATE_EDGES] = create_edges_env_var or ''
 
 
 class TestYamlPolicies(TestYamlPoliciesBase):
@@ -16,7 +30,10 @@ class TestYamlPolicies(TestYamlPoliciesBase):
 
     def __init__(self, args):
         graph_manager = KubernetesGraphManager(db_connector=NetworkxConnector())
-        super().__init__(graph_manager, "checkov/kubernetes/checks/graph_checks",
+        real_graph_checks_relative_path = "checkov/kubernetes/checks/graph_checks"
+        real_graph_checks_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..',
+                                              real_graph_checks_relative_path)
+        super().__init__(graph_manager, real_graph_checks_path,
                          os.path.dirname(__file__) + "/test_checks", 'kubernetes', __file__, args)
 
     def test_AllowedCapabilities(self):
@@ -24,6 +41,10 @@ class TestYamlPolicies(TestYamlPoliciesBase):
 
     def test_AllowPrivilegeEscalation(self):
         self.go('AllowPrivilegeEscalation')
+
+    @with_k8s_graph_flags()
+    def test_RoleBindingPE(self) -> None:
+        self.go('RoleBindingPE')
 
     def create_report_from_graph_checks_results(self, checks_results, check):
         report = Report("kubernetes")
