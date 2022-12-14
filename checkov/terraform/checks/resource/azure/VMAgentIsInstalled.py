220d2caf0930f5cf80a0c6231269635065afcf34
gruebel <anton.gruebel@gmail.com>
2022-12-14 20:13:20 +0100

    adjust checks

--- checkov/terraform/checks/resource/azure/VMAgentIsInstalled.py
+++ checkov/terraform/checks/resource/azure/VMAgentIsInstalled.py
@@ -3,19 +3,25 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 
 class VMAgentIsInstalled(BaseResourceValueCheck):
-    def __init__(self):
-
+    def __init__(self) -> None:
         name = "Ensure VM agent is installed"
         id = "CKV_AZURE_179"
-        supported_resources = ['azurerm_windows_virtual_machine',
-                               'azurerm_windows_virtual_machine_scale_set',
-                               'azurerm_linux_virtual_machine_scale_set',
-                               'azurerm_linux_virtual_machine']
-        categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
-                         missing_block_result=CheckResult.FAILED)
+        supported_resources = (
+            "azurerm_windows_virtual_machine",
+            "azurerm_windows_virtual_machine_scale_set",
+            "azurerm_linux_virtual_machine_scale_set",
+            "azurerm_linux_virtual_machine",
+        )
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+            missing_block_result=CheckResult.FAILED,
+        )
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "provision_vm_agent"
 
 
--- checkov/terraform/checks/resource/azure/WinVMAutomaticUpdates.py
+++ checkov/terraform/checks/resource/azure/WinVMAutomaticUpdates.py
@@ -3,17 +3,17 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 
 class WinVMAutomaticUpdates(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         If enabled, updates are automatically applied
         """
         name = "Ensure Windows VM enables automatic updates"
         id = "CKV_AZURE_177"
-        supported_resources = ['azurerm_windows_virtual_machine', 'azurerm_windows_virtual_machine_scale_set']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("azurerm_windows_virtual_machine", "azurerm_windows_virtual_machine_scale_set")
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "enable_automatic_updates"
 
 
