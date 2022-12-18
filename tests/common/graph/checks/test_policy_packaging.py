a5804a0a2c1f5319a0b19cc1f6a8aaca5c37d807
gruebel <anton.gruebel@gmail.com>
2022-12-18 10:13:37 +0100

    add further details about the need of the __init__ file

--- tests/common/graph/checks/test_policy_packaging.py
+++ tests/common/graph/checks/test_policy_packaging.py
@@ -2,7 +2,12 @@ from pathlib import Path
 
 
 def test_graph_policies_packaging_requirements() -> None:
-    """Checks, if all graph_checks folder have a __init__.py file"""
+    """Checks, if all graph_checks folder have a __init__.py file
+
+    It is needed to add a __init__.py in the base of a folder
+    to treat it as a module and indicate Python to add it during
+    installation of the package.
+    """
 
     root_dir = Path(__file__).parents[4]
 
