2d9afb75bd119626257698a66c9411ae949e0e3c
ipeleg <ipeleg@paloaltonetworks.com>
2022-12-18 15:17:13 +0200

    dediccated error for failing downloading twistcli

--- checkov/main.py
+++ checkov/main.py
@@ -377,12 +377,15 @@ def run(banner: str = checkov_banner, argv: List[str] = sys.argv[1:]) -> Optiona
             return None
         files = [os.path.abspath(config.dockerfile_path)]
         runner = sca_image_runner()
-        result = runner.run(
-            root_folder='',
-            image_id=config.docker_image,
-            dockerfile_path=config.dockerfile_path,
-            runner_filter=runner_filter,
-        )
+        try:
+            result = runner.run(
+                root_folder='',
+                image_id=config.docker_image,
+                dockerfile_path=config.dockerfile_path,
+                runner_filter=runner_filter,
+            )
+        except TwistcliDownloadingError:
+            return 2
 
         results = result if isinstance(result, list) else [result]
         if len(results) > 1:
