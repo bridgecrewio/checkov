d8abd2458a3063bbf068a5ca423c1a9def750475
Anton Grübel <anton.gruebel@gmail.com>
2022-12-21 15:36:10 +0100

    feat(sca): add GCP Terraform resources for Image Referencer (#4094)
    * add GCP Terraform resources for Image Referencer
    
    * fix linting

---
+++ tests/terraform/image_referencer/resources/gcp/cloud_run.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_service" "example" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
