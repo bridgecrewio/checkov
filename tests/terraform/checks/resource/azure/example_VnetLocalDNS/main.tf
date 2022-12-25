7f9d2afbf9f447ffa127f03288dc53c3a21eaf29
yaaraverner <yaara@vernermail.com>
2022-12-25 16:02:53 +0200

    fix edge-case in check

--- tests/terraform/checks/resource/azure/example_VnetLocalDNS/main.tf
+++ tests/terraform/checks/resource/azure/example_VnetLocalDNS/main.tf
@@ -111,3 +111,26 @@ resource "azurerm_virtual_network" "fail3" {
     environment = "Production"
   }
 }
+
+resource "azurerm_virtual_network" "unknown" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["v"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
