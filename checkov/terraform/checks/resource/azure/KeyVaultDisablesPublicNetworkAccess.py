a1aaa863f842e5aa3a7756d919ef5afc332d53f0
pazbechor <pbechor@paloaltonetworks.com>
2022-12-27 11:00:02 +0200

    Add unique scan_resource_conf for the new check - KeyVaultDisablesPublicNetworkAccess

--- checkov/terraform/checks/resource/azure/KeyVaultDisablesPublicNetworkAccess.py
+++ checkov/terraform/checks/resource/azure/KeyVaultDisablesPublicNetworkAccess.py
@@ -1,4 +1,4 @@
-from checkov.common.models.enums import CheckCategories
+from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
@@ -15,6 +15,24 @@ class KeyVaultDisablesPublicNetworkAccess(BaseResourceValueCheck):
 
     def get_expected_value(self):
         return False
+
+    def scan_resource_conf(self, conf) -> CheckResult:
+        """
+        KeyVaultDisablesPublicNetworkAccess unique logic.
+        public_network_access_enabled default value is True (when creating it)
+        If it False - check pass
+        Otherwise, we check that network_acls configure exists.
+
+        """
+        network_acls_key = "network_acls"
+        if self.get_inspected_key() in conf:
+            conf_value = conf.get(self.get_inspected_key())
+            conf_value = conf_value[0] if isinstance(conf_value, list) else conf_value
+            if self.get_expected_value() == conf_value:
+                return CheckResult.PASSED
+        if conf.get(network_acls_key):
+            return CheckResult.PASSED
+        return CheckResult.FAILED
 
 
 check = KeyVaultDisablesPublicNetworkAccess()
