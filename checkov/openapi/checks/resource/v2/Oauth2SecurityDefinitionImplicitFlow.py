507658732e876551bb2820c0a99cd33fca41c456
arielkru <63583491+arielkru@users.noreply.github.com>
2022-12-28 07:58:18 +0000

    feat(openapi): Add v2 openAPI new checks (#4112)
    * CKV_OPENAPI_8
    
    * CKV_OPENAPI_9
    
    * CKV_OPENAPI_10
    
    * CKV_OPENAPI_11
    
    * CKV_OPENAPI_12
    
    * CKV_OPENAPI_13
    
    * CKV_OPENAPI_14
    
    * CKV_OPENAPI_15
    
    * CKV_OPENAPI_16
    
    * CKV_OPENAPI_17
    
    * CKV_OPENAPI_18
    
    * use self.is_start_end_line instead of self.irrelevant_keys
    
    * CKV_OPENAPI_19
    
    * CKV_OPENAPI_20
    
    * CKV_OPENAPI_21
    
    * fix structure
    
    * fix test structures
    
    * linting
    
    * improve regex
    
    * remove comment
    
    * update test
    
    * update tests
    
    * move tests to one file (test_python_policies)
    
    * remove URL validation checks (not security related)
    
    * remove URL check tests
    
    * anton's suggesstions
    
    * typo
    
    * adjust type hints
    
    * fix linting
    
    * fix PR comments
    
    Co-authored-by: gruebel <anton.gruebel@gmail.com>

--- checkov/openapi/checks/resource/v2/Oauth2SecurityDefinitionImplicitFlow.py
+++ checkov/openapi/checks/resource/v2/Oauth2SecurityDefinitionImplicitFlow.py
@@ -6,12 +6,12 @@ from checkov.common.checks.enums import BlockType
 from checkov.openapi.checks.resource.v2.BaseOpenapiCheckV2 import BaseOpenapiCheckV2
 
 
-class Oauth2SecurityRequirement(BaseOpenapiCheckV2):
+class Oauth2SecurityDefinitionImplicitFlow(BaseOpenapiCheckV2):
     def __init__(self) -> None:
-        id = "CKV_OPENAPI_2"
-        name = "Ensure that if the security scheme is not of type 'oauth2', the array value must be empty - version 2.0 files"
-        categories = (CheckCategories.API_SECURITY,)
-        supported_resources = ('security',)
+        id = "CKV_OPENAPI_12"
+        name = "Ensure no security definition is using implicit flow on OAuth2, which is deprecated - version 2.0 files"
+        categories = [CheckCategories.API_SECURITY]
+        supported_resources = ["securityDefinitions"]
         super().__init__(
             name=name,
             id=id,
@@ -21,27 +21,20 @@ class Oauth2SecurityRequirement(BaseOpenapiCheckV2):
         )
 
     def scan_openapi_conf(self, conf: dict[str, Any], entity_type: str) -> tuple[CheckResult, dict[str, Any]]:
-        security_values = conf.get("security") or [{}]
-        security_definitions = conf.get("securityDefinitions") or {}
-        non_oauth2_keys = []
+        security_definitions = conf.get('securityDefinitions') or {}
 
         for auth_key, auth_dict in security_definitions.items():
             if self.is_start_end_line(auth_key):
                 continue
-            auth_type = auth_dict.get("type")
-            if auth_type.lower() != "oauth2":
-                non_oauth2_keys.append(auth_key)
-
-        for auth_dict in security_values:
             if not isinstance(auth_dict, dict):
                 return CheckResult.UNKNOWN, conf
-            for key, auth_list in auth_dict.items():
-                if self.is_start_end_line(key):
-                    continue
-                if key in non_oauth2_keys and auth_list:
+            auth_type = auth_dict.get('type', '')
+            if auth_type.lower() == 'oauth2':
+                auth_flow = auth_dict.get('flow', '')
+                if auth_flow == 'implicit':
                     return CheckResult.FAILED, auth_dict
 
         return CheckResult.PASSED, conf
 
 
-check = Oauth2SecurityRequirement()
+check = Oauth2SecurityDefinitionImplicitFlow()
