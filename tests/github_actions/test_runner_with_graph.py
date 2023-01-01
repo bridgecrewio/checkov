656946111cec225133747d0eb27a80798df20da5
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2023-01-01 12:35:23 +0200

    change defs and defs_raw in test_runner_with_graph.py to be static

--- tests/github_actions/test_runner_with_graph.py
+++ tests/github_actions/test_runner_with_graph.py
@@ -18,8 +18,8 @@ def test_runner_with_existing_graph():
     file_path = Path(__file__).parent / "gha/.github/workflows/failed.yaml"
     file_dir = [str(file_path)]
     checks = ["CKV2_GHA_1"]
-    definitions, definitions_raw = get_gha_files_definitions(root_folder=str(Path(__file__).parent / "gha"),
-                                                             files=[str(Path(__file__).parent / "gha/.github/workflows/failed.yaml")])
+    definitions = {'/Users/mamelchenko/development/checkov/tests/github_actions/gha/.github/workflows/failed.yaml': {'name': 'read-only', 'on': {'pull_request': {'types': ['opened', 'synchronize', 'labeled', 'unlabeled'], '__startline__': 5, '__endline__': 7}, '__startline__': 4, '__endline__': 7}, 'permissions': 'write-all', 'jobs': {'example': {'runs-on': 'ubuntu-latest', 'steps': [{'uses': 'actions/checkout@12345678', '__startline__': 13, '__endline__': 14}, {'run': 'echo "working hard"\n', '__startline__': 14, '__endline__': 16}], '__startline__': 11, '__endline__': 16}, '__startline__': 10, '__endline__': 16}, '__startline__': 1, '__endline__': 16}}
+    definitions_raw = {'/Users/mamelchenko/development/checkov/tests/github_actions/gha/.github/workflows/failed.yaml': [(1, 'name: read-only\n'), (2, '\n'), (3, 'on:\n'), (4, '  pull_request:\n'), (5, '    types: [ opened, synchronize, labeled, unlabeled ]\n'), (6, '\n'), (7, 'permissions: write-all\n'), (8, '\n'), (9, 'jobs:\n'), (10, '  example:\n'), (11, '    runs-on: ubuntu-latest\n'), (12, '    steps:\n'), (13, '      - uses: actions/checkout@12345678\n'), (14, '      - run: |\n'), (15, '          echo "working hard"\n')]}
     context = build_gha_definitions_context(definitions=definitions, definitions_raw=definitions_raw)
 
     graph_runner = Runner()
