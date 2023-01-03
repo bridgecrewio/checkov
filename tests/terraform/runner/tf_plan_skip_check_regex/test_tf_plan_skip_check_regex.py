8bed329b144048875d55109334f58804f58ed038
pazbechor <pbechor@paloaltonetworks.com>
2023-01-03 10:30:00 +0200

    Adding tests to tfplan

--- tests/terraform/runner/tf_plan_skip_check_regex/test_tf_plan_skip_check_regex.py
+++ tests/terraform/runner/tf_plan_skip_check_regex/test_tf_plan_skip_check_regex.py
@@ -23,14 +23,45 @@ class TestTFplanSkipCheckRegex(unittest.TestCase):
                 "CKV_ADO_*:.*.json",
                 "CKV2_ADO_*:.*.json",
                 "CKV_OCI_*:.*.json",
+                "CKV2_OCI_*:.*.json",
                 "CKV_GIT_*:.*.json",
-                "CKV2_GIT_1:.*.json",
+                "CKV2_GIT_1:.*.json"
             ])
         )
         summary = report.get_summary()
 
         self.assertEqual(summary['passed'], 0)
         self.assertEqual(summary['failed'], 0)
+        # As skip is not being inserted to result in base check scan
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+
+
+    def test_skip_all_checks_but_one(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        test_files_dir = current_dir + "/resource"
+        report = runner.run(
+            root_folder=test_files_dir,
+            runner_filter=RunnerFilter(skip_checks=[
+                "CKV_AWS_*:.*.json$",
+                # "CKV2_AWS_*:.*.json$",
+                "CKV_NCP_*:.*.json$",
+                "CKV_AZURE_*:.*.json",
+                "CKV2_AZURE_*:.*.json",
+                "CKV2_GCP_*:.*.json",
+                "CKV_ADO_*:.*.json",
+                "CKV2_ADO_*:.*.json",
+                "CKV_OCI_*:.*.json",
+                "CKV2_OCI_*:.*.json",
+                "CKV_GIT_*:.*.json",
+                "CKV2_GIT_1:.*.json"
+            ])
+        )
+        summary = report.get_summary()
+
+        self.assertEqual(summary['passed'], 9)
+        self.assertEqual(summary['failed'], 13)
         # As skip is not being inserted to result in base check scan
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
