320982a451e09313427d026f4b6606104fcac21d
James Woolfenden <james.woolfenden@gmail.com>
2023-01-04 10:00:42 +0000

    check kms policy doers nto define public access

---
+++ checkov/terraform/checks/resource/gcp/GoogleKMSKeyIsPublic.py
@@ -0,0 +1,35 @@
+from typing import Dict, Any
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class GoogleKMSKeyIsPublic(BaseResourceCheck):
+    def __init__(self) -> None:
+        """
+        ensure key is not publicly available
+        KMS Crypto Key policy should not set 'allUsers' or 'allAuthenticatedUsers' in the attribute 'member'/'members'
+        """
+        name = "KMS policy should not define public access"
+        id = "CKV_GCP_112"
+        supported_resources = ["google_kms_crypto_key_iam_policy"]
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: Dict[str, Any]) -> CheckResult:
+        fails = ["allUsers", "allAuthenticatedUsers"]
+        if conf.get("policy_data") and isinstance(conf.get("policy_data"), list):
+            policy_data = conf.get("policy_data")
+            for policy in policy_data:
+                bindings = policy.get("bindings")
+                for binding in bindings:
+                    if binding.get("members") and isinstance(binding.get("members"), list):
+                        members = binding.get("members")
+                        for member in members:
+                            if member in fails:
+                                return CheckResult.FAILED
+            return CheckResult.PASSED
+        return CheckResult.UNKNOWN
+
+
+check = GoogleKMSKeyIsPublic()
---
+++ tests/terraform/checks/resource/gcp/example_GoogleKMSKeyIsPublic/main.tf
@@ -0,0 +1,65 @@
+#data "google_iam_policy" "fail_access" {
+#  binding {
+#    role    = "roles/cloudkms.cryptoKeyEncrypter"
+#    members = ["allUsers"]
+#  }
+#}
+
+resource "google_kms_crypto_key_iam_policy" "fail" {
+  crypto_key_id = google_kms_crypto_key.positive1.id
+  policy_data = jsonencode({
+
+    bindings = [{
+      role    = "roles/cloudkms.cryptoKeyEncrypter"
+      members = ["allUsers"]
+  }] })
+}
+
+#data "google_iam_policy" "pass_access" {
+#  binding {
+#    role    = "roles/cloudkms.cryptoKeyEncrypter"
+#    members = ["jameswoolfenden"]
+#  }
+#}
+
+resource "google_kms_crypto_key_iam_policy" "pass" {
+  crypto_key_id = google_kms_crypto_key.pass.id
+  policy_data = jsonencode({
+    bindings = [
+      {
+        role    = "roles/cloudkms.cryptoKeyEncrypter"
+        members = ["jameswoolfenden"]
+  }] })
+}
+
+resource "google_kms_crypto_key_iam_policy" "fail2" {
+  crypto_key_id = google_kms_crypto_key.pass.id
+  policy_data = jsonencode({
+    bindings = [
+      {
+        members = [
+          "user:jane@example.com",
+        ]
+        role = "roles/cloudkms.admin"
+      },
+      {
+        members = [
+          "allAuthenticatedUsers",
+        ]
+        role = "roles/cloudkms.cryptoKeyDecrypter"
+      },
+    ]
+  })
+}
+
+resource "google_kms_crypto_key_iam_policy" "pass2" {
+  crypto_key_id = google_kms_crypto_key.pass.id
+  policy_data   = <<HERE
+{
+    "bindings": [{
+      "role": "roles/cloudkms.cryptoKeyEncrypter",
+      "members": ["user:jameswoolfeden"]
+    }]
+}
+HERE
+}
