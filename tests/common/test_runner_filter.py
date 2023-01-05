ff0aab0b6f9141d3e810855765dc213051c40ee7
pazbechor <pbechor@paloaltonetworks.com>
2023-01-05 17:53:51 +0200

    assert dict equal in different way

--- tests/common/test_runner_filter.py
+++ tests/common/test_runner_filter.py
@@ -668,7 +668,11 @@ class TestRunnerFilter(unittest.TestCase):
         }
         runner_filter = RunnerFilter(resource_attr_to_omit_paths=["resource_attr_to_omit_configs/first.json"])
         assert runner_filter.resource_attr_to_omit
-        assert dict(runner_filter.resource_attr_to_omit) == first_file_real_parsed_content
+        for k, v in runner_filter.resource_attr_to_omit.items():
+            assert v == first_file_real_parsed_content.get(k)
+
+        for k, v in first_file_real_parsed_content.items():
+            assert v == runner_filter.resource_attr_to_omit.get(k)
 
     def test_resource_attr_to_omit_load_config_sanity_absolute_path(self):
         """
@@ -688,7 +692,11 @@ class TestRunnerFilter(unittest.TestCase):
         absolute_path = os.getcwd() + "/resource_attr_to_omit_configs/first.json"
         runner_filter = RunnerFilter(resource_attr_to_omit_paths=[absolute_path])
         assert runner_filter.resource_attr_to_omit
-        assert dict(runner_filter.resource_attr_to_omit) == first_file_real_parsed_content
+        for k, v in runner_filter.resource_attr_to_omit.items():
+            assert v == first_file_real_parsed_content.get(k)
+
+        for k, v in first_file_real_parsed_content.items():
+            assert v == runner_filter.resource_attr_to_omit.get(k)
 
     def test_resource_attr_to_omit_load_config_corrupted(self):
         absolute_path = os.getcwd() + "/resource_attr_to_omit_configs/corrupted.json"
@@ -707,7 +715,13 @@ class TestRunnerFilter(unittest.TestCase):
         corrupted_absolute_path = os.getcwd() + "/resource_attr_to_omit_configs/corrupted.json"
         fine_relative_path = "resource_attr_to_omit_configs/first.json"
         runner_filter = RunnerFilter(resource_attr_to_omit_paths=[corrupted_absolute_path, fine_relative_path])
+
         assert runner_filter.resource_attr_to_omit
+        for k, v in runner_filter.resource_attr_to_omit.items():
+            assert v == first_file_real_parsed_content.get(k)
+
+        for k, v in first_file_real_parsed_content.items():
+            assert v == runner_filter.resource_attr_to_omit.get(k)
         assert dict(runner_filter.resource_attr_to_omit) == first_file_real_parsed_content
 
     def test_resource_attr_to_omit_load_config_int_value(self):
@@ -722,7 +736,11 @@ class TestRunnerFilter(unittest.TestCase):
         relative_path = "resource_attr_to_omit_configs/third.json"
         runner_filter = RunnerFilter(resource_attr_to_omit_paths=[relative_path])
         assert runner_filter.resource_attr_to_omit
-        assert dict(runner_filter.resource_attr_to_omit) == third_file_real_parsed_content
+        for k, v in runner_filter.resource_attr_to_omit.items():
+            assert v == third_file_real_parsed_content.get(k)
+
+        for k, v in third_file_real_parsed_content.items():
+            assert v == runner_filter.resource_attr_to_omit.get(k)
 
     def test_resource_attr_to_omit_load_config_sanity_combine(self):
         combined_file_real_parsed_content = {
@@ -738,8 +756,13 @@ class TestRunnerFilter(unittest.TestCase):
         absolute_path = os.getcwd() + "/resource_attr_to_omit_configs/first.json"
         relative_path = "resource_attr_to_omit_configs/second.json"
         runner_filter = RunnerFilter(resource_attr_to_omit_paths=[absolute_path, relative_path])
+
         assert runner_filter.resource_attr_to_omit
-        assert dict(runner_filter.resource_attr_to_omit) == combined_file_real_parsed_content
+        for k, v in runner_filter.resource_attr_to_omit.items():
+            assert v == combined_file_real_parsed_content.get(k)
+
+        for k, v in combined_file_real_parsed_content.items():
+            assert v == runner_filter.resource_attr_to_omit.get(k)
 
 
 if __name__ == '__main__':
