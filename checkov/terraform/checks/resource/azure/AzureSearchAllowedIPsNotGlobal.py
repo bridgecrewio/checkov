a9f5eb80f2d1ef8b02de341e9a9bc0c1a91697ed
gruebel <anton.gruebel@gmail.com>
2023-01-12 20:49:29 +0100

    adjust check names

--- checkov/terraform/checks/resource/azure/AzureSearchAllowedIPsNotGlobal.py
+++ checkov/terraform/checks/resource/azure/AzureSearchAllowedIPsNotGlobal.py
@@ -1,21 +1,24 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckCategories
 from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
-from typing import List, Any
 
 
 class AzureSearchAllowedIPsNotGlobal(BaseResourceNegativeValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         # Setting the allowed ips to include global routes CIDR - 0.0.0.0/0 makes the resource public
-        name = "Ensure Azure search service allowed IPS does not give public Access"
+        name = "Ensure Azure Cognitive Search service allowed IPS does not give public Access"
         id = "CKV_AZURE_210"
-        supported_resources = ['azurerm_search_service']
+        supported_resources = ("azurerm_search_service",)
         categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
         return "allowed_ips"
 
-    def get_forbidden_values(self) -> List[Any]:
+    def get_forbidden_values(self) -> list[Any]:
         return ["0.0.0.0/0"]
 
 
--- checkov/terraform/checks/resource/azure/AzureSearchSLAQueryUpdates.py
+++ checkov/terraform/checks/resource/azure/AzureSearchSLAQueryUpdates.py
@@ -1,28 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from typing import Dict, List, Any
 
 
 class AzureSearchSQLQueryUpdates(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         # Cognitive Search services support indexing and querying. Indexing is the process of loading content
         # into the service to make it searchable. Querying is the process where a client searches for content
         # by sending queries to the index.
         # Cognitive Search supports a configurable number of replicas.
         # Having multiple replicas allows queries and index updates to load balance across multiple replicas.
         # To receive a Service Level Agreement (SLA) for Search index queries a minimum of 2 replicas is required.
-        name = "Ensure that Azure Search maintains SLA for search index queries"
+        name = "Ensure that Azure Cognitive Search maintains SLA for search index queries"
         id = "CKV_AZURE_209"
-        supported_resources = ['azurerm_search_service']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("azurerm_search_service",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        if conf.get("replica_count"):
-            count = conf.get("replica_count")[0]
-            if count >= 2:
-                return CheckResult.PASSED
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
         self.evaluated_keys = ["replica_count"]
+
+        replica_count = conf.get("replica_count")
+        if replica_count and isinstance(replica_count, list):
+            if replica_count[0] >= 2:
+                return CheckResult.PASSED
+
         return CheckResult.FAILED
 
 
