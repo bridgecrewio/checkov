c129cb63d09cc666892455606b8c4cd135e27cd6
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2023-01-15 17:35:27 +0200

    add indents to json in test

--- tests/circleci_pipelines/image_referencer/test_manager.py
+++ tests/circleci_pipelines/image_referencer/test_manager.py
@@ -23,13 +23,17 @@ def test_extract_images_from_workflow_nested(circle_ci_filepath_workflow_no_imag
     file_path = '/tmp/test_path'
     config = {
         'workspace_root': '/go/src/github.com/gruntwork-io/terragrunt',
-        'defaults': {'working_directory': '/go/src/github.com/gruntwork-io/terragrunt', 'docker': [
-            {'image': '087285199408.dkr.ecr.us-east-1.amazonaws.com/circle-ci-test-image-base:go1.11',
-             '__startline__': 6, '__endline__': 8}], '__startline__': 3, '__endline__': 8},
-        'jobs': {
-            'install_dependencies': {'working_directory': '/go/src/github.com/gruntwork-io/terragrunt', 'docker': [
+        'defaults': {
+            'working_directory': '/go/src/github.com/gruntwork-io/terragrunt', 'docker': [
                 {'image': '087285199408.dkr.ecr.us-east-1.amazonaws.com/circle-ci-test-image-base:go1.11',
-                 '__startline__': 6, '__endline__': 8}], 'steps': ['checkout'], '__startline__': 11, '__endline__': 32}
+                    '__startline__': 6, '__endline__': 8}], '__startline__': 3, '__endline__': 8
+        },
+        'jobs': {
+            'install_dependencies': {
+                'working_directory': '/go/src/github.com/gruntwork-io/terragrunt', 'docker': [
+                {'image': '087285199408.dkr.ecr.us-east-1.amazonaws.com/circle-ci-test-image-base:go1.11',
+                 '__startline__': 6, '__endline__': 8}], 'steps': ['checkout'], '__startline__': 11, '__endline__': 32
+            }
         }, '__startline__': 1, '__endline__': 143
     }
 
