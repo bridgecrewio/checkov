8dfbb74b69bb087479e1b2cb274202acf91d23b7
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-01-15 13:37:58 +0200

    fix(secrets): Revert "fix(secrets): Change secret validation status message" (#4248)
    Revert "fix(secrets): Change secret validation status message (#4238)"
    
    This reverts commit 8e7275eed1117e2fee92d073a8e18ff1c090b7d9.

--- checkov/secrets/consts.py
+++ checkov/secrets/consts.py
@@ -6,7 +6,6 @@ class ValidationStatus(Enum):
     VALID = 'Valid'
     INVALID = 'Invalid'
     UNKNOWN = 'Unknown'
-    UNAVAILABLE = 'UNAVAILABLE'
 
     def __str__(self) -> str:
         # needed, because of a Python 3.11 change
