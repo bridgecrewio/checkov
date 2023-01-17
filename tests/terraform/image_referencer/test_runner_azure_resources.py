2771dbdf84e938859cd604ecf3ad13bc0c4b739d
James Woolfenden <james.woolfenden@gmail.com>
2023-01-17 11:08:48 +0000

    wrong test numbers

--- tests/terraform/image_referencer/test_runner_azure_resources.py
+++ tests/terraform/image_referencer/test_runner_azure_resources.py
@@ -168,8 +168,8 @@ def test_app_service_linux_web_resources(mocker: MockerFixture, image_cached_res
     sca_image_report = next(report for report in reports if report.check_type == CheckType.SCA_IMAGE)
 
     assert len(tf_report.resources) == 2
-    assert len(tf_report.passed_checks) == 2
-    assert len(tf_report.failed_checks) == 11
+    assert len(tf_report.passed_checks) == 4
+    assert len(tf_report.failed_checks) == 12
     assert len(tf_report.skipped_checks) == 0
     assert len(tf_report.parsing_errors) == 0
 
