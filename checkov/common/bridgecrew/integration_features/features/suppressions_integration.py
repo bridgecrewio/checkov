a467c7de49ac55bd15f03f39c1dee688a81a8e22
tronxd <tron47@gmail.com>
2023-01-18 18:59:11 +0200

    flake8 lint

--- checkov/common/bridgecrew/integration_features/features/suppressions_integration.py
+++ checkov/common/bridgecrew/integration_features/features/suppressions_integration.py
@@ -215,7 +215,7 @@ class SuppressionsIntegration(BaseIntegrationFeature):
 
     def get_policy_level_suppressions(self) -> List[str]:
         policy_level_suppressions = []
-        for check_id, check_suppressions in self.suppressions.items():
+        for _, check_suppressions in self.suppressions.items():
             for suppression in check_suppressions:
                 if suppression.get("suppressionType") == "Policy":
                     policy_level_suppressions.append(suppression['policyId'])
