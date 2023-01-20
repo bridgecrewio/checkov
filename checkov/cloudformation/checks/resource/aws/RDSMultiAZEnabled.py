00e941042fff6edbf65a2106245f166e95846660
Lorenzo <ljgallegos1321@gmail.com>
2023-01-20 13:45:04 -0700

    Fix CKV_AWS_157 CloudFormation false positive for Aurora instances

--- checkov/cloudformation/checks/resource/aws/RDSMultiAZEnabled.py
+++ checkov/cloudformation/checks/resource/aws/RDSMultiAZEnabled.py
@@ -1,8 +1,7 @@
-from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
 from checkov.common.models.enums import CheckCategories
 
-
-class RDSMultiAZEnabled(BaseResourceValueCheck):
+class RDSMultiAZEnabled(BaseResourceCheck):
     def __init__(self):
         name = "Ensure that RDS instances have Multi-AZ enabled"
         id = "CKV_AWS_157"
@@ -10,8 +9,18 @@ class RDSMultiAZEnabled(BaseResourceValueCheck):
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
-        return 'Properties/MultiAZ'
-
+    def scan_resource_conf(self, conf):
+        properties = conf.get("Properties")
+        aurora = "aurora"
+        if properties:
+            engine = properties.get("Engine")
+            if engine and engine in aurora:
+                return CheckResult.PASSED
+            value = properties.get("MultiAZ")
+            if isinstance(value, bool):
+                value = str(value).lower()
+            if value == "true":
+                return CheckResult.PASSED
+        return CheckResult.FAILED
 
 check = RDSMultiAZEnabled()
--- tests/cloudformation/checks/resource/aws/test_RDSMultiAZEnabled.py
+++ tests/cloudformation/checks/resource/aws/test_RDSMultiAZEnabled.py
@@ -1,40 +1,26 @@
-import os
-import unittest
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories
 
-from checkov.cloudformation.checks.resource.aws.RDSMultiAZEnabled import check
-from checkov.cloudformation.runner import Runner
-from checkov.runner_filter import RunnerFilter
+class RDSMultiAZEnabled(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure that RDS instances have Multi-AZ enabled"
+        id = "CKV_AWS_157"
+        supported_resources = ['AWS::RDS::DBInstance']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
+    def scan_resource_conf(self, conf):
+        properties = conf.get("Properties")
+        aurora = "aurora"
+        if properties:
+            engine = properties.get("Engine")
+            if engine and engine in aurora:
+                return CheckResult.PASSED
+            value = properties.get("MultiAZ")
+            if isinstance(value, bool):
+                value = str(value).lower()
+            if value == "true":
+                return CheckResult.PASSED
+        return CheckResult.FAILED
 
-class TestRDSMultiAZEnabled(unittest.TestCase):
-
-    def test_summary(self):
-        runner = Runner()
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-
-        test_files_dir = current_dir + "/example_RDSMultiAZEnabled"
-        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
-        summary = report.get_summary()
-
-        passing_resources = {
-            "AWS::RDS::DBInstance.MyDBEnabled",
-        }
-        failing_resources = {
-            "AWS::RDS::DBInstance.MyDBDefault",
-            "AWS::RDS::DBInstance.MyDBDisabled",
-        }
-
-        passed_check_resources = set([c.resource for c in report.passed_checks])
-        failed_check_resources = set([c.resource for c in report.failed_checks])
-
-        self.assertEqual(summary['passed'], 1)
-        self.assertEqual(summary['failed'], 2)
-        self.assertEqual(summary['skipped'], 0)
-        self.assertEqual(summary['parsing_errors'], 0)
-
-        self.assertEqual(passing_resources, passed_check_resources)
-        self.assertEqual(failing_resources, failed_check_resources)
-
-
-if __name__ == '__main__':
-    unittest.main()
+check = RDSMultiAZEnabled()
