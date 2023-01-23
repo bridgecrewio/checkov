1c499cb8b66700a881217ee8af72a98392ac2158
Lorenzo <ljgallegos1321@gmail.com>
2023-01-23 16:37:18 -0700

    Fix import

--- checkov/cloudformation/checks/resource/aws/RDSMultiAZEnabled.py
+++ checkov/cloudformation/checks/resource/aws/RDSMultiAZEnabled.py
@@ -1,5 +1,5 @@
 from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.models.enums import CheckCategories
+from checkov.common.models.enums import CheckResult, CheckCategories
 
 class RDSMultiAZEnabled(BaseResourceCheck):
     def __init__(self):
