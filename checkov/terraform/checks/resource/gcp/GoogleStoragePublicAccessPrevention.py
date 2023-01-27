6c35873a789349fc875452a26bc2cfa407708029
Anthony Martines <anthony.martines@cybelangel.com>
2023-01-27 19:49:01 +0100

    feat: Add CKV_GCP_112 to check if buckets have public access prevention enforced

---
+++ checkov/terraform/checks/resource/gcp/GoogleStoragePublicAccessPrevention.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+
+class GoogleStoragePublicAccessPrevention(BaseResourceValueCheck):
+    def __init__(self) -> None:
+        name = "Public access prevention should be enforced"
+        id = "CKV_GCP_112"
+        supported_resources = ["google_storage_bucket"]
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self) -> str:
+        return "public_access_prevention"
+
+    def get_expected_value(self) -> Any:
+        return "enforced"
+
+
+check = GoogleStoragePublicAccessPrevention()
