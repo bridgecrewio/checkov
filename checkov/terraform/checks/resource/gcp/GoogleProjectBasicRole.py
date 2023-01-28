38162284109250371a056074c080c1a85f78282a
Anthony Martines <anthony.martines@cybelangel.com>
2023-01-28 11:42:19 +0100

    feat(terraform/gcp): Add CKV_GCP_115 to ensure Basic role are not used at Project level

---
+++ checkov/terraform/checks/resource/gcp/GoogleProjectBasicRole.py
@@ -0,0 +1,14 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.gcp.AbsGoogleBasicRoles import AbsGoogleBasicRoles
+
+
+class GoogleProjectBasicRoles(AbsGoogleBasicRoles):
+    def __init__(self) -> None:
+        name = "Usage of basic roles at project level should be avoided."
+        id = "CKV_GCP_115"
+        supported_resources = ('google_project_iam_member', 'google_project_iam_binding')
+        categories = (CheckCategories.IAM,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+
+check = GoogleProjectBasicRoles()
