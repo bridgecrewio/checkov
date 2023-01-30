6e1dfed6b9e29aa4d352c3e1fa3ca68ee1e688f7
Anton Grübel <anton.gruebel@gmail.com>
2023-01-30 17:36:34 +0100

    fix(sca): extend image name validation (#4377)
    * extend image name validation
    
    * fix test name
    
    Co-authored-by: Peleg Litvak <peleg.litvak@gmail.com>
    
    ---------
    
    Co-authored-by: Peleg Litvak <peleg.litvak@gmail.com>

--- tests/common/images/test_base_image_referencer.py
+++ tests/common/images/test_base_image_referencer.py
@@ -15,8 +15,14 @@ class TestImageReferencerBase(unittest.TestCase):
             "gcr.io/[\"${{'develop': {'project_id': 'develop'}, 'production': {'project_id': 'production'}}[\"var.env\"].project_id}\"]/notifier:aa123aa"
         ))
 
+    def test_invalid_image_name_var_reference(self):
+        self.assertFalse(self.run_is_valid_public_image_valid('gcr.io/example/base:$IMAGE_TAG'))
+
     def test_localhost_image_name(self):
         self.assertFalse(self.run_is_valid_public_image_valid('localhost:320000/video-conferencing-ms-example'))
+
+    def test_cname_with_port_image_name(self):
+        self.assertFalse(self.run_is_valid_public_image_valid('example.local:5004/video-conferencing-ms-example:1.2.3'))
 
     def test_valid_image_name(self):
         self.assertTrue(self.run_is_valid_public_image_valid('node:16'))
