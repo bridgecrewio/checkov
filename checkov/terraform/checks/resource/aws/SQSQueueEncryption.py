973c89472660b02b626136138d209bd9e31e6275
Anton Grübel <anton.gruebel@gmail.com>
2023-02-01 22:46:43 +0100

    adjust check

--- checkov/terraform/checks/resource/aws/SQSQueueEncryption.py
+++ checkov/terraform/checks/resource/aws/SQSQueueEncryption.py
@@ -1,4 +1,3 @@
-
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
@@ -7,17 +6,20 @@ class SQSQueueEncryption(BaseResourceCheck):
     def __init__(self) -> None:
         name = "Ensure all data stored in the SQS queue is encrypted"
         id = "CKV_AWS_27"
-        supported_resources = ['aws_sqs_queue']
-        categories = [CheckCategories.ENCRYPTION]
+        supported_resources = ('aws_sqs_queue',)
+        categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if conf.get('sqs_managed_sse_enabled') and isinstance(conf.get('sqs_managed_sse_enabled'), list):
-            if conf.get('sqs_managed_sse_enabled')[0]:
+        sqs_managed_sse_enabled = conf.get("sqs_managed_sse_enabled")
+        if sqs_managed_sse_enabled and isinstance(sqs_managed_sse_enabled, list):
+            if sqs_managed_sse_enabled[0]:
                 return CheckResult.PASSED
             return CheckResult.FAILED
-        if conf.get("kms_master_key_id") and isinstance(conf.get('kms_master_key_id'), list):
-            if conf.get('kms_master_key_id')[0]:
+
+        kms_master_key_id = conf.get("kms_master_key_id")
+        if kms_master_key_id and isinstance(kms_master_key_id, list):
+            if kms_master_key_id[0]:
                 return CheckResult.PASSED
             return CheckResult.FAILED
         return CheckResult.FAILED
