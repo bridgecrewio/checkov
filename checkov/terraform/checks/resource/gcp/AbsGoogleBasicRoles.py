fbada8b23b94549c888774ce13a530ae1d744063
ToNYmSss <anthonymartines01@gmail.com>
2023-02-01 16:09:54 +0100

    feat(terraform/gcp): add CKV_GCP_115, CKV_GCP_116 & CKV_GCP_117

---
+++ checkov/terraform/checks/resource/gcp/AbsGoogleBasicRoles.py
@@ -0,0 +1,16 @@
+from checkov.common.models.enums import CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+BASIC_ROLES = [
+    "roles/owner",
+    "roles/editor",
+    "roles/viewer",
+]
+
+
+class AbsGoogleBasicRoles(BaseResourceCheck):
+    def scan_resource_conf(self, conf):
+        self.evaluated_keys = ['role']
+        if 'role' in conf and conf['role'][0] in BASIC_ROLES:
+            return CheckResult.FAILED
+        return CheckResult.PASSED
