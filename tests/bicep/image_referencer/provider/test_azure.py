a4a13b6991e3dcf94969bf4dc08b60846497e9d0
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2023-02-01 11:15:15 +0200

    feat(sca): add extract nodes igraph (#4359)
    * add extract_resource
    
    * test
    
    * test
    
    * test
    
    * test
    
    * add terraform test
    
    * add terraform test
    
    * add terraform test
    
    * itertools.zip_longest
    
    * use mock.patch.dict
    
    * use with mock.patch.dict
    
    * use with mock.patch.dict
    
    * .
    
    * not using parameterized_class
    
    * not using parameterized_class

--- tests/bicep/image_referencer/provider/test_azure.py
+++ tests/bicep/image_referencer/provider/test_azure.py
@@ -1,32 +1,50 @@
+import os
+
+from unittest import mock
+
+import igraph
+import pytest
 from networkx import DiGraph
 
+from checkov.common.graph.graph_builder import CustomAttributes
 from checkov.common.images.image_referencer import Image
 from checkov.terraform.image_referencer.provider.azure import AzureTerraformProvider
 
 
-def extract_images_from_resources():
+@pytest.mark.parametrize("graph_framework", ['NETWORKX', 'IGRAPH'])
+def extract_images_from_resources(graph_framework):
     # given
     resource = {
-        "file_path_": "/batch.bicep",
-        "__end_line__": 26,
-        "__start_line__": 1,
-        "properties": {
-            "virtualMachineConfiguration": {
-                "containerConfiguration": {
-                    "containerImageNames": ["nginx", "python:3.9-alpine"],
-                    "containerRegistries": {
-                        "password": "myPassword",
-                        "registryServer": "myContainerRegistry.azurecr.io",
-                        "username": "myUserName",
+            "file_path_": "/batch.bicep",
+            "__end_line__": 26,
+            "__start_line__": 1,
+            "properties": {
+                "virtualMachineConfiguration": {
+                    "containerConfiguration": {
+                        "containerImageNames": ["nginx", "python:3.9-alpine"],
+                        "containerRegistries": {
+                            "password": "myPassword",
+                            "registryServer": "myContainerRegistry.azurecr.io",
+                            "username": "myUserName",
+                        },
+                        "type": "DockerCompatible",
                     },
-                    "type": "DockerCompatible",
-                },
-            }
-        },
-        "resource_type": "Microsoft.Batch/batchAccounts/pools",
-    }
-    graph = DiGraph()
-    graph.add_node(1, **resource)
+                }
+            },
+            "resource_type": "Microsoft.Batch/batchAccounts/pools",
+        }
+    if graph_framework == 'IGRAPH':
+        graph = igraph.Graph()
+        graph.add_vertex(
+            name='1',
+            block_type_='resource',
+            resource_type=resource[
+                CustomAttributes.RESOURCE_TYPE] if CustomAttributes.RESOURCE_TYPE in resource else None,
+            attr=resource,
+        )
+    else:
+        graph = DiGraph()
+        graph.add_node(1, **resource)
 
     # when
     azure_provider = AzureTerraformProvider(graph_connector=graph)
@@ -39,7 +57,8 @@ def extract_images_from_resources():
     ]
 
 
-def test_extract_images_from_resources_with_no_image():
+@pytest.mark.parametrize('graph_framework', ["NETWORKX", "IGRAPH"])
+def test_extract_images_from_resources_with_no_image(graph_framework):
     # given
     resource = {
         "file_path_": "/batch.bicep",
@@ -60,12 +79,23 @@ def test_extract_images_from_resources_with_no_image():
         },
         "resource_type": "Microsoft.Batch/batchAccounts/pools",
     }
-    graph = DiGraph()
-    graph.add_node(1, **resource)
+    if graph_framework == 'IGRAPH':
+        graph = igraph.Graph()
+        graph.add_vertex(
+            name='1',
+            block_type_='resource',
+            resource_type=resource[
+                CustomAttributes.RESOURCE_TYPE] if CustomAttributes.RESOURCE_TYPE in resource else None,
+            attr=resource,
+        )
+    else:
+        graph = DiGraph()
+        graph.add_node(1, **resource)
 
     # when
-    azure_provider = AzureTerraformProvider(graph_connector=graph)
-    images = azure_provider.extract_images_from_resources()
+    with mock.patch.dict('os.environ', {'CHECKOV_GRAPH_FRAMEWORK': graph_framework}):
+        azure_provider = AzureTerraformProvider(graph_connector=graph)
+        images = azure_provider.extract_images_from_resources()
 
     # then
     assert not images
