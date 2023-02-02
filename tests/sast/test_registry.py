fe9947017e469cae86fb3467b8b29783e97fc4b2
achia <achiar99@gmail.com>
2023-02-02 14:53:13 +0200

    fix tests

--- tests/sast/test_registry.py
+++ tests/sast/test_registry.py
@@ -5,27 +5,27 @@ import os
 
 
 def test_sast_registry_only_python():
-    registry = Registry()
-
     checks_dir = os.path.join(pathlib.Path(__file__).parent.resolve(), 'checks')
+    registry = Registry(checks_dir)
+
     sast_langs = [SastLanguages.PYTHON]
-    registry.load_checks(checks_dir, sast_langs)
+    registry.load_checks( sast_langs)
     assert registry.checks == [os.path.join(checks_dir, 'python_rule.yaml')]
     
 
 def test_sast_registry_only_java():
-    registry = Registry()
-
     checks_dir = os.path.join(pathlib.Path(__file__).parent.resolve(), 'checks')
+    registry = Registry(checks_dir)
+
     sast_langs = [SastLanguages.JAVA]
-    registry.load_checks(checks_dir, sast_langs)
+    registry.load_checks(sast_langs)
     assert registry.checks == [os.path.join(checks_dir, 'java_rule.yaml')]
     
 
 def test_sast_registry_all():
-    registry = Registry()
-
     checks_dir = os.path.join(pathlib.Path(__file__).parent.resolve(), 'checks')
+    registry = Registry(checks_dir)
+
     sast_langs = [SastLanguages.JAVA, SastLanguages.PYTHON]
-    registry.load_checks(checks_dir, sast_langs)
+    registry.load_checks(sast_langs)
     assert registry.checks == [os.path.join(checks_dir, 'java_rule.yaml'), os.path.join(checks_dir, 'python_rule.yaml')]
