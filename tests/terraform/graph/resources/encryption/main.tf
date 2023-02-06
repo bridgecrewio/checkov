b3b1d98837625f2e4e3b906e3822278b38ca95c0
Barak Fatal <barakf156@gmail.com>
2023-02-06 16:02:58 +0200

    updated local graph checks for terraform to support s3 default encryption

--- tests/terraform/graph/resources/encryption/main.tf
+++ tests/terraform/graph/resources/encryption/main.tf
@@ -113,7 +113,7 @@ resource "aws_kinesis_stream" "unencrypted_stream" {
   shard_count = 1
 }
 
-resource "aws_s3_bucket" "unencrypted_bucket" {
+resource "aws_s3_bucket" "encrypted_bucket_by_default" {
   bucket = "unencrypted"
 }
 
