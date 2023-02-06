5b69ca5b7733da7c04675aea06d3fddafcdfa62f
YaaraVerner <86768411+YaaraVerner@users.noreply.github.com>
2023-02-06 13:45:45 +0000

    Update checkov/kustomize/runner.py
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- checkov/kustomize/runner.py
+++ checkov/kustomize/runner.py
@@ -158,11 +158,10 @@ class K8sKustomizeRunner(K8sRunner):
         # Moves report generation logic out of run() method in Runner class.
         # Allows function overriding of a much smaller function than run() for other "child" frameworks such as Kustomize, Helm
         # Where Kubernetes CHECKS are needed, but the specific file references are to another framework for the user output (or a mix of both).
-        if self.context is None:
-            # this shouldn't happen
-            logging.error("Context for Kustomize runner was not set")
-            return report
         if not self.context:
+            if self.context is None:
+                # this shouldn't happen
+                logging.error("Context for Kustomize runner was not set")
             return report
 
         kustomize_metadata = self.report_mutator_data['kustomizeMetadata'],
