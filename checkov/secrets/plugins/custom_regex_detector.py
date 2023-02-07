756634cf605b83edf5d4604efe834836a6f2246e
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2023-02-07 11:18:37 +0200

    feat(secrets): Add args to analyze line is added and is removed for git history scan (#4426)
    * add is_added_and_is_removed
    
    * is_added and is_removed default False
    
    * is_added and is_removed default False

--- checkov/secrets/plugins/custom_regex_detector.py
+++ checkov/secrets/plugins/custom_regex_detector.py
@@ -39,9 +39,11 @@ class CustomRegexDetector(RegexBasedDetector):
             line_number: int = 0,
             context: Optional[CodeSnippet] = None,
             raw_context: Optional[CodeSnippet] = None,
+            is_added: bool = False,
+            is_removed: bool = False,
             **kwargs: Any
     ) -> Set[PotentialSecret]:
-        """This examines a line and finds all possible secret values in it."""
+        """This examines a line and finds all possible secret values in it"""
         output: Set[PotentialSecret] = set()
         for match, regex in self.analyze_string(line, **kwargs):
             try:
