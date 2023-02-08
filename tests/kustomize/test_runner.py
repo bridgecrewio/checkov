5c1815d4ee5157031185afe783304076ae2dfd8f
achia <achiar99@gmail.com>
2023-02-08 12:49:32 +0200

    add test

--- tests/kustomize/test_runner.py
+++ tests/kustomize/test_runner.py
@@ -57,7 +57,7 @@ class TestRunnerValid(unittest.TestCase):
             # Kustomize deals with absolute paths
             # self.assertEqual(record.repo_file_path in record.file_path)
             self.assertNotEqual(record.file_path, record.file_abs_path)
-            self.assertIn(record.repo_file_path, record.file_path)
+            self.assertEqual(record.repo_file_path, record.file_path)
 
     @unittest.skipIf(os.name == "nt" or not kustomize_exists(), "kustomize not installed or Windows OS")
     def test_record_relative_path_with_direct_oberlay(self):
@@ -82,7 +82,7 @@ class TestRunnerValid(unittest.TestCase):
         for record in all_checks:
             # Kustomize deals with absolute paths
             # self.assertEqual(record.repo_file_path in record.file_path)
-            self.assertIn(record.repo_file_path, record.file_path)
+            self.assertEqual(record.repo_file_path, record.file_path)
 
     @unittest.skipIf(os.name == "nt" or not kustomize_exists(), "kustomize not installed or Windows OS")
     def test_record_relative_path_with_direct_prod2_oberlay(self):
@@ -107,7 +107,7 @@ class TestRunnerValid(unittest.TestCase):
         for record in all_checks:
             # Kustomize deals with absolute paths
             # self.assertEqual(record.repo_file_path in record.file_path)
-            self.assertIn(record.repo_file_path, record.file_path)
+            self.assertEqual(record.repo_file_path, record.file_path)
     
     @unittest.skipIf(os.name == "nt" or not kustomize_exists(), "kustomize not installed or Windows OS")
     def test_no_file_type_exists(self):
