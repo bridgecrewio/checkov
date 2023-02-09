1b02b27d9ee2673ab95693faedb4067b7b917a38
Anton Grübel <anton.gruebel@gmail.com>
2023-02-09 10:45:54 +0100

    improve base GCP role check

--- checkov/terraform/checks/resource/gcp/AbsGoogleBasicRoles.py
+++ checkov/terraform/checks/resource/gcp/AbsGoogleBasicRoles.py
@@ -1,16 +1,17 @@
 from checkov.common.models.enums import CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
-BASIC_ROLES = [
+BASIC_ROLES = {
     "roles/owner",
     "roles/editor",
     "roles/viewer",
-]
+}
 
 
 class AbsGoogleBasicRoles(BaseResourceCheck):
     def scan_resource_conf(self, conf):
         self.evaluated_keys = ['role']
-        if 'role' in conf and conf['role'][0] in BASIC_ROLES:
+        role = conf.get("role")
+        if role and isinstance(role, list) and role[0] in BASIC_ROLES:
             return CheckResult.FAILED
         return CheckResult.PASSED
