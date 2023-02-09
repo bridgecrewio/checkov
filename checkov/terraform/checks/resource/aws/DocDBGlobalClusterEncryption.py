2a8fe0093f9789e99260d55648503773491932d8
Anton Grübel <anton.gruebel@gmail.com>
2023-02-09 10:57:05 +0100

    add type hints

--- checkov/terraform/checks/resource/aws/DocDBGlobalClusterEncryption.py
+++ checkov/terraform/checks/resource/aws/DocDBGlobalClusterEncryption.py
@@ -3,14 +3,14 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 
 class DocDBGlobalClusterEncryption(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure DocDB Global Cluster is encrypted at rest (default is unencrypted)"
         id = "CKV_AWS_292"
-        supported_resources = ['aws_docdb_global_cluster']
-        categories = [CheckCategories.ENCRYPTION]
+        supported_resources = ('aws_docdb_global_cluster',)
+        categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "storage_encrypted"
 
 
