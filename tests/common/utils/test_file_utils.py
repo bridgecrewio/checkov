0454b3554d4998bb7bb909efd367b68932f4e094
pazbechor <pbechor@paloaltonetworks.com>
2023-02-09 10:17:08 +0200

    adding tests

---
+++ tests/common/utils/test_file_utils.py
@@ -0,0 +1,14 @@
+from checkov.common.util.file_utils import read_file_safe, get_file_size_safe
+
+def test_sanity_read_file():
+    pass
+
+def test_failure_read_file():
+    pass
+
+
+def test_sanity_read_file():
+    pass
+
+def test_failure_read_file():
+    pass
