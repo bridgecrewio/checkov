bad192d67587eed418d5a0e1e942ab99379fd25a
Joseph de CLERCK <clerckj@amazon.fr>
2023-02-20 12:33:20 -0500

    Fix Linter

--- checkov/cloudformation/checks/resource/aws/EC2Credentials.py
+++ checkov/cloudformation/checks/resource/aws/EC2Credentials.py
@@ -1,12 +1,9 @@
-import base64
-import binascii
 from typing import List
 from base64 import b64decode
 
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
 from checkov.common.util.secrets import get_secrets_from_string
-
 
 
 class EC2Credentials(BaseResourceCheck):
@@ -24,7 +21,7 @@ class EC2Credentials(BaseResourceCheck):
                 # In some case, the UserData might be a base64 encoded string which will slow down (3 minutes)
                 # the get_secrets_from_string function.
                 try:
-                    user_data_str = base64.b64decode(user_data).decode()
+                    user_data_str = b64decode(user_data).decode()
                 except Exception:
                     # Cast to string as user data object can look slightly different depending
                     # on Yaml or JSON CF Templates and how the B64 conversion is done.
