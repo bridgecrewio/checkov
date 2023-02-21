18b424551298f1c6e9830ca0429b88895425517b
lirshindalman <lirshindelman@gmail.com>
2023-02-21 17:36:31 +0200

    fix comments

--- checkov/secrets/scan_git_history.py
+++ checkov/secrets/scan_git_history.py
@@ -94,8 +94,9 @@ def get_added_and_removed_commit_hash(
     """
     if not enable_git_history_secret_scan:
         return None, None
-    added_commit_hash = key.split('_')[0]
-    removed_commit_hash = key.split('_')[1] if key.split('_')[1] != GIT_HISTORY_NOT_BEEN_REMOVED else None
+    split_key = key.split('_')
+    added_commit_hash = split_key[0]
+    removed_commit_hash = split_key[1] if split_key[1] != GIT_HISTORY_NOT_BEEN_REMOVED else None
     return added_commit_hash, removed_commit_hash
 
 
@@ -109,29 +110,44 @@ def set_secret_map(file_results: List[PotentialSecret], secret_map: Dict[str, Li
 
     for secret in file_results:
         secret_key = f'{secret.filename}_{secret.secret_hash}_{secret.type.replace(" ", "-")}'
-        if not all(value in equal_secret_in_commit[secret_key] for value in ['added', 'removed']):
-            if secret.is_added:
-                if secret_key not in secret_map:
-                    secret_map[secret_key] = []
-                else:
-                    all_removed = all(
-                        potential_secret.get('removed_commit_hash') for potential_secret in secret_map[secret_key])
-                    # Update secret map with the new potential secret
-                    if all_removed:
-                        secret_map[secret_key][0].update({'potential_secret': secret, 'removed_commit_hash': ''})
-                        continue
-                secret_map[secret_key].append(
-                    {'added_commit_hash': commit_hash, 'removed_commit_hash': '', 'potential_secret': secret})
+        if all(value in equal_secret_in_commit[secret_key] for value in ['added', 'removed']):
+            continue
+        if secret.is_added:
+            add_new_secret_to_map(secret_key, secret_map, commit_hash, secret)
+        if secret.is_removed:
+            update_removed_secret_in_map(secret_map, commit_hash, secret_key, secret, file_name)
 
-            if secret.is_removed:
-                # Try to find the corresponding added secret in the git history secret map
-                try:
-                    for secret_in_file in secret_map[secret_key]:
-                        # Compared by filename, secret_hash, and type
-                        if secret == secret_in_file['potential_secret']:
-                            if secret_in_file['potential_secret'].is_added:
-                                secret_in_file['removed_commit_hash'] = commit_hash
-                                secret_in_file['potential_secret'] = secret
-                                break
-                except KeyError:
-                    logging.error(f"No added secret commit found for secret in file {file_name}.")
+
+def add_new_secret_to_map(secret_key: str,
+                          secret_map: Dict[str, List[EnrichedPotentialSecret]],
+                          commit_hash: str,
+                          secret: PotentialSecret) -> None:
+    if secret_key not in secret_map:
+        secret_map[secret_key] = []
+    else:
+        all_removed = all(
+            potential_secret.get('removed_commit_hash') for potential_secret in secret_map[secret_key])
+        # Update secret map with the new potential secret
+        if all_removed:
+            secret_map[secret_key][0].update({'potential_secret': secret, 'removed_commit_hash': ''})
+            return
+    secret_map[secret_key].append(
+        {'added_commit_hash': commit_hash, 'removed_commit_hash': '', 'potential_secret': secret})
+
+
+def update_removed_secret_in_map(secret_map: Dict[str, List[EnrichedPotentialSecret]],
+                                 commit_hash: str,
+                                 secret_key: str,
+                                 secret: PotentialSecret,
+                                 file_name: str) -> None:
+    # Try to find the corresponding added secret in the git history secret map
+    try:
+        for secret_in_file in secret_map[secret_key]:
+            # Compared by filename, secret_hash, and type
+            if secret.__eq__(secret_in_file['potential_secret']):
+                if secret_in_file['potential_secret'].is_added:
+                    secret_in_file['removed_commit_hash'] = commit_hash
+                    secret_in_file['potential_secret'] = secret
+                    break
+    except KeyError:
+        logging.error(f"No added secret commit found for secret in file {file_name}.")
