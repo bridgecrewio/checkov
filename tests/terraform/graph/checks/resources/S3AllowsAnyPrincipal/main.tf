8c445891aee7973492c68b492ec2089543904437
James Woolfenden <james.woolfenden@gmail.com>
2023-02-22 11:35:46 +0000

    fix withg json path

--- tests/terraform/graph/checks/resources/S3AllowsAnyPrincipal/main.tf
+++ tests/terraform/graph/checks/resources/S3AllowsAnyPrincipal/main.tf
@@ -51,23 +51,23 @@ resource "aws_s3_bucket_policy" "fail2" {
   bucket = aws_s3_bucket.b.id
   policy = data.aws_iam_policy_document.fail2.json
 }
-#
-#data "aws_iam_policy_document" "fail2" {
-#  statement {
-#    principals {
-#      type        = "AWS"
-#      identifiers = ["*"]
-#    }
-#    actions   = ["s3:GetObject"]
-#    resources = ["${aws_s3_bucket.b.arn}/*"]
-#  }
-#  statement {
-#    principals {
-#      type        = "AWS"
-#      identifiers = ["some_arn"]
-#    }
-#    actions   = ["s3:GetObject"]
-#    resources = ["${aws_s3_bucket.c.arn}/*"]
-#  }
-#
-#}
+
+data "aws_iam_policy_document" "fail2" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+  }
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["some_arn"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.c.arn}/*"]
+  }
+
+}
