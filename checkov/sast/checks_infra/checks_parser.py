4325e48f8114b3a2383a7e8b22761aca48f1d3cb
arielk <ar.kruszyn@gmail.com>
2023-02-26 02:09:44 +0200

    split parser

--- checkov/sast/checks_infra/checks_parser.py
+++ checkov/sast/checks_infra/checks_parser.py
@@ -13,17 +13,17 @@ from checkov.common.util.type_forcers import force_list
 class SastCheckParser:
     def parse_raw_check_to_semgrep(self, raw_check: Dict[str, Dict[str, Any]], check_file: str | None = None) -> Dict[str, Any]:
         semgrep_rule: Dict[str, Any] = {}
-        if not self.raw_check_is_valid(raw_check):
+        if not self._raw_check_is_valid(raw_check):
             logging.error(f'cant parse the following policy: {raw_check}')
         try:
-            semgrep_rule = self.parse_rule_metadata(raw_check, check_file, semgrep_rule)
-            semgrep_rule.update(self.parse_definition(raw_check['definition']))
+            semgrep_rule = self._parse_rule_metadata(raw_check, check_file, semgrep_rule)
+            semgrep_rule.update(self._parse_definition(raw_check['definition']))
         except Exception as e:
             logging.error(f'the policy in file {check_file} is misconfigured so it could not be parsed properly.\n{e}')
 
         return semgrep_rule
 
-    def raw_check_is_valid(self, raw_check: Dict[str, Any]) -> bool:
+    def _raw_check_is_valid(self, raw_check: Dict[str, Any]) -> bool:
         metadata = raw_check.get('metadata')
         if not metadata:
             raise AttributeError('BQL policy is missing the metadata field')
@@ -33,11 +33,10 @@ class SastCheckParser:
             raise AttributeError('BQL policy metadata is missing a severity')
         if not raw_check.get('scope', {}).get('languages'):
             raise AttributeError('BQL policy metadata is missing languages')
-
         else:
             return True
 
-    def parse_rule_metadata(self, bql_policy: Dict[str, Any], check_file, semgrep_rule: Dict[str, Any]) \
+    def _parse_rule_metadata(self, bql_policy: Dict[str, Any], check_file, semgrep_rule: Dict[str, Any]) \
             -> Dict[str, Any]:
         metadata = bql_policy['metadata']
         semgrep_rule[SemgrepAttribute.ID.value] = metadata['id']
@@ -60,81 +59,91 @@ class SastCheckParser:
         semgrep_rule['metadata'] = metadata_obj
         return semgrep_rule
 
-    def parse_definition(self, definition: Dict[str, Any]) -> Dict[str, Any]:
+    def _parse_definition(self, definition: Dict[str, Any]) -> Dict[str, Any]:
         definitions = force_list(definition)
         conf = {}
         if len(definitions) > 1:
-            conf[SemgrepAttribute.PATTERNS.value] = self.get_definitions_list_items(definitions)
+            return {SemgrepAttribute.PATTERNS.value: self._get_definitions_list_items(definitions)}
         elif len(definitions) == 1:
             definition = definitions[0]
             if not isinstance(definition, dict):
                 raise TypeError(f'bad definition type, got {type(definition)} instead of dict')
             if definition.get(BqlConditionType.OR.value):
-                conf[SemgrepAttribute.PATTERN_EITHER.value] = self.get_definitions_list_items(
-                    definition[BqlConditionType.OR.value])
+                return {SemgrepAttribute.PATTERN_EITHER.value: self._get_definitions_list_items(
+                    definition[BqlConditionType.OR.value])}
             elif definition.get(BqlConditionType.AND.value):
-                conf[SemgrepAttribute.PATTERNS.value] = self.get_definitions_list_items(
-                    definition[BqlConditionType.AND.value])
+                return {SemgrepAttribute.PATTERNS.value: self._get_definitions_list_items(
+                    definition[BqlConditionType.AND.value])}
             else:
-                # handle a single definition
-                cond_type = definition.get('cond_type', '')
-                if not cond_type:
-                    raise AttributeError('BQL policy is missing a condition type')
-                operator = definition.get('operator', '')
-                if not operator:
-                    raise AttributeError(f'BQL policy condition type: {cond_type} is missing an operator')
-                definition_value = definition.get('value')
-                if not definition_value:
-                    raise AttributeError(f'BQL policy condition type: {cond_type} is missing a definition value')
-
-                if cond_type == BqlConditionType.PATTERN:
-                    semgrep_attr = PATTERN_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
-                    if not semgrep_attr:
-                        raise AttributeError(f'BQL policy pattern condition contains an unknown operator: {operator}')
-                    return {semgrep_attr: definition_value}
-
-                elif cond_type == BqlConditionType.VARIABLE:
-                    metavariable_condition_object = {}
-                    metavariable = definition.get('variable')
-                    metavariable_condition_object[SemgrepAttribute.METAVARIABLE.value] = metavariable
-                    metavariable_condition_key = VARIABLE_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
-                    if not metavariable_condition_key:
-                        raise AttributeError(f'BQL policy variable condition contains an unknown operator: {operator}')
-
-                    if operator == Operators.REGEX_MATCH:
-                        metavariable_condition_object[SemgrepAttribute.REGEX.value] = definition_value
-
-                    elif operator == Operators.PATTERN_MATCH:
-                        if isinstance(definition_value, str):
-                            metavariable_condition_object[SemgrepAttribute.PATTERN.value] = definition_value
-                        else:
-                            metavariable_condition = self.parse_definition(definition_value)
-                            metavariable_condition_object.update(metavariable_condition)
-
-                    elif operator in COMPARISON_VALUES:
-                        metavariable_condition_object[
-                            SemgrepAttribute.COMPARISON.value] = f'{metavariable} {COMPARISON_VALUE_TO_SYMBOL[operator]} {definition_value}'
-
-                    return {metavariable_condition_key: metavariable_condition_object}
-
-                elif cond_type == BqlConditionType.FILTER:
-                    semgrep_attr = FILTER_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
-                    if not semgrep_attr:
-                        raise AttributeError(f'BQL filter condition contains an unknown operator: {operator}')
-                    return {semgrep_attr: definition_value}
-
+                return self._parse_single_definition(definition)
         return conf
 
-    def get_definitions_list_items(self, definitions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    def _parse_single_definition(self, definition):
+        cond_type = definition.get('cond_type', '')
+        if not cond_type:
+            raise AttributeError('BQL policy is missing a condition type')
+        operator = definition.get('operator', '')
+        if not operator:
+            raise AttributeError(f'BQL policy condition type: {cond_type} is missing an operator')
+        definition_value = definition.get('value')
+        if not definition_value:
+            raise AttributeError(f'BQL policy condition type: {cond_type} is missing a definition value')
+
+        if cond_type == BqlConditionType.PATTERN:
+            return self._parse_pattern_cond_type(operator, definition_value)
+
+        elif cond_type == BqlConditionType.VARIABLE:
+            return self._parse_variable_cond_type(operator, definition, definition_value)
+
+        elif cond_type == BqlConditionType.FILTER:
+            return self._parse_filter_cond_type(operator, definition_value)
+
+    def _parse_pattern_cond_type(self, operator, definition_value):
+        semgrep_attr = PATTERN_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
+        if not semgrep_attr:
+            raise AttributeError(f'BQL policy pattern condition contains an unknown operator: {operator}')
+        return {semgrep_attr: definition_value}
+
+    def _parse_variable_cond_type(self, operator, definition, definition_value):
+        metavariable_condition_object = {}
+        metavariable = definition.get('variable')
+        metavariable_condition_object[SemgrepAttribute.METAVARIABLE.value] = metavariable
+        metavariable_condition_key = VARIABLE_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
+        if not metavariable_condition_key:
+            raise AttributeError(f'BQL policy variable condition contains an unknown operator: {operator}')
+
+        if operator == Operators.REGEX_MATCH:
+            metavariable_condition_object[SemgrepAttribute.REGEX.value] = definition_value
+
+        elif operator == Operators.PATTERN_MATCH:
+            if isinstance(definition_value, str):
+                metavariable_condition_object[SemgrepAttribute.PATTERN.value] = definition_value
+            else:
+                metavariable_condition = self._parse_definition(definition_value)
+                metavariable_condition_object.update(metavariable_condition)
+
+        elif operator in COMPARISON_VALUES:
+            metavariable_condition_object[
+                SemgrepAttribute.COMPARISON.value] = f'{metavariable} {COMPARISON_VALUE_TO_SYMBOL[operator]} {definition_value}'
+
+        return {metavariable_condition_key: metavariable_condition_object}
+
+    def _parse_filter_cond_type(self, operator, definition_value):
+        semgrep_attr = FILTER_OPERATOR_TO_SEMGREP_ATTR.get(operator, '')
+        if not semgrep_attr:
+            raise AttributeError(f'BQL filter condition contains an unknown operator: {operator}')
+        return {semgrep_attr: definition_value}
+
+    def _get_definitions_list_items(self, definitions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         conf = []
         for definition in definitions:
             if definition.get(BqlConditionType.OR.value):
-                conf.append({SemgrepAttribute.PATTERN_EITHER.value: self.get_definitions_list_items(
+                conf.append({SemgrepAttribute.PATTERN_EITHER.value: self._get_definitions_list_items(
                     definition[BqlConditionType.OR.value])})
             elif definition.get(BqlConditionType.AND.value):
-                conf.append({SemgrepAttribute.PATTERNS.value: self.get_definitions_list_items(
+                conf.append({SemgrepAttribute.PATTERNS.value: self._get_definitions_list_items(
                     definition[BqlConditionType.AND.value])})
             else:
-                conf.append(self.parse_definition(definition))
+                conf.append(self._parse_definition(definition))
 
         return conf
