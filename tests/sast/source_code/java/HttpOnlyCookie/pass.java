7bcdf9f78b8ca80275f1e50a20833af3d4d33bc4
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2023-02-28 17:15:46 +0200

    fix tests

--- tests/sast/source_code/java/HttpOnlyCookie/pass.java
+++ tests/sast/source_code/java/HttpOnlyCookie/pass.java
@@ -1,8 +1,7 @@
 class Connector {
     @javax.jws.WebMethod
     void connect(HttpServletRequest req){
-        javax.servlet.http.Cookie cook = new Cookie("cookie");
-        cook.setHttpOnly(false);
-        resp.addCookie(cook);
+        HttpServletResponse res = new HttpServletResponse();
+        res.setHeader("Access-Control-Allow-Origin", "false");
     }
 }
--- tests/sast/source_code/java/InsecureCookie/fail.java
+++ tests/sast/source_code/java/InsecureCookie/fail.java
@@ -2,6 +2,7 @@ class Connector {
     @javax.jws.WebMethod
     void connect(HttpServletRequest req){
           javax.servlet.http.Cookie cook = new Cookie("cookie");
+          cook.setSecure(false);
           req.addCookie(cook);
     }
 }
