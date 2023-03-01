57069f67b6888be94f1121b1215c592f534f8ec9
James Woolfenden <james.woolfenden@gmail.com>
2023-03-01 14:00:12 +0000

    fix(terraform): fix CKV_AWS_70 test and add graph for coverage of data source (#4542)
    * fix one test and add graph for coverage of data source
    
    * fix withg json path
    
    * check type
    
    * bump
    
    * change id
    
    * remove comment
    
    * revert graph check - covered by CKV_AWS_283
    
    * vain attempt to comply with undocumented assertion policy
    
    * fudge the data

---
+++ tests/terraform/checks/resource/aws/example_S3AllowsAnyPrincipal/main.tf
@@ -0,0 +1,185 @@
+
+resource "aws_s3_bucket" "unknown" {
+  bucket = "bucket"
+}
+
+resource "aws_s3_bucket" "fail3" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "*"
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "fail2" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": ["*"]
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "fail" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "pass" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "some_arn"
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "pass2" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Deny",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "pass" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "some_arn"
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "fail" {
+  bucket = "bucket"
+
+  policy = <<POLICY
+{
+    "Id": "Policy1597273448050",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1597273446725",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::bucket/*",
+            "Principal": {
+                "AWS": "*"
+            }
+        }
+    ]
+}
+POLICY
+}
+
+
+resource "aws_s3_bucket_policy" "json" {
+  bucket = aws_s3_bucket.b.id
+  policy = data.aws_iam_policy_document.test.json
+}
+
+data "aws_iam_policy_document" "test" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+  }
+}
