74f235f19c09039a4714fa5428e1e17cfe75dc0e
James Woolfenden <james.woolfenden@gmail.com>
2023-03-02 12:52:26 +0000

    fix(terraform): AWS - there's a new sg vpc ingress rule (#4575)
    * theres a new sg vpc ingress rule
    
    * slight logic adjustment
    
    ---------
    
    Co-authored-by: Anton Grübel <anton.gruebel@gmail.com>

--- tests/terraform/checks/resource/aws/test_SecurityGroupRuleDescription.py
+++ tests/terraform/checks/resource/aws/test_SecurityGroupRuleDescription.py
@@ -1,160 +1,46 @@
 import unittest
+from pathlib import Path
 
-import hcl2
-
-from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.SecurityGroupRuleDescription import check
+from checkov.terraform.runner import Runner
 
 
 class TestSecurityGroupRuleDescription(unittest.TestCase):
+    def test(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_SecurityGroupRuleDescription"
 
-    def test_failure(self):
-        hcl_res = hcl2.loads("""
-                            resource "aws_security_group" "example_sg" {
-                              egress {
-                                description = "Allow outgoing communication"
-                                cidr_blocks = ["0.0.0.0/0"]
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "false"
-                                to_port     = "0"
-                              }
-                              egress {
-                                cidr_blocks = ["10.0.0.0/0"]
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "false"
-                                to_port     = "0"
-                              }
-                              ingress {
-                                description = "Self Reference"
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "true"
-                                to_port     = "0"
-                              }
-                            
-                              name = "example-lambda"
-                            
-                              tags = {
-                                Name = "example-sg"
-                              }
-                            
-                              vpc_id = aws_vpc.vpc.id
-                            }
-                """)
-        resource_conf = hcl_res['resource'][0]['aws_security_group']['example_sg']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
 
+        # then
+        summary = report.get_summary()
 
-    def test_sucess_sg_desc(self):
-        hcl_res = hcl2.loads("""
-                            resource "aws_security_group" "example_sg" {
-                            description = "sg_desc"
-                              egress {
-                                description = "Allow outgoing communication"
-                                cidr_blocks = ["0.0.0.0/0"]
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "false"
-                                to_port     = "0"
-                              }
-                              egress {
-                                description = "Egress description"
-                                cidr_blocks = ["10.0.0.0/0"]
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "false"
-                                to_port     = "0"
-                              }
-                            
-                              ingress {
-                                description = "Self Reference"
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "true"
-                                to_port     = "0"
-                              }
-                            
-                              name = "example-lambda"
-                            
-                              tags = {
-                                Name = "example-sg"
-                              }
-                            
-                              vpc_id = aws_vpc.vpc.id
-                            }
-                """)
-        resource_conf = hcl_res['resource'][0]['aws_security_group']['example_sg']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
-
-    def test_success_sg(self):
-        hcl_res = hcl2.loads("""
-                            resource "aws_security_group" "example_sg" {
-                              description = "SG description"
-                              egress {
-                                description = "Allow outgoing communication"
-                                cidr_blocks = ["0.0.0.0/0"]
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "false"
-                                to_port     = "0"
-                              }
-                            
-                              ingress {
-                                description = "Self Reference"
-                                from_port   = "0"
-                                protocol    = "-1"
-                                self        = "true"
-                                to_port     = "0"
-                              }
-                            
-                              name = "example-lambda"
-                            
-                              tags = {
-                                Name = "example-sg"
-                              }
-                            
-                              vpc_id = aws_vpc.vpc.id
-                            }
-                """)
-        resource_conf = hcl_res['resource'][0]['aws_security_group']['example_sg']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
-
-    def test_failure_sg_rule(self):
-        hcl_res = hcl2.loads("""
-        resource "aws_security_group_rule" "example_sg_rule_failure" {
-          type = "ingress"
-          from_port = 3389
-          to_port = 3389
-          protocol = "tcp"
-          cidr_blocks = "0.0.0.0/0"
-          security_group_id = "sg-123456"
+        passing_resources = {
+            "aws_security_group.pass",
+            "aws_security_group.pass2",
+            "aws_security_group_rule.pass",
+            "aws_vpc_security_group_ingress_rule.pass"
         }
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_security_group_rule']['example_sg_rule_failure']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
 
-    def test_success_sg_rule(self):
-        hcl_res = hcl2.loads("""
-        resource "aws_security_group_rule" "example_sg_rule_success" {
-          type = "ingress"
-          description = "SG rule description"
-          from_port = 3389
-          to_port = 3389
-          protocol = "tcp"
-          cidr_blocks = "0.0.0.0/0"
-          security_group_id = "sg-123456"
+        failing_resources = {
+            "aws_security_group.fail",
+            "aws_security_group_rule.fail",
+            "aws_vpc_security_group_ingress_rule.fail"
         }
-        """)
-        resource_conf = hcl_res['resource'][0]['aws_security_group_rule']['example_sg_rule_success']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
