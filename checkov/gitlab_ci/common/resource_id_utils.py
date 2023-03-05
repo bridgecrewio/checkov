5cb1d06422495a072b21cc220d575ef1d558a4e9
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2023-03-05 14:34:40 +0200

    revert change to string resource generation

--- checkov/gitlab_ci/common/resource_id_utils.py
+++ checkov/gitlab_ci/common/resource_id_utils.py
@@ -13,7 +13,9 @@ def generate_resource_key_recursive(conf: dict[str, Any] | list[str] | str, key:
         if isinstance(value, dict) and value[START_LINE] <= start_line <= end_line <= value[END_LINE]:
             next_key = f'{key}.{k}' if key else k
             return generate_resource_key_recursive(value, next_key, start_line, end_line)
-        if isinstance(value, list) or isinstance(value, str):
+        if isinstance(value, list):
             continue
+        if isinstance(value, str):
+            return key
 
     return key
