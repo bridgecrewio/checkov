ed1438ad640be579a12afb00150db3eb0609667d
Max Amelchenko <maxamel2002@gmail.com>
2023-03-06 09:23:52 +0200

    feat(gitlab): fix wrong resource in gitlab-ci (#4610)
    * fix wrong resource in gitlab-ci
    
    * revert change to string resource generation
    
    * skip top level list only
    
    ---------
    
    Co-authored-by: Max Amelchenko <mamelchenko@paloaltonetworks.com>

--- tests/gitlab_ci/test_runner.py
+++ tests/gitlab_ci/test_runner.py
@@ -21,7 +21,7 @@ class TestRunnerValid(unittest.TestCase):
         )
         self.assertEqual(len(report.failed_checks), 5)
         self.assertEqual(report.parsing_errors, [])
-        self.assertEqual(len(report.passed_checks), 7)
+        self.assertEqual(len(report.passed_checks), 9)
         self.assertEqual(report.skipped_checks, [])
         report.print_console()
 
@@ -55,8 +55,25 @@ class TestRunnerValid(unittest.TestCase):
         )
         self.assertEqual(len(report.failed_checks), 0)
         self.assertEqual(report.parsing_errors, [])
+        self.assertEqual(len(report.passed_checks), 8)
+        self.assertEqual(report.skipped_checks, [])
+
+    def test_runner_image_resources(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        valid_dir_path = os.path.join(current_dir, "resources/resource_images")
+        runner = Runner()
+        report = runner.run(
+            root_folder=valid_dir_path,
+            runner_filter=RunnerFilter(framework=['gitlab_ci'], checks=['CKV_GITLABCI_3'])
+        )
+        self.assertEqual(len(report.failed_checks), 0)
+        self.assertEqual(report.parsing_errors, [])
         self.assertEqual(len(report.passed_checks), 4)
         self.assertEqual(report.skipped_checks, [])
+        self.assertEqual(report.passed_checks[0].resource, 'prebuild')
+        self.assertEqual(report.passed_checks[1].resource, 'build.image')
+        self.assertEqual(report.passed_checks[2].resource, 'deploy')
+        self.assertEqual(report.passed_checks[3].resource, 'deploy')
 
 
 if __name__ == "__main__":
