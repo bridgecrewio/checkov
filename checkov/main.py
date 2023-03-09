7ae7b6cf096f7402ac127b63eba67fd2426fe3a9
Anton Grübel <anton.gruebel@gmail.com>
2023-03-09 14:58:38 +0100

    feat(general): add Terraform JSON support (#4626)
    * add Terraform JSON support
    
    * fix tests
    
    * fix PR comments

--- checkov/main.py
+++ checkov/main.py
@@ -69,6 +69,7 @@ from checkov.secrets.runner import Runner as secrets_runner
 from checkov.serverless.runner import Runner as sls_runner
 from checkov.terraform.plan_runner import Runner as tf_plan_runner
 from checkov.terraform.runner import Runner as tf_graph_runner
+from checkov.terraform_json.runner import TerraformJsonRunner
 from checkov.version import version
 from checkov.yaml_doc.runner import Runner as yaml_runner
 from checkov.bicep.runner import Runner as bicep_runner
@@ -114,6 +115,7 @@ DEFAULT_RUNNERS = [
     circleci_pipelines_runner(),
     azure_pipelines_runner(),
     ansible_runner(),
+    TerraformJsonRunner(),
 ]
 
 
