1bd2eabcfdc2f2ced918f68a98160191657de169
gruebel <anton.gruebel@gmail.com>
2023-03-11 20:10:19 +0100

    small check adjustments

--- checkov/terraform/checks/resource/aws/DMSEndpointUsesCMK.py
+++ checkov/terraform/checks/resource/aws/DMSEndpointUsesCMK.py
@@ -1,34 +1,35 @@
 from __future__ import annotations
 
+from typing import Any
+
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
 from checkov.common.models.enums import CheckCategories, CheckResult
 
 
 class DMSEndpointUsesCMK(BaseResourceCheck):
     def __init__(self) -> None:
-        name = "Ensure DMS endpoint Uses Customer managed key"
+        name = "Ensure DMS endpoint uses Customer Managed Key (CMK)"
         id = "CKV_AWS_296"
         supported_resources = ("aws_dms_endpoint",)
         categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        if 'engine_name' in conf and isinstance(conf.get('engine_name'), list):
-            engine = conf.get("engine_name")[0]
-            if engine == "s3":
-                self.evaluated_keys = ['s3_settings']
-                if conf.get("s3_settings") and isinstance(conf.get("s3_settings"), list):
-                    settings = conf.get("s3_settings")[0]
-                    if settings.get('server_side_encryption_kms_key_id'):
-                        return CheckResult.PASSED
-                    self.evaluated_keys = ['s3_settings/server_side_encryption_kms_key_id']
-                return CheckResult.FAILED
-            else:
-                if 'kms_key_arn' in conf and isinstance(conf.get('kms_key_arn'), list):
-                    if conf.get('kms_key_arn')[0]:
-                        return CheckResult.PASSED
-                self.evaluated_keys = ['kms_key_arn']
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        engine_name = conf.get("engine_name")
+        if engine_name and isinstance(engine_name, list) and engine_name[0] == "s3":
+            self.evaluated_keys = ["s3_settings"]
+            s3_settings = conf.get("s3_settings")
+            if s3_settings and isinstance(s3_settings, list):
+                self.evaluated_keys = ["s3_settings/server_side_encryption_kms_key_id"]
+                settings = s3_settings[0]
+                if settings.get("server_side_encryption_kms_key_id"):
+                    return CheckResult.PASSED
             return CheckResult.FAILED
+
+        self.evaluated_keys = ["kms_key_arn"]
+        kms_key = conf.get("kms_key_arn")
+        if kms_key and isinstance(kms_key, list) and kms_key[0]:
+            return CheckResult.PASSED
 
         return CheckResult.FAILED
 
--- checkov/terraform/checks/resource/aws/DatasyncLocationExposesSecrets.py
+++ checkov/terraform/checks/resource/aws/DatasyncLocationExposesSecrets.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.models.enums import CheckCategories, CheckResult
+from typing import Any
+
+from checkov.common.models.consts import ANY_VALUE
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class DatasyncLocationExposesSecrets(BaseResourceCheck):
+class DatasyncLocationExposesSecrets(BaseResourceNegativeValueCheck):
     def __init__(self) -> None:
-        name = "Ensure Datasync Location object storage doesn't expose secrets"
+        name = "Ensure DataSync Location Object Storage doesn't expose secrets"
         id = "CKV_AWS_295"
         supported_resources = ("aws_datasync_location_object_storage",)
         categories = (CheckCategories.SECRETS,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        self.evaluated_keys = ['secret_key']
-        if 'secret_key' in conf:
-            return CheckResult.FAILED
-        return CheckResult.PASSED
+    def get_inspected_key(self) -> str:
+        return "secret_key"
+
+    def get_forbidden_values(self) -> list[Any]:
+        return [ANY_VALUE]
 
 
 check = DatasyncLocationExposesSecrets()
