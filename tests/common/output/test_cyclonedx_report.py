4cf5888da5e3eed27b9cf8ec5d8a6757dea7ea0e
James Woolfenden <james.woolfenden@gmail.com>
2023-03-13 17:28:11 +0000

    fragile tests

--- tests/common/output/test_cyclonedx_report.py
+++ tests/common/output/test_cyclonedx_report.py
@@ -206,7 +206,7 @@ def test_create_schema_version_1_3(mocker: MockerFixture):
 
     # then
     assert len(cyclonedx.bom.components) == 1
-    assert len(next(iter(cyclonedx.bom.components)).get_vulnerabilities()) == 4
+    assert len(next(iter(cyclonedx.bom.components)).get_vulnerabilities()) == 5
 
     assert "http://cyclonedx.org/schema/bom/1.3" in output
 
