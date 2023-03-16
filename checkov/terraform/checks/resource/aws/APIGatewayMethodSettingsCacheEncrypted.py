7c0b32ac3586b0e389a5aa49709ffbc63d89f045
James Woolfenden <james.woolfenden@gmail.com>
2023-03-16 10:42:08 +0000

    Update APIGatewayMethodSettingsCacheEncrypted.py

--- checkov/terraform/checks/resource/aws/APIGatewayMethodSettingsCacheEncrypted.py
+++ checkov/terraform/checks/resource/aws/APIGatewayMethodSettingsCacheEncrypted.py
@@ -10,7 +10,7 @@ class APIGatewayMethodSettingCacheEncrypted(BaseResourceValueCheck):
         NIST.800-53.r5 SC-28(1), NIST.800-53.r5 SC-7(10), NIST.800-53.r5 SI-7(6)
         """
         name = "Ensure API Gateway method setting caching is set to encrypted"
-        id = "CKV_AWS_307"
+        id = "CKV_AWS_308"
         supported_resources = ['aws_api_gateway_method_settings']
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
