ba246b43455631f53bd930273fe25d893efed92c
gruebel <anton.gruebel@gmail.com>
2023-03-17 17:06:06 +0100

    adjust check name

--- checkov/terraform/checks/resource/aws/LambdaFunctionIsNotPublic.py
+++ checkov/terraform/checks/resource/aws/LambdaFunctionIsNotPublic.py
@@ -6,7 +6,7 @@ from typing import Any
 
 
 class LambdaFunctionIsNotPublic(BaseResourceNegativeValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         PCI DSS v3.2.1/1.2.1, PCI DSS v3.2.1/1.3.1, PCI DSS v3.2.1/1.3.2, PCI DSS v3.2.1/1.3.4, PCI DSS v3.2.1/7.2.1,
         NIST.800-53.r5 AC-21, NIST.800-53.r5 AC-3, NIST.800-53.r5 AC-3(7), NIST.800-53.r5 AC-4, NIST.800-53.r5 AC-4(21),
@@ -15,7 +15,7 @@ class LambdaFunctionIsNotPublic(BaseResourceNegativeValueCheck):
         Lambda function policies should prohibit public access
         """
 
-        name = "Ensure that AWS Lambda function is not Public"
+        name = "Ensure that AWS Lambda function is not publicly accessible"
         id = "CKV_AWS_301"
         supported_resources = ("aws_lambda_permission",)
         categories = (CheckCategories.GENERAL_SECURITY,)
@@ -30,7 +30,7 @@ class LambdaFunctionIsNotPublic(BaseResourceNegativeValueCheck):
         return "principal"
 
     def get_forbidden_values(self) -> list[Any]:
-        return "*"
+        return ["*"]
 
 
 check = LambdaFunctionIsNotPublic()
