aa04f011636d37f9bd0ec08916f399e352e13c1f
ChanochShayner <chsh32340@gmail.com>
2023-03-20 13:45:58 +0200

    try with o_definitions func

--- checkov/terraform/graph_builder/foreach_handler.py
+++ checkov/terraform/graph_builder/foreach_handler.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import itertools
 import logging
 import json
 import re
@@ -27,7 +28,7 @@ class ForeachHandler(object):
         self.local_graph = local_graph
 
     def handle_foreach_rendering(self, foreach_blocks: dict[str, list[int]]) -> None:
-        self.handle_foreach_rendering_for_module(foreach_blocks.get(BlockType.MODULE))
+        # self.handle_foreach_rendering_for_module(foreach_blocks.get(BlockType.MODULE))
         self._handle_foreach_rendering_for_resource(foreach_blocks.get(BlockType.RESOURCE))
 
     def _handle_foreach_rendering_for_resource(self, resources_blocks: list[int]) -> None:
@@ -308,33 +309,35 @@ class ForeachHandler(object):
         """
         modules_blocks (list[int]): list of module blocks indexes in the graph that contains for_each / counts.
         """
-        module_to_render: list[list[int]] = []
-        main_module_object = self.local_graph.vertices_by_module_dependency.get(None)
-        module_to_render.append(main_module_object[BlockType.MODULE])
-        for module_idx_level in module_to_render:
-            for module_idx in module_idx_level:
+        current_level = [None]
+        main_module_modules = deepcopy(self.local_graph.vertices_by_module_dependency.get(None)[BlockType.MODULE])
+        modules_to_render = main_module_modules
+        while modules_to_render:
+            for module_idx in modules_to_render:
                 if module_idx not in modules_blocks:
                     continue
                 else:
                     module_block = self.local_graph.vertices[module_idx]
                     for_each = module_block.attributes.get(FOREACH_STRING)
                     count = module_block.attributes.get(COUNT_STRING)
+                    sub_graph = self._build_sub_graph(modules_blocks)
+                    self._render_sub_graph(sub_graph, blocks_to_render=modules_blocks)
                     if for_each:
                         if not self._is_static_statement(module_idx):
-                            sub_graph = self._build_sub_graph(modules_blocks)
-                            self._render_sub_graph(sub_graph, blocks_to_render=modules_blocks)
                             for_each = self._handle_static_statement(module_idx, sub_graph)
                             if not self._is_static_statement(module_idx, sub_graph):
                                 continue
                         self.duplicate_module_with_for_each(module_idx, for_each)
                     elif count:
                         if not self._is_static_statement(module_idx):
-                            sub_graph = self._build_sub_graph(modules_blocks)
-                            self._render_sub_graph(sub_graph, blocks_to_render=modules_blocks)
                             count = self._handle_static_statement(module_idx, sub_graph)
                             if not self._is_static_statement(module_idx, sub_graph):
                                 continue
                         self.duplicate_module_with_count(module_idx, count)
+
+            modules_to_render = self._get_modules_to_render(current_level)
+            # self.local_graph._arrange_graph_data()
+            # self.local_graph._build_edges()
 
     def _duplicate_module(self, module_idx: int, for_each: dict[str, Any] | list[str]) -> list[int]:
         start_len = len(self.local_graph.vertices)
@@ -347,4 +350,11 @@ class ForeachHandler(object):
     def duplicate_module_with_count(self, module_idx: int, count: int) -> None:
         self._create_new_resources_count(count, module_idx)
 
-
+    def _get_modules_to_render(self, current_level: list[TFModule | None]):
+        rendered_modules = [self.local_graph.vertices_by_module_dependency[curr]['module'] for curr in current_level][0]
+        current_level.clear()
+        for m_idx in rendered_modules:
+            m = self.local_graph.vertices[m_idx]
+            current_level.append(TFModule(m.path, m.name, m.source_module_object, m.for_each_index))
+        modules_to_render = [self.local_graph.vertices_by_module_dependency[curr]['module'] for curr in current_level]
+        return list(itertools.chain.from_iterable(modules_to_render))
