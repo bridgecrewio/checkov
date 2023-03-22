b9f447fc7db7d1d9cc7738112455b6fa43dd182e
James Woolfenden <james.woolfenden@gmail.com>
2023-03-22 16:23:52 +0000

    redshift should be set to automatically take snapshots

---
+++ tests/terraform/checks/resource/aws/test_RedshiftClusterAutoSnap.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+from checkov.runner_filter import RunnerFilter
+from checkov.terraform.checks.resource.aws.RedshiftClusterAutoSnap import check
+from checkov.terraform.runner import Runner
+
+
+class TestRedshiftClusterAutoSnap(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_RedshiftClusterAutoSnap"
+        report = runner.run(
+            root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id])
+        )
+        summary = report.get_summary()
+
+        passing_resources = {
+            "aws_redshift_cluster.pass",
+            "aws_redshift_cluster.pass2",
+        }
+        failing_resources = {
+            "aws_redshift_cluster.fail",
+        }
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == "__main__":
+    unittest.main()
