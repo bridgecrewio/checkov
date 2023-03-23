e1ca27ae3d825c839a568eb55cb9feeb0e1b93d8
ChanochShayner <chsh32340@gmail.com>
2023-03-23 14:47:04 +0200

    small changes, add checks to resourc address

--- checkov/terraform/graph_builder/foreach_handler.py
+++ checkov/terraform/graph_builder/foreach_handler.py
@@ -489,30 +489,30 @@ class ForeachHandler(object):
         main_module_modules = deepcopy(self.local_graph.vertices_by_module_dependency.get(None)[BlockType.MODULE])
         modules_to_render = main_module_modules
 
-        # TODO add documentation on logic here + Move the render graph to here
+        # The for_each rendering will be by modules levels, and not deeper in the graph.
         while modules_to_render:
+            sub_graph = self._build_sub_graph(modules_blocks)
+            self._render_sub_graph(sub_graph, blocks_to_render=modules_blocks)
             for module_idx in modules_to_render:
                 module_block = self.local_graph.vertices[module_idx]
                 for_each = module_block.attributes.get(FOREACH_STRING)
                 count = module_block.attributes.get(COUNT_STRING)
-                sub_graph = self._build_sub_graph(modules_blocks)
-                self._render_sub_graph(sub_graph, blocks_to_render=modules_blocks)
                 if for_each:
                     for_each = self._handle_static_statement(module_idx, sub_graph)
                     if not self._is_static_statement(module_idx, sub_graph):
                         continue
-                    self.duplicate_module_with_for_each(module_idx, for_each)
+                    self._duplicate_module_with_for_each(module_idx, for_each)
                 elif count:
                     count = self._handle_static_statement(module_idx, sub_graph)
                     if not self._is_static_statement(module_idx, sub_graph):
                         continue
-                    self.duplicate_module_with_count(module_idx, count)
+                    self._duplicate_module_with_count(module_idx, count)
             modules_to_render = self._get_modules_to_render(current_level)
 
-    def duplicate_module_with_for_each(self, module_idx: int, for_each: dict[str, Any] | list[str]) -> None:
+    def _duplicate_module_with_for_each(self, module_idx: int, for_each: dict[str, Any] | list[str]) -> None:
         self._create_new_resources_foreach(for_each, module_idx)
 
-    def duplicate_module_with_count(self, module_idx: int, count: int) -> None:
+    def _duplicate_module_with_count(self, module_idx: int, count: int) -> None:
         self._create_new_resources_count(count, module_idx)
 
     def _get_modules_to_render(self, current_level: list[TFModule | None]):
