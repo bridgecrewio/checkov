1aa6fe8a4eb01b37cb34dcfb2418c698785586f0
Anton Grübel <anton.gruebel@gmail.com>
2023-03-23 09:40:07 +0100

    Update ElasticBeanstalkUseEnhancedHealthChecks.py

--- checkov/terraform/checks/resource/aws/ElasticBeanstalkUseEnhancedHealthChecks.py
+++ checkov/terraform/checks/resource/aws/ElasticBeanstalkUseEnhancedHealthChecks.py
@@ -3,26 +3,26 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 
 
 class ElasticBeanstalkUseEnhancedHealthChecks(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         NIST.800-53.r5 CA-7, NIST.800-53.r5 SI-2
-        Elastic Beanstalk environments should have enhanced health reporting enabled
         """
-        name = "Ensure E"
+        name = "Ensure Elastic Beanstalk environments have enhanced health reporting enabled"
         id = "CKV_AWS_312"
-        supported_resources = ['aws_elastic_beanstalk_environment']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ('aws_elastic_beanstalk_environment',)
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if conf.get("setting"):
-            settings = conf.get("setting")
+        settings = conf.get("setting")
+        if settings and isinstance(settings, list):
             if isinstance(settings[0], list):
                 settings = settings[0]
             for setting in settings:
-                if isinstance(setting.get("namespace"), list) and \
-                        setting.get("namespace")[0] == "aws:elasticbeanstalk:healthreporting:system":
-                    if isinstance(setting.get("value"), list) and setting.get("value")[0] == "enhanced":
+                namespace = setting.get("namespace")
+                if isinstance(namespace, list) and namespace[0] == "aws:elasticbeanstalk:healthreporting:system":
+                    setting_val = setting.get("value")
+                    if isinstance(setting_val, list) and setting_val[0] == "enhanced":
                         return CheckResult.PASSED
         return CheckResult.FAILED
 
