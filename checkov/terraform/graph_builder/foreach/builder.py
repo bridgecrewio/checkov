8fe6b2345c3cf48f39a1327c5962961bab32d257
Barak Fatal <35402131+bo156@users.noreply.github.com>
2023-03-23 17:58:54 +0200

    feat(terraform): refactor foreach handler for modules (#4738)
    * Moved foreach_handler to a dedicated dir
    
    * Middle of refactor - started seperating to handlers per resource type
    
    * mid refactor - seperated to abstract handler, resource and module
    
    * All tests pass
    
    * flake8
    
    * mypy

---
+++ checkov/terraform/graph_builder/foreach/builder.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import typing
+
+from checkov.terraform.graph_builder.foreach.module_handler import ForeachModuleHandler
+from checkov.terraform.graph_builder.foreach.resource_handler import ForeachResourceHandler
+from checkov.terraform.graph_builder.graph_components.block_types import BlockType
+
+if typing.TYPE_CHECKING:
+    from checkov.terraform.graph_builder.local_graph import TerraformLocalGraph
+
+
+class ForeachBuilder:
+    def __init__(self, local_graph: TerraformLocalGraph):
+        self._resource_handler = ForeachResourceHandler(local_graph)
+        self._module_handler = ForeachModuleHandler(local_graph)
+
+    def handle(self, foreach_blocks: dict[str, list[int]]) -> None:
+        self._module_handler.handle(foreach_blocks.get(BlockType.MODULE, []))
+        self._resource_handler.handle(foreach_blocks.get(BlockType.RESOURCE, []))
---
+++ checkov/terraform/graph_builder/foreach/consts.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Optional, Any
+from typing_extensions import TypeAlias
+
+
+FOREACH_STRING = 'for_each'
+COUNT_STRING = 'count'
+REFERENCES_VALUES = r"(var|module|local)\."
+FOR_EACH_BLOCK_TYPE: TypeAlias = "dict[int, Optional[list[str] | dict[str, Any] | int]]"
+COUNT_KEY = 'count.index'
+EACH_KEY = 'each.key'
+EACH_VALUE = 'each.value'
