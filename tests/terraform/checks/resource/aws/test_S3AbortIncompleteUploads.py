7b031251ad2d274489e8964d8199425bb63f5e4f
gruebel <anton.gruebel@gmail.com>
2023-03-24 16:50:27 +0100

    refine S3 lifecycle check CKV_AWS_300

--- tests/terraform/checks/resource/aws/test_S3AbortIncompleteUploads.py
+++ tests/terraform/checks/resource/aws/test_S3AbortIncompleteUploads.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from pathlib import Path
 
 from checkov.runner_filter import RunnerFilter
 from checkov.terraform.checks.resource.aws.S3AbortIncompleteUploads import check
@@ -8,11 +9,13 @@ from checkov.terraform.runner import Runner
 
 class TestS3AbortIncompleteUploads(unittest.TestCase):
     def test(self):
-        runner = Runner()
-        current_dir = os.path.dirname(os.path.realpath(__file__))
+        # given
+        test_files_dir = Path(__file__).parent / "example_S3AbortIncompleteUploads"
 
-        test_files_dir = current_dir + "/example_S3AbortIncompleteUploads"
-        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        # then
         summary = report.get_summary()
 
         passing_resources = {
@@ -22,10 +25,11 @@ class TestS3AbortIncompleteUploads(unittest.TestCase):
         failing_resources = {
             "aws_s3_bucket_lifecycle_configuration.fail",
             "aws_s3_bucket_lifecycle_configuration.fail2",
+            "aws_s3_bucket_lifecycle_configuration.fail3",
         }
 
-        passed_check_resources = set([c.resource for c in report.passed_checks])
-        failed_check_resources = set([c.resource for c in report.failed_checks])
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
 
         self.assertEqual(summary["passed"], len(passing_resources))
         self.assertEqual(summary["failed"], len(failing_resources))
