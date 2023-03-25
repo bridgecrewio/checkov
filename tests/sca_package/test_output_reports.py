04937f2d6b40f43c5036f94ce4cd656c23d74dbd
gruebel <anton.gruebel@gmail.com>
2023-03-25 23:37:41 +0100

    fix tests

--- tests/sca_package/test_output_reports.py
+++ tests/sca_package/test_output_reports.py
@@ -26,7 +26,7 @@ def _get_deterministic_items_in_cyclonedx(pretty_xml_as_list: List[str]) -> List
     filtered_list = []
     for i, line in enumerate(pretty_xml_as_list):
         if not any(word in line for word in black_list_words):
-            if i == 0 or not any(tool_name in pretty_xml_as_list[i-1] for tool_name in ("<name>checkov</name>", "<name>cyclonedx-python-lib</name>")):
+            if i == 0 or not any(tool_name in pretty_xml_as_list[i-1] for tool_name in ("<ns0:name>checkov</ns0:name>", "<ns0:name>cyclonedx-python-lib</ns0:name>")):
                 filtered_list.append(line)
     return filtered_list
 
