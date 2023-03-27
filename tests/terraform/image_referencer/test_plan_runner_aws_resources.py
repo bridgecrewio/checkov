a9a1386a45dc9037549a2a2d6d4ddf58e2632478
James Woolfenden <james.woolfenden@gmail.com>
2023-03-27 09:43:40 +0100

    fragile tests

--- tests/terraform/image_referencer/test_plan_runner_aws_resources.py
+++ tests/terraform/image_referencer/test_plan_runner_aws_resources.py
@@ -150,7 +150,7 @@ def test_codebuild_resources(mocker: MockerFixture, graph_framework):
     sca_image_report = next(report for report in reports if report.check_type == CheckType.SCA_IMAGE)
 
     assert len(tf_report.resources) == 3
-    assert len(tf_report.passed_checks) == 6
+    assert len(tf_report.passed_checks) == 8
     assert len(tf_report.failed_checks) == 0
     assert len(tf_report.skipped_checks) == 0
     assert len(tf_report.parsing_errors) == 0
