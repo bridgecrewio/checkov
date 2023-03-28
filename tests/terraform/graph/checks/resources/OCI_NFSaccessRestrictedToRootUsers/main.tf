0f2ff9e47ad85e733a8d6a96c9cb9391b0c93540
Praveen <prdeshpande@paloaltonetworks.com>
2023-03-28 15:41:53 +0530

    Added a OCI YAML policy: OCI_NFSaccessRestrictedToRootUsers

--- tests/terraform/graph/checks/resources/OCI_NFSaccessRestrictedToRootUsers/main.tf
+++ tests/terraform/graph/checks/resources/OCI_NFSaccessRestrictedToRootUsers/main.tf
@@ -1,3 +1,5 @@
+# PASS 1: identity_squash = ROOT, anonymous_gid = 65534 & anonymous_uid = 65534
+
 resource "oci_file_storage_export" "pass_1" {
 
   export_options {
@@ -16,31 +18,69 @@ resource "oci_file_storage_export" "pass_1" {
     anonymous_uid = 65534
     
   }
-
 }
 
+# PASS 2: identity_squash does not contain ROOT, so no validations will run on this snippet.
+
+resource "oci_file_storage_export" "pass_2" {
+  export_set_id  = oci_file_storage_export_set.fss_pud_export_set.id
+  file_system_id = oci_file_storage_file_system.fss_pud_file_system.id
+  path           = var.export_path_fss_pud
+
+  export_options {
+    source                         = var.pud_subnet_cidr_block
+    access                         = "READ_WRITE"
+    identity_squash                = "NONE"
+    require_privileged_source_port = true
+  }
+  export_options {
+    source                         = var.pud_web_subnet_cidr_block
+    access                         = "READ_WRITE"
+    identity_squash                = "NONE"
+    require_privileged_source_port = true
+  }
+}
+
+# FAIL 1: identity_squash = ROOT but anonymous_gid & anonymous_uid don't exist
 
 resource "oci_file_storage_export" "fail_1" {
-  export_set_id  = oci_file_storage_export_set.fss_sap_export_set.id
-  file_system_id = oci_file_storage_file_system.fss_sap_file_system.id
-  path           = var.export_path_fss_sap
+  export_set_id  = oci_file_storage_export_set.fss_pud_export_set.id
+  file_system_id = oci_file_storage_file_system.fss_pud_file_system.id
+  path           = var.export_path_fss_pud
 
   export_options {
-    source                         = var.sap_subnet_cidr_block
+    source                         = var.pud_subnet_cidr_block
+    access                         = "READ_WRITE"
+    identity_squash                = "ROOT"
+    require_privileged_source_port = true
+  }
+  export_options {
+    source                         = var.pud_web_subnet_cidr_block
+    access                         = "READ_WRITE"
+    identity_squash                = "NONE"
+    require_privileged_source_port = true
+  }
+}
+
+# FAIL 2: identity_squash = ROOT but anonymous_gid & anonymous_uid NOT equals to 65534
+
+resource "oci_file_storage_export" "fail_2" {
+  export_set_id  = oci_file_storage_export_set.fss_pud_export_set.id
+  file_system_id = oci_file_storage_file_system.fss_pud_file_system.id
+  path           = var.export_path_fss_pud
+
+  export_options {
+    source                         = var.pud_subnet_cidr_block
     access                         = "READ_WRITE"
     identity_squash                = "NONE"
     require_privileged_source_port = true
   }
   export_options {
-    source                         = var.sap_web_subnet_cidr_block
+    source                         = var.pud_web_subnet_cidr_block
     access                         = "READ_WRITE"
-    identity_squash                = "NONE"
-    require_privileged_source_port = true
-  }
-  export_options {
-    source                         = var.database_subnet_cidr_block
-    access                         = "READ_WRITE"
-    identity_squash                = "NONE"
+    identity_squash                = "ROOT"
+    anonymous_gid = 0
+    anonymous_uid = 4294967295
     require_privileged_source_port = true
   }
 }
