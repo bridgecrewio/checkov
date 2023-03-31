38f2a40f54aca303cf1484f6e28b3ea1ac3d3f2b
James Woolfenden <james.woolfenden@gmail.com>
2023-03-31 10:41:18 +0100

    APIm backend uses only HTTPS

---
+++ checkov/terraform/checks/resource/azure/APIManagementBackendHTTPS.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class APIManagementBackendHTTPS(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure API management backend uses https"
+        id = "CKV_AZURE_215"
+        supported_resources = ['azurerm_api_management_backend']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if 'https' in conf.get('url'):
+            return CheckResult.PASSED
+        self.evaluated_keys = ['/url/']
+        return CheckResult.FAILED
+
+
+check = APIManagementBackendHTTPS()
