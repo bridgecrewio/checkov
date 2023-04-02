c0e249cd5f1c21e4d2417872393c0bcfae6b27c5
bschwab003 <brianmschwab@gmail.com>
2023-04-02 00:25:21 -0600

    fix(terraform): Length resolvers evaluate length of `dict` as 1. (#4808)
    Update the existing "length" operators to evaluate dict objects as a length of 1.  See https://github.com/bridgecrewio/checkov/issues/4751
    
    Co-authored-by: Brian Schwab <brian.m.schwab@pwc.com>

--- tests/terraform/graph/checks_infra/attribute_solvers/length_equals_solver/test_solver.py
+++ tests/terraform/graph/checks_infra/attribute_solvers/length_equals_solver/test_solver.py
@@ -37,3 +37,14 @@ class TestLengthEquals(TestBaseSolver):
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_dict_length_equals(self):
+        # this is just a basic check to make sure the operator works
+        # we'll check all the other combinations more directly (because coming up with all the policy combos is painful)
+        root_folder = '../../../resources/lengths'
+        check_id = "DictLengthEquals"
+        should_pass = ['aws_security_group.sg3']
+        should_fail = ['aws_security_group.sg2']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
