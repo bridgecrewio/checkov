2e1fbd7acd274d61abbbcd94fc58dbdb0d30f694
gruebel <anton.gruebel@gmail.com>
2023-04-03 23:55:27 +0200

    improve check logic

--- checkov/terraform/checks/resource/gcp/GoogleSubnetworkPrivateGoogleEnabled.py
+++ checkov/terraform/checks/resource/gcp/GoogleSubnetworkPrivateGoogleEnabled.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
@@ -10,11 +14,11 @@ class GoogleSubnetworkPrivateGoogleEnabled(BaseResourceValueCheck):
         categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf) -> CheckResult:
-        if conf.get("purpose") \
-                and isinstance(conf.get('purpose'), list) \
-                and conf.get("purpose")[0] == "INTERNAL_HTTPS_LOAD_BALANCER":
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        purpose = conf.get("purpose")
+        if purpose and isinstance(purpose, list) and purpose[0] == "INTERNAL_HTTPS_LOAD_BALANCER":
             return CheckResult.UNKNOWN
+
         return super().scan_resource_conf(conf)
 
     def get_inspected_key(self) -> str:
