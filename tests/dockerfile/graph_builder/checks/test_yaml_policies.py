e6266e7eb721a493c895ccd62db19874e6826246
james-otten-pan <69821911+james-otten-pan@users.noreply.github.com>
2023-04-04 06:57:17 -0400

    feat(dockerfile): Add CKV2_DOCKER_15 for yum-config-manager sslverify (#4622)
    * Add CKV2_DOCKER_15 for yum-config-manager sslverify
    
    * Case insensitivity on the regex
    
    While this could technically lead to things like YuM CoNfIg-... getting matched,
    I don't think it will happen in practice. Also, if your dockerfile does that,
    this policy will be the least of your worries.

--- tests/dockerfile/graph_builder/checks/test_yaml_policies.py
+++ tests/dockerfile/graph_builder/checks/test_yaml_policies.py
@@ -85,6 +85,9 @@ class TestYamlPolicies(TestYamlPoliciesBase):
     def test_EnvGitSslNoVerify(self):
         self.go("EnvGitSslNoVerify")
 
+    def test_RunYumConfigManagerSslVerify(self):
+        self.go("RunYumConfigManagerSslVerify")
+
     def test_registry_load(self):
         registry = self.get_checks_registry()
         self.assertGreater(len(registry.checks), 0)
