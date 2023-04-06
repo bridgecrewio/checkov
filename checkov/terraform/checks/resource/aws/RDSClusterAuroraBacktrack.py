48476e7cd656f31a1d2499fb77f978a7dcce5b32
gruebel <anton.gruebel@gmail.com>
2023-04-06 10:45:36 +0200

    improve check logic

--- checkov/terraform/checks/resource/aws/RDSClusterAuroraBacktrack.py
+++ checkov/terraform/checks/resource/aws/RDSClusterAuroraBacktrack.py
@@ -1,40 +1,43 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
+
+SUPPORTED_ENGINES = {"aurora", "aurora-mysql"}
 
 
-class RDSClusterAuroraBacktrack(BaseResourceCheck):
-    def __init__(self):
+class RDSClusterAuroraBacktrack(BaseResourceNegativeValueCheck):
+    def __init__(self) -> None:
         """
         NIST.800-53.r5 CP-10, NIST.800-53.r5 CP-6, NIST.800-53.r5 CP-6(1), NIST.800-53.r5 CP-6(2), NIST.800-53.r5 CP-9,
         NIST.800-53.r5 SI-13(5)	Amazon Aurora clusters should have backtracking enabled
         """
         name = "Ensure that RDS Aurora Clusters have backtracking enabled"
         id = "CKV_AWS_326"
-        supported_resources = ['aws_rds_cluster']
-        categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        supported_resources = ("aws_rds_cluster",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+            missing_attribute_result=CheckResult.FAILED,
+        )
 
-    def scan_resource_conf(self, conf):
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        engine = conf.get("engine")
+        if engine and isinstance(engine, list) and engine[0] not in SUPPORTED_ENGINES:
+            return CheckResult.UNKNOWN
 
-        if conf.get("engine") and isinstance(conf.get("engine"), list):
-            if len(conf.get("engine")) > 0:
-                engine = conf.get("engine")[0]
-                if engine in ["aurora", "aurora-mysql"]:
-                    if RDSClusterAuroraBacktrack.backtracked(conf):
-                        return CheckResult.PASSED
-                    return CheckResult.FAILED
-                return CheckResult.UNKNOWN
-        if RDSClusterAuroraBacktrack.backtracked(conf):
-            return CheckResult.PASSED
-        return CheckResult.FAILED
+        return super().scan_resource_conf(conf=conf)
 
-    @staticmethod
-    def backtracked(conf):
-        if conf.get("backtrack_window") and isinstance(conf.get("backtrack_window"), list):
-            if len(conf.get("backtrack_window")) > 0:
-                if conf.get("backtrack_window")[0] > 0:
-                    return True
-        return False
+    def get_inspected_key(self) -> str:
+        return "backtrack_window"
+
+    def get_forbidden_values(self) -> list[Any]:
+        return [0]
 
 
 check = RDSClusterAuroraBacktrack()
