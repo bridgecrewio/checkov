2ed856e5790d7558c21e6748a44655823964d6e2
gruebel <anton.gruebel@gmail.com>
2023-04-07 23:43:32 +0200

    move the jsonpath cache to class level

--- tests/terraform/graph/checks_infra/test_base.py
+++ tests/terraform/graph/checks_infra/test_base.py
@@ -1,6 +1,7 @@
 import os
 from unittest import TestCase
 from unittest import mock
+from unittest.mock import MagicMock
 
 from checkov.common.checks_infra.checks_parser import GraphCheckParser
 from checkov.common.checks_infra.registry import Registry
@@ -30,6 +31,37 @@ class TestBaseSolver(TestCase):
     def test_unrendered_variable_source(self):
         self.assertTrue(BaseAttributeSolver._is_variable_dependant("var.location", "Terraform"))
         self.assertTrue(BaseAttributeSolver._is_variable_dependant("var.location", "terraform"))
+
+    def test_get_cached_jsonpath_statement(self):
+        # given
+        statement = "policy.Statement[?(@.Effect == Allow)].Action[*]"
+        solver_1 = BaseAttributeSolver(
+            resource_types=["aws_iam_policy"],
+            attribute=statement,
+            value="iam:*",
+            is_jsonpath_check=True,
+        )
+        solver_2 = BaseAttributeSolver(
+            resource_types=["aws_iam_policy"],
+            attribute=statement,
+            value="iam:*",
+            is_jsonpath_check=True,
+        )
+        jsonpath_parse_mock = MagicMock()
+
+        self.assertEqual(len(BaseAttributeSolver.jsonpath_parsed_statement_cache), 0)
+
+        # when
+        solver_1._get_cached_jsonpath_statement(statement=statement)
+        self.assertEqual(len(BaseAttributeSolver.jsonpath_parsed_statement_cache), 1)
+
+        # patch jsonpath_ng.parse to be able to check it was really not called again and the cache was properly used
+        with mock.patch("checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver.parse", side_effect=jsonpath_parse_mock):
+            solver_2._get_cached_jsonpath_statement(statement=statement)
+
+        # then
+        self.assertEqual(len(BaseAttributeSolver.jsonpath_parsed_statement_cache), 1)
+        jsonpath_parse_mock.assert_not_called()  # jsonpath_ng.parse shouldn't have been called again
 
 
 def verify_report(report, expected_results):
