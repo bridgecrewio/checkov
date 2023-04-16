f11446339890c16dc3ec4ce7172ca84805c2cb4d
Omry Mendelovich <omry155@gmail.com>
2023-04-16 15:08:28 +0300

    fix file path

--- checkov/secrets/git_history_store.py
+++ checkov/secrets/git_history_store.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import copy
+from pathlib import Path
 from collections import defaultdict
 from typing import TYPE_CHECKING, Dict, List, Optional
 from typing_extensions import TypedDict
@@ -111,7 +112,7 @@ class GitHistorySecretStore:
                     temp_secrets_by_file_value_type[new_secret_key].append(enriched_potential_secret)
         self.secrets_by_file_value_type.update(temp_secrets_by_file_value_type)
 
-    def get_added_and_removed_commit_hash(self, key: str, secret: PotentialSecret) -> EnrichedPotentialSecretMetadata:
+    def get_added_and_removed_commit_hash(self, key: str, secret: PotentialSecret, root_folder: str) -> EnrichedPotentialSecretMetadata:
         """
         now we have only the current commit_hash - in the added_commit_hash or in the removed_commit_hash.
         in the next step we will add the connection and the missing data
@@ -122,7 +123,15 @@ class GitHistorySecretStore:
         """
         try:
             secret_key = get_secret_key(secret.filename, secret.secret_hash, secret.type)  # by value type
-            enriched_secrets = self.secrets_by_file_value_type[secret_key]
+            enriched_secrets = self.secrets_by_file_value_type.get(secret_key, None)
+            if not enriched_secrets:
+                # sometimes the secret key is from the project path instead of abs path
+                filename = f'{root_folder}/{secret.filename}'
+                secret_key = get_secret_key(filename, secret.secret_hash, secret.type)  # by value type
+                enriched_secrets = self.secrets_by_file_value_type.get(secret_key, None)
+                if not enriched_secrets:
+                    logging.warning(f'Did not find added_commit_hash and removed_commit_hash for {secret_key}')
+                    return {}
             chosen_secret = enriched_secrets[0]
             if len(enriched_secrets) > 1:
                 res = key.split("_")
