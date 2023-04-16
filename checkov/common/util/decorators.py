57cc36376d8ad03368768f831dec3a863a6688e8
Omry Mendelovich <omry155@gmail.com>
2023-04-16 10:05:12 +0300

    remove it

--- checkov/common/util/decorators.py
+++ checkov/common/util/decorators.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from datetime import timedelta
 import logging
-import os
 from functools import wraps
 from timeit import default_timer
 from typing import TypeVar, Callable
@@ -24,15 +23,11 @@ def time_it(func: Callable[P, T]) -> Callable[P, T]:
             end = default_timer()
 
             func_path = f"{func.__code__.co_filename.replace('.py', '')}.{func.__name__}"
-            info = f"'{func_path}' took: {timedelta(seconds=end - start)}\n"
-            if os.getenv("PYCHARM_HOSTED") == "1":
-                # PYCHARM_HOSTED env variable equals 1 when debugging via jetbrains IDE.
-                with open('time_it.txt', 'a') as f:
-                    f.writelines(info)
-            logging.info(info)
+            logging.info(f"'{func_path}' took: {timedelta(seconds=end - start)}\n")
 
             return output
         except Exception as e:
             # we don't want exception in wrapper to affect real run
             logging.warning(f"[time_it] got exception: {e}")
+            return func(*args, **kwargs)
     return wrapper
