cbaab492cef097ef737ed360e2dc722157de10ac
yaaraverner <yaara@vernermail.com>
2023-04-16 16:23:06 +0300

    fix lint

--- checkov/kustomize/image_referencer/base_provider.py
+++ checkov/kustomize/image_referencer/base_provider.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
-from typing import Any, Mapping
-from networkx import DiGraph
+from typing import Any, Mapping, TYPE_CHECKING, Union
+
+
 from checkov.kubernetes.image_referencer.base_provider import BaseKubernetesProvider
 from checkov.common.images.graph.image_referencer_provider import _ExtractImagesCallableAlias
 from checkov.common.graph.graph_builder.graph_components.attribute_names import CustomAttributes
 
+if TYPE_CHECKING:
+    from igraph import Graph
+    from networkx import DiGraph
+
 
 class BaseKustomizeProvider(BaseKubernetesProvider):
-    def __init__(self, graph_connector: DiGraph, supported_resource_types: dict[str, _ExtractImagesCallableAlias] |
+    def __init__(self, graph_connector: Union[Graph, DiGraph], supported_resource_types: dict[str, _ExtractImagesCallableAlias] |
                                                                            Mapping[str, _ExtractImagesCallableAlias],
                  report_mutator_data: dict[str, dict[str, Any]]) -> None:
         super().__init__(
@@ -18,6 +23,6 @@ class BaseKustomizeProvider(BaseKubernetesProvider):
 
     def _get_resource_path(self, resource: dict[str, Any]) -> str:
         k8s_path = resource.get(CustomAttributes.FILE_PATH)
-        dir_path = self.report_mutator_data.get('kustomizeFileMappings').get(k8s_path)
-        file_metadata = self.report_mutator_data['kustomizeMetadata'][dir_path]
+        dir_path = self.report_mutator_data.get('kustomizeFileMappings', {}).get(k8s_path)
+        file_metadata = self.report_mutator_data.get('kustomizeMetadata', {}).get(dir_path, {})
         return file_metadata.get('filePath')
--- checkov/kustomize/image_referencer/provider/kustomize.py
+++ checkov/kustomize/image_referencer/provider/kustomize.py
@@ -1,14 +1,17 @@
-from typing import Any, Union, Dict
+from __future__ import annotations
 
-import networkx
-import igraph
+from typing import Any, Dict, TYPE_CHECKING
 
 from checkov.kubernetes.image_referencer.provider.k8s import SUPPORTED_K8S_IMAGE_RESOURCE_TYPES
 from checkov.kustomize.image_referencer.base_provider import BaseKustomizeProvider
 
+if TYPE_CHECKING:
+    from igraph import Graph
+    from networkx import DiGraph
+
 
 class KustomizeProvider(BaseKustomizeProvider):
-    def __init__(self, graph_connector: Union[igraph.Graph, networkx.DiGraph], report_mutator_data: Dict[str, Dict[str, Any]]):
+    def __init__(self, graph_connector: Graph | DiGraph, report_mutator_data: Dict[str, Dict[str, Any]]):
         super().__init__(
             graph_connector=graph_connector,
             supported_resource_types=SUPPORTED_K8S_IMAGE_RESOURCE_TYPES,
