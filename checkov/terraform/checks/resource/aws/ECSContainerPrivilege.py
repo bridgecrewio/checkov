5cb9b5c35e51990d7bb8f9b64ec11e5990b6b67a
gruebel <anton.gruebel@gmail.com>
2023-04-17 08:30:19 +0900

    add evaluated_keys

--- checkov/terraform/checks/resource/aws/ECSContainerPrivilege.py
+++ checkov/terraform/checks/resource/aws/ECSContainerPrivilege.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 
 
 class ECSContainerPrivilege(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         NIST.800-53.r5 AC-2(1), NIST.800-53.r5 AC-3, NIST.800-53.r5 AC-3(15), NIST.800-53.r5 AC-3(7),
         NIST.800-53.r5 AC-5, NIST.800-53.r5 AC-6
@@ -13,19 +13,22 @@ class ECSContainerPrivilege(BaseResourceCheck):
         """
         name = "Ensure ECS containers should run as non-privileged"
         id = "CKV_AWS_334"
-        supported_resources = ["aws_ecs_task_definition"]
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("aws_ecs_task_definition",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        if 'container_definitions' in conf.keys():
-            if conf.get("container_definitions") and isinstance(conf.get("container_definitions"), list):
-                containers = conf.get("container_definitions")[0]
-                if len(containers) > 0:
-                    for container in containers:
-                        if isinstance(container, dict) and container.get("privilege"):
-                            return CheckResult.FAILED
-                    return CheckResult.PASSED
+        self.evaluated_keys = ["container_definitions"]
+        container_definitions = conf.get("container_definitions")
+        if container_definitions and isinstance(container_definitions, list):
+            containers = container_definitions[0]
+            if containers and isinstance(containers, list):
+                for idx, container in enumerate(containers):
+                    if isinstance(container, dict) and container.get("privilege"):
+                        self.evaluated_keys = [f"container_definitions/[0]/[{idx}]/privilege"]
+                        return CheckResult.FAILED
+                return CheckResult.PASSED
+
         return CheckResult.UNKNOWN
 
 
