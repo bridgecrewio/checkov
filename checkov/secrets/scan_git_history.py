3faa395f3bbfd2cb63a7f46d7255dcca85113081
marynaKK <kabirmaryna@gmail.com>
2023-04-20 12:03:25 +0300

    remove flag

--- checkov/secrets/scan_git_history.py
+++ checkov/secrets/scan_git_history.py
@@ -213,7 +213,7 @@ class GitHistoryScanner:
     def _get_first_commit(self) -> List[Commit]:
         first_commit_sha = self.repo.git.log('--format=%H', '--max-parents=0', 'HEAD').split()[0]
         first_commit = self.repo.commit(first_commit_sha)
-        empty_tree_sha = bytes.fromhex(hashlib.sha1(b'tree 0\0', usedforsecurity=False).hexdigest())  # nosec
+        empty_tree_sha = bytes.fromhex(hashlib.sha1(b'tree 0\0').hexdigest())  # nosec
         empty_tree = Tree(self.repo, empty_tree_sha)
         git_diff = empty_tree.diff(first_commit, create_patch=True)
 
