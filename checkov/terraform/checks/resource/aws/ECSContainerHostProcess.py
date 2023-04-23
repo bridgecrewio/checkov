3420360f8012aa73d56e8127b5e8e13ab05783ec
gruebel <anton.gruebel@gmail.com>
2023-04-23 23:23:28 +0900

    update check and tests

--- checkov/terraform/checks/resource/aws/ECSContainerHostProcess.py
+++ checkov/terraform/checks/resource/aws/ECSContainerHostProcess.py
@@ -5,28 +5,36 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 
 
 class ECSContainerHostProcess(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         NIST.800-53.r5 CA-9(1), NIST.800-53.r5 CM-2
         ECS task definitions should not share the host's process namespace
         """
         name = "Ensure ECS task definitions should not share the host's process namespace"
         id = "CKV_AWS_335"
-        supported_resources = ["aws_ecs_task_definition"]
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("aws_ecs_task_definition",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        if 'container_definitions' in conf.keys():
-            if conf.get("container_definitions") and isinstance(conf.get("container_definitions"), list):
-                containers = conf.get("container_definitions")[0]
-                if len(containers) > 0:
-                    for container in containers:
-                        if isinstance(container, dict) and container.get("pidMode"):
-                            pidmode = container.get("pidMode")
-                            if pidmode == "host":
-                                return CheckResult.FAILED
-                    return CheckResult.PASSED
+        self.evaluated_keys = ["container_definitions"]
+        container_definitions = conf.get("container_definitions")
+        if container_definitions and isinstance(container_definitions, list):
+            containers = container_definitions[0]
+            if containers and isinstance(containers, list):
+                for idx, container in enumerate(containers):
+                    if isinstance(container, dict) and container.get("pidMode") == "host":
+                        self.evaluated_keys = [f"container_definitions/[{idx}]/pidMode"]
+                        return CheckResult.FAILED
+                return CheckResult.PASSED
+            elif isinstance(containers, dict):
+                # TF plan file case
+                for idx, container in enumerate(container_definitions):
+                    if isinstance(container, dict) and container.get("pidMode") == "host":
+                        self.evaluated_keys = [f"container_definitions/[{idx}]/pidMode"]
+                        return CheckResult.FAILED
+                return CheckResult.PASSED
+
         return CheckResult.UNKNOWN
 
 
