82892ea1aa10641af86a360fe2fb48e85159e062
ChanochShayner <57212002+ChanochShayner@users.noreply.github.com>
2023-04-24 15:55:31 +0300

    Update tf_parser.py

--- checkov/terraform/tf_parser.py
+++ checkov/terraform/tf_parser.py
@@ -203,6 +203,7 @@ class TFParser:
             if not self.should_loaded_file(file, root_dir):
                 continue
                 
+            #  Dont run over the nested because we already run on them - dont remove.
             if file.tf_source_modules:
                 continue
 
