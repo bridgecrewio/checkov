46b988db52f010080a35762233bdf10b218f98d3
gruebel <anton.gruebel@gmail.com>
2023-04-26 21:34:40 +0900

    adjust check logic

--- checkov/terraform/checks/resource/azure/APIManagementBackendHTTPS.py
+++ checkov/terraform/checks/resource/azure/APIManagementBackendHTTPS.py
@@ -1,21 +1,28 @@
+from __future__ import annotations
+
+from typing import Any
+
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
 class APIManagementBackendHTTPS(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure API management backend uses https"
         id = "CKV_AZURE_215"
-        supported_resources = ['azurerm_api_management_backend']
-        categories = [CheckCategories.ENCRYPTION]
+        supported_resources = ("azurerm_api_management_backend",)
+        categories = (CheckCategories.ENCRYPTION,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        if isinstance(conf.get('url'), list):
-            if 'https' in conf.get('url')[0]:
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        self.evaluated_keys = ["url"]
+        url = conf.get("url")
+        if url and isinstance(url, list):
+            if "https" in url[0]:
                 return CheckResult.PASSED
-            self.evaluated_keys = ['/url/']
+
             return CheckResult.FAILED
+
         return CheckResult.UNKNOWN
 
 
