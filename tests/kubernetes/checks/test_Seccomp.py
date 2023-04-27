f94acd546c350c1b786ca4aaea16327a6c65992b
Taylor <tsmith.v11@gmail.com>
2023-04-27 15:42:37 -0700

    Updated ckv_k8s_31
    Catch when all containers have the type RuntImeDefault

--- tests/kubernetes/checks/test_Seccomp.py
+++ tests/kubernetes/checks/test_Seccomp.py
@@ -19,8 +19,8 @@ class TestSeccomp(unittest.TestCase):
         passed_resources = [check.resource for check in report.passed_checks]
         failed_resources = [check.resource for check in report.failed_checks]
 
-        self.assertEqual(summary["passed"], 8)
-        self.assertEqual(summary["failed"], 2)
+        self.assertEqual(summary["passed"], 9)
+        self.assertEqual(summary["failed"], 3)
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
