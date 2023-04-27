34a72e6f29a6f0b09268f137d6d9defa145149e9
James Woolfenden <james.woolfenden@gmail.com>
2023-04-27 10:35:34 +0100

    NACL should restrict ingress

---
+++ checkov/terraform/checks/resource/aws/NetworkACLUnrestricted.py
@@ -0,0 +1,21 @@
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+
+class NetworkACLUnrestricted(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure NACL ingress is not Allow All"
+        id = "CKV_AWS_352"
+        supported_resources = ['aws_network_acl_rule']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if isinstance(conf.get('egress'), list) and conf.get('egress')[0]:
+            return CheckResult.UNKNOWN
+        if isinstance(conf.get('from_port'), list) and conf.get('from_port')[0]:
+            return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = NetworkACLUnrestricted()
