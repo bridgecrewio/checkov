752e9f5638e80431e18d71c873e42b9f4255baf0
James Woolfenden <james.woolfenden@gmail.com>
2023-04-28 14:59:53 +0100

    add check to look at star resources

---
+++ tests/terraform/checks/resource/aws/example_IAMStarResourcePolicyDocument/main.tf
@@ -0,0 +1,77 @@
+resource "aws_iam_policy" "fail" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+resource "aws_iam_policy" "pass" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:ec2:us-east-1:123456789012:instance/*"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "fail" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "pass" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:ec2:us-east-1:123456789012:instance/*"
+      },
+    ]
+  })
+}
