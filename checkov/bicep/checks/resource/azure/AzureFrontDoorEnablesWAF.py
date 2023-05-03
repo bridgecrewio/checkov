eb8686b1ddf46e4f7710456b1a7ee965e9ad603a
James Woolfenden <james.woolfenden@gmail.com>
2023-05-03 17:14:32 +0100

    ARM and bicep checks for CKV_AZURE_121

---
+++ checkov/bicep/checks/resource/azure/AzureFrontDoorEnablesWAF.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from checkov.common.models.consts import ANY_VALUE
+
+from checkov.bicep.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class AzureFrontDoorEnablesWAF(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure that Azure Front Door enables WAF"
+        id = "CKV_AZURE_121"
+        supported_resources = ['Microsoft.Network/frontDoors']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "properties/frontendEndpoints/[0]/properties/webApplicationFirewallPolicyLink/id"
+
+    def get_expected_value(self):
+        return ANY_VALUE
+
+
+check = AzureFrontDoorEnablesWAF()
