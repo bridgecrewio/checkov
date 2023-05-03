4ef5ba4ede508cfb7534ce7ca96290da8e882d10
Praveen <prdeshpande@paloaltonetworks.com>
2023-05-03 12:42:08 +0530

    Partial completion of AzureEnableDefenderForDNS

--- tests/terraform/graph/checks/resources/AzureEnableDefenderForDNS/main.tf
+++ tests/terraform/graph/checks/resources/AzureEnableDefenderForDNS/main.tf
@@ -1,4 +1,10 @@
-resource "azurerm_security_center_subscription_pricing" "example" {
-  tier          = "Standard"
-  resource_type = "VirtualMachines"
+resource "azurerm_security_center_subscription_pricing" "fail" {
+  tier          = "Free"
+  resource_type = "DNS"
 }
+
+resource "azurerm_security_center_subscription_pricing" "pass" {
+  tier          = "Standard"
+  xresource_type = "dns"
+}
+
