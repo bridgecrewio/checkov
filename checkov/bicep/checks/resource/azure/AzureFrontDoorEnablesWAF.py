72ef2a981e25f39538b8d4e7528d1c1052eeba3b
James Woolfenden <james.woolfenden@gmail.com>
2023-05-04 11:23:59 +0100

    Add annotations

--- checkov/bicep/checks/resource/azure/AzureFrontDoorEnablesWAF.py
+++ checkov/bicep/checks/resource/azure/AzureFrontDoorEnablesWAF.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from checkov.common.models.consts import ANY_VALUE
+from typing import Any
 
 from checkov.bicep.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckCategories
@@ -13,10 +14,10 @@ class AzureFrontDoorEnablesWAF(BaseResourceValueCheck):
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "properties/frontendEndpoints/[0]/properties/webApplicationFirewallPolicyLink/id"
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> Any:
         return ANY_VALUE
 
 
