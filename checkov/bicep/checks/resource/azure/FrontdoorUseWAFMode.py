ca0e5330aac34d6117b0987f4aeedb936d198a0a
James Woolfenden <james.woolfenden@gmail.com>
2023-05-05 14:27:18 +0100

    feat(arm): Ensure that Azure Front Door uses WAF in "Detection" or "Prevention" modes CKV_AZURE_123

--- checkov/bicep/checks/resource/azure/FrontdoorUseWAFMode.py
+++ checkov/bicep/checks/resource/azure/FrontdoorUseWAFMode.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Any, List
 
 from checkov.bicep.checks.resource.base_resource_check import BaseResourceCheck
 from checkov.common.models.enums import CheckResult, CheckCategories
@@ -14,7 +14,7 @@ class FrontdoorUseWAFMode(BaseResourceCheck):
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf) -> CheckResult:
+    def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
         if conf.get('properties') and isinstance(conf.get('properties'), dict):
             properties = conf.get('properties')
             if isinstance(properties.get('policySettings'), dict) and properties.get('policySettings'):
