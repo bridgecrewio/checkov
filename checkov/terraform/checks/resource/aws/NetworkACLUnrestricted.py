0ac8b6e5ac63be1f5804b68e61a83f5d4847fa72
Anton Grübel <anton.gruebel@gmail.com>
2023-05-17 23:54:11 +0900

    adjust check name

--- checkov/terraform/checks/resource/aws/NetworkACLUnrestricted.py
+++ checkov/terraform/checks/resource/aws/NetworkACLUnrestricted.py
@@ -3,17 +3,19 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 
 
 class NetworkACLUnrestricted(BaseResourceCheck):
-    def __init__(self):
-        name = "Ensure NACL ingress is not allow all Ports"
+    def __init__(self) -> None:
+        name = "Ensure NACL ingress does not allow all Ports"
         id = "CKV_AWS_352"
-        supported_resources = ['aws_network_acl_rule']
-        categories = [CheckCategories.NETWORKING]
+        supported_resources = ('aws_network_acl_rule',)
+        categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        if isinstance(conf.get('egress'), list) and conf.get('egress')[0]:
+        egress = conf.get('egress')
+        if egress and isinstance(egress, list) and egress[0]:
             return CheckResult.UNKNOWN
-        if isinstance(conf.get('from_port'), list) and conf.get('from_port')[0]:
+        port = conf.get('from_port')
+        if port and isinstance(port, list) and port[0]:
             return CheckResult.PASSED
         return CheckResult.FAILED
 
