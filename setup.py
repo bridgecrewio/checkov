4089234118cc7b43cd455008cbda0d170b72a4d1
Saar Ettinger <saarettinger@gmail.com>
2023-05-18 20:37:27 +0300

    fixed setup.py and pipfile

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
         "yarl",
         "openai",
         "spdx-tools<0.8.0",
-        "license-expression",
+        "license-expression==30.1.0",
     ],
     dependency_links=[],  # keep it empty, needed for pipenv-setup
     license="Apache License 2.0",
