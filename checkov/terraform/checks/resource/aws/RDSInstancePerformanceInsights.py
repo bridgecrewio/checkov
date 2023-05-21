3814b0a712a6a501f51eaea05089987276bfc637
Anton Grübel <anton.gruebel@gmail.com>
2023-05-21 15:37:21 +0900

    adjust category

--- checkov/terraform/checks/resource/aws/RDSInstancePerformanceInsights.py
+++ checkov/terraform/checks/resource/aws/RDSInstancePerformanceInsights.py
@@ -3,15 +3,14 @@ from checkov.common.models.enums import CheckCategories
 
 
 class RDSInstancePerformanceInsights(BaseResourceValueCheck):
-
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure that RDS instances have performance insights enabled"
         id = "CKV_AWS_353"
-        supported_resources = ['aws_rds_cluster_instance', 'aws_db_instance']
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ('aws_rds_cluster_instance', 'aws_db_instance')
+        categories = (CheckCategories.LOGGING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return 'performance_insights_enabled'
 
 
