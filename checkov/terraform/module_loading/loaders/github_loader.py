41484b53abbc3dcf828b632ce9b82ce1ec66aca6
gruebel <anton.gruebel@gmail.com>
2023-05-22 19:44:04 +0900

    add missing future annotation

--- checkov/terraform/module_loading/loaders/github_loader.py
+++ checkov/terraform/module_loading/loaders/github_loader.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from checkov.terraform.module_loading.loaders.git_loader import GenericGitLoader
--- checkov/terraform/module_loading/loaders/registry_loader.py
+++ checkov/terraform/module_loading/loaders/registry_loader.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import logging
 from http import HTTPStatus
