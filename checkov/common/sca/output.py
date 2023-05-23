eca291f0ae93815a82cae8e7416233b31130b196
nazoulay <noazoulay1@gmail.com>
2023-05-23 17:09:44 +0300

    tests

--- checkov/common/sca/output.py
+++ checkov/common/sca/output.py
@@ -159,7 +159,7 @@ def create_report_cve_record(
         }
 
     code_block = [(0, f"{package_name}: {package_version}")]
-    file_line_range = file_line_range or [0, 0]
+    file_line_range = get_package_lines(package) or file_line_range or [0, 0]
 
     details = {
         "id": cve_id,
@@ -355,8 +355,7 @@ def add_to_reports_dependency_tree_cves(check_class: str | None, packages_map: d
                                      runner_filter=runner_filter, sca_details=sca_details,
                                      scan_data_format=scan_data_format, report_type=report_type, report=report,
                                      root_package=root_package, inline_suppressions=inline_suppressions,
-                                     used_private_registry=used_private_registry,
-                                     file_line_range=get_package_lines(root_package))
+                                     used_private_registry=used_private_registry)
 
         for dep in root_package.get("vulnerable_dependencies", []):
             for dep_cve in dep.get("cves", []):
@@ -373,8 +372,7 @@ def add_to_reports_dependency_tree_cves(check_class: str | None, packages_map: d
                                          scan_data_format=scan_data_format, report_type=report_type, report=report,
                                          root_package=root_package, root_package_fixed_version=root_package_fixed_version,
                                          inline_suppressions=inline_suppressions,
-                                         used_private_registry=used_private_registry,
-                                         file_line_range=get_package_lines(dep))
+                                         used_private_registry=used_private_registry)
 
 
 def add_cve_record_to_report(vulnerability_details: dict[str, Any], package_name: str, package_version: str,
