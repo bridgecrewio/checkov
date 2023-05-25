46236406a2d116eda9e0fc688f2476eef444510a
gruebel <anton.gruebel@gmail.com>
2023-05-25 14:54:49 +0900

    exclude unrestrictable actions in CKV_AWS_355 and CKV_AWS_356

--- checkov/terraform/checks/data/aws/ResourcePolicyDocument.py
+++ checkov/terraform/checks/data/aws/ResourcePolicyDocument.py
@@ -1,38 +1,21 @@
-from typing import Dict, List, Any
+from __future__ import annotations
 
-from checkov.terraform.checks.data.base_check import BaseDataCheck
-from checkov.common.models.enums import CheckResult, CheckCategories
+from typing import Dict, List, Any, TYPE_CHECKING
+
+from checkov.terraform.checks.data.base_cloudsplaining_data_iam_check import BaseTerraformCloudsplainingDataIAMCheck
+
+if TYPE_CHECKING:
+    from cloudsplaining.scan.policy_document import PolicyDocument
 
 
-class ResourcePolicyDocument(BaseDataCheck):
+class ResourcePolicyDocument(BaseTerraformCloudsplainingDataIAMCheck):
     def __init__(self) -> None:
-        name = 'Ensure IAM policies limit resource access'
+        name = 'Ensure no IAM policies documents allow "*" as a statement\'s resource for restrictable actions'
         id = "CKV_AWS_356"
-        supported_data = ["aws_iam_policy_document"]
-        categories = [CheckCategories.IAM]
-        super().__init__(name=name, id=id, categories=categories, supported_data=supported_data)
+        super().__init__(name=name, id=id)
 
-    def scan_data_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        """
-            validates iam policy document
-            https://learn.hashicorp.com/terraform/aws/iam-policy
-        :param conf: aws_kms_key configuration
-        :return: <CheckResult>
-        """
-
-        for statement in conf.get("statement", []):
-            if not isinstance(statement, list):
-                statement = [statement]
-            for stmt in statement:
-                if (
-                    isinstance(stmt, dict)
-                    and stmt.get("effect", ["Allow"]) == ["Allow"]
-                    and stmt.get("resources")
-                    and ["*"] == stmt["resources"][0]
-                ):
-                    return CheckResult.FAILED
-
-        return CheckResult.PASSED
+    def cloudsplaining_analysis(self, policy: PolicyDocument) -> list[str] | list[Dict[str, Any]]:
+        return policy.all_allowed_unrestricted_actions
 
 
 check = ResourcePolicyDocument()
--- tests/terraform/checks/data/aws/example_ResourcePolicyDocument/main.tf
+++ tests/terraform/checks/data/aws/example_ResourcePolicyDocument/main.tf
@@ -72,3 +72,17 @@ data "aws_iam_policy_document" "pass" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "pass_unrestrictable" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
