a4eb4ee7e9155767af277d9f743f1ad6520e89c0
Adam Varsano <avarsano@paloaltonetworks.com>
2023-05-30 21:56:35 +0300

    suppression is not working

--- checkov/common/bridgecrew/platform_integration.py
+++ checkov/common/bridgecrew/platform_integration.py
@@ -98,6 +98,7 @@ class BcPlatformIntegration:
         self.repo_branch: str | None = None
         self.skip_fixes = False
         self.skip_download = False
+        self.sourceId: str | None = None
         self.bc_source: SourceType | None = None
         self.bc_source_version: str | None = None
         self.timestamp: str | None = None
