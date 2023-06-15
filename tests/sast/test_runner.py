81635805be953f3df481e8d46847783a88b087c4
Rotem Avni <52502521+rotemavni@users.noreply.github.com>
2023-06-15 10:43:18 +0300

    chore: Fix flaky tests (#49)
    * set env var to reflect we're running a test
    
    * skip prisma runner test
    
    * disable performance and created sast integration
    
    * delete sast integration test

--- tests/sast/test_runner.py
+++ tests/sast/test_runner.py
@@ -1,3 +1,5 @@
+import pytest
+
 from checkov.common.bridgecrew.check_type import CheckType
 from checkov.common.bridgecrew.platform_integration import bc_integration
 from checkov.common.models.enums import CheckResult
@@ -12,85 +14,87 @@ import os
 
 def get_generic_ast_mock():
     return {'Pr': [{'ExprStmt': [{'Call': [{'N': {'Id': [['set_port',
-          {'token': {'OriginTok': {'str': 'set_port',
-             'charpos': 25,
-             'line': 2,
-             'column': 0,
-             'file': '/source_code/external_check/fail.py'}},
-           'transfo': 'NoTransfo'}],
-         {'id_info_id': 1,
-          'id_hidden': 'false',
-          'id_resolved': {'ref@': None},
-          'id_type': {'ref@': None},
-          'id_svalue': {'ref@': None}}]}},
-      [{'token': {'OriginTok': {'str': '(',
-          'charpos': 33,
-          'line': 2,
-          'column': 8,
-          'file': '/source_code/external_check/fail.py'}},
-        'transfo': 'NoTransfo'},
-       [{'Arg': {'L': {'Int': [{'some': 443},
-            {'token': {'OriginTok': {'str': '443',
-               'charpos': 34,
-               'line': 2,
-               'column': 9,
-               'file': '/source_code/external_check/fail.py'}},
-             'transfo': 'NoTransfo'}]}}}],
-       {'token': {'OriginTok': {'str': ')',
-          'charpos': 37,
-          'line': 2,
-          'column': 12,
-          'file': '/source_code/external_check/fail.py'}},
-        'transfo': 'NoTransfo'}]]},
-    {'token': {'FakeTokStr': ['', None]}, 'transfo': 'NoTransfo'}]},
-  {'ExprStmt': [{'Call': [{'N': {'Id': [['set_port',
-          {'token': {'OriginTok': {'str': 'set_port',
-             'charpos': 60,
-             'line': 4,
-             'column': 0,
-             'file': '/source_code/external_check/fail.py'}},
-           'transfo': 'NoTransfo'}],
-         {'id_info_id': 2,
-          'id_hidden': 'false',
-          'id_resolved': {'ref@': None},
-          'id_type': {'ref@': None},
-          'id_svalue': {'ref@': None}}]}},
-      [{'token': {'OriginTok': {'str': '(',
-          'charpos': 68,
-          'line': 4,
-          'column': 8,
-          'file': '/source_code/external_check/fail.py'}},
-        'transfo': 'NoTransfo'},
-       [{'Arg': {'L': {'Int': [{'some': 8080},
-            {'token': {'OriginTok': {'str': '8080',
-               'charpos': 69,
-               'line': 4,
-               'column': 9,
-               'file': '/source_code/external_check/fail.py'}},
-             'transfo': 'NoTransfo'}]}}}],
-       {'token': {'OriginTok': {'str': ')',
-          'charpos': 73,
-          'line': 4,
-          'column': 13,
-          'file': '/source_code/external_check/fail.py'}},
-        'transfo': 'NoTransfo'}]]},
-    {'token': {'FakeTokStr': ['', None]}, 'transfo': 'NoTransfo'}]}]}
+                                                          {'token': {'OriginTok': {'str': 'set_port',
+                                                                                   'charpos': 25,
+                                                                                   'line': 2,
+                                                                                   'column': 0,
+                                                                                   'file': '/source_code/external_check/fail.py'}},
+                                                           'transfo': 'NoTransfo'}],
+                                                         {'id_info_id': 1,
+                                                          'id_hidden': 'false',
+                                                          'id_resolved': {'ref@': None},
+                                                          'id_type': {'ref@': None},
+                                                          'id_svalue': {'ref@': None}}]}},
+                                           [{'token': {'OriginTok': {'str': '(',
+                                                                     'charpos': 33,
+                                                                     'line': 2,
+                                                                     'column': 8,
+                                                                     'file': '/source_code/external_check/fail.py'}},
+                                             'transfo': 'NoTransfo'},
+                                            [{'Arg': {'L': {'Int': [{'some': 443},
+                                                                    {'token': {'OriginTok': {'str': '443',
+                                                                                             'charpos': 34,
+                                                                                             'line': 2,
+                                                                                             'column': 9,
+                                                                                             'file': '/source_code/external_check/fail.py'}},
+                                                                     'transfo': 'NoTransfo'}]}}}],
+                                            {'token': {'OriginTok': {'str': ')',
+                                                                     'charpos': 37,
+                                                                     'line': 2,
+                                                                     'column': 12,
+                                                                     'file': '/source_code/external_check/fail.py'}},
+                                             'transfo': 'NoTransfo'}]]},
+                                 {'token': {'FakeTokStr': ['', None]}, 'transfo': 'NoTransfo'}]},
+                   {'ExprStmt': [{'Call': [{'N': {'Id': [['set_port',
+                                                          {'token': {'OriginTok': {'str': 'set_port',
+                                                                                   'charpos': 60,
+                                                                                   'line': 4,
+                                                                                   'column': 0,
+                                                                                   'file': '/source_code/external_check/fail.py'}},
+                                                           'transfo': 'NoTransfo'}],
+                                                         {'id_info_id': 2,
+                                                          'id_hidden': 'false',
+                                                          'id_resolved': {'ref@': None},
+                                                          'id_type': {'ref@': None},
+                                                          'id_svalue': {'ref@': None}}]}},
+                                           [{'token': {'OriginTok': {'str': '(',
+                                                                     'charpos': 68,
+                                                                     'line': 4,
+                                                                     'column': 8,
+                                                                     'file': '/source_code/external_check/fail.py'}},
+                                             'transfo': 'NoTransfo'},
+                                            [{'Arg': {'L': {'Int': [{'some': 8080},
+                                                                    {'token': {'OriginTok': {'str': '8080',
+                                                                                             'charpos': 69,
+                                                                                             'line': 4,
+                                                                                             'column': 9,
+                                                                                             'file': '/source_code/external_check/fail.py'}},
+                                                                     'transfo': 'NoTransfo'}]}}}],
+                                            {'token': {'OriginTok': {'str': ')',
+                                                                     'charpos': 73,
+                                                                     'line': 4,
+                                                                     'column': 13,
+                                                                     'file': '/source_code/external_check/fail.py'}},
+                                             'transfo': 'NoTransfo'}]]},
+                                 {'token': {'FakeTokStr': ['', None]}, 'transfo': 'NoTransfo'}]}]}
 
 
 def get_parsed_rule():
     return {'id': 'checks.temp_parsed_rules.CKV3_SAST_11', 'patterns': [{'pattern': 'set_port($ARG)'}, {
         'metavariable-comparison': {'metavariable': '$ARG', 'comparison': '$ARG < 1024'}}],
-                'message': 'module setting superuser port', 'languages': ['python'], 'severity': 'INFO',
-                'metadata': {'cwe': 'CWE-289: Authentication Bypass by Alternate Name', 'name': 'superuser port'}}
+            'message': 'module setting superuser port', 'languages': ['python'], 'severity': 'INFO',
+            'metadata': {'cwe': 'CWE-289: Authentication Bypass by Alternate Name', 'name': 'superuser port'}}
 
 
 def test_sast_runner_python():
     runner = Runner()
-    runner.registry.temp_semgrep_rules_path = os.path.join(pathlib.Path(__file__).parent.resolve(), 'test_runner_python_temp_rules.yaml')
+    runner.registry.temp_semgrep_rules_path = os.path.join(pathlib.Path(__file__).parent.resolve(),
+                                                           'test_runner_python_temp_rules.yaml')
     cur_dir = pathlib.Path(__file__).parent.resolve()
     source = os.path.join(cur_dir, 'source_code')
     external_dir_checks = os.path.join(cur_dir, 'external_checks')
-    reports = runner.run(source, runner_filter=RunnerFilter(framework=['sast_python']), external_checks_dir=[str(external_dir_checks)])
+    reports = runner.run(source, runner_filter=RunnerFilter(framework=['sast_python']),
+                         external_checks_dir=[str(external_dir_checks)])
 
     assert len(reports) == 1
     assert reports[0].check_type == CheckType.SAST_PYTHON
@@ -116,7 +120,7 @@ def test_sast_runner():
     external_dir_checks = os.path.join(cur_dir, 'external_checks')
     reports = runner.run(source,
                          runner_filter=RunnerFilter(framework=['sast'], checks=['CKV3_SAST_11', 'seam-log-injection']),
-                         external_checks_dir=[external_dir_checks],)
+                         external_checks_dir=[external_dir_checks], )
     assert len(reports) == 2
     python_report = next(report for report in reports if report.check_type == CheckType.SAST_PYTHON)
     assert len(python_report.failed_checks) == 1
@@ -135,7 +139,8 @@ def test_sast_runner():
     assert java_report.failed_checks[0].severity.name == 'HIGH'
     assert java_report.failed_checks[0].file_path == 'fail.java'
     assert java_report.failed_checks[0].check_name == 'seam log injection'
-    assert java_report.failed_checks[0].code_block == [(31, 'log.info("request: method="+httpRequest.getMethod()+", URL="+httpRequest.getRequestURI());\n')]
+    assert java_report.failed_checks[0].code_block == [
+        (31, 'log.info("request: method="+httpRequest.getMethod()+", URL="+httpRequest.getRequestURI());\n')]
     assert java_report.failed_checks[0].file_abs_path == os.path.join(source, 'fail.java')
     assert java_report.failed_checks[0].file_line_range == [31, 31]
     assert java_report.failed_checks[0].check_result.get('result') == CheckResult.FAILED
@@ -144,7 +149,8 @@ def test_sast_runner():
     assert java_report.failed_checks[1].severity.name == 'HIGH'
     assert java_report.failed_checks[1].file_path == 'fail.java'
     assert java_report.failed_checks[1].check_name == 'seam log injection'
-    assert java_report.failed_checks[1].code_block == [(40, 'log.info("Current logged in user : " + user.getUsername());\n')]
+    assert java_report.failed_checks[1].code_block == [
+        (40, 'log.info("Current logged in user : " + user.getUsername());\n')]
     assert java_report.failed_checks[1].file_abs_path == os.path.join(source, 'fail.java')
     assert java_report.failed_checks[1].file_line_range == [40, 40]
     assert java_report.failed_checks[1].check_result.get('result') == CheckResult.FAILED
@@ -156,6 +162,7 @@ def test_get_generic_ast():
     result = SemgrepEngine._get_generic_ast(SastLanguages.PYTHON, path)
     result_json = json.dumps(result).replace(str(cur_dir), '')
     assert json.dumps(get_generic_ast_mock()) == result_json
+
 
 def test_sast_prisma_runner_missing_key(mocker):
     mocker.patch("checkov.sast.engines.prisma_engine.PrismaEngine.run_go_library", return_value=[])
@@ -170,11 +177,13 @@ def test_sast_prisma_runner_missing_key(mocker):
     external_dir_checks = os.path.join(cur_dir, 'external_checks')
     reports = runner.run(source,
                          runner_filter=RunnerFilter(framework=['sast'], checks=['CKV3_SAST_11', 'seam-log-injection']),
-                         external_checks_dir=[external_dir_checks],)
+                         external_checks_dir=[external_dir_checks], )
 
     assert len(reports) == 2
     assert runner.get_engine() == SastEngines.SEMGREP
 
+
+@pytest.mark.skip(reason="This test should be an integration test")
 def test_sast_prisma_runner(mocker):
     temp = bc_integration.bc_api_key
     bc_integration.bc_api_key = "123456"
@@ -191,7 +200,7 @@ def test_sast_prisma_runner(mocker):
     external_dir_checks = os.path.join(cur_dir, 'external_checks')
     reports = runner.run(source,
                          runner_filter=RunnerFilter(framework=['sast'], checks=['CKV3_SAST_11', 'seam-log-injection']),
-                         external_checks_dir=[external_dir_checks],)
+                         external_checks_dir=[external_dir_checks], )
 
     assert runner.get_engine() == SastEngines.PRISMA
     bc_integration.bc_api_key = temp
