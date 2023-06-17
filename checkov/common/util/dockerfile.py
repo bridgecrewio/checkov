d12fcf1ac7c0c1e7d06f0cf32c647af246fae2be
WP-LKL <larsklucke@gmail.com>
2023-06-17 21:00:33 +0200

    backwards compatible: deprecate  old variant

--- checkov/common/util/dockerfile.py
+++ checkov/common/util/dockerfile.py
@@ -1,7 +1,13 @@
 import re
+import warnings
 
 DOCKERFILE_MASK = re.compile(r"^(?:.+\.)?[Dd]ockerfile(?:\..+)?$(?<!\.[Dd]ockerignore)")
 
 
 def is_dockerfile(file: str) -> bool:
     return re.fullmatch(DOCKERFILE_MASK, file) is not None
+
+
+def is_docker_file(file: str) -> bool:
+    warnings.warn("Please use is_dockerfile()", DeprecationWarning)
+    return is_dockerfile(file)
