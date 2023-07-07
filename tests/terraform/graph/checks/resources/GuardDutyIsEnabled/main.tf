30ba6462235168400130f7b6238ba0ed28715829
gruebel <anton.gruebel@gmail.com>
2023-07-07 11:48:02 +0200

    support new field in CKV2_AWS_3

--- tests/terraform/graph/checks/resources/GuardDutyIsEnabled/main.tf
+++ tests/terraform/graph/checks/resources/GuardDutyIsEnabled/main.tf
@@ -1,21 +1,43 @@
-resource "aws_guardduty_detector" "ok" {
+# pass
+
+resource "aws_guardduty_detector" "ok_old" {
   enable = true
 }
+
+resource "aws_guardduty_organization_configuration" "ok_old" {
+  auto_enable = true
+  detector_id = aws_guardduty_detector.ok_old.id
+}
+
+resource "aws_guardduty_detector" "all" {
+  enable = true
+}
+
+resource "aws_guardduty_organization_configuration" "all" {
+  auto_enable_organization_members = "ALL"
+  detector_id                      = aws_guardduty_detector.all.id
+}
+
+# fail
 
 resource "aws_guardduty_detector" "not_ok" {
   enable = true
 }
 
-resource "aws_guardduty_organization_configuration" "example" {
-  auto_enable = true
-  detector_id = aws_guardduty_detector.ok.id
-}
-
-resource "aws_guardduty_detector" "not_ok_false" {
+resource "aws_guardduty_detector" "not_ok_false_old" {
   enable = true
 }
 
-resource "aws_guardduty_organization_configuration" "example" {
+resource "aws_guardduty_organization_configuration" "not_ok_false_old" {
   auto_enable = false
-  detector_id = aws_guardduty_detector.not_ok_false.id
+  detector_id = aws_guardduty_detector.not_ok_false_old.id
+}
+
+resource "aws_guardduty_detector" "none" {
+  enable = true
+}
+
+resource "aws_guardduty_organization_configuration" "none" {
+  auto_enable_organization_members = "NONE"
+  detector_id                      = aws_guardduty_detector.none.id
 }
