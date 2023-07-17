3fbe4d4a9c86becfb0395298f8063ab97267e0dd
Barak Fatal <barakf156@gmail.com>
2023-07-17 14:09:49 +0300

    Renamed to indicate it's general for templates

--- checkov/common/template_logger_adapter.py
+++ checkov/common/template_logger_adapter.py
@@ -7,13 +7,13 @@ from typing import Any, MutableMapping
 from checkov.common.util.type_forcers import convert_str_to_bool
 
 
-class RemoveCodeInstancesLoggerAdapter(LoggerAdapter[Logger]):
+class TemplatesLoggerAdapter(LoggerAdapter[Logger]):
     """
-        A custom logger adapter designed to decide if we want to reduce some logs from the default logger.
-        Could be used to reduce logs size.
-        First use case is to log without the actual code of resources, which takes a lot of the logs size.
-        The default is to log everything in order to keep api the same.
-        """
+    A custom logger adapter designed to decide if we want to reduce some logs from the default logger.
+    Could be used to reduce logs size.
+    First use case is to log without the actual code of resources, which takes a lot of the logs size.
+    The default is to log everything in order to keep api the same.
+    """
     CODE_TEMPLATES: list[str] = []
     CODE_MSG_REPLACEMENT = '<resource-code>'
 
@@ -23,12 +23,12 @@ class RemoveCodeInstancesLoggerAdapter(LoggerAdapter[Logger]):
 
     def process(self, msg: Any, kwargs: MutableMapping[str, Any]) -> tuple[Any, MutableMapping[str, Any]]:
         if isinstance(msg, str) and not self._allow_code_logging:
-            for code_template in RemoveCodeInstancesLoggerAdapter.CODE_TEMPLATES:
-                msg = msg.replace(code_template, RemoveCodeInstancesLoggerAdapter.CODE_MSG_REPLACEMENT)
+            for code_template in TemplatesLoggerAdapter.CODE_TEMPLATES:
+                msg = msg.replace(code_template, TemplatesLoggerAdapter.CODE_MSG_REPLACEMENT)
         return msg, kwargs
 
 
-def get_logger_with_code_adapter(logger: Logger, allow_code_logging: bool | None = None) -> RemoveCodeInstancesLoggerAdapter:
+def get_logger_with_template_adapter(logger: Logger, allow_code_logging: bool | None = None) -> TemplatesLoggerAdapter:
     if allow_code_logging is None:
         allow_code_logging = convert_str_to_bool(os.environ.get("CHECKOV_ALLOW_CODE_LOGGING", True))
-    return RemoveCodeInstancesLoggerAdapter(logger, allow_code_logging=allow_code_logging)
+    return TemplatesLoggerAdapter(logger, allow_code_logging=allow_code_logging)
