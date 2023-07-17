fa31bf891cd4713e024fb099de02a99406cbe859
Barak Fatal <barakf156@gmail.com>
2023-07-17 13:26:04 +0300

    replaced logger with logger adapter

--- checkov/common/custom_code_logger.py
+++ checkov/common/custom_code_logger.py
@@ -1,114 +1,34 @@
 from __future__ import annotations
 
 import os
-from logging import Logger
-from typing import Mapping, Any
+from logging import Logger, LoggerAdapter
+from typing import Any, MutableMapping
 
 from checkov.common.util.type_forcers import convert_str_to_bool
 
 
-class CustomCodeLogger(Logger):
+class RemoveCodeInstancesLoggerAdapter(LoggerAdapter):
     """
-    A custom logger designed to decide if we want to reduce some logs from the default logger.
-    Could be used to reduce logs size.
-    First use case is to log without the actual code of resources, which takes a lot of the logs size.
-    The default is to log everything in order to keep api the same.
-    """
+        A custom logger adapter designed to decide if we want to reduce some logs from the default logger.
+        Could be used to reduce logs size.
+        First use case is to log without the actual code of resources, which takes a lot of the logs size.
+        The default is to log everything in order to keep api the same.
+        """
     CODE_TEMPLATES: list[str] = []
     CODE_MSG_REPLACEMENT = '<resource-code>'
 
-    def __init__(self, name: str, allow_code_logging: bool | None = None):
-        super().__init__(name)
+    def __init__(self, logger: Logger, allow_code_logging: bool | None = None):
+        super().__init__(logger)
         if allow_code_logging is None:
             allow_code_logging = convert_str_to_bool(os.environ.get("CHECKOV_ALLOW_CODE_LOGGING", True))
         self._allow_code_logging = allow_code_logging
 
-    def info(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-    ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().info(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def debug(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().debug(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def warning(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().warning(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def warn(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().warn(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def error(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().error(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def exception(
-            self,
-            msg: object,
-            *args: object,
-            exc_info: Any = True,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().exception(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def critical(
-            self,
-            msg: object,
-            *args: object,            
-            exc_info: Any = None,
-            stack_info: bool = False,
-            stacklevel: int = 1,
-            extra: Mapping[str, object] | None = None,
-        ) -> None:
-        msg = self._remove_code_parts_from_msg(msg)
-        super().critical(msg, *args, exc_info, stack_info, stacklevel, extra)
-
-    def _remove_code_parts_from_msg(self, msg: object) -> object:
+    def process(self, msg: Any, kwargs: MutableMapping[str, Any]) -> tuple[Any, MutableMapping[str, Any]]:
         if isinstance(msg, str) and not self._allow_code_logging:
-            for code_template in CustomCodeLogger.CODE_TEMPLATES:
-                msg = msg.replace(code_template, CustomCodeLogger.CODE_MSG_REPLACEMENT)
-        return msg
+            for code_template in RemoveCodeInstancesLoggerAdapter.CODE_TEMPLATES:
+                msg = msg.replace(code_template, RemoveCodeInstancesLoggerAdapter.CODE_MSG_REPLACEMENT)
+        return msg, kwargs
+
+
+def get_logger_with_code_adapter(logger: Logger, allow_code_logging: bool) -> RemoveCodeInstancesLoggerAdapter:
+    return RemoveCodeInstancesLoggerAdapter(logger, allow_code_logging=allow_code_logging)
