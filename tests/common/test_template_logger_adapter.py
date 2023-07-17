6b1436a3614d38a67224b77830ebc01f44e89b3e
Barak Fatal <barakf156@gmail.com>
2023-07-17 14:09:49 +0300

    Renamed to indicate it's general for templates

--- tests/common/test_template_logger_adapter.py
+++ tests/common/test_template_logger_adapter.py
@@ -3,7 +3,7 @@ import logging
 import mock
 import pytest
 
-from checkov.common.custom_code_logger import get_logger_with_code_adapter, RemoveCodeInstancesLoggerAdapter
+from checkov.common.custom_code_logger import get_logger_with_template_adapter, TemplatesLoggerAdapter
 
 
 TEST_CODE_TEMPLATES_TO_REPLACE = "THIS-SHOULD-BE-REPLACED!"
@@ -16,20 +16,20 @@ def logger() -> logging.Logger:
 
 
 def test_logger_adapter_removes_custom_parts_from_msg(caplog, logger: logging.Logger) -> None:
-    with mock.patch("checkov.common.custom_code_logger.RemoveCodeInstancesLoggerAdapter.CODE_TEMPLATES",
+    with mock.patch("checkov.common.template_logger_adapter.TemplatesLoggerAdapter.CODE_TEMPLATES",
                     [TEST_CODE_TEMPLATES_TO_REPLACE]):
-        code_logger_adapter = get_logger_with_code_adapter(logger, allow_code_logging=False)
+        code_logger_adapter = get_logger_with_template_adapter(logger, allow_code_logging=False)
         code_logger_adapter.warning(TEST_CODE_TEMPLATES_TO_REPLACE)
         assert TEST_CODE_TEMPLATES_TO_REPLACE not in caplog.text
-        assert RemoveCodeInstancesLoggerAdapter.CODE_MSG_REPLACEMENT in caplog.text
+        assert TemplatesLoggerAdapter.CODE_MSG_REPLACEMENT in caplog.text
 
 
 def test_logger_adapter_does_not_remove_msg_if_allow_code_logging_enabled(caplog, logger: logging.Logger) -> None:
-    with mock.patch("checkov.common.custom_code_logger.RemoveCodeInstancesLoggerAdapter.CODE_TEMPLATES",
+    with mock.patch("checkov.common.template_logger_adapter.TemplatesLoggerAdapter.CODE_TEMPLATES",
                     [TEST_CODE_TEMPLATES_TO_REPLACE]):
-        code_logger_adapter = get_logger_with_code_adapter(logger)
+        code_logger_adapter = get_logger_with_template_adapter(logger)
         code_logger_adapter.warning(TEST_CODE_TEMPLATES_TO_REPLACE)
         assert TEST_CODE_TEMPLATES_TO_REPLACE in caplog.text
-        assert RemoveCodeInstancesLoggerAdapter.CODE_MSG_REPLACEMENT not in caplog.text
+        assert TemplatesLoggerAdapter.CODE_MSG_REPLACEMENT not in caplog.text
 
 
