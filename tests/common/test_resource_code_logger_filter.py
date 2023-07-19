ad107ace8c3f68ceed248b570235f663384041b0
Barak Fatal <barakf156@gmail.com>
2023-07-19 11:36:04 +0300

    Fixed loggers names

--- tests/common/test_resource_code_logger_filter.py
+++ tests/common/test_resource_code_logger_filter.py
@@ -10,7 +10,7 @@ TEST_CODE_TEMPLATES_TO_REPLACE = "THIS-SHOULD-BE-REPLACED!"
 def test_code_logger_filter_do_not_log_if_not_allowed(caplog) -> None:
     with mock.patch("checkov.common.resource_code_logger_filter.ResourceCodeFilter.CODE_TEMPLATES",
                     [TEST_CODE_TEMPLATES_TO_REPLACE]):
-        logger = logging.getLogger("code logging allowed")
+        logger = logging.getLogger("code logging not allowed")
         add_resource_code_filter_to_logger(logger, allow_code_logging=False)
         logger.warning(TEST_CODE_TEMPLATES_TO_REPLACE)
         assert TEST_CODE_TEMPLATES_TO_REPLACE not in caplog.text
@@ -19,13 +19,13 @@ def test_code_logger_filter_do_not_log_if_not_allowed(caplog) -> None:
 def test_code_logger_filter_logs_if_allowed(caplog) -> None:
     with mock.patch("checkov.common.resource_code_logger_filter.ResourceCodeFilter.CODE_TEMPLATES",
                     [TEST_CODE_TEMPLATES_TO_REPLACE]):
-        logger = logging.getLogger("code logging not allowed")
+        logger = logging.getLogger("code logging allowed")
         add_resource_code_filter_to_logger(logger)
         logger.warning(TEST_CODE_TEMPLATES_TO_REPLACE)
         assert TEST_CODE_TEMPLATES_TO_REPLACE in caplog.text
 
 
-def test_code_logger_filter_logs_based_on_arg(caplog) -> None:
+def test_code_logger_filter_logs_based_on_arg_not_allowed(caplog) -> None:
     with mock.patch("checkov.common.resource_code_logger_filter.ResourceCodeFilter.CODE_TEMPLATES",
                     [TEST_CODE_TEMPLATES_TO_REPLACE]):
         logger = logging.getLogger("code logging not allowed")
