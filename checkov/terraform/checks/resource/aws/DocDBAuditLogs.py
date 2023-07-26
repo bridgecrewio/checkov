55931a554ab35e74e3912e5db763a409f7a2399a
gruebel <anton.gruebel@gmail.com>
2023-07-26 11:19:21 +0200

    fix lint

--- checkov/terraform/checks/resource/aws/DocDBAuditLogs.py
+++ checkov/terraform/checks/resource/aws/DocDBAuditLogs.py
@@ -12,18 +12,18 @@ class DocDBAuditLogs(BaseResourceCheck):
     def __init__(self):
         name = "Ensure DocDB has audit logs enabled"
         id = "CKV_AWS_104"
-        supported_resources = ('aws_docdb_cluster_parameter_group',)
+        supported_resources = ("aws_docdb_cluster_parameter_group",)
         categories = (CheckCategories.LOGGING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
-        self.evaluated_keys = ['parameter']
-        
-        if 'parameter' in conf:
+        self.evaluated_keys = ["parameter"]
+
+        if "parameter" in conf:
             for idx, elem in enumerate(conf["parameter"]):
                 if isinstance(elem, dict) and elem["name"][0] == "audit_logs":
-                    if elem["value"][0] in ACCEPTED_VALUES:
-                        self.evaluated_keys = [f'parameter/[{idx}]/name', f'parameter/[{idx}]/value']
+                    if any(v in elem["value"][0] for v in ACCEPTED_VALUES):
+                        self.evaluated_keys = [f"parameter/[{idx}]/name", f"parameter/[{idx}]/value"]
                         return CheckResult.PASSED
         return CheckResult.FAILED
 
