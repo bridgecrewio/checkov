6df67477f1c0a64fef34f384fa689ae34d5b37f8
ChanochShayner <chsh32340@gmail.com>
2023-08-01 11:59:13 +0300

    fix the ts file

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,9 @@
-import { danger, warn, fail, schedule } from 'danger';
+const { danger, fail } = require('danger');
 
-// Add your Danger rules and checks here
-export default async () => {
+module.exports = async () => {
+  // Add your Danger rules and checks here
   // For example, you can fail the build if the PR title is too short
   if (danger.github?.pr.title.length < 10) {
-    warn('PR title is too short. Please provide a descriptive title.');
+    fail('PR title is too short. Please provide a descriptive title.');
   }
 };
