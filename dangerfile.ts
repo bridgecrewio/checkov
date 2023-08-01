a14e618fc8197627c2813c1d0dd9543d06799a01
ChanochShayner <chsh32340@gmail.com>
2023-08-01 16:39:29 +0300

    throw new Error

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,7 +3,7 @@ const { danger, fail, schedule } = require('danger');
 async function failOnPTTitle() {
   console.log('Running Danger...');
   if (danger.github?.pr.title.length < 1000) {
-    fail('PR title is too short. Please provide a descriptive title.');
+    throw new Error('PR title is too short. Please provide a descriptive title.');
   }
 }
 schedule(failOnPTTitle);
