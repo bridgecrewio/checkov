bfb667aa6cdc47995ecc8fdd77b811d17d91830e
ChanochShayner <chsh32340@gmail.com>
2023-08-01 13:53:11 +0300

    log

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,7 +3,8 @@ const { danger, fail } = require('danger');
 module.exports = async () => {
   // Add your Danger rules and checks here
   // For example, you can fail the build if the PR title is too short
-  if (danger.github?.pr.title.length < 10) {
+  console.log('Running Danger...');
+  if (danger.github?.pr.title.length < 1000) {
     fail('PR title is too short. Please provide a descriptive title.');
   }
 };
