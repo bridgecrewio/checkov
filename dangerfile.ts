399f5a6d12b8241b9a6146ac106dea6e7db3355e
ChanochShayner <chsh32340@gmail.com>
2023-08-01 16:21:30 +0300

    trying ts func

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,9 @@
-const { danger, fail } = require('danger');
+const { danger, fail, schedule } = require('danger');
 
-module.exports = async () => {
-  // Add your Danger rules and checks here
-  // For example, you can fail the build if the PR title is too short
+async function failOnPTTitle() {
   console.log('Running Danger...');
   if (danger.github?.pr.title.length < 1000) {
     fail('PR title is too short. Please provide a descriptive title.');
   }
-};
+}
+schedule(failOnPTTitle);
