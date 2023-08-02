3d33fb81c77c0beb60bee5c085895a22db2e9b1d
ChanochShayner <chsh32340@gmail.com>
2023-08-02 09:34:57 +0300

    Some fixes

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,6 @@
 const { danger, fail, schedule } = require('danger');
 
-async function failOnPTTitle() {
+async function runDanger() {
   console.log('Running Danger...');
-  if (danger.github?.pr.title.length < 1000) {
-    fail('PR title is too short. Please provide a descriptive title.');
-  }
 }
-schedule(failOnPTTitle);
+schedule(runDanger);
