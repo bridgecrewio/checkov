258bdb5d418cbc2c1989802b119986cb947949df
gruebel <anton.gruebel@gmail.com>
2023-08-13 19:13:20 +0200

    upper bound spdx to 0.9.0

--- setup.py
+++ setup.py
@@ -109,8 +109,8 @@ setup(
         "requests>=2.27.0",
         "yarl",
         "openai",
-        "spdx-tools>=0.8.0",
-        "license-expression==30.1.0",
+        "spdx-tools>=0.8.0,<0.9.0",
+        "license-expression",
     ],
     dependency_links=[],  # keep it empty, needed for pipenv-setup
     license="Apache License 2.0",
