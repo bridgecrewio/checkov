1832d616e20f17571fd451cda55adacfdf910afb
James Woolfenden <james.woolfenden@gmail.com>
2023-08-17 15:56:38 +0100

    uncomment resource

--- tests/terraform/checks/resource/azure/example_AzureManagedDiskEncryptionSet/main.tf
+++ tests/terraform/checks/resource/azure/example_AzureManagedDiskEncryptionSet/main.tf
@@ -11,20 +11,20 @@ resource "azurerm_managed_disk" "fail" {
     environment = "staging"
   }
 }
-#
-#resource "azurerm_managed_disk" "pass" {
-#  name                   = "acctestmd1"
-#  location               = "West US 2"
-#  resource_group_name    = azurerm_resource_group.example.name
-#  storage_account_type   = "Standard_LRS"
-#  create_option          = "Empty"
-#  disk_size_gb           = "1"
-#  disk_encryption_set_id = "koko"
-#  tags = {
-#    environment = "staging"
-#  }
-#}
-#
+
+resource "azurerm_managed_disk" "pass" {
+  name                   = "acctestmd1"
+  location               = "West US 2"
+  resource_group_name    = azurerm_resource_group.example.name
+  storage_account_type   = "Standard_LRS"
+  create_option          = "Empty"
+  disk_size_gb           = "1"
+  disk_encryption_set_id = "koko"
+  tags = {
+    environment = "staging"
+  }
+}
+
 
 resource "azurerm_resource_group" "example" {
   location = "West US 2"
