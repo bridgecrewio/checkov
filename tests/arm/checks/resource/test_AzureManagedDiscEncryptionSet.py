c1a7dee7c5b0579711986f98d528f76121ea9d1a
James Woolfenden <james.woolfenden@gmail.com>
2023-08-17 15:33:11 +0100

    translate tf chgeck to arm

--- tests/arm/checks/resource/test_AzureManagedDiscEncryptionSet.py
+++ tests/arm/checks/resource/test_AzureManagedDiscEncryptionSet.py
@@ -1,13 +1,13 @@
 import unittest
 from pathlib import Path
 
+from checkov.arm.checks.resource.AzureManagedDiskEncryptionSet import check
+from checkov.arm.runner import Runner
 from checkov.runner_filter import RunnerFilter
-from checkov.terraform.runner import Runner
-from checkov.terraform.checks.resource.azure.AzureManagedDiskEncryptionSet import check
 
 
 class TestAzureManagedDiskEncryptionSet(unittest.TestCase):
-    def test(self):
+    def test_summary(self):
         # given
         test_files_dir = Path(__file__).parent / "example_AzureManagedDiskEncryptionSet"
 
@@ -18,25 +18,24 @@ class TestAzureManagedDiskEncryptionSet(unittest.TestCase):
         summary = report.get_summary()
 
         passing_resources = {
-            "azurerm_managed_disk.pass",
+            "Microsoft.Compute/disks.pike",
         }
+
         failing_resources = {
-            "azurerm_managed_disk.fail",
+            "Microsoft.Compute/disks.failed",
         }
-        skipped_resources = {}
 
         passed_check_resources = {c.resource for c in report.passed_checks}
         failed_check_resources = {c.resource for c in report.failed_checks}
 
         self.assertEqual(summary["passed"], len(passing_resources))
         self.assertEqual(summary["failed"], len(failing_resources))
-        self.assertEqual(summary["skipped"], len(skipped_resources))
+        self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
-        self.assertEqual(passing_resources, passed_check_resources)
-        self.assertEqual(failing_resources, failed_check_resources)
+        self.assertSetEqual(passing_resources, passed_check_resources)
+        self.assertSetEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == "__main__":
     unittest.main()
-
