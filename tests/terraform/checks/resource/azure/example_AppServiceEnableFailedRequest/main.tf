2ecf805545486f0f40ce5e1b06908200ea6891c7
James Woolfenden <james.woolfenden@gmail.com>
2023-08-18 14:51:35 +0100

    revert

--- tests/terraform/checks/resource/azure/example_AppServiceEnableFailedRequest/main.tf
+++ tests/terraform/checks/resource/azure/example_AppServiceEnableFailedRequest/main.tf
@@ -9,7 +9,23 @@ resource "azurerm_app_service" "fail3" {
   }
 }
 
-az
+resource "azurerm_app_service" "pass" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  https_only          = true
+  logs {
+    failed_request_tracing_enabled = true
+  }
+  storage_account {
+    name         = "test_name"
+    type         = "AzureFiles"
+    account_name = "test_account_name"
+    share_name   = "test_share_name"
+    access_key   = "test_access_key"
+  }
+}
 
 resource "azurerm_app_service" "fail2" {
   name                = "example-app-service"
