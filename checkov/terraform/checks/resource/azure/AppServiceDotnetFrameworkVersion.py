14a71d1799eb6091b12eabbc4b4079e3346698b0
James Woolfenden <james.woolfenden@gmail.com>
2023-08-22 17:24:25 +0100

    Update AppServiceDotnetFrameworkVersion.py

--- checkov/terraform/checks/resource/azure/AppServiceDotnetFrameworkVersion.py
+++ checkov/terraform/checks/resource/azure/AppServiceDotnetFrameworkVersion.py
@@ -1,16 +1,17 @@
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from typing import List
 
 
 class AppServiceDotnetFrameworkVersion(BaseResourceCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure that 'Net Framework' version is the latest, if used as a part of the web app"
         id = "CKV_AZURE_80"
         supported_resources = ['azurerm_app_service', 'azurerm_windows_web_app']
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
+    def scan_resource_conf(self, conf) -> CheckResult:
         if conf.get('site_config') and isinstance(conf.get('site_config'), list):
             site_config = conf.get('site_config')[0]
             if site_config.get('dotnet_framework_version') and isinstance(site_config.get('dotnet_framework_version'), list):
@@ -26,10 +27,8 @@ class AppServiceDotnetFrameworkVersion(BaseResourceCheck):
 
         return CheckResult.UNKNOWN
 
-    # def get_inspected_key(self):
-    #     return "site_config/0/dotnet_framework_version"
 
-    def get_expected_values(self):
+    def get_expected_values(self) -> List[str]:
         return ["v6.0", "v7.0"]
 
 
