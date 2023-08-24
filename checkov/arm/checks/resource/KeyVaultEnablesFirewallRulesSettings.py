b5cb6b8d86abdafaa06972382334683b88aa2ad8
James Woolfenden <james.woolfenden@gmail.com>
2023-08-24 12:59:18 +0100

    feat(arm): Implement CKV_AZURE_109 in arm

---
+++ checkov/arm/checks/resource/KeyVaultEnablesFirewallRulesSettings.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
+
+
+class KeyVaultEnablesFirewallRulesSettings(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure that key vault allows firewall rules settings"
+        id = "CKV_AZURE_109"
+        supported_resources = ['Microsoft.KeyVault/vaults']
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "properties/networkAcls/defaultAction"
+
+    def get_expected_value(self):
+        return "Deny"
+
+
+check = KeyVaultEnablesFirewallRulesSettings()
