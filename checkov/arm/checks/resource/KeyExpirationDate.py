685987c24b132d9319e55920f992550026d09386
James Woolfenden <james.woolfenden@gmail.com>
2023-08-29 14:49:42 +0100

    feat(arm): implement CKV_AZURE_40 for ARM

--- checkov/arm/checks/resource/KeyExpirationDate.py
+++ checkov/arm/checks/resource/KeyExpirationDate.py
@@ -4,17 +4,17 @@ from checkov.common.models.consts import ANY_VALUE
 
 
 class KeyExpirationDate(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure that the expiration date is set on all keys"
         id = "CKV_AZURE_40"
         supported_resources = ['Microsoft.KeyVault/vaults/keys']
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return 'properties/rotationPolicy/attributes/expiryTime'
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> str:
         return ANY_VALUE
 
 
