8fa70f9a3a2d8732acfda58283226e8691c0669d
James Woolfenden <james.woolfenden@gmail.com>
2023-08-29 15:44:13 +0100

    feat(arm):  implement CKV_AZURE_95 for ARM

---
+++ checkov/arm/checks/resource/VMScaleSetsAutoOSImagePatchingEnabled.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
+
+
+class VMScaleSetsAutoOSImagePatchingEnabled(BaseResourceValueCheck):
+    def __init__(self) -> None:
+        name = "Ensure that automatic OS image patching is enabled for Virtual Machine Scale Sets"
+        id = "CKV_AZURE_95"
+        supported_resources = ['Microsoft.Compute/virtualMachineScaleSets']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_expected_value(self) -> bool:
+        return True
+
+    def get_inspected_key(self) -> str:
+        return "properties/extensionsProfile/extensions/properties/enableAutomaticUpgrade"
+
+
+check = VMScaleSetsAutoOSImagePatchingEnabled()
