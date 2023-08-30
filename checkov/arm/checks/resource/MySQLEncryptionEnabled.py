c8bae7811a0077a504402a7f586272fe440606e6
James Woolfenden <james.woolfenden@gmail.com>
2023-08-30 11:27:39 +0100

    feat(arm): implement CKV_AZURE_96 for ARM

---
+++ checkov/arm/checks/resource/MySQLEncryptionEnabled.py
@@ -0,0 +1,33 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_check import BaseResourceCheck
+from typing import Any
+
+
+class MySQLEncryptionEnabled(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure that MySQL server enables infrastructure encryption"
+        id = "CKV_AZURE_96"
+        supported_resources = ['Microsoft.DBforMySQL/flexibleServers']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
+        if conf.get("properties") and isinstance(conf.get("properties"), dict):
+            properties = conf.get("properties")
+            self.evaluated_keys = ['properties']
+
+            if properties.get('dataencryption') and isinstance(properties.get('dataencryption'), dict):
+                dataencryption = properties.get('dataencryption')
+                self.evaluated_keys = ['properties/dataencryption']
+                if dataencryption is None:
+                    return CheckResult.FAILED
+                if dataencryption.get('type') == "AzureKeyVault" and isinstance(dataencryption.get('type'), str):
+                    return CheckResult.PASSED
+            # unparsed
+            if isinstance(properties.get('dataencryption'), str):
+                return CheckResult.UNKNOWN
+            return CheckResult.FAILED
+        return CheckResult.UNKNOWN
+
+
+check = MySQLEncryptionEnabled()
