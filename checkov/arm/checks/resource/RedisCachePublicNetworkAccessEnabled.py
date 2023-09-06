c742dbefaf5d62baf059fbb908d05673067f115b
Anton Grübel <anton.gruebel@gmail.com>
2023-09-06 09:19:10 +0200

    changed category

--- checkov/arm/checks/resource/RedisCachePublicNetworkAccessEnabled.py
+++ checkov/arm/checks/resource/RedisCachePublicNetworkAccessEnabled.py
@@ -3,11 +3,11 @@ from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
 
 class RedisCachePublicNetworkAccessEnabled(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure that Azure Cache for Redis disables public network access"
         id = "CKV_AZURE_89"
-        supported_resources = ['Microsoft.Cache/redis']
-        categories = [CheckCategories.IAM]
+        supported_resources = ('Microsoft.Cache/redis',)
+        categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
