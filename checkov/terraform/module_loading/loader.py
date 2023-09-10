fc66b84beb04f3abd56a37d31234c41c6c5ca544
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2023-09-10 19:04:56 +0300

    fix yet another mypy issue

--- checkov/terraform/module_loading/loader.py
+++ checkov/terraform/module_loading/loader.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import logging
 import os
 from abc import ABC, abstractmethod
@@ -30,7 +28,7 @@ class ModuleLoader(ABC):
         self.inner_module: Optional[str] = None
         self.root_dir = ""  # root dir for storing external modules
 
-    def __eq__(self, loader: ModuleLoader) -> bool:
+    def __eq__(self, loader: object) -> bool:
         return type(loader) is type(self)
 
     @abstractmethod
