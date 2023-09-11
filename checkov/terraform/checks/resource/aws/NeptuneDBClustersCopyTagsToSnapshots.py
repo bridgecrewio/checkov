99e6fae55a09ba5e0a4e6de6ca0673fb1acfe86d
Omry Mendelovich <omry155@gmail.com>
2023-09-11 09:41:52 +0300

    fix

--- checkov/terraform/checks/resource/aws/NeptuneDBClustersCopyTagsToSnapshots.py
+++ checkov/terraform/checks/resource/aws/NeptuneDBClustersCopyTagsToSnapshots.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 class NeptuneDBClustersCopyTagsToSnapshots(BaseResourceValueCheck):
     def __init__(self):
         description = "Neptune DB clusters should be configured to copy tags to snapshots"
-        id = "CKV_AWS_361"
+        id = "CKV_AWS_362"
         supported_resources = ['aws_neptune_cluster']
         categories = [CheckCategories.BACKUP_AND_RECOVERY]
         super().__init__(name=description, id=id, categories=categories, supported_resources=supported_resources)
