ef1433e38944de520b5e183239ee12c95405a978
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2023-09-13 14:01:55 +0300

    feat(general): add rustworkx (#5511)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * add _get_operation for rustworkx
    
    * add _get_operation for rustworkx
    
    * add _get_operation for rustworkx
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * fix typing
    
    * fix tests
    
    * fix linting
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: gruebel <anton.gruebel@gmail.com>

--- tests/ansible/checks/graph_checks/test_yaml_policies.py
+++ tests/ansible/checks/graph_checks/test_yaml_policies.py
@@ -6,6 +6,8 @@ from parameterized import parameterized_class
 
 from checkov.common.graph.db_connectors.networkx.networkx_db_connector import NetworkxConnector
 from checkov.common.graph.db_connectors.igraph.igraph_db_connector import IgraphConnector
+from checkov.common.graph.db_connectors.rustworkx.rustworkx_db_connector import RustworkxConnector
+from checkov.common.graph.db_connectors.utils import set_db_connector_by_graph_framework
 
 from checkov.common.graph.graph_builder import CustomAttributes
 from checkov.common.models.enums import CheckResult
@@ -19,15 +21,12 @@ from tests.common.graph.checks.test_yaml_policies_base import TestYamlPoliciesBa
 
 @parameterized_class([
    {"graph_framework": "NETWORKX"},
-   {"graph_framework": "IGRAPH"}
+   {"graph_framework": "IGRAPH"},
+   {"graph_framework": "RUSTWORKX"}
 ])
 class TestYamlPolicies(TestYamlPoliciesBase):
     def __init__(self, args):
-        db_connector = None
-        if self.graph_framework == 'NETWORKX':
-            db_connector = NetworkxConnector()
-        elif self.graph_framework == 'IGRAPH':
-            db_connector = IgraphConnector()
+        db_connector = set_db_connector_by_graph_framework(self.graph_framework)
         graph_manager = ObjectGraphManager(db_connector=db_connector, source="Ansible")
         super().__init__(
             graph_manager=graph_manager,
