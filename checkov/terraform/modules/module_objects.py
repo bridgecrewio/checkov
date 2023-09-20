fe6f84778b5551a53ea3480e34411377695c28e1
Barak Fatal <barakf156@gmail.com>
2023-09-20 13:34:56 +0300

    flake8

--- checkov/terraform/modules/module_objects.py
+++ checkov/terraform/modules/module_objects.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import json
 from collections.abc import Iterator
 from dataclasses import dataclass
-from typing import Optional, Any
+from typing import Any
 
 
 @dataclass(frozen=True)
