e0033c5c0786d8f54aaab54332eac7d683f0e445
Barak Fatal <35402131+bo156@users.noreply.github.com>
2023-09-26 13:35:59 +0300

    Revert "feat(general): add rustworkx (#5511) (#5565)"
    This reverts commit 286d1f3cf3f3e8004fa88a257c396a6e7555ddce.

--- tests/bicep/graph/checks/test_yaml_policies.py
+++ tests/bicep/graph/checks/test_yaml_policies.py
@@ -6,8 +6,7 @@ from checkov.bicep.graph_manager import BicepGraphManager
 from checkov.common.graph.db_connectors.networkx.networkx_db_connector import NetworkxConnector
 from parameterized import parameterized_class
 from checkov.common.graph.db_connectors.igraph.igraph_db_connector import IgraphConnector
-from checkov.common.graph.db_connectors.rustworkx.rustworkx_db_connector import RustworkxConnector
-from checkov.common.graph.db_connectors.utils import set_db_connector_by_graph_framework
+
 
 from checkov.common.graph.graph_builder import CustomAttributes
 from checkov.common.models.enums import CheckResult
@@ -18,12 +17,15 @@ from tests.common.graph.checks.test_yaml_policies_base import TestYamlPoliciesBa
 
 @parameterized_class([
    {"graph_framework": "NETWORKX"},
-   {"graph_framework": "IGRAPH"},
-   {"graph_framework": "RUSTWORKX"}
+   {"graph_framework": "IGRAPH"}
 ])
 class TestYamlPolicies(TestYamlPoliciesBase):
     def __init__(self, args):
-        db_connector = set_db_connector_by_graph_framework(self.graph_framework)
+        db_connector = None
+        if self.graph_framework == 'NETWORKX':
+            db_connector = NetworkxConnector()
+        elif self.graph_framework == 'IGRAPH':
+            db_connector = IgraphConnector()
         graph_manager = BicepGraphManager(db_connector=db_connector)
         super().__init__(
             graph_manager=graph_manager,
