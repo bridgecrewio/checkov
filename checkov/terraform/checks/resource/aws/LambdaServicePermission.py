04feca22f83ab9c7141931a23f078bff83a90e58
Andrew Loughran <andrew.loughran@digital.cabinet-office.gov.uk>
2023-09-30 13:08:56 +0100

    Applying self.get_evaluated_keys to terraform

--- checkov/terraform/checks/resource/aws/LambdaServicePermission.py
+++ checkov/terraform/checks/resource/aws/LambdaServicePermission.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
@@ -5,7 +7,7 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 class LambdaServicePermission(BaseResourceCheck):
     def __init__(self):
         # This is the full description of your check
-        name = "Ensure that AWS Lambda function permissions delegated to AWS services are limited by SourceArn or SourceAccount"
+        description = "Ensure that AWS Lambda function permissions delegated to AWS services are limited by SourceArn or SourceAccount"
 
         # This is the Unique ID for your check
         id = "CKV_AWS_293"
@@ -20,19 +22,22 @@ class LambdaServicePermission(BaseResourceCheck):
     def scan_resource_conf(self, conf):
         # Replace this with the custom logic for your check
         principal = conf.get("principal")
-        if principal and isintsance(principal, list) and isinstance(principal[0], str):
+        if principal and isinstance(principal, list) and isinstance(principal[0], str):
             principal_parts = principal[0].split('.')
         try:
             if principal_parts[1] == 'amazonaws' and principal_parts[2] == 'com':  # This confirms that the principal is set as a service principal.
                 if 'source_arn' in conf or 'source_account' in conf:  # If either of these are set, we're good and the check should pass.
-                    self.evaluated_keys = ["principal", "source_account", "source_arn"]
+                    self.evaluated_keys = self.get_evaluated_keys()
                     return CheckResult.PASSED
                 else:
-                    self.evaluated_keys = ["principal", "source_account", "source_arn"]
+                    self.evaluated_keys = self.get_evaluated_keys()
                     return CheckResult.FAILED
         except IndexError:
             return CheckResult.UNKNOWN
         return CheckResult.UNKNOWN
+
+    def get_evaluated_keys(self) -> List[str]:
+        return ["principal", "source_arn", "source_account"]
 
 
 check = LambdaServicePermission()
