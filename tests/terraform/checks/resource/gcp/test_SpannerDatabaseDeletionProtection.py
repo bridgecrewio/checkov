0ecff620ecc7ad19697d4b51532927783b27fbd2
James Woolfenden <james.woolfenden@gmail.com>
2023-10-06 14:16:51 +0100

    feat(terraform): drop and deletion checks for spanner

---
+++ tests/terraform/checks/resource/gcp/test_SpannerDatabaseDeletionProtection.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+
+from checkov.terraform.checks.resource.gcp.SpannerDatabaseDeletionProtection import check
+from checkov.runner_filter import RunnerFilter
+from checkov.terraform.runner import Runner
+
+
+class TestSpannerDatabaseDeletionProtection(unittest.TestCase):
+
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_SpannerDatabaseDeletionProtection"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            'google_spanner_database.pass',
+            'google_spanner_database.pass2',
+        }
+        failing_resources = {
+            'google_spanner_database.fail',
+        }
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
