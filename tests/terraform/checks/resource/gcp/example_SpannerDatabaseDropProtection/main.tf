10ca310936701cad6ee6be342df4ff7a60922cbf
James Woolfenden <james.woolfenden@gmail.com>
2023-10-06 14:16:51 +0100

    feat(terraform): drop and deletion checks for spanner

---
+++ tests/terraform/checks/resource/gcp/example_SpannerDatabaseDropProtection/main.tf
@@ -0,0 +1,40 @@
+resource "google_spanner_database" "fail" {
+  instance = google_spanner_instance.example.name
+  name     = "my-database"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
+  enable_drop_protection=false
+  #   encryption_config {
+  #     kms_key_name=
+  #   }
+}
+
+resource "google_spanner_database" "fail2" {
+  instance = google_spanner_instance.example.name
+  name     = "my-database"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
+  #   encryption_config {
+  #     kms_key_name=
+  #   }
+}
+
+resource "google_spanner_database" "pass" {
+  instance = google_spanner_instance.example.name
+  name     = "my-database"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
+  enable_drop_protection=true
+     encryption_config {
+       kms_key_name= google_kms_crypto_key.example.name
+     }
+}
