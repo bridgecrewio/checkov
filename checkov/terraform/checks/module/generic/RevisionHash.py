628822373fcab817a8f7da16fce6460b7f43d17d
zameer712 <mrz013@lns.maersk.com>
2023-10-16 18:13:43 +0530

    fix(terraform): Fixing issues with checks CKV_AZURE_226 & CKV_AZURE_227 (#5638) * Fixing CKV_AZURE_226
    * Fixed issue with CKV_AZURE_227
    
    * fix linting and test
    
    ---------
    
    Co-authored-by: Thomas Defise <thde@bankingcircle.com>
    Co-authored-by: gruebel <anton.gruebel@gmail.com>
    
    **By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.**
    
    [//]: # "
        # PR Title
        Be aware that we use the title to create changelog automatically and therefore only allow specific prefixes
        - break:    to indicate a breaking change, this supersedes any of the types
        - feat:     to indicate new features or checks
        - fix:      to indicate a bugfix or handling of edge cases of existing checks
        - docs:     to indicate an update to our documentation
        - chore:    to indicate adjustments to workflow files or dependency updates
        - platform: to indicate a change needed for the platform
        Additionally a scope is needs to be added to the prefix, which indicates the targeted framework, in doubt choose 'general'.
        #
        Allowed prefixs:
        ansible|argo|arm|azure|bicep|bitbucket|circleci|cloudformation|dockerfile|github|gha|gitlab|helm|kubernetes|kustomize|openapi|sca|secrets|serverless|terraform|general|graph|terraform_plan|terraform_json
        #
        ex.
        feat(terraform): add CKV_AWS_123 to ensure that VPC Endpoint Service is configured for Manual Acceptance
    "
    
    ## Description
    
    *Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change.*
    
    Fixes # (issue)
    
    ## New/Edited policies (Delete if not relevant)
    
    ### Description
    *Include a description of what makes it a violation and any relevant external links.*
    
    ### Fix
    *How does someone fix the issue in code and/or in runtime?*
    
    ## Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests that prove my feature, policy, or fix is effective and works
    - [ ] New and existing tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream modules

--- checkov/terraform/checks/module/generic/RevisionHash.py
+++ checkov/terraform/checks/module/generic/RevisionHash.py
@@ -7,11 +7,11 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.module.base_module_check import BaseModuleCheck
 
 COMMIT_ID_PATTERN = re.compile(r"\?(ref=)(?P<commit_id>([0-9a-f]{5,40}))")
-
+TAG_PATTERN = re.compile(r"\?(ref=)(?P<tag>[^/]+)")
 
 class RevisionHash(BaseModuleCheck):
     def __init__(self) -> None:
-        name = "Ensure Terraform module sources use a commit hash"
+        name = "Ensure Terraform module sources use a commit hash or Git tag"
         id = "CKV_TF_1"
         categories = [CheckCategories.SUPPLY_CHAIN]
         super().__init__(name=name, id=id, categories=categories)
@@ -21,13 +21,13 @@ class RevisionHash(BaseModuleCheck):
         if source and isinstance(source, list):
             source_url = source[0]
             if source_url.startswith(("./", "../")):
-                # local modules can't be pinned to a commit hash
+                # local modules can't be pinned to a commit hash or tag
                 return CheckResult.UNKNOWN
-            if "?ref" in source_url and re.search(COMMIT_ID_PATTERN, source_url):
-                # do first a quick lookup, if '?ref' exists in the string before actually searching for the commit hash
-                return CheckResult.PASSED
+            if "?ref" in source_url:
+                if re.search(COMMIT_ID_PATTERN, source_url) or re.search(TAG_PATTERN, source_url):
+                    # do first a quick lookup, if '?ref' exists in the string before actually searching for the commit hash or tag
+                    return CheckResult.PASSED
 
         return CheckResult.FAILED
-
 
 check = RevisionHash()
