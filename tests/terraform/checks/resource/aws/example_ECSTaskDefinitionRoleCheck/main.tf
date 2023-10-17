e46f155290d47f9c95ac8d9818e1ed0368d960ac
ChanochShayner <chsh32340@gmail.com>
2023-10-17 15:28:40 +0300

    Support merge inside jsondecode

--- tests/terraform/checks/resource/aws/example_ECSTaskDefinitionRoleCheck/main.tf
+++ tests/terraform/checks/resource/aws/example_ECSTaskDefinitionRoleCheck/main.tf
@@ -39,3 +39,30 @@ resource "aws_ecs_task_definition" "fail" {
   execution_role_arn = "aws_iam_role.example.arn"
   task_role_arn = "aws_iam_role.example.arn"
 }
+
+resource "aws_ecs_task_definition" "service01" {
+  family = "service"
+  container_definitions = jsonencode([
+    merge(
+      {
+        name  = "first"
+        image = "service-first"
+      },
+      {
+        cpu       = 10
+        memory    = 512
+        essential = true
+        portMappings = [
+          {
+            containerPort = 80
+            hostPort      = 80
+          }
+        ]
+      }
+    )
+  ])
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+}
