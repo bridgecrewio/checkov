61e2de34dbc117b253aaf50f664d17cb54dc4a2f
gruebel <anton.gruebel@gmail.com>
2023-10-18 15:36:34 +0200

    adjust logic

--- checkov/arm/checks/resource/DataFactoryUsesGitRepository.py
+++ checkov/arm/checks/resource/DataFactoryUsesGitRepository.py
@@ -1,4 +1,6 @@
-from typing import Dict, List, Any
+from __future__ import annotations
+
+from typing import Any
 
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.arm.base_resource_check import BaseResourceCheck
@@ -8,21 +10,20 @@ class DataFactoryUsesGitRepository(BaseResourceCheck):
     def __init__(self) -> None:
         name = "Ensure that Azure Data Factory uses Git repository for source control"
         id = "CKV_AZURE_103"
-        supported_resources = ["Microsoft.DataFactory/factories"]
-        categories = [CheckCategories.GENERAL_SECURITY]
+        supported_resources = ("Microsoft.DataFactory/factories",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        if conf.get("properties") and isinstance(conf.get("properties"), dict):
-            properties = conf.get("properties")
-            if properties.get("repoConfiguration") and not isinstance(properties.get("repoConfiguration"), str):
-                repo = properties.get("repoConfiguration")
-                if repo.get("type") is not None:
-                    return CheckResult.PASSED
-            if properties.get("repoConfiguration") is None or properties.get("repoConfiguration") == "":
+    def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
+        properties = conf.get("properties")
+        if properties and isinstance(properties, dict):
+            self.evaluated_keys = ["properties/repoConfiguration/type"]
+            repo = properties.get("repoConfiguration")
+            if not repo:
                 return CheckResult.FAILED
+            if repo and isinstance(repo, dict) and repo.get("type") is not None:
+                return CheckResult.PASSED
             return CheckResult.UNKNOWN
-        self.evaluated_keys = ['properties']
         return CheckResult.FAILED
 
 
