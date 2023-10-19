a6996724bbb6a3349373dff259bbcd5c15759fb8
Thomas Defise <thde@bankingcircle.com>
2023-10-19 11:34:52 +0200

    Fixed issues with the unit test

--- tests/terraform/checks/resource/azure/test_AppServiceEnvironmentZoneRedundant.py
+++ tests/terraform/checks/resource/azure/test_AppServiceEnvironmentZoneRedundant.py
@@ -24,15 +24,15 @@ class TestAppServiceEnvironmentZoneRedundant(unittest.TestCase):
             "azurerm_app_service_environment_v3.fail1",
             "azurerm_app_service_environment_v3.fail2",
         }
+        skipped_resources = {}
 
-        passed_check_resources = {c.resource for c in report.passed_checks}
-        failed_check_resources = {c.resource for c in report.failed_checks}
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
 
-        self.assertEqual(summary["passed"], 2)
-        self.assertEqual(summary["failed"], 1)
-        self.assertEqual(summary["skipped"], 0)
-        self.assertEqual(summary["parsing_errors"], 0)
-        self.assertEqual(summary["resource_count"], 6)  # 3 unknown
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], len(skipped_resources))
+        self.assertEqual(summary['parsing_errors'], 0)
 
         self.assertEqual(passing_resources, passed_check_resources)
         self.assertEqual(failing_resources, failed_check_resources)
