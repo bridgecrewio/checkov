45d5d8baf966dec1623170e0b44472f48063aeac
Praveen <prdeshpande@paloaltonetworks.com>
2023-11-02 13:30:31 +0530

    Optimised CKV2_AZURE_41

--- tests/terraform/graph/checks/resources/AzureStorageAccConfig_SAS_expirePolicy/main.tf
+++ tests/terraform/graph/checks/resources/AzureStorageAccConfig_SAS_expirePolicy/main.tf
@@ -6,9 +6,9 @@ variable "location" {
   default = "northeurope"
 }
 
-# Case 1: Pass: shared_access_key_enabled = false AND sas_policy exists
+# Case 1: Pass: shared_access_key_enabled = false doesn't matter if sas_policy exists or not
 
-resource "azurerm_storage_account" "pass" {
+resource "azurerm_storage_account" "pass_1" {
   name                     = "pud-storage2023abc1"
   resource_group_name      = var.rg-name
   location                 = var.location
@@ -26,7 +26,39 @@ resource "azurerm_storage_account" "pass" {
   }
 }
 
-# Case 2: Fail: None of the arguments exist
+# Case 2: Pass: shared_access_key_enabled is False and it's okay if sas_policy.expiration_period is empty
+
+resource "azurerm_storage_account" "pass_2" {
+  name                     = "pud-storage2023abc4"
+  resource_group_name      = var.rg-name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  shared_access_key_enabled = false
+
+  sas_policy {
+    expiration_period = ""
+  }
+
+}
+
+# Case 3: Pass: shared_access_key_enabled is True but expiration_period is configured.
+
+resource "azurerm_storage_account" "pass_3" {
+  name                     = "pud-storage2023abc4"
+  resource_group_name      = var.rg-name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  shared_access_key_enabled = true
+
+  sas_policy {
+    expiration_period = "90.00:00:00"
+  }
+
+}
+
+# Case 4: Fail: None of the arguments exist, so by default "sas_policy.expiration_period" is true
 
 resource "azurerm_storage_account" "fail_1" {
   name                     = "pud-storage2023abc2"
@@ -36,7 +68,7 @@ resource "azurerm_storage_account" "fail_1" {
   account_replication_type = "GRS"
 }
 
-# Case 3: FAIL: shared_access_key_enabled is True and sas_policy.expiration_period is empty
+# Case 5: FAIL: shared_access_key_enabled is True and "sas_policy.expiration_period" is NOT configured
 
 resource "azurerm_storage_account" "fail_2" {
   name                     = "pud-storage2023abc3"
@@ -53,34 +85,3 @@ resource "azurerm_storage_account" "fail_2" {
 
 }
 
-# Case 4: FAIL: shared_access_key_enabled is False but sas_policy.expiration_period is empty
-
-resource "azurerm_storage_account" "fail_3" {
-  name                     = "pud-storage2023abc4"
-  resource_group_name      = var.rg-name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  shared_access_key_enabled = false
-
-  sas_policy {
-    expiration_period = ""
-  }
-
-}
-
-# Case 4: FAIL: shared_access_key_enabled is True
-
-resource "azurerm_storage_account" "fail_4" {
-  name                     = "pud-storage2023abc4"
-  resource_group_name      = var.rg-name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  shared_access_key_enabled = true
-
-  sas_policy {
-    expiration_period = "90.00:00:00"
-  }
-
-}
