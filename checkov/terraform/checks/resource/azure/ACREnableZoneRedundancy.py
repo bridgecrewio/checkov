15265d85c30a767587bf14c07897d2c31b8bd1ea
tal66 <77445020+tal66@users.noreply.github.com>
2023-11-11 19:50:11 +0000

    feat(terraform): Ensure ACR is zone-redundant

---
+++ checkov/terraform/checks/resource/azure/ACREnableZoneRedundancy.py
@@ -0,0 +1,40 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+from typing import Any
+
+
+class ACREnableZoneRedundancy(BaseResourceCheck):
+
+    def __init__(self) -> None:
+        """
+        Zone redundancy provides resiliency and high availability to
+        a registry or replication resource in a specific region.
+        """
+        name = "Ensure Azure Container Registry (ACR) is zone redundant"
+        id = "CKV_AZURE_233"
+        supported_resources = ("azurerm_container_registry",)
+        categories = (CheckCategories.BACKUP_AND_RECOVERY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        # supported only with Premium
+        sku = conf.get("sku")
+        if sku != ["Premium"]:
+            return CheckResult.PASSED
+
+        # check registry
+        if conf.get("zone_redundancy_enabled") != [True]:
+            return CheckResult.FAILED
+
+        # check each replica
+        replications = conf.get("georeplications", {})
+        for replica in replications:
+            zone_redundancy_enabled = replica.get('zone_redundancy_enabled', [])
+            if zone_redundancy_enabled != [True]:
+                return CheckResult.FAILED
+
+        return CheckResult.PASSED
+
+
+check = ACREnableZoneRedundancy()
