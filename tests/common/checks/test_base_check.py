a0d4ac0ad3887ee8bc3aeb2931b9dac7149a3c02
gruebel <anton.gruebel@gmail.com>
2023-11-20 21:19:11 +0100

    fix abstract test

--- tests/common/checks/test_base_check.py
+++ tests/common/checks/test_base_check.py
@@ -96,7 +96,7 @@ class TestBaseCheck(unittest.TestCase):
             TestCheckUnknownSignature()
 
         self.assertIsInstance(context.exception, TypeError)
-        self.assertRegex(context.exception.args[0], r"Can't instantiate abstract class TestCheckUnknownSignature with abstract method(s)? scan_entity_conf")
+        self.assertRegex(context.exception.args[0], r"Can't instantiate abstract class TestCheckUnknownSignature")
 
     def test_details_reinitializing_after_execution(self):
         check = TestCheckDetails()
