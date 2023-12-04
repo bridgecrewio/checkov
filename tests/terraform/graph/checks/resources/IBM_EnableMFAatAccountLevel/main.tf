13078350645e9dd2f4cd98ee1855284b3754aa6b
Praveen <prdeshpande@paloaltonetworks.com>
2023-12-04 14:53:57 +0530

    Added SKIP comments to terraform scripts

--- tests/terraform/graph/checks/resources/IBM_EnableMFAatAccountLevel/main.tf
+++ tests/terraform/graph/checks/resources/IBM_EnableMFAatAccountLevel/main.tf
@@ -4,13 +4,13 @@
 resource "ibm_iam_account_settings" "pass" {
     mfa                           = "LEVEL3"
     session_expiration_in_seconds = "40000"
-    restrict_create_platform_apikey = "RESTRICTED"
+    restrict_create_platform_apikey = "RESTRICTED" # checkov:skip=CKV_SECRET_6 test secret
 }
 
 # Case 2: Fail: as 'mfa' argument does NOT exist
 
 resource "ibm_iam_account_settings" "fail_1" {
-  restrict_create_platform_apikey = "NOT_RESTRICTED"
+  restrict_create_platform_apikey = "NOT_RESTRICTED" # checkov:skip=CKV_SECRET_6 test secret
 }
 
 # Case 3: Fail: as 'mfa' equals to 'None'
