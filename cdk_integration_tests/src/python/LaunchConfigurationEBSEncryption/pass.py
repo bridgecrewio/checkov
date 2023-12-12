4375c1cc93f7aa15a21cdc2e484ee39a95df4b70
razreik <razreik@paloaltonetworks.com>
2023-12-12 15:39:05 +0200

    added tests

--- cdk_integration_tests/src/python/LaunchConfigurationEBSEncryption/pass.py
+++ cdk_integration_tests/src/python/LaunchConfigurationEBSEncryption/pass.py
@@ -22,3 +22,23 @@ class MyAutoScalingLaunchConfig(core.Stack):
 app = core.App()
 MyAutoScalingLaunchConfig(app, "MyAutoScalingLaunchConfig")
 app.synth()
+
+class MyAutoScalingLaunchConfig(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define Launch Configuration
+        launch_config = autoscaling.CfnLaunchConfiguration(
+            self, 'MyLaunchConfiguration',
+            image_id='ami-12345678',  # Replace with your desired AMI ID
+            instance_type='t2.micro',  # Replace with your desired instance type
+            block_device_mappings=[{
+                'deviceName': '/dev/xvda',
+            }]
+            # Other properties for your Launch Configuration
+        )
+
+app = core.App()
+MyAutoScalingLaunchConfig(app, "MyAutoScalingLaunchConfig")
+app.synth()
+
--- cdk_integration_tests/src/python/RedShiftSSL/pass.py
+++ cdk_integration_tests/src/python/RedShiftSSL/pass.py
@@ -22,3 +22,68 @@ class MyRedshiftClusterParameterGroupStack(core.Stack):
 app = core.App()
 MyRedshiftClusterParameterGroupStack(app, "MyRedshiftClusterParameterGroupStack")
 app.synth()
+
+
+class MyRedshiftClusterParameterGroupStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define Redshift Cluster Parameter Group with require_ssl parameter
+        redshift.CfnClusterParameterGroup(
+            self, 'MyRedshiftClusterParameterGroup',
+            description='My Redshift Parameter Group',
+            parameter_group_family='redshift-1.0',
+            parameters=[
+                redshift.CfnClusterParameterGroup.ParameterProperty(
+                    parameter_value='true',
+                    parameter_name='require_ssl'
+                )
+                # Add other parameters if needed
+            ]
+        )
+
+app = core.App()
+MyRedshiftClusterParameterGroupStack(app, "MyRedshiftClusterParameterGroupStack")
+app.synth()
+
+
+class MyRedshiftClusterParameterGroupStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define Redshift Cluster Parameter Group with require_ssl parameter
+        redshift.CfnClusterParameterGroup(
+            self, 'MyRedshiftClusterParameterGroup',
+            description='My Redshift Parameter Group',
+            parameter_group_family='redshift-1.0',
+            parameters=[
+                {'parameterName': 'require_ssl','parameterValue': 'true'}
+                # Add other parameters if needed
+            ]
+        )
+
+app = core.App()
+MyRedshiftClusterParameterGroupStack(app, "MyRedshiftClusterParameterGroupStack")
+app.synth()
+
+
+class MyRedshiftClusterParameterGroupStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define Redshift Cluster Parameter Group with require_ssl parameter
+        redshift.CfnClusterParameterGroup(
+            self, 'MyRedshiftClusterParameterGroup',
+            description='My Redshift Parameter Group',
+            parameter_group_family='redshift-1.0',
+            parameters=[
+                {'parameterValue': 'true','parameterName': 'require_ssl'}
+                # Add other parameters if needed
+            ]
+        )
+
+app = core.App()
+MyRedshiftClusterParameterGroupStack(app, "MyRedshiftClusterParameterGroupStack")
+app.synth()
+
+
