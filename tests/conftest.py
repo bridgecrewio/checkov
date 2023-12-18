096675b4d79d667d2ead51d0b96066b690d67e5d
Ayal Regev <ayal@redefine.dev>
2023-12-18 16:43:34 +0200

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
 # Reverse order
 def pytest_collection_modifyitems(session, config, items):
+    print(" -------------------- Reversing tests order --------------------")
     items[:] = items[::-1]
