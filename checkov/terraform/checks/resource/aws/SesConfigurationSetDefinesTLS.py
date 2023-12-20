6b8f6a73254d779089688c09b458dda1a1cf6c98
James Woolfenden <james.woolfenden@gmail.com>
2023-12-20 14:53:08 +0000

    annotations

--- checkov/terraform/checks/resource/aws/SesConfigurationSetDefinesTLS.py
+++ checkov/terraform/checks/resource/aws/SesConfigurationSetDefinesTLS.py
@@ -4,7 +4,7 @@ from checkov.common.models.enums import CheckCategories, CheckResult
 
 class SesConfigurationSetDefinesTLS(BaseResourceValueCheck):
 
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure SES Configuration Set enforces TLS usage"
         id = "CKV_AWS_365"
         supported_resources = ['aws_ses_configuration_set']
@@ -12,7 +12,7 @@ class SesConfigurationSetDefinesTLS(BaseResourceValueCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
                          missing_block_result=CheckResult.FAILED)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "delivery_options/[0]/tls_policy"
 
     def get_expected_value(self) -> str:
