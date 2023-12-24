91eb218454735d006cc72f59219af0e466ab9d4b
adam varsano <avarsano@paloaltonetworks.com>
2023-12-24 09:16:35 +0200

    revert some changes

--- checkov/common/parallelizer/parallel_runner.py
+++ checkov/common/parallelizer/parallel_runner.py
@@ -25,8 +25,7 @@ class ParallelRunner:
     ) -> None:
         self.workers_number = (workers_number if workers_number else os.cpu_count()) or 1
         self.os = platform.system()
-        self.type: str | ParallelizationType = parallelization_type if parallelization_type else self.get_default_parallelization_type(self.os)
-
+        self.type: str | ParallelizationType = parallelization_type
         custom_type = os.getenv("CHECKOV_PARALLELIZATION_TYPE")
         if custom_type:
             self.type = custom_type
