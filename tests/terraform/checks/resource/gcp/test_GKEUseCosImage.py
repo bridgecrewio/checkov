2cfcadb35cd343ba2cd3056180adabeb185445bc
James Woolfenden <james.woolfenden@gmail.com>
2024-01-03 15:12:44 +0000

    fix(terraform): cos image only relevant to gke <1.24 (#5676)
    * fix(terraform) cos image only relavent to gke <1.24
    
    * trim docs

--- tests/terraform/checks/resource/gcp/test_GKEUseCosImage.py
+++ tests/terraform/checks/resource/gcp/test_GKEUseCosImage.py
@@ -1,140 +1,39 @@
 import unittest
-
-import hcl2
+import os
 
 from checkov.terraform.checks.resource.gcp.GKEUseCosImage import check
-from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
+from checkov.terraform.runner import Runner
 
 
 class TestGKEUseCosImage(unittest.TestCase):
 
-    def test_failure(self):
-        hcl_res = hcl2.loads("""
-    resource "google_container_node_pool" "tfer" {
-      autoscaling {
-        max_node_count = "4"
-        min_node_count = "1"
-      }
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
 
-      cluster            = google_container_cluster.tfer.name
-      initial_node_count = "2"
-      location           = "us-west1"
+        test_files_dir = current_dir + "/example_GKEUseCosImage"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
 
-      management {
-        auto_repair  = "true"
-        auto_upgrade = "true"
-      }
-
-      max_pods_per_node = "110"
-      name              = "async-pool-2"
-
-      node_config {
-        disk_size_gb = "400"
-        disk_type    = "pd-ssd"
-        image_type   = "SomethingElse"
-
-        labels = {
-          async = "true"
+        passing_resources = {
+            'google_container_node_pool.pass',
         }
 
-        local_ssd_count = "0"
-        machine_type    = "custom-32-65536"
-
-        metadata = {
-          async                    = "true"
-          disable-legacy-endpoints = "true"
+        failing_resources = {
+            'google_container_node_pool.fail',
         }
 
-        oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-        preemptible     = "false"
-        service_account = "default"
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
 
-        shielded_instance_config {
-          enable_integrity_monitoring = "true"
-          enable_secure_boot          = "true"
-        }
-      }
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
 
-      node_count     = "1"
-      node_locations = ["us-west1-b", "us-west1-a"]
-      project        = "test-project"
-
-      upgrade_settings {
-        max_surge       = "1"
-        max_unavailable = "0"
-      }
-
-      version = "1.14.10-gke.36"
-      zone    = "us-west1"
-    }
-                    """)
-        resource_conf = hcl_res['resource'][0]['google_container_node_pool']['tfer']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
-
-    def test_success(self):
-        hcl_res = hcl2.loads("""
-resource "google_container_node_pool" "tfer" {
-  autoscaling {
-    max_node_count = "4"
-    min_node_count = "1"
-  }
-
-  cluster            = google_container_cluster.tfer.name
-  initial_node_count = "2"
-  location           = "us-west1"
-
-  management {
-    auto_repair  = "true"
-    auto_upgrade = "true"
-  }
-
-  max_pods_per_node = "110"
-  name              = "async-pool-2"
-
-  node_config {
-    disk_size_gb = "400"
-    disk_type    = "pd-ssd"
-    image_type   = "COS"
-
-    labels = {
-      async = "true"
-    }
-
-    local_ssd_count = "0"
-    machine_type    = "custom-32-65536"
-
-    metadata = {
-      async                    = "true"
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-    preemptible     = "false"
-    service_account = "default"
-
-    shielded_instance_config {
-      enable_integrity_monitoring = "true"
-      enable_secure_boot          = "true"
-    }
-  }
-
-  node_count     = "1"
-  node_locations = ["us-west1-b", "us-west1-a"]
-  project        = "test-project"
-
-  upgrade_settings {
-    max_surge       = "1"
-    max_unavailable = "0"
-  }
-
-  version = "1.14.10-gke.36"
-  zone    = "us-west1"
-}
-                """)
-        resource_conf = hcl_res['resource'][0]['google_container_node_pool']['tfer']
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == '__main__':
