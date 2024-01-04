f9c0561922e578d9fb068b35f58c6fbd824f88f3
Nimrod <nimrod@getbaz.com>
2024-01-04 10:56:47 +0200

    fix(terraform): Support "pass_prefix_list" for SG rules

--- checkov/terraform/checks/resource/aws/AbsSecurityGroupUnrestrictedIngress.py
+++ checkov/terraform/checks/resource/aws/AbsSecurityGroupUnrestrictedIngress.py
@@ -82,6 +82,9 @@ class AbsSecurityGroupUnrestrictedIngress(BaseResourceCheck):
         if from_port == 0 and to_port == 0:
             to_port = 65535
 
+        if conf.get('prefix_list_ids'):
+            return False
+
         if from_port is not None and to_port is not None and (from_port <= self.port <= to_port) or (
                 protocol == '-1' and from_port == 0 and to_port == 65535):
             if conf.get('cidr_blocks'):
--- tests/terraform/graph/checks/resources/SGAttachedToResource/main.tf
+++ tests/terraform/graph/checks/resources/SGAttachedToResource/main.tf
@@ -731,6 +731,8 @@ resource "aws_rds_cluster" "pass_rds_cluster" {
   vpc_security_group_ids = [aws_security_group.pass_rds_cluster.id]
 }
 
+resource "aws_security_group" "pass_rds_db_sg" {}
+
 # Redshift
 
 resource "aws_security_group" "pass_redshift" {
