d09637d03e83d3d522fccdc8eafaafbf80231c9b
achia <achiar99@gmail.com>
2024-01-07 09:58:02 +0200

    achia test cdk

--- cdk_integration_tests/utils.py
+++ cdk_integration_tests/utils.py
@@ -12,6 +12,9 @@ def load_failed_checks_from_file(lang: str) -> Dict[str, List[Dict[str, Any]]]:
         data = f.read()
         report = json.loads(data)
         assert report is not None
+        print("-------------")
+        print(str(report))
+        print("-------------")
         results = report.get("results", {})
         failed_checks = results.get("failed_checks")
         results = {}
