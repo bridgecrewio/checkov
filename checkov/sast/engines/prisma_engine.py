dccc8dabd249802ec1976d9ca5da2b969e4d32d4
achia <achiar99@gmail.com>
2024-01-07 13:29:35 +0200

    .

--- checkov/sast/engines/prisma_engine.py
+++ checkov/sast/engines/prisma_engine.py
@@ -254,12 +254,6 @@ class PrismaEngine(SastEngine):
         # convert our byte array to a string
         analyze_code_string = analyze_code_bytes.decode('utf-8')
         d = json.loads(analyze_code_string)
-        print('--------')
-        a = ''
-        for nn in d.get('rule_match').get('python').keys():
-            print(nn)
-        # print(a)
-        print('--------')
         try:
             result = self.create_prisma_report(d)
         except ValidationError as e:
