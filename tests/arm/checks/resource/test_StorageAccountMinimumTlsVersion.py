af2814281e339fccbd48189477e97e7539bdb4d6
oshimko <oshimko@paloaltonetworks.com>
2024-01-31 20:09:18 +0200

    Adding test case where no minimumTlsVersion is set

--- tests/arm/checks/resource/test_StorageAccountMinimumTlsVersion.py
+++ tests/arm/checks/resource/test_StorageAccountMinimumTlsVersion.py
@@ -15,7 +15,7 @@ class TestStorageAccountMinimumTlsVersion(unittest.TestCase):
         report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
         self.assertEqual(summary['passed'], 1)
-        self.assertEqual(summary['failed'], 2)
+        self.assertEqual(summary['failed'], 3)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
 
