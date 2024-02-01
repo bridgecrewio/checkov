9106f41da20b9f39d1fcddf07f5537c98bc0e332
oshimko <oshimko@paloaltonetworks.com>
2024-02-01 18:32:10 +0200

    Revert "Update checkov/arm/checks/resource/StorageAccountMinimumTlsVersion.py"
    This reverts commit a358eda24ab9e3e7108212c0d019bb892efcc548.

--- checkov/arm/checks/resource/StorageAccountMinimumTlsVersion.py
+++ checkov/arm/checks/resource/StorageAccountMinimumTlsVersion.py
@@ -22,7 +22,9 @@ class StorageAccountMinimumTlsVersion(BaseResourceCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
-        if conf.get("properties", {}).get("minimumTlsVersion", "") in ["TLS1_2"]:
+        if "properties" in conf and \
+            "minimumTlsVersion" in conf["properties"] and \
+                conf["properties"]["minimumTlsVersion"] in ['TLS1_2']:
             return CheckResult.PASSED
         return CheckResult.FAILED
 
