5fe4cb66c3587960ca2f71f4479a8e07b16d7982
gruebel <anton.gruebel@gmail.com>
2024-02-02 20:19:06 +0100

    keep resource_types empty for filter blocks

--- tests/common/checks_infra/test_checks_parser.py
+++ tests/common/checks_infra/test_checks_parser.py
@@ -4,6 +4,7 @@ import yaml
 from _pytest.logging import LogCaptureFixture
 
 from checkov.common.checks_infra.checks_parser import GraphCheckParser
+from checkov.common.graph.checks_infra.enums import SolverType
 
 EXAMPLES_DIR = Path(__file__).parent / "examples"
 
@@ -76,3 +77,31 @@ def test_validate_check_config_invalid_definition(caplog: LogCaptureFixture):
         f"Custom policy {file_path} has an invalid 'definition' block type 'NoneType', "
         "needs to be either a 'list' or 'dict'"
     ]
+
+
+def test__parse_raw_check_with_filter_type():
+    # https://github.com/bridgecrewio/checkov/issues/5928
+
+    # given
+    raw_check = {
+        "attribute": "resource_type",
+        "cond_type": "filter",
+        "operator": "within",
+        "value": ["aws_codecommit_repository"],
+    }
+    resources_types = [
+        "aws_root",
+        "aws_root_access_key",
+    ]
+    providers = ["aws"]
+
+    # when
+    check = GraphCheckParser()._parse_raw_check(
+        raw_check=raw_check, resources_types=resources_types, providers=providers
+    )
+
+    # then
+    assert check.attribute == "resource_type"
+    assert check.attribute_value == ["aws_codecommit_repository"]
+    assert not check.resource_types  # this should be empty and not include 'aws_root', etc.
+    assert check.type == SolverType.FILTER
