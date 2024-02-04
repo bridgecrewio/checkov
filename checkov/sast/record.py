a0e65df42061c6f527ce5c0725ff1dbc6c92290c
Rabea Zreik <47931006+RabeaZr@users.noreply.github.com>
2024-02-04 09:26:51 +0200

    feat(sast): add dataflow to output (#5987)
    * add dataflow
    
    * fix lint
    
    * small change
    
    * assure that dataflow != none
    
    * add type
    
    * test commit
    
    * remove \n
    
    * test
    
    * make it work
    
    * fix test
    
    ---------
    
    Co-authored-by: achia <achiar99@gmail.com>

--- checkov/sast/record.py
+++ checkov/sast/record.py
@@ -6,7 +6,7 @@ from checkov.common.bridgecrew.severities import Severity
 from checkov.common.models.enums import CheckResult
 from checkov.common.output.record import Record
 from checkov.common.typing import _CheckResult
-from checkov.common.sast.report_types import MatchMetadata
+from checkov.common.sast.report_types import MatchMetadata, MatchLocation
 
 
 class SastRecord(Record):
@@ -68,18 +68,22 @@ class SastRecord(Record):
 
         severity_message = f'\tSeverity: {self.severity.name}\n' if self.severity and self.show_severity else ''
 
-        file_details = f'{self.file_path}:{" -> ".join([str(x) for x in self.file_line_range])}' if \
-            self.file_line_range[0] != self.file_line_range[-1] else \
-            f'{self.file_path}:{str(self.file_line_range[0])}'
-        code_lines = self.get_code_lines_string(self.code_block)
-        detail = self.get_details_string(self.details)
-        caller_file_details = self.get_caller_file_details_string(self.caller_file_path,
-                                                                  self.caller_file_line_range)
         evaluation_message = self.get_evaluation_string(self.evaluations, self.code_block)
 
-        status_message = colored("\t{} for file - {}\n".format(status, file_details), status_color)
-
         cwe_message = colored(f'\t{self.cwe}\n') if self.cwe else ''
+
+        if self.metadata and self.metadata.taint_mode and self.metadata.taint_mode.data_flow and \
+                len(self.metadata.taint_mode.data_flow) > 0:
+            code_lines, file_details = self.get_code_lines_taint(self.metadata.taint_mode.data_flow)
+        else:
+            file_details = f'{self.file_path}:{" -> ".join([str(x) for x in self.file_line_range])}' if \
+                self.file_line_range[0] != self.file_line_range[-1] else \
+                f'{self.file_path}:{str(self.file_line_range[0])}'
+            code_lines = self.get_code_lines_string(self.code_block)
+
+        detail = self.get_details_string(self.details)
+        caller_file_details = self.get_caller_file_details_string(self.caller_file_path, self.caller_file_line_range)
+        status_message = colored("\t{} for file - {}\n".format(status, file_details), status_color)
 
         if self.check_result["result"] == CheckResult.FAILED and code_lines and not compact:
             return f"{check_message}{severity_message}{status_message}{cwe_message}{detail}{caller_file_details}{guideline_message}{code_lines}{evaluation_message}\n"
@@ -88,3 +92,24 @@ class SastRecord(Record):
             return f"{check_message}{severity_message}{status_message}{cwe_message}{suppress_comment}{detail}{caller_file_details}{guideline_message}\n"
         else:
             return f"{check_message}{severity_message}{status_message}{cwe_message}{detail}{caller_file_details}{evaluation_message}{guideline_message}\n"
+
+    def get_code_lines_taint(self, dataflows: List[MatchLocation]) -> Tuple[str, str]:
+        code_lines = ""
+        last_file = dataflows[0].path.split('/')[-1]
+        last_line_num = dataflows[0].start.row
+        code_lines += colored("\t\t" + last_file, 'light_yellow')
+        file_details = last_file
+        for df in dataflows:
+            cur_file = df.path.split('/')[-1]
+            cur_line_num = df.start.row
+            if cur_file != last_file:
+                code_lines += colored("\t\t" + cur_file, 'light_yellow')
+                file_details += "->" + cur_file
+                last_file = cur_file
+            else:
+                if cur_line_num != last_line_num and cur_line_num != last_line_num + 1:
+                    code_lines += colored("\t\t...", 'light_yellow')
+            code_lines += self.get_code_lines_string([(cur_line_num, df.code_block)])
+            file_details += "->" + str(cur_line_num)
+            last_line_num = cur_line_num
+        return code_lines, file_details
