5dff24f2b596fc9e3a76d21b86eca6b9664eb64b
achia <achiar99@gmail.com>
2024-02-05 15:11:18 +0200

    fix cdk suppressed checks

--- cdk_integration_tests/utils.py
+++ cdk_integration_tests/utils.py
@@ -7,15 +7,21 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def load_failed_checks_from_file(lang: str) -> Dict[str, List[Dict[str, Any]]]:
-    report_path = os.path.join(current_dir, '..', f'checkov_report_cdk_{lang}.json')
+    report_path ='/Users/arosenfeld/Desktop/dev/checkov/aa.json'
     with open(report_path) as f:
         data = f.read()
         report = json.loads(data)
         assert report is not None
         results = report.get("results", {})
         failed_checks = results.get("failed_checks")
+        skipped_checks = results.get("skipped_checks")
         results = {}
         for check in failed_checks:
+            check_id = check['check_id']
+            if not results.get(check_id):
+                results[check_id] = []
+            results[check_id].append(check)
+        for check in skipped_checks:
             check_id = check['check_id']
             if not results.get(check_id):
                 results[check_id] = []
