768ef4aaa08610e5dd8c039c616fd3948fb6b8ea
Taylor <tsmith.v11@gmail.com>
2024-02-07 19:58:08 -0800

    CKV_AWS_80

--- tests/cloudformation/checks/resource/aws/test_MSKClusterLogging.py
+++ tests/cloudformation/checks/resource/aws/test_MSKClusterLogging.py
@@ -1,27 +1,27 @@
 import os
 import unittest
 
-from checkov.cloudformation.checks.resource.aws.MSKClusterNodesArePrivate import check
+from checkov.cloudformation.checks.resource.aws.MSKClusterLogging import check
 from checkov.cloudformation.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestMSKClusterNodesArePrivate(unittest.TestCase):
+class TestMSKClusterLogging(unittest.TestCase):
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_MSKClusterNodesArePrivate"
+        test_files_dir = current_dir + "/example_MSKClusterLogging"
         report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
         passing_resources = {
             "AWS::MSK::Cluster.MSKClusterPASS",
-            "AWS::MSK::Cluster.MSKClusterPASS2"
+            "AWS::MSK::Cluster.MSKClusterPASSskip",
         }
         failing_resources = {
-            "AWS::MSK::Cluster.MSKCluster",
-            "AWS::MSK::Cluster.MSKCluster2"
+            "AWS::MSK::Cluster.MSKClusterFAILempty",
+            "AWS::MSK::Cluster.MSKClusterFAILdeclared",
         }
 
         passed_check_resources = set([c.resource for c in report.passed_checks])
