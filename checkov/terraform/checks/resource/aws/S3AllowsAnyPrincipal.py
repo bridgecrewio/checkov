5e5ca8e0ff1db1ef842985643135fb69f6b9d15d
Taylor <tsmith.v11@gmail.com>
2024-02-23 14:18:27 -0800

    Fix tests

--- checkov/terraform/checks/resource/aws/S3AllowsAnyPrincipal.py
+++ checkov/terraform/checks/resource/aws/S3AllowsAnyPrincipal.py
@@ -8,16 +8,16 @@ import json
 from typing import List
 
 
-def check_conditions(statement):
+def check_conditions(statement) -> bool:
     # Check if 'Condition' key exists
     if 'Condition' not in statement:
-        return CheckResult.FAILED
+        return False
 
     condition = statement['Condition']
 
     # Pass if they define bad ARNs. Assumes they are not too narrow
     if any(key in condition for key in ['ArnNotEquals', 'ArnNotLike']):
-        return CheckResult.PASSED
+        return True
 
     # Handling 'ArnEquals' and 'ArnLike'
     for arn_key in ['ArnEquals', 'ArnLike']:
@@ -28,9 +28,9 @@ def check_conditions(statement):
                     principal_arn = condition[arn_key][principal_key]
                     # Fail if the  Condition is for all ARNs of any resource
                     if re.match(r'^arn:aws:[a-z0-9-]+::\*.*$', principal_arn):
-                        return CheckResult.FAILED
+                        return False
             # Passed if 'aws:PrincipalArn' or 'aws:SourceArn' do not match because then they are specific
-            return CheckResult.PASSED
+            return True
 
     # Handle VPC sources. Other sources not specific enough
     # Leaves out the NOT conditions as too broad ('StringNotEquals', 'StringNotEqualsIgnoreCase', 'StringNotLike')
@@ -39,15 +39,15 @@ def check_conditions(statement):
         for condition_type in string_conditions:
             if condition_type in condition:
                 if any(source in condition[condition_type] for source in ['aws:sourceVpce', 'aws:SourceVpc']):
-                    return CheckResult.PASSED
+                    return True
 
     # Default fail if none of the above conditions are met
-    return CheckResult.FAILED
+    return False
 
 
 class S3AllowsAnyPrincipal(BaseResourceCheck):
 
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure S3 bucket does not allow an action with any Principal"
         id = "CKV_AWS_70"
         supported_resources = ['aws_s3_bucket', 'aws_s3_bucket_policy']
@@ -74,14 +74,17 @@ class S3AllowsAnyPrincipal(BaseResourceCheck):
                     continue
                 principal = statement['Principal']
                 if principal == '*':
-                    return check_conditions(statement)
+                    if check_conditions(statement):
+                        return CheckResult.PASSED
+                    CheckResult.FAILED
                 if 'AWS' in statement['Principal']:
                     # Can be a string or an array of strings
                     aws = statement['Principal']['AWS']
                     if (isinstance(aws, str) and aws == '*') or (isinstance(aws, list) and '*' in aws):
-                        return check_conditions(statement)
-
-
+                        if check_conditions(statement):
+                            return CheckResult.PASSED
+                        CheckResult.FAILED
+        
         return CheckResult.PASSED
 
     def get_evaluated_keys(self) -> List[str]:
