ad87324bb8a927fd41bfae332edbe42ce7c16390
Steve Vaknin <svaknin@paloaltonetworks.com>
2024-04-01 16:15:02 +0300

    feature(terraform): support s3 bucket name for references

--- checkov/terraform/graph_builder/local_graph.py
+++ checkov/terraform/graph_builder/local_graph.py
@@ -38,6 +38,8 @@ from checkov.common.util.consts import RESOLVED_MODULE_ENTRY_NAME
 
 MODULE_RESERVED_ATTRIBUTES = ("source", "version")
 CROSS_VARIABLE_EDGE_PREFIX = '[cross-variable] '
+S3_BUCKET_RESOURCE_NAME = "aws_s3_bucket"
+S3_BUCKET_REFERENCE_ATTRIBUTE = "bucket"
 
 
 class Undetermined(TypedDict):
@@ -94,6 +96,12 @@ class TerraformLocalGraph(LocalGraph[TerraformBlock]):
                 edges_count = len(self.edges)
                 self._build_cross_variable_edges()
                 logging.info(f"Found {len(self.edges) - edges_count} cross variable edges")
+            if strtobool(os.getenv("CHECKOV_EXPERIMENTAL_AWS_S3_NAME_REFERENCES", "True")):
+                # experimental flag on building S3 edges by name for terraform graph
+                logging.info("Building S3 edges name references")
+                edges_count = len(self.edges)
+                self._build_s3_name_reference_edges()
+                logging.info(f"Found {len(self.edges) - edges_count} S3 name references edges")
         else:
             self.update_vertices_fields()
 
@@ -186,6 +194,13 @@ class TerraformLocalGraph(LocalGraph[TerraformBlock]):
                                 referenced_modules: Optional[List[Dict[str, Any]]] = None) -> None:
 
         attribute_is_leaf = get_attribute_is_leaf(vertex)
+        # attributes_items = list(vertex.attributes.items())
+        # if vertex.block_type == BlockType.VARIABLE:
+        #     attributes_items.append(('_variable_referencer', f"var.{vertex.name}"))
+        # for attribute_key, attribute_value in attributes_items:
+        #     if attribute_key != "_variable_referencer" and (attribute_key in reserved_attribute_names or attribute_has_nested_attributes(
+        #             attribute_key, vertex.attributes, attribute_is_leaf
+        #     )):
         for attribute_key, attribute_value in vertex.attributes.items():
             if attribute_key in reserved_attribute_names or attribute_has_nested_attributes(
                     attribute_key, vertex.attributes, attribute_is_leaf
@@ -322,6 +337,27 @@ class TerraformLocalGraph(LocalGraph[TerraformBlock]):
                     any(self.vertices[e.dest].block_type != BlockType.RESOURCE for e in referenced_vertices):
                 modules = vertex.breadcrumbs.get(CustomAttributes.SOURCE_MODULE, [])
                 self._build_edges_for_vertex(origin_node_index, vertex, aliases, resources_types, True, modules)
+
+    def _build_s3_name_reference_edges(self) -> None:
+        resources_types = self.get_resources_types_in_graph()
+        if S3_BUCKET_RESOURCE_NAME not in resources_types:
+            return
+        bucket_mapping = {}
+        for origin_node_index, referenced_vertices in self.out_edges.items():
+            vertex = self.vertices[origin_node_index]
+            for referenced_vertice in referenced_vertices:
+                if referenced_vertice.label == S3_BUCKET_REFERENCE_ATTRIBUTE:
+                    current = bucket_mapping.get(referenced_vertice.dest, {"bucket_resource_index": None, "referenced_vertices": list()})
+                    if vertex.id.startswith(f"{S3_BUCKET_RESOURCE_NAME}."):
+                        current["bucket_resource_index"] = origin_node_index
+                    else:
+                        current["referenced_vertices"] = current["referenced_vertices"] + [referenced_vertice]
+                    bucket_mapping[referenced_vertice.dest] = current
+
+        for destination, mapping in bucket_mapping.items():
+            if self.vertices[destination].block_type == BlockType.VARIABLE:
+                for reference_vertex in mapping["referenced_vertices"]:
+                    self.create_edge(mapping["bucket_resource_index"], reference_vertex.origin, S3_BUCKET_REFERENCE_ATTRIBUTE, True)
 
     def create_edge(self, origin_vertex_index: int, dest_vertex_index: int, label: str,
                     cross_variable_edges: bool = False) -> bool:
