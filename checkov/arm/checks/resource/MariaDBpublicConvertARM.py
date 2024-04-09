51dd3ce1df8fc81d6857ca0d693ce9eba54f7bb0
goldyBH <goldy36969@gmail.com>
2024-04-09 14:54:42 +0300

    chore: update release notes

--- checkov/arm/checks/resource/MariaDBpublicConvertARM.py
+++ checkov/arm/checks/resource/MariaDBpublicConvertARM.py
@@ -7,12 +7,12 @@ class MariaDBpublicConvertARM(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure that MariaDB server enables geo-redundant backups"
         id = "CKV_AZURE_129"
-        supported_resources = ["Microsoft.DBforMariaDB/servers"]
+        supported_resources = "Microsoft.DBforMariaDB/servers"
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
-        return "public_network_access_enabled"
+        return "properties/publicNetworkAccess"
 
     def get_expected_value(self) -> Any:
         return False
