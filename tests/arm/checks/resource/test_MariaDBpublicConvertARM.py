0057d4b98d6ed384e4e92569fbb0997d9debc5cb
chani6887 <chsh6887@gmail.com>
2024-04-10 12:49:29 +0300

    chore: update release notes

--- tests/arm/checks/resource/test_MariaDBpublicConvertARM.py
+++ tests/arm/checks/resource/test_MariaDBpublicConvertARM.py
@@ -8,27 +8,25 @@ from checkov.runner_filter import RunnerFilter
 class TestMariaDBPublicConvertARM(unittest.TestCase):
     def test_summary(self):
         test_files_dir = Path(__file__).parent / "example_MariaDBPublicConvertToARM"
-
         report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
-
         summary = report.get_summary()
-
         passing_resources = {
             "Microsoft.DBforMariaDB/servers.pass",
         }
         failing_resources = {
             "Microsoft.DBforMariaDB/servers.fail",
         }
+
         passed_check_resources = {c.resource for c in report.passed_checks}
         failed_check_resources = {c.resource for c in report.failed_checks}
 
-        assert summary["passed"] == len(passing_resources)
-        assert summary["failed"] == len(failing_resources)
-        assert summary["skipped"] == 0
-        assert summary["parsing_errors"] == 0
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
 
-        assert passed_check_resources == passing_resources
-        assert failed_check_resources == failing_resources
+        self.assertEqual(passed_check_resources,passing_resources)
+        self.assertEqual(failed_check_resources,failing_resources)
 
 
 if __name__ == '__main__':
