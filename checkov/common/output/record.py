6a82a47278dcebd4241a3c07a37150506211ee01
Denys Potapov <denys.potapov@gmail.com>
2024-04-11 14:22:36 +0300

    fix more

--- checkov/common/output/record.py
+++ checkov/common/output/record.py
@@ -203,9 +203,13 @@ class Record:
 
         severity_message = f'\tSeverity: {self.severity.name}\n' if self.severity else ''
 
-        file_details = colored(
-            "\tFile: {}:{}\n".format(self.file_path, "-".join([str(x) for x in self.file_line_range])), "magenta"
-        )
+        if self.file_line_range:
+            file_details = colored(
+                "\tFile: {}:{}\n".format(self.file_path, "-".join([str(x) for x in self.file_line_range])), "magenta"
+            )
+        else:
+            file_details = ''
+
         code_lines = self.get_code_lines_string(self.code_block)
         detail = self.get_details_string(self.details)
         caller_file_details = self.get_caller_file_details_string(self.caller_file_path, self.caller_file_line_range)
