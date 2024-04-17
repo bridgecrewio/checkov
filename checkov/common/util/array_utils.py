dbc244ef3efe536833022450a1638b73a028a38b
ipeleg <ipeleg@paloaltonetworks.com>
2024-04-17 08:52:58 +0300

    using the new api license/get-licenses-violations instead of packages/get-licenses-violations (which is deprecated) + using chunks

---
+++ checkov/common/util/array_utils.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+def chunk_array(arr: list[Any], per_chunk: int) -> list[list[Any]]:
+    result_array = []
+    chunk_index = -1
+
+    for index, item in enumerate(arr):
+        if index % per_chunk == 0:
+            result_array.append([])
+            chunk_index += 1
+        result_array[chunk_index].append(item)
+
+    return result_array
