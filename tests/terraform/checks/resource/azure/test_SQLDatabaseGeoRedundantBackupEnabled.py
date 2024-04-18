ce6658fd3978815b5052dfd0557bbe787530e26f
Thomas Defise <thde@bankingcircle.com>
2024-04-18 20:02:54 +0200

    Added missing call

--- tests/terraform/checks/resource/azure/test_SQLDatabaseGeoRedundantBackupEnabled.py
+++ tests/terraform/checks/resource/azure/test_SQLDatabaseGeoRedundantBackupEnabled.py
@@ -36,3 +36,7 @@ class TestSQLDatabaseGeoRedundantBackupEnabled(unittest.TestCase):
 
         self.assertEqual(passing_resources, passed_check_resources)
         self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
