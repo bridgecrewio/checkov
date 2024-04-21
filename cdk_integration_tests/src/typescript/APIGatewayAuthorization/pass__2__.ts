e1f1f05164a5eeb0cd7e509c7810b0d268c7a73a
Omry Mendelovich <omry155@gmail.com>
2024-04-21 13:18:42 +0300

    improve tests

--- cdk_integration_tests/src/typescript/APIGatewayAuthorization/pass__2__.ts
+++ cdk_integration_tests/src/typescript/APIGatewayAuthorization/pass__2__.ts
@@ -7,9 +7,7 @@ const resource: Resource = new Resource(); // Placeholder for actual resource in
 
 // Test cases for the policy patterns
 
-// This should not match any pattern as it includes an authorization type
-// SANITIZER
-const method3 = resource.addMethod('PUT', new apigateway.MockIntegration(), {
-  authorizationType: apigateway.AuthorizationType.COGNITO,
-  apiKeyRequired: true
+// This should not match any pattern as it includes an authorization type and is not open
+const method4 = resource.addMethod('DELETE', new apigateway.MockIntegration(), {
+  authorizationType: apigateway.AuthorizationType.IAM
 });
