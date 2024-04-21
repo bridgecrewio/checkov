530b19b313429dd33b48bded38516f84de9126bd
Taylor <tsmith.v11@gmail.com>
2024-04-20 22:43:20 -0700

    Update range includes

--- checkov/common/checks_infra/solvers/attribute_solvers/range_includes_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/range_includes_attribute_solver.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any, Dict, List
+from typing import Optional, Any, Dict, List, Union
 from checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver import BaseAttributeSolver
 from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.util.type_forcers import force_int
@@ -8,9 +8,13 @@ class RangeIncludesAttributeSolver(BaseAttributeSolver):
     operator = Operators.RANGE_INCLUDES  # noqa: CCE003  # a static attribute
 
     def __init__(
-        self, resource_types: List[str], attribute: Optional[str], value: Any, is_jsonpath_check: bool = False
+            self, resource_types: List[str], attribute: Optional[str], value: Union[Any, List[Any]],
+            is_jsonpath_check: bool = False
     ) -> None:
-        super().__init__(resource_types, attribute, force_int(value), is_jsonpath_check)
+        # Convert value to a list if it's not already one to unify handling
+        value = [force_int(v) if isinstance(v, (str, int)) else v for v in
+                 (value if isinstance(value, list) else [value])]
+        super().__init__(resource_types, attribute, value, is_jsonpath_check)
 
     def _get_operation(self, vertex: Dict[str, Any], attribute: Optional[str]) -> bool:
         attr = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
@@ -18,6 +22,12 @@ class RangeIncludesAttributeSolver(BaseAttributeSolver):
         if attr is None:
             return False
 
+        if isinstance(attr, list):
+            return any(self._check_value(value, attr_val) for attr_val in attr for value in self.value)
+
+        return any(self._check_value(value, attr) for value in self.value)
+
+    def _check_value(self, value, attr):
         # expects one of the following values:
         # - an actual int
         # - a string that parses to an int
@@ -28,11 +38,14 @@ class RangeIncludesAttributeSolver(BaseAttributeSolver):
             return True
 
         if isinstance(attr, str) and attr.count("-") == 1:
-            try:
-                start, end = attr.split("-")
-                return True if force_int(start) <= self.value <= force_int(end) else False
-            except (TypeError, ValueError):
-                # Occurs if there are not two entries or if one is not an int, in which case we just give up
-                return False
+            return self._check_range(value, attr)
 
-        return True if force_int(attr) == self.value else False
+        return force_int(attr) == value
+
+    def _check_range(self, value, range_str):
+        try:
+            start, end = range_str.split("-")
+            return force_int(start) <= value <= force_int(end)
+        except (TypeError, ValueError):
+            # Occurs if there are not two entries or if one is not an int, in which case we just give up
+            return False
--- tests/terraform/graph/checks_infra/attribute_solvers/range_includes_solver/test_solver.py
+++ tests/terraform/graph/checks_infra/attribute_solvers/range_includes_solver/test_solver.py
@@ -17,8 +17,9 @@ class TestRangeIncludesSolver(TestBaseSolver):
     def test_range_includes_int_solver(self):
         root_folder = 'resources'
         check_id = "RangeIncludesInt"
-        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6']
-        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7', 'test.fail8']
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
+        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
+                       'test.fail8', 'test.fail9']
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
@@ -26,9 +27,9 @@ class TestRangeIncludesSolver(TestBaseSolver):
     def test_range_includes_string_solver(self):
         root_folder = 'resources'
         check_id = "RangeIncludesString"
-        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6']
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
         should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
-                       'test.fail8']
+                       'test.fail8', 'test.fail9']
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
@@ -36,8 +37,9 @@ class TestRangeIncludesSolver(TestBaseSolver):
     def test_range_includes_int_jsonpath_solver(self):
         root_folder = 'resources'
         check_id = "JsonPathRangeIncludesInt"
-        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6']
-        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7', 'test.fail8']
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
+        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
+                       'test.fail8', 'test.fail9']
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
@@ -45,9 +47,29 @@ class TestRangeIncludesSolver(TestBaseSolver):
     def test_range_includes_string_jsonpath_solver(self):
         root_folder = 'resources'
         check_id = "JsonPathRangeIncludesString"
-        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6']
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
         should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
-                       'test.fail8']
+                       'test.fail8', 'test.fail9']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_range_includes_list_solver(self):
+        root_folder = 'resources'
+        check_id = "RangeIncludesList"
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
+        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
+                       'test.fail8', 'test.fail9']
+        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
+
+        self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
+
+    def test_range_includes_string_jsonpath_solver(self):
+        root_folder = 'resources'
+        check_id = "JsonPathRangeIncludesList"
+        should_pass = ['test.pass1', 'test.pass2', 'test.pass3', 'test.pass4', 'test.pass5', 'test.pass6', 'test.pass7']
+        should_fail = ['test.fail1', 'test.fail2', 'test.fail3', 'test.fail4', 'test.fail5', 'test.fail6', 'test.fail7',
+                       'test.fail8', 'test.fail9']
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results, check_id=check_id)
