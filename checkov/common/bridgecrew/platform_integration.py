aeae6cd654c37b49735adf8a1ec3eb3b790ff1cd
Steve Vaknin <svaknin@paloaltonetworks.com>
2024-04-30 14:46:13 +0300

    change type

--- checkov/common/bridgecrew/platform_integration.py
+++ checkov/common/bridgecrew/platform_integration.py
@@ -162,7 +162,7 @@ class BcPlatformIntegration:
         self.scan_dir: List[str] = []
         self.scan_file: List[str] = []
         self.sast_custom_policies: str = ''
-        self.internal_notifier: Callable[[str, Any], None] = lambda event, payload: None
+        self.internal_notifier: Callable[[str, Dict], None] = lambda event, payload: None
 
     def init_instance(self, platform_integration_data: dict[str, Any]) -> None:
         """This is mainly used for recreating the instance without interacting with the platform again"""
