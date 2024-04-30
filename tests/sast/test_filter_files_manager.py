7a1eed2cd0dc2a3aa7b89a745d12ffa85ac772ef
achia <achiar99@gmail.com>
2024-04-30 16:25:09 +0300

    fix path

--- tests/sast/test_filter_files_manager.py
+++ tests/sast/test_filter_files_manager.py
@@ -1,10 +1,11 @@
 from checkov.sast.engines.files_filter_manager import FilesFilterManager
 from checkov.common.sast.consts import SastLanguages
 import pathlib
+import os
 
 
 def test_sast_js_filtered_files_by_ts():
-    test_dir = str(pathlib.Path(__file__).parent.resolve()) + '/source_code/js_filtered_build_ts'
+    test_dir =  os.path.join(pathlib.Path(__file__).parent.resolve(), 'source_code', 'js_filtered_build_ts')
     files_filter_manager = FilesFilterManager(test_dir, SastLanguages.JAVASCRIPT)
     filtered_paths = files_filter_manager.get_files_to_filter()
     assert len(filtered_paths) == 2823
