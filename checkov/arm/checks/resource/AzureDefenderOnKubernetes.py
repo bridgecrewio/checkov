8bb16fcc32983c4e04c3046d59f791b8d9d9d7ef
tehila86127 <tehila86127@gmail.com>
2024-05-02 14:25:47 +0300

    added new arm policy for resource: AzureDefenderOnKubernetes

---
+++ checkov/arm/checks/resource/AzureDefenderOnKubernetes.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+
+class AzureDefenderOnKubernetes(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure that Azure Defender is set to On for Kubernetes"
+        id = "CKV_AZURE_85"
+        supported_resources = ("Microsoft.Security/pricings",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        if "name" in conf:
+            if conf["name"] != "KubernetesService":
+                return CheckResult.PASSED
+            properties = conf.get('properties')
+            if not properties or not isinstance(properties, dict):
+                return CheckResult.FAILED
+            pricingTier = properties.get('pricingTier')
+            if not pricingTier:
+                return CheckResult.FAILED
+            if pricingTier == "Free":
+                return CheckResult.FAILED
+            return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+
+
+check = AzureDefenderOnKubernetes()
---
+++ tests/arm/checks/resource/test_AzureDefenderOnKubernetes.py
@@ -0,0 +1,27 @@
+import unittest
+from pathlib import Path
+
+from checkov.arm.checks.resource.AzureDefenderOnKubernetes import check
+from checkov.arm.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+
+class TestAzureDefenderOnKubernetes(unittest.TestCase):
+    def test_summary(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_AzureDefenderOnKubernetes"
+
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        # then
+        summary = report.get_summary()
+
+        self.assertEqual(summary['passed'], 1)
+        self.assertEqual(summary['failed'], 4)
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
