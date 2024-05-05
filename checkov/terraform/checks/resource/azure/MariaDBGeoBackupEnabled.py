1af01bffa35499125b545d9d5f0795d603519143
chani6887 <chsh6887@gmail.com>
2024-05-05 13:01:58 +0300

    revert Unnecessary changes

--- checkov/terraform/checks/resource/azure/MariaDBGeoBackupEnabled.py
+++ checkov/terraform/checks/resource/azure/MariaDBGeoBackupEnabled.py
@@ -1,23 +1,17 @@
-from typing import Any
-
 from checkov.common.models.enums import CheckCategories
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-
-class MariaDBPublicAccessDisabled(BaseResourceValueCheck):
+class MariaDBGeoBackupEnabled(BaseResourceValueCheck):
     def __init__(self) -> None:
-        name = "Ensure 'public network access enabled' is set to 'False' for MariaDB servers"
-        id = "CKV_AZURE_48"
+        name = "Ensure that MariaDB server enables geo-redundant backups"
+        id = "CKV_AZURE_129"
         supported_resources = ("azurerm_mariadb_server",)
-        categories = (CheckCategories.NETWORKING,)
+        categories = (CheckCategories.BACKUP_AND_RECOVERY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
-        return "public_network_access_enabled"
-
-    def get_expected_value(self) -> Any:
-        return False
+        return "geo_redundant_backup_enabled"
 
 
-check = MariaDBPublicAccessDisabled()
+check = MariaDBGeoBackupEnabled()
