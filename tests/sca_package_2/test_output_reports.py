c2617c78ac3b5bac9105fca13ed93196555e19b9
Steve Vaknin <svaknin@paloaltonetworks.com>
2024-05-05 16:39:04 +0300

    revert sca changes

--- tests/sca_package_2/test_output_reports.py
+++ tests/sca_package_2/test_output_reports.py
@@ -317,7 +317,7 @@ def test_sarif_output(sca_package_report_2_with_skip_scope_function):
     # then
     sarif_output["runs"][0]["tool"]["driver"]["version"] = "2.0.x"
     expected_sarif_json = {
-        "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/main/sarif-2.1/schema/sarif-schema-2.1.0.json",
+        "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
         "version": "2.1.0",
         "runs": [
             {
