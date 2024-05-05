2edd47c86e1fcd69abdf2b0ac07a538d1bbae969
shoshiGit <shoshiwolpe@gmail.com>
2024-05-05 18:52:08 +0300

    AzureDefenderOnStorage as arm update

--- tests/arm/checks/resource/test_AzureDefenderOnStorage.py
+++ tests/arm/checks/resource/test_AzureDefenderOnStorage.py
@@ -2,38 +2,36 @@ import unittest
 
 import json
 
-from checkov.arm.checks.resource.AzureDefenferOnStorage import check
+from checkov.arm.checks.resource.AzureDefenderOnStorage import check
 from checkov.common.models.enums import CheckResult
+from pathlib import Path
+from checkov.arm.runner import Runner
+from checkov.runner_filter import RunnerFilter
 
 
 class TestAzureDefenderOnStorage(unittest.TestCase):
-    def test_failure1(self):
-        resource_conf = {
-            "type": "Microsoft.Security/pricings",
-            "apiVersion": "2018-06-01",
-            "name": "azurermSecurityCenterSubscriptionPricing",
-            "properties": {
-                "tier": "Free",
-                "resourceType": "StorageAccounts"
-            }
+    def test_summary(self):
+        test_files_dir = Path(__file__).parent / "example_AzureDefenderOnStorage"
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+        passing_resources = {
+            "Microsoft.Security/pricings.pass",
+        }
+        failing_resources = {
+            "Microsoft.Security/pricings.fail",
+
         }
 
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
 
-    def test_success(self):
-        resource_conf = {
-            "type": "Microsoft.Security/pricings",
-            "apiVersion": "2018-06-01",
-            "name": "azurermSecurityCenterSubscriptionPricing",
-            "properties": {
-                "tier": "Standard",
-                "resourceType": "StorageAccounts"
-            }
-        }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
 
+        self.assertEqual(passed_check_resources, passing_resources)
+        self.assertEqual(failed_check_resources, failing_resources)
 
 if __name__ == '__main__':
     unittest.main()
