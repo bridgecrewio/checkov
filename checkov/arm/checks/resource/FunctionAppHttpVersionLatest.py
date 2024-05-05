613d09fbc2fd7bf9d05f7e70abc7b6cbc81f6a40
Rachel <bb50305030@gmail.com>
2024-05-05 12:10:05 +0300

    function app version latest

---
+++ checkov/arm/checks/resource/FunctionAppHttpVersionLatest.py
@@ -0,0 +1,23 @@
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class FunctionAppHttpVersionLatest(BaseResourceValueCheck):
+
+    def __init__(self):
+        name = "Ensure that 'HTTP Version' is the latest, if used to run the Function app"
+        id = "CKV_AZURE_67"
+        supported_resources = ("Microsoft.Web/sites/slots",)
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'properties/siteConfig/http20Enabled'
+
+
+check = FunctionAppHttpVersionLatest()
+
