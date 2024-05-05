b730be2c061e4d4b1ab3b42bf3e82f8652e1372b
Rachel <bb50305030@gmail.com>
2024-05-05 11:31:42 +0300

    app cors test

---
+++ checkov/arm/checks/resource/AppServiceDisallowCORS.py
@@ -0,0 +1,26 @@
+from checkov.arm.base_resource_negative_value_check import BaseResourceNegativeValueCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+
+
+class AppServiceDisallowCORS(BaseResourceNegativeValueCheck):
+    def __init__(self):
+        name = "Ensure that CORS disallows every resource to access app services"
+        id = "CKV_AZURE_57"
+        supported_resources = ("Microsoft.Web/sites",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+            missing_block_result=CheckResult.PASSED
+        )
+
+    def get_inspected_key(self):
+        return 'properties/siteConfig/cors/allowedOrigins'
+
+    def get_forbidden_values(self):
+        return ['*']
+
+
+check = AppServiceDisallowCORS()
