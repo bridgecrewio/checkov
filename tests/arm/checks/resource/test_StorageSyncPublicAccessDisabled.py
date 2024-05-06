3d5ec00dbb9348b8cb18d17079b36f990197aed9
project <esti188174@gmail.com>
2024-05-06 12:34:22 +0300

    StorageSyncPublicAccessDisabledUpdate

--- tests/arm/checks/resource/test_StorageSyncPublicAccessDisabled.py
+++ tests/arm/checks/resource/test_StorageSyncPublicAccessDisabled.py
@@ -1,76 +1,39 @@
-import os
 import unittest
+from pathlib import Path
 from checkov.arm.checks.resource.StorageSyncPublicAccessDisabled import check
 from checkov.arm.runner import Runner
 from checkov.runner_filter import RunnerFilter
-from checkov.common.models.enums import CheckResult
 
 
 class TestStorageSyncPublicAccessDisabled(unittest.TestCase):
-    def test_failure_1(self):
-        resource_conf = {
-            "type": "Microsoft.StorageSync/storageSyncServices",
-            "apiVersion": "2021-02-01",
-            "name": "example-storage-sync",
-            "properties": {
-                "storageSyncServiceStatus": "Registered",
-                "storageSyncServiceProperties": {
-                    "trustState": "Enabled",
-                    "storageSyncServiceUid": "65fdd65b-ea5d-4a00-bf7f-40c41ba39ae4",
-                    "provisioningState": "Succeeded"
-                },
-                "location": "East US",
-                "tags": {
-                    "foo": "bar"
-                }
-            }
+    def test_summary(self):
+        # given
+        test_files_dir = Path(__file__).parent / "example_StorageSyncPublicAccessDisabled"
+
+        # when
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        # then
+        summary = report.get_summary()
+
+        passing_resources = {
+            "Microsoft.StorageSync/storageSyncServices.pass",
         }
-        scan_result = check.scan_resource_conf(conf=resource_conf)
-        self.assertEqual(CheckResult.FAILED, scan_result)
+        failing_resources = {
+            "Microsoft.StorageSync/storageSyncServices.fail1",
+            "Microsoft.StorageSync/storageSyncServices.fail2",
+        }
 
-        def test_failure_1(self):
-            resource_conf = {
-                "type": "Microsoft.StorageSync/storageSyncServices",
-                "apiVersion": "2021-02-01",
-                "name": "example-storage-sync",
-                "properties": {
-                    "storageSyncServiceStatus": "Registered",
-                    "storageSyncServiceProperties": {
-                        "trustState": "Enabled",
-                        "storageSyncServiceUid": "65fdd65b-ea5d-4a00-bf7f-40c41ba39ae4",
-                        "provisioningState": "Succeeded"
-                    },
-                    "location": "East US",
-                    "incoming_traffic_policy": "AllowAllTraffic",
-                    "tags": {
-                        "foo": "bar"
-                    }
-                }
-            }
-            scan_result = check.scan_resource_conf(conf=resource_conf)
-            self.assertEqual(CheckResult.FAILED, scan_result)
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
 
-            def test_success(self):
-                resource_conf = {
-                    "type": "Microsoft.StorageSync/storageSyncServices",
-                    "apiVersion": "2021-02-01",
-                    "name": "example-storage-sync",
-                    "properties": {
-                        "storageSyncServiceStatus": "Registered",
-                        "storageSyncServiceProperties": {
-                            "trustState": "Enabled",
-                            "storageSyncServiceUid": "65fdd65b-ea5d-4a00-bf7f-40c41ba39ae4",
-                            "provisioningState": "Succeeded"
-                        },
-                        "location": "East US",
-                        "incoming_traffic_policy": "AllowVirtualNetworksOnly",
-                        "tags": {
-                            "foo": "bar"
-                        }
-                    }
-                }
-                scan_result = check.scan_resource_conf(conf=resource_conf)
-                self.assertEqual(CheckResult.PASSED, scan_result)
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == '__main__':
