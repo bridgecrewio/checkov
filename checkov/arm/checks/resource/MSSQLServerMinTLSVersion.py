3bcc611ad08e89022a751696003aa1a711814ef0
chani <chanig847@gmail.com>
2024-05-06 19:10:31 +0300

    MSSQLServerMinTLSVersion

--- checkov/arm/checks/resource/MSSQLServerMinTLSVersion.py
+++ checkov/arm/checks/resource/MSSQLServerMinTLSVersion.py
@@ -17,7 +17,7 @@ class MSSQLServerMinTLSVersion(BaseResourceValueCheck):
     def get_inspected_key(self) -> str:
         return "properties/minimalTlsVersion"
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> str:
         return "1.2"
 
 
