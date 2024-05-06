532a4ed7be3cb45b5621ccc5ebe1dbd629d68bb6
chani6887 <chsh6887@gmail.com>
2024-05-06 10:20:00 +0300

    revert Unnecessary changes

--- checkov/arm/checks/resource/MariaDBPublicAccessDisabled.py
+++ checkov/arm/checks/resource/MariaDBPublicAccessDisabled.py
@@ -7,8 +7,8 @@ class MariaDBpublicConvertARM(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure that MariaDB server enables geo-redundant backups"
         id = "CKV_AZURE_48"
-        supported_resources = ["Microsoft.DBforMariaDB/servers"]
-        categories = [CheckCategories.NETWORKING]
+        supported_resources = ("Microsoft.DBforMariaDB/servers",)
+        categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
