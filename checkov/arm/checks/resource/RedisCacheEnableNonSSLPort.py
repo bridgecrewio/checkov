799a57b2028fc9de2132cbbff09e6f8208a16fba
unu87 <151782629+unu87@users.noreply.github.com>
2024-05-06 10:12:29 +0300

    add commit RedisCacheEnableNonSSLPort

---
+++ checkov/arm/checks/resource/RedisCacheEnableNonSSLPort.py
@@ -0,0 +1,26 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
+
+
+class RedisCacheEnableNonSSLPort(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure that only SSL are enabled for Cache for Redis"
+        id = "CKV_AZURE_91"
+        supported_resources = ("Microsoft.Cache/Redis",)
+        categories = (CheckCategories.NETWORKING,)
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+            missing_block_result=CheckResult.PASSED,
+        )
+
+    def get_inspected_key(self) -> str:
+        return "properties/enableNonSSLPort"
+
+    def get_expected_value(self) -> bool:
+        return False
+
+
+check = RedisCacheEnableNonSSLPort()
