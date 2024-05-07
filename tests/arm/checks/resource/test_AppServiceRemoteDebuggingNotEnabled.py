e4d25746a3a45850eeb6241e630f2ec4135ed55b
tehila86127 <tehila86127@gmail.com>
2024-05-07 21:03:32 +0300

    added new arm policy for resource: AppServiceRemoteDebuggingNotEnabled

---
+++ tests/arm/checks/resource/test_AppServiceRemoteDebuggingNotEnabled.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from checkov.runner_filter import RunnerFilter
+from checkov.arm.runner import Runner
+from checkov.arm.checks.resource.AppServiceRemoteDebuggingNotEnabled import check
+
+
+class TestAppServiceRemoteDebuggingNotEnabled(unittest.TestCase):
+
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = os.path.join(current_dir, "example_AppServiceRemoteDebuggingNotEnabled")
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            "Microsoft.Web/sites.pass",
+        }
+        failing_resources = {
+            "Microsoft.Web/sites.fail",
+        }
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary["passed"], len(passing_resources))
+        self.assertEqual(summary["failed"], len(failing_resources))
+        self.assertEqual(summary["skipped"], 0)
+        self.assertEqual(summary["parsing_errors"], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
