c8bb5432b6fda1e80e1fbb4789bb218bb93931a5
tehila86127 <tehila86127@gmail.com>
2024-05-07 15:05:46 +0300

    added new arm policy for resource: AppServiceIdentityProviderEnabled

--- checkov/arm/checks/resource/AppServiceIdentityProviderEnabled.py
+++ checkov/arm/checks/resource/AppServiceIdentityProviderEnabled.py
@@ -5,17 +5,17 @@ from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
 
 class AppServiceIdentityProviderEnabled(BaseResourceValueCheck):
-    def __init__(self) -> None:
+    def __init__(self):
         name = "Ensure that Managed identity provider is enabled for web apps"
         id = "CKV_AZURE_71"
         supported_resources = ('Microsoft.Web/sites',)
         categories = (CheckCategories.GENERAL_SECURITY,)
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self) -> str:
+    def get_inspected_key(self)->str:
         return "identity/type"
 
-    def get_expected_value(self) -> Any:
+    def get_expected_value(self)->Any:
         return ANY_VALUE
 
 
