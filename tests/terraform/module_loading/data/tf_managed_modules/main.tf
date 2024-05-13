461a2524f2d0b8fa3a703a31c8575b63c5924e65
Ruben <rubeng318@ucla.edu>
2024-05-13 12:44:46 -0700

    chore(terraform): Added additional samples for fix

--- tests/terraform/module_loading/data/tf_managed_modules/main.tf
+++ tests/terraform/module_loading/data/tf_managed_modules/main.tf
@@ -1,3 +1,4 @@
+# Exists locally
 module "log_group" {
   source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
 
@@ -5,6 +6,7 @@ module "log_group" {
   retention_in_days = 7
 }
 
+# This version does not exists, and download modules flag is not used so no results expected
 module "log_group_v4" {
   source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
   version = "~> 4.0"
@@ -12,3 +14,22 @@ module "log_group_v4" {
   name_prefix       = "my-log-group-"
   retention_in_days = 7
 }
+
+# Need to verify this type of comment is not an issue.
+#module "log_group_pound_comment" {
+#  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+#
+#  name_prefix       = "my-log-group-"
+#  retention_in_days = 7
+#}
+
+
+# Need to verify this type of comment is not an issue.
+/*
+module "log_group_star_comment" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+
+  name_prefix       = "my-log-group-"
+  retention_in_days = 7
+}
+*/
