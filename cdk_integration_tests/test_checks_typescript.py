3aa9e8f6d17da7aec2f7cc2937372973ed9b0298
tsegev <tsegev@paloaltonetworks.com>
2024-05-15 17:49:55 +0300

    update tests for TS CDKs legacy checks

--- cdk_integration_tests/test_checks_typescript.py
+++ cdk_integration_tests/test_checks_typescript.py
@@ -280,32 +280,28 @@ def test_CKV_AWS_3_EBSEncryption(failed_checks):
     run_check(check_results=failed_checks, check_id="CKV_AWS_3", policy_name="EBSEncryption", language="typescript")
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_CKV_AWS_18_S3BucketLogging(failed_checks):
-    run_check(check_results=failed_checks, check_id="CKV_AWS_18", policy_name="S3BucketLogging", language="typescript")
-
-
-@pytest.mark.skip(reason="Not implemented yet")
-def test_CKV_AWS_19_S3BucketEncryption(failed_checks):
-    run_check(check_results=failed_checks, check_id="CKV_AWS_19", policy_name="S3BucketEncryption",
+def CKV_AWS_377_S3BucketLogging(failed_checks):
+    run_check(check_results=failed_checks, check_id="CKV_AWS_377", policy_name="S3BucketLogging",
               language="typescript")
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_CKV_AWS_21_S3BucketVersioning(failed_checks):
-    run_check(check_results=failed_checks, check_id="CKV_AWS_21", policy_name="S3BucketVersioning",
+def CKV_AWS_375_S3BucketEncryption(failed_checks):
+    run_check(check_results=failed_checks, check_id="CKV_AWS_375", policy_name="S3BucketEncryption",
               language="typescript")
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_CKV_AWS_145_S3BucketKMSEncryption(failed_checks):
-    run_check(check_results=failed_checks, check_id="CKV_AWS_145", policy_name="S3BucketKMSEncryption",
+def CKV_AWS_379_S3BucketVersioning(failed_checks):
+    run_check(check_results=failed_checks, check_id="CKV_AWS_379", policy_name="S3BucketVersioning",
               language="typescript")
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_CKV2_AWS_6_S3BucketPublicAccessBlock(failed_checks):
-    run_check(check_results=failed_checks, check_id="CKV2_AWS_6", policy_name="S3BucketPublicAccessBlock",
+def CKV_AWS_376_S3BucketKMSEncryption(failed_checks):
+    run_check(check_results=failed_checks, check_id="CKV_AWS_376", policy_name="S3BucketKMSEncryption",
+              language="typescript")
+
+
+def CKV_AWS_378_S3BucketPublicAccessBlock(failed_checks):
+    run_check(check_results=failed_checks, check_id="CKV_AWS_378", policy_name="S3BucketPublicAccessBlock",
               language="typescript")
 
 
