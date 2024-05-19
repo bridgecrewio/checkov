efb0fcf1fd2dc94a466fc845eb56f042c2e03f41
SaraWeinberg1234 <w3231000@gmail.com>
2024-05-19 13:55:49 +0300

    change between any and str

--- checkov/arm/checks/resource/AppServiceJavaVersion.py
+++ checkov/arm/checks/resource/AppServiceJavaVersion.py
@@ -1,6 +1,6 @@
+from typing import Any
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.arm.base_resource_value_check import BaseResourceValueCheck
-from typing import Any
 
 
 class AppServiceJavaVersion(BaseResourceValueCheck):
