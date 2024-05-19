67f58a19c5324890f6c6950d8fbd3eefbf79948a
userrut <rut83364@gmail.com>
2024-05-19 14:56:44 +0300

    add a new arm policy AzureDefenderDisabledForResManager

---
+++ checkov/arm/checks/resource/AzureDefenderDisabledForResManager.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+
+class AzureDefenderDisabledForResManager(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure that Azure Defender for cloud is set to On for Resource Manager"
+        id = "CKV_AZURE_234"
+        supported_resources = ("Microsoft.Security/pricings",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        if "properties" in conf:
+            if "pricingTier" in conf["properties"]:
+                if str(conf["properties"]["pricingTier"]).lower() != "standard":
+                    if str(conf["properties"]["resourceType"]).lower() == "arm":
+                        return CheckResult.FAILED
+        return CheckResult.PASSED
+
+    def get_evaluated_keys(self) -> list[str]:
+        return ["resourceType", "pricingTier"]
+
+
+check = AzureDefenderDisabledForResManager()
---
+++ tests/arm/checks/resource/test_AzureDefenderDisabledForResManager.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+from checkov.runner_filter import RunnerFilter
+from checkov.arm.runner import Runner
+from checkov.arm.checks.resource.AzureDefenderDisabledForResManager import check
+
+class TestAzureDefenderDisabledForResManager(unittest.TestCase):
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = os.path.join(current_dir, "example_AzureDefenderDisabledForResManager")
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            'Microsoft.Security/pricings.pass',
+            'Microsoft.Security/pricings.pass2',
+            'Microsoft.Security/pricings.pass3',
+        }
+        failing_resources = {
+            'Microsoft.Security/pricings.fail',
+        }
+        skipped_resources = {}
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], len(skipped_resources))
+        self.assertEqual(summary['parsing_errors'], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
