bf8c881cdd7e2e01bc530caeaab5cc58c1916f50
SaraWeinberg1234 <w3231000@gmail.com>
2024-05-19 13:38:06 +0300

    first time

---
+++ tests/arm/checks/resource/test_ACRGeoreplicated.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+
+from checkov.runner_filter import RunnerFilter
+from checkov.arm.runner import Runner
+from checkov.arm.checks.resource.ACRGeoreplicated import check
+
+
+class TestACRGeoreplicated(unittest.TestCase):
+
+    def test(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = os.path.join(current_dir, "example_ACRGeoreplicated")
+        report = runner.run(root_folder=test_files_dir,
+                            runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            'Microsoft.ContainerRegistry/registries.pass',
+        }
+        failing_resources = {
+            'Microsoft.ContainerRegistry/registries.fail',
+            'Microsoft.ContainerRegistry/registries.fail2',
+            'Microsoft.ContainerRegistry/registries.fail3'
+        }
+        skipped_resources = {}
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], len(skipped_resources))
+        self.assertEqual(summary['parsing_errors'], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
