8e9af878d126dc65b670de76549762daa07fd8fe
tehila86127 <tehila86127@gmail.com>
2024-05-20 11:55:28 +0300

    update arm policy for resource: AzureDefenderOnKubernetes

--- tests/arm/checks/resource/test_AzureDefenderOnKubernetes.py
+++ tests/arm/checks/resource/test_AzureDefenderOnKubernetes.py
@@ -16,11 +16,25 @@ class TestAzureDefenderOnKubernetes(unittest.TestCase):
 
         # then
         summary = report.get_summary()
+        passing_resources = {
+            "Microsoft.Security/pricings.KubernetesService",
+            "Microsoft.Security/pricings.KeyVaults",
+            "Microsoft.Security/pricings.SqlServerVirtualMachines",
+        }
+        failing_resources = {
+            "Microsoft.Security/pricings.KubernetesService",
+        }
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
 
         self.assertEqual(summary['passed'], 3)
         self.assertEqual(summary['failed'], 1)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == "__main__":
