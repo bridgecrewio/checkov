388b9ecc0c50bc39ef60faf399f2c7eacd74f4d5
tehila86127 <tehila86127@gmail.com>
2024-05-21 14:22:57 +0300

    update arm policy for resource: AppServiceRemoteDebuggingNotEnabled

--- checkov/arm/checks/resource/AppServiceRemoteDebuggingNotEnabled.py
+++ checkov/arm/checks/resource/AppServiceRemoteDebuggingNotEnabled.py
@@ -1,4 +1,4 @@
-from checkov.common.models.enums import CheckCategories,CheckResult
+from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
 
@@ -6,15 +6,10 @@ class AppServiceRemoteDebuggingNotEnabled(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure that remote debugging is not enabled for app services"
         id = "CKV_AZURE_72"
-        supported_resources = ('Microsoft.Web/sites',)
-        categories = (CheckCategories.GENERAL_SECURITY,)
-        super().__init__(
-            name=name,
-            id=id,
-            categories=categories,
-            supported_resources=supported_resources,
-            missing_block_result=CheckResult.PASSED,
-        )
+        supported_resources = ["Microsoft.Web/sites",]
+        categories = [CheckCategories.GENERAL_SECURITY,]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED,)
 
     def get_inspected_key(self) -> str:
         return "properties/siteConfig/remoteDebuggingEnabled"
