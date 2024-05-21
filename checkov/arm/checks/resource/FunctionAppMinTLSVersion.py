0c19f81949be54d2b0300c5adf921676da034187
rutiNalenger <frutiz2641@gmail.com>
2024-05-21 11:32:04 +0300

    error correction

--- checkov/arm/checks/resource/FunctionAppMinTLSVersion.py
+++ checkov/arm/checks/resource/FunctionAppMinTLSVersion.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.arm.base_resource_value_check import BaseResourceValueCheck
@@ -8,7 +8,7 @@ class FunctionAppMinTLSVersion(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure Function app is using the latest version of TLS encryption"
         id = "CKV_AZURE_145"
-        supported_resources = ('Microsoft.Web/sites','Microsoft.Web/sites/slots',)
+        supported_resources = ('Microsoft.Web/sites', 'Microsoft.Web/sites/slots',)
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
                          missing_block_result=CheckResult.PASSED)
@@ -19,7 +19,7 @@ class FunctionAppMinTLSVersion(BaseResourceValueCheck):
     def get_expected_value(self) -> Any:
         return 1.2
 
-    def get_expected_values(self):
+    def get_expected_values(self) -> List[str]:
         return ["1.2", 1.2]
 
 
