4f8f36477e3afb05cb6c77f6e9f52baa1fb00ef0
chani6887 <chsh6887@gmail.com>
2024-05-21 13:33:11 +0300

    New files of the new policy

---
+++ checkov/arm/checks/resource/ACRContainerScanEnabled.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+
+class ACRContainerScanEnabled(BaseResourceCheck):
+    SKUS = {"Standard", "Premium"}  # noqa: CCE003  # a static attribute
+
+    def __init__(self) -> None:
+        name = "Enable vulnerability scanning for container images."
+        id = "CKV_AZURE_163"
+        supported_resources = ("Microsoft.ContainerRegistry/registries",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        if (
+            "sku" in conf.keys()
+            and isinstance(conf["sku"], str)
+            and conf["sku"] in ACRContainerScanEnabled.SKUS
+        ):
+            return CheckResult.PASSED
+
+        return CheckResult.FAILED
+
+
+check = ACRContainerScanEnabled()
