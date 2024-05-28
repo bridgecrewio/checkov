c743f94e427ce0e848094294db1e3c0883716755
userrut <rut83364@gmail.com>
2024-05-28 12:15:57 +0300

    add a new arm policy AzureDefenderDisabledForResManager

--- checkov/arm/checks/resource/AzureDefenderDisabledForResManager.py
+++ checkov/arm/checks/resource/AzureDefenderDisabledForResManager.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from typing import Any
+
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.arm.base_resource_check import BaseResourceCheck
 
