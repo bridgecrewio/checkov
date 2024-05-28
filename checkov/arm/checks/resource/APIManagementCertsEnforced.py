9463dc9e30e27d1000aea71052fad4b9a47ac343
goldyBH <166286970+goldyBH@users.noreply.github.com>
2024-05-28 10:50:11 +0300

    Update APIManagementCertsEnforced.py
    mypy errors

--- checkov/arm/checks/resource/APIManagementCertsEnforced.py
+++ checkov/arm/checks/resource/APIManagementCertsEnforced.py
@@ -1,16 +1,18 @@
 from checkov.arm.base_resource_check import BaseResourceCheck
 from checkov.common.models.enums import CheckCategories, CheckResult
+from typing import Dict, Any, List
 
 
 class APIManagementCertsEnforced(BaseResourceCheck):
-    def __init__(self):
-        name = "Ensure Client Certificates are enforced for API management"
-        id = "CKV_AZURE_152"
-        supported_resources = ('Microsoft.ApiManagement/service',)
-        categories = [CheckCategories.ENCRYPTION]
+    def __init__(self) -> None:
+        name: str = "Ensure Client Certificates are enforced for API management"
+        id: str = "CKV_AZURE_152"
+        supported_resources: tuple = ('Microsoft.ApiManagement/service',)
+        categories: List[CheckCategories] = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        self.evaluated_keys: List[str] = []
 
-    def scan_resource_conf(self, conf):
+    def scan_resource_conf(self, conf: Dict[str, Any]) -> CheckResult:
         if 'enableClientCertificate' in conf['properties']:
             client_certificate_enabled = conf['properties']['enableClientCertificate']
             if client_certificate_enabled is True:
