2931e2e5eceb9c7761a8c5ca8d146695ea5b3520
userrut <rut83364@gmail.com>
2024-05-28 14:29:48 +0300

    add a new arm policy AzureDefenderOnKeyVaults

---
+++ checkov/arm/checks/resource/AzureDefenderOnKeyVaults.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+
+class AzureDefenderOnKeyVaults(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure that Azure Defender is set to On for Key Vault"
+        id = "CKV_AZURE_87"
+        supported_resources = ("Microsoft.Security/pricings",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        properties = conf.get('properties')
+        if properties and isinstance(properties, dict):
+            pricing_tier = properties.get("pricingTier")
+            if conf.get("name", [None]) != "KeyVaults" or pricing_tier == "Standard":
+                return CheckResult.PASSED
+            else:
+                return CheckResult.FAILED
+
+    def get_evaluated_keys(self) -> list[str]:
+        return ["name", "properties/pricingTier"]
+
+
+check = AzureDefenderOnKeyVaults()
