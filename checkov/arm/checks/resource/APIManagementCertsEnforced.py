9c0e1fec5327a07fce72800ab9c032f9998bdcb0
goldyBH <166286970+goldyBH@users.noreply.github.com>
2024-05-28 13:18:41 +0300

    Update APIManagementCertsEnforced.py

--- checkov/arm/checks/resource/APIManagementCertsEnforced.py
+++ checkov/arm/checks/resource/APIManagementCertsEnforced.py
@@ -4,13 +4,13 @@ from typing import Dict, Any, List, Tuple
 
 
 class APIManagementCertsEnforced(BaseResourceCheck):
-def __init__(self) -> None:
-    name: str = "Ensure Client Certificates are enforced for API management"
-    id: str = "CKV_AZURE_152"
-    supported_resources: tuple = ('Microsoft.ApiManagement/service',)
-    categories: List[CheckCategories] = [CheckCategories.ENCRYPTION]
-    super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-    self.evaluated_keys: List[str] = []
+    def __init__(self) -> None:
+        name: str = "Ensure Client Certificates are enforced for API management"
+        id: str = "CKV_AZURE_152"
+        supported_resources: tuple = ('Microsoft.ApiManagement/service',)
+        categories: List[CheckCategories] = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        self.evaluated_keys: List[str] = []
 
 
     def scan_resource_conf(self, conf: Dict[str, Any]) -> CheckResult:
