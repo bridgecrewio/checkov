6500f7e3a34d3293e3f6db8d813cd16b3fe9684e
cshayner <cshayner@paloaltonetworks.com>
2024-05-30 14:49:53 +0300

    Adding ARM check

---
+++ checkov/arm/checks/resource/CognitiveServicesEnableLocalAuth.py
@@ -0,0 +1,26 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
+
+
+class CognitiveServicesEnableLocalAuth(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure that Cognitive Services accounts enable local authentication"
+        id = "CKV_AZURE_238"
+        supported_resources = ('Microsoft.CognitiveServices/accounts', )
+        categories = (CheckCategories.NETWORKING, )
+        super().__init__(
+            name=name,
+            id=id,
+            categories=categories,
+            supported_resources=supported_resources,
+            missing_block_result=CheckResult.PASSED
+        )
+
+    def get_inspected_key(self):
+        return 'properties/disableLocalAuth'
+
+    def get_expected_value(self):
+        return False
+
+
+check = CognitiveServicesEnableLocalAuth()
