9dced5033f469d580ecbd0c580915b06ffafe724
ChanochShayner <57212002+ChanochShayner@users.noreply.github.com>
2024-05-31 09:15:59 +0300

    feat(terraform): AWS SageMaker notebook instance KMS Key (#6374)
    * Adding aws_sagemaker_notebook_instance to CKV_AWS_187
    
    * Adding "AWS::SageMaker::NotebookInstance" to CKV_AWS_198
    
    * cfn fix
    
    * UT fix
    
    * CR
    
    * cfn lint
    
    * cfn lint
    
    * cfn lint

--- checkov/cloudformation/checks/resource/aws/SagemakerNotebookEncryptedWithCMK.py
+++ checkov/cloudformation/checks/resource/aws/SagemakerNotebookEncryptedWithCMK.py
@@ -1,21 +1,26 @@
+from typing import List
+
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.consts import ANY_VALUE
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckCategories
 
 
-class SagemakerDomainEncryptedWithCMK(BaseResourceValueCheck):
+class SagemakerNotebookEncryptedWithCMK(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure Sagemaker domain and notebook instance are encrypted by KMS using a customer managed Key (CMK)"
         id = "CKV_AWS_187"
-        supported_resources = ['aws_sagemaker_domain', 'aws_sagemaker_notebook_instance']
+        supported_resources = ("AWS::SageMaker::NotebookInstance", "AWS::SageMaker::Domain")
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
-        return "kms_key_id"
+        return 'Properties/KmsKeyId'
 
     def get_expected_value(self):
         return ANY_VALUE
 
+    def get_evaluated_keys(self) -> List[str]:
+        return ['Properties/KmsKeyId']
 
-check = SagemakerDomainEncryptedWithCMK()
+
+check = SagemakerNotebookEncryptedWithCMK()
--- tests/cloudformation/checks/resource/aws/test_SagemakerNotebookEncryptedWithCMK.py
+++ tests/cloudformation/checks/resource/aws/test_SagemakerNotebookEncryptedWithCMK.py
@@ -1,28 +1,28 @@
 import os
 import unittest
 
+from checkov.cloudformation.checks.resource.aws.SagemakerNotebookEncryptedWithCMK import check
+from checkov.cloudformation.runner import Runner
 from checkov.runner_filter import RunnerFilter
-from checkov.terraform.checks.resource.aws.SagemakerDomainEncryptedWithCMK import check
-from checkov.terraform.runner import Runner
 
-class TestSagemakerDomainEncryptedWithCMK(unittest.TestCase):
-    def test(self):
+
+class TestSagemakerNotebookEncryptedWithCMK(unittest.TestCase):
+
+    def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_SagemakerDomainEncryptedWithCMK"
-        report = runner.run(
-            root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id])
-        )
+        test_files_dir = current_dir + "/example_SagemakerNotebookEncryptedWithCMK"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
         passing_resources = {
-            "aws_sagemaker_domain.pass",
-            "aws_sagemaker_notebook_instance.pass",
+            "AWS::SageMaker::NotebookInstance.BasicNotebookInstancePASS",
+            "AWS::SageMaker::Domain.BasicDomainPASS"
         }
         failing_resources = {
-            "aws_sagemaker_domain.fail",
-            "aws_sagemaker_notebook_instance.fail",
+            "AWS::SageMaker::NotebookInstance.BasicNotebookInstanceFAIL",
+            "AWS::SageMaker::Domain.BasicDomainFAIL"
         }
 
         passed_check_resources = set([c.resource for c in report.passed_checks])
