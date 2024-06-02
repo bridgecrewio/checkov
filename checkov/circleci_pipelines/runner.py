bc89ac1d233020968f46a61f40c9c8eae1d17fc2
tlevi <tomerlevi1983@gmail.co>
2024-06-02 13:54:24 +0300

    fix(circleci): disable image referencer check on Circleci files

--- checkov/circleci_pipelines/runner.py
+++ checkov/circleci_pipelines/runner.py
@@ -4,17 +4,14 @@ import logging
 import os
 from typing import TYPE_CHECKING, Any, Iterable
 
-from checkov.circleci_pipelines.image_referencer.manager import CircleCIImageReferencerManager
-from checkov.common.images.image_referencer import Image, ImageReferencerMixin
-from checkov.common.output.report import CheckType, Report
 from checkov.circleci_pipelines.registry import registry
-from checkov.common.util.type_forcers import force_dict
+from checkov.common.images.image_referencer import ImageReferencerMixin
+from checkov.common.output.report import CheckType, Report
 from checkov.runner_filter import RunnerFilter
 from checkov.yaml_doc.runner import Runner as YamlRunner
 
 if TYPE_CHECKING:
     from checkov.common.checks.base_check_registry import BaseCheckRegistry
-    from networkx import DiGraph
 
 WORKFLOW_DIRECTORY = "circleci"
 
@@ -89,37 +86,4 @@ class Runner(ImageReferencerMixin["dict[str, dict[str, Any] | list[dict[str, Any
         runner_filter = runner_filter or RunnerFilter()
         report = super().run(root_folder=root_folder, external_checks_dir=external_checks_dir,
                              files=files, runner_filter=runner_filter, collect_skip_comments=collect_skip_comments)
-        if runner_filter.run_image_referencer:
-            if files:
-                # 'root_folder' shouldn't be empty to remove the whole path later and only leave the shortened form
-                root_folder = os.path.split(os.path.commonprefix(files))[0]
-
-            image_report = self.check_container_image_references(
-                root_path=root_folder,
-                runner_filter=runner_filter,
-                definitions=self.definitions
-            )
-
-            if image_report:
-                return [report, image_report]  # type:ignore[list-item]  # report can only be of type Report, not a list
-
         return report
-
-    def extract_images(
-        self,
-        graph_connector: DiGraph | None = None,
-        definitions: dict[str, dict[str, Any] | list[dict[str, Any]]] | None = None,
-        definitions_raw: dict[str, list[tuple[int, str]]] | None = None
-    ) -> list[Image]:
-        images: list[Image] = []
-        if not definitions:
-            return images
-
-        for file_path, config in definitions.items():
-            _config = force_dict(config) or {}
-            if not config:
-                continue
-            manager = CircleCIImageReferencerManager(workflow_config=_config, file_path=file_path)
-            images.extend(manager.extract_images_from_workflow())
-
-        return images
