c41a9a8f144ab847a0bbd27fcce07f40e2c85d41
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2024-06-03 13:13:57 +0300

    experimentaly remove the code changes to test out the test suite

--- checkov/common/checks_infra/solvers/attribute_solvers/base_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/base_attribute_solver.py
@@ -154,8 +154,7 @@ class BaseAttributeSolver(BaseSolver):
     def _evaluate_attribute_matches(
         self, vertex: dict[str, Any], attribute_matches: list[str], filtered_attribute_matches: list[str]
     ) -> bool | None:
-        from common.checks_infra.solvers import NotContainsAttributeSolver
-        if self.is_jsonpath_check or isinstance(self, NotContainsAttributeSolver):
+        if self.is_jsonpath_check:
             if self.resource_type_pred(vertex, self.resource_types) and all(
                 self._get_operation(vertex=vertex, attribute=attr) for attr in filtered_attribute_matches
             ):
