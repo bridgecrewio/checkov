f4e6fb55a04dcf7de21abc4dc1680cc6e2d4d537
userrut <rut83364@gmail.com>
2024-06-04 12:08:40 +0300

    New arm policy change AzureDefenderOnKeyVaults

--- checkov/arm/checks/resource/AzureDefenderOnKeyVaults.py
+++ checkov/arm/checks/resource/AzureDefenderOnKeyVaults.py
@@ -15,16 +15,13 @@ class AzureDefenderOnKeyVaults(BaseResourceCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
-        properties = conf.get('properties')
-        if properties and isinstance(properties, dict):
-            pricing_tier = properties.get("pricingTier")
-            if conf.get("name", [None]) != "KeyVaults" or pricing_tier == "Standard":
-                return CheckResult.PASSED
-            else:
-                return CheckResult.FAILED
+        if conf["properties"]["pricingTier"] == "Standard":
+            return CheckResult.PASSED
+        else:
+            return CheckResult.FAILED
 
     def get_evaluated_keys(self) -> list[str]:
-        return ["name", "properties/pricingTier"]
+        return ["pricingTier"]
 
 
 check = AzureDefenderOnKeyVaults()
