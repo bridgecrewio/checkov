1e3999a4f4201237c3451f9e9575115fdcdc79fe
razreik <razreik@paloaltonetworks.com>
2024-06-04 10:47:38 +0300

    add 6 policies

---
+++ checkov/cloudformation/checks/resource/aws/SagemakerDataQualityJobDefinitionEncryption.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.consts import ANY_VALUE
+from checkov.common.models.enums import CheckCategories
+
+
+class SagemakerDataQualityJobDefinitionEncryption(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure Amazon Sagemaker Data Quality Job uses KMS to encrypt model artifacts"
+        id = "CKV_AWS_380"
+        supported_resources = ["AWS::SageMaker::DataQualityJobDefinition"]
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'Properties/DataQualityJobOutputConfig/KmsKeyId'
+
+    def get_expected_value(self):
+        return ANY_VALUE
+
+    def get_evaluated_keys(self) -> List[str]:
+        return ['Properties/DataQualityJobOutputConfig/KmsKeyId']
+
+
+check = SagemakerDataQualityJobDefinitionEncryption()
---
+++ checkov/cloudformation/checks/resource/aws/SagemakerDataQualityJobDefinitionVolumeEncryption.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.consts import ANY_VALUE
+from checkov.common.models.enums import CheckCategories
+
+
+class SagemakerDataQualityJobDefinitionVolumeEncryption(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure Amazon Sagemaker Data Quality Job uses KMS to encrypt data on attached storage volume"
+        id = "CKV_AWS_381"
+        supported_resources = ["AWS::SageMaker::DataQualityJobDefinition"]
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'Properties/JobResources/ClusterConfig/VolumeKmsKeyId'
+
+    def get_expected_value(self):
+        return ANY_VALUE
+
+    def get_evaluated_keys(self) -> List[str]:
+        return ['Properties/JobResources/ClusterConfig/VolumeKmsKeyId']
+
+
+check = SagemakerDataQualityJobDefinitionVolumeEncryption()
