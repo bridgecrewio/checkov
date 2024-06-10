37a77ea306bcb1d0ae881304015c9b0d23979180
shoshiGit <shoshiwolpe@gmail.com>
2024-06-10 19:55:27 +0300

    StorageAccountName

--- checkov/arm/checks/resource/StorageAccountName.py
+++ checkov/arm/checks/resource/StorageAccountName.py
@@ -8,35 +8,32 @@ from checkov.arm.base_resource_check import BaseResourceCheck
 
 
 STO_NAME_REGEX = re.compile(r"^[a-z0-9]{3,24}$")
-#VARIABLE_REFS = ("local.", "module.", "var.", "random_string.", "random_id.", "random_integer.", "random_pet.",
- #                "azurecaf_name", "each.")
-VARIABLE_REFS = ("variables('", "parameters('", "concat(", "uniqueString(", "resourceGroup().id")
-
+VARIABLE_REFS = ("local.", "module.", "var.", "random_string.", "random_id.", "random_integer.", "random_pet.",
+                 "azurecaf_name", "each.")
 
 
 class StorageAccountName(BaseResourceCheck):
     def __init__(self) -> None:
         """
-          Initializes a check to ensure that Storage Accounts adhere to the naming rules.
+        Initializes a check to ensure that Storage Accounts adhere to the naming rules.
 
-          The naming reference for Storage Accounts can be found here:
-          https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview#naming-storage-accounts
-
-          """
+        The naming reference for Storage Accounts can be found here:
+        https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview#naming-storage-accounts
+        """
         name = "Ensure Storage Accounts adhere to the naming rules"
         id = "CKV_AZURE_43"
-        supported_resources = ['Microsoft.Storage/storageAccounts',]
-        categories = [CheckCategories.CONVENTION,]
+        supported_resources = ['Microsoft.Storage/storageAccounts']
+        categories = [CheckCategories.CONVENTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf: dict[str, typing.Any]) -> CheckResult:
         """
-            The Storage Account naming reference:
-           https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview#naming-storage-accounts
+        The Storage Account naming reference:
+        https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview#naming-storage-accounts
         :param conf: azurerm_storage_account configuration
-       :return: <CheckResult>
+        :return: <CheckResult>
         """
-        #  between 3 and 24 characters may contain numbers and lowercases letters only
+        # between 3 and 24 characters may contain numbers and lowercases letters only
         # unique
 
         if "name" in conf.keys():
