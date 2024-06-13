7e8abc875963e8b67009c007cc2b2a5c825595e5
Chana Libarman <154217669+chani6887@users.noreply.github.com>
2024-06-13 12:11:42 +0300

    Update MariaDBGeoBackupEnabled.py

--- checkov/arm/checks/resource/MariaDBGeoBackupEnabled.py
+++ checkov/arm/checks/resource/MariaDBGeoBackupEnabled.py
@@ -2,6 +2,8 @@ from checkov.common.models.enums import CheckCategories
 from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
 class MariaDBGeoBackupEnabled(BaseResourceValueCheck):
+
+    
     def __init__(self) -> None:
         name = "Ensure that MariaDB server enables geo-redundant backups"
         id = "CKV_AZURE_129"
@@ -14,5 +16,6 @@ class MariaDBGeoBackupEnabled(BaseResourceValueCheck):
 
     def get_expected_value(self) -> str:
         return "Enabled"
+
 
 check = MariaDBGeoBackupEnabled()
