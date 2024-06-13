db0769bd04a5cf6ce3689880bbd97507def35205
Rachel <bb50305030@gmail.com>
2024-06-14 00:58:56 +0300

    feat(Arm) VnetLocalDNS

--- checkov/arm/checks/resource/VnetLocalDNS.py
+++ checkov/arm/checks/resource/VnetLocalDNS.py
@@ -18,28 +18,28 @@ class VnetLocalDNS(BaseResourceCheck):
         categories = [CheckCategories.NETWORKING, ]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+    def scan_resource_conf(self, conf: Dict[str, Dict[str, Dict[str, List[Any]]]]) -> CheckResult:
         if "properties" in conf and "dhcpOptions" in conf["properties"]:
             if "dnsServers" in conf["properties"]["dhcpOptions"]:
                 if isinstance(conf["properties"]["dhcpOptions"]["dnsServers"], list):
                     dns_servers = conf["properties"]["dhcpOptions"]["dnsServers"]
                     if dns_servers:
                         for ip in dns_servers:
-                            if "addressSpace" in conf["properties"] and conf["properties"][
-                                "addressSpace"] and "addressPrefixes" in conf["properties"]["addressSpace"]:
-                                if isinstance(conf["properties"]["addressSpace"]["addressPrefixes"], list):
-                                    address_spaces = conf["properties"]["addressSpace"]["addressPrefixes"]
-                                    if isinstance(address_spaces, list):
-                                        for address_range in address_spaces:
-                                            if not isinstance(address_range, str):
-                                                continue
-                                            try:
-                                                net = ip_network(address_range)
-                                                ip_add = ip_address(ip) if isinstance(ip, str) else None
-                                            except ValueError:
-                                                return CheckResult.UNKNOWN
-                                            if isinstance(ip, str) and ip_add in net:
-                                                return CheckResult.PASSED
+                            if "addressSpace" in conf["properties"] and conf["properties"]["addressSpace"]:
+                                if "addressPrefixes" in conf["properties"]["addressSpace"]:
+                                    if isinstance(conf["properties"]["addressSpace"]["addressPrefixes"], list):
+                                        address_spaces = conf["properties"]["addressSpace"]["addressPrefixes"]
+                                        if isinstance(address_spaces, list):
+                                            for address_range in address_spaces:
+                                                if not isinstance(address_range, str):
+                                                    continue
+                                                try:
+                                                    net = ip_network(address_range)
+                                                    ip_add = ip_address(ip) if isinstance(ip, str) else None
+                                                except ValueError:
+                                                    return CheckResult.UNKNOWN
+                                                if isinstance(ip, str) and ip_add in net:
+                                                    return CheckResult.PASSED
                     self.evaluated_keys = ["dnsServers"]
                     return CheckResult.FAILED
         return CheckResult.PASSED
