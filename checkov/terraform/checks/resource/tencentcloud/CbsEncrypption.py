e99c5a7c7dbe8988d3664fc2f9d870ae8c465723
mikatong <mikatong@tencent.com>
2024-06-14 14:34:47 +0800

    support CBSEncryption

---
+++ checkov/terraform/checks/resource/tencentcloud/CbsEncrypption.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import \
+    BaseResourceValueCheck
+
+
+class CBSEncryption(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure all data stored in the CBS is securely encrypted"
+        id = "CKV_TC_1"
+        supported_resources = ['tencentcloud_cbs_storage']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "encrypt"
+
+    def get_expected_value(self) -> bool:
+        return True
+
+check = CBSEncryption()
