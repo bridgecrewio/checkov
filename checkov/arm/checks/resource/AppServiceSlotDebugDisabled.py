6d60fef70479bab332287fba4b2bc91cc46f8e6a
Rachel <bb50305030@gmail.com>
2024-06-16 14:40:34 +0300

    feat(ARM): AppServiceSlotDebugDisabled CKV_AZURE_155

--- checkov/arm/checks/resource/AppServiceSlotDebugDisabled.py
+++ checkov/arm/checks/resource/AppServiceSlotDebugDisabled.py
@@ -3,7 +3,7 @@ from checkov.common.models.enums import CheckCategories, CheckResult
 
 
 class AppServiceSlotDebugDisabled(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure debugging is disabled for the App service slot"
         id = "CKV_AZURE_155"
         supported_resources = ('Microsoft.Web/sites/slots', 'Microsoft.Web/sites',)
@@ -11,10 +11,10 @@ class AppServiceSlotDebugDisabled(BaseResourceValueCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
                          missing_block_result=CheckResult.PASSED)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "properties/siteConfig/remoteDebuggingEnabled"
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> bool:
         return False
 
 
