534d0862775d3d5f0c311d9e09b372f099cfa463
userrut <rut83364@gmail.com>
2024-06-23 11:54:03 +0300

    A new resource arm policy CosmosDBAccountsRestrictedAccess

---
+++ tests/arm/checks/resource/test_CosmosDBAccountsRestrictedAccess.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+
+import hcl2
+
+from checkov.arm.checks.resource.CosmosDBAccountsRestrictedAccess import check
+from checkov.arm.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+
+class TestCosmosDBAccountsRestrictedAccess(unittest.TestCase):
+
+    def test_summary(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_CosmosDBAccountsRestrictedAccess"
+        report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        passing_resources = {
+            "Microsoft.DocumentDB/databaseAccounts.pass",
+            "Microsoft.DocumentDB/databaseAccounts.pass2",
+            "Microsoft.DocumentDB/databaseAccounts.pass3",
+            "Microsoft.DocumentDB/databaseAccounts.pass4",
+        }
+        failing_resources = {
+            "Microsoft.DocumentDB/databaseAccounts.fail",
+            "Microsoft.DocumentDB/databaseAccounts.fail2",
+            "Microsoft.DocumentDB/databaseAccounts.fail3",
+            "Microsoft.DocumentDB/databaseAccounts.fail4",
+        }
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary['passed'], 4)
+        self.assertEqual(summary['failed'], 4)
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
