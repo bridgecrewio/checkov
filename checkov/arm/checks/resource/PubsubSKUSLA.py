c5dd1d1ac17140a480003238f2df4b54fd5fe68b
shoshiGit <shoshiwolpe@gmail.com>
2024-06-23 14:07:30 +0300

    initial commit

---
+++ checkov/arm/checks/resource/PubsubSKUSLA.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from checkov.arm.base_resource_negative_value_check import BaseResourceNegativeValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class PubsubSKUSLA(BaseResourceNegativeValueCheck):
+    def __init__(self) -> None:
+        name = "Ensure Web PubSub uses a SKU with an SLA"
+        id = "CKV_AZURE_175"
+        supported_resources = ["Microsoft.SignalRService/webPubSub"]
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self) -> str:
+        return "sku/name"
+
+    def get_forbidden_values(self) -> List[str]:
+        return ["Free_F1"]
+
+
+check = PubsubSKUSLA()
