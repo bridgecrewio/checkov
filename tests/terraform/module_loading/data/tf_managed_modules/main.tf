6355b74d0b3ac543ee5ad362c1c05546d265402d
lirshindalman <lirshindelman@gmail.com>
2024-06-27 13:36:19 +0300

    add comment in ths test

--- tests/terraform/module_loading/data/tf_managed_modules/main.tf
+++ tests/terraform/module_loading/data/tf_managed_modules/main.tf
@@ -13,6 +13,7 @@ module "log_group_v4" {
   retention_in_days = 7
 }
 
+#NOTE - THIS IS COMMENTED AS PART OF THE TEST TO CHECK HANDLING OF COMMENTED OUT MODULES. DO NOT DELETE
 # Need to verify this type of comment is not an issue.
 #module "log_group_pound_comment" {
 #  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
