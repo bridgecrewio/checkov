a81645a83cfe5f2a1e7ba38ec337c6148585d91e
Shoshi Wolpe <158214649+shoshiGit@users.noreply.github.com>
2024-06-30 13:37:16 +0300

    Update enum.py

--- checkov/common/comment/enum.py
+++ checkov/common/comment/enum.py
@@ -1,3 +1,3 @@
 import re
 
-COMMENT_REGEX = re.compile(r'(checkov:skip=|bridgecrew:skip=) *([A-Za-z_\d]+(?:,[A-Za-z_\d]+)*)*(:[^\n]+)?')
+COMMENT_REGEX = re.compile(r'(checkov:skip=|bridgecrew:skip=) *([A-Za-z_\d]+)(:[^\n]+)?')
