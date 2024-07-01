30030ebfe6898b482c3e0076d9c64142c1aff876
tehila86127 <tehila86127@gmail.com>
2024-07-01 12:14:19 +0300

    update arm policy for resource: APIManagementPublicAccess

--- checkov/arm/checks/resource/APIManagementPublicAccess.py
+++ checkov/arm/checks/resource/APIManagementPublicAccess.py
@@ -1,30 +1,22 @@
-from __future__ import annotations
-
 from typing import Any
 
-from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.arm.base_resource_negative_value_check import BaseResourceNegativeValueCheck
+from checkov.common.models.enums import CheckCategories
+from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
 
-class APIManagementPublicAccess(BaseResourceNegativeValueCheck):
+class APIManagementPublicAccess(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure API management public access is disabled"
         id = "CKV_AZURE_174"
         supported_resources = ("Microsoft.ApiManagement/service",)
         categories = (CheckCategories.NETWORKING,)
-        super().__init__(
-            name=name,
-            id=id,
-            categories=categories,
-            supported_resources=supported_resources,
-            missing_block_result=CheckResult.FAILED,
-        )
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
         return "properties/publicNetworkAccess"
 
-    def get_forbidden_values(self) -> Any:
-        return "Enabled"
+    def get_expected_value(self) -> Any:
+        return "Disabled"
 
 
 check = APIManagementPublicAccess()
