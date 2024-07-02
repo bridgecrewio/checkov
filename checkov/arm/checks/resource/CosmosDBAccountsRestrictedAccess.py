34d61f37c139051c3925bbd5827d47a93d36cdd0
userrut <rut83364@gmail.com>
2024-07-02 10:49:11 +0300

    New change to CosmosDBAccountsRestrictedAccess resource pool policy

--- checkov/arm/checks/resource/CosmosDBAccountsRestrictedAccess.py
+++ checkov/arm/checks/resource/CosmosDBAccountsRestrictedAccess.py
@@ -1,4 +1,4 @@
-from typing import Dict,Any,Optional
+from typing import Dict, Any, Optional
 
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.arm.base_resource_check import BaseResourceCheck
@@ -14,17 +14,18 @@ class CosmosDBAccountsRestrictedAccess(BaseResourceCheck):
 
     def scan_resource_conf(self, conf: Dict[str, Any]) -> CheckResult:
         properties: Optional[Dict[str, Any]] = conf.get('properties')
-        if 'enableMultipleWriteLocations' not in properties or properties['enableMultipleWriteLocations']:
-            self.evaluated_keys = ['enableMultipleWriteLocations']
-            if 'isVirtualNetworkFilterEnabled' in properties and properties['isVirtualNetworkFilterEnabled']:
-                self.evaluated_keys.append('isVirtualNetworkFilterEnabled')
-                if 'virtualNetworkRules' in properties and properties['virtualNetworkRules']:
-                    self.evaluated_keys.append('virtualNetworkRules')
-                    return CheckResult.PASSED
-                if 'ipRules' in properties and properties['ipRules']:
-                    self.evaluated_keys.append('ipAddressOrRange')
-                    return CheckResult.PASSED
-            return CheckResult.FAILED
+        if properties is not None:
+            if 'enableMultipleWriteLocations' not in properties or properties['enableMultipleWriteLocations']:
+                self.evaluated_keys = ['enableMultipleWriteLocations']
+                if 'isVirtualNetworkFilterEnabled' in properties and properties['isVirtualNetworkFilterEnabled']:
+                    self.evaluated_keys.append('isVirtualNetworkFilterEnabled')
+                    if 'virtualNetworkRules' in properties and properties['virtualNetworkRules']:
+                        self.evaluated_keys.append('virtualNetworkRules')
+                        return CheckResult.PASSED
+                    if 'ipRules' in properties and properties['ipRules']:
+                        self.evaluated_keys.append('ipAddressOrRange')
+                        return CheckResult.PASSED
+                return CheckResult.FAILED
         return CheckResult.PASSED
 
 
