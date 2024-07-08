2ffa361ff274cc0a53aec385e97dca163740de51
Tomer Aviassaf <taviassaf@paloaltonetworks.com>
2024-07-08 15:02:44 +0300

    add cmk check and SQLPoolDataEncription check

---
+++ checkov/arm/checks/resource/SynapseWorkspaceCMKEncryption.py
@@ -0,0 +1,24 @@
+
+from checkov.arm.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+
+class SynapseWorkspaceCMKEncryption(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure Azure Synapse Workspace is encrypted with a CMK"
+        id = "CKV_AZURE_239"
+        supported_resources = ['Microsoft.Synapse/workspaces']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf) -> CheckResult:
+        encryption = conf.get("properties", {}).get("encryption", {})
+        
+        if "cmk" in encryption:
+            return CheckResult.PASSED
+        
+        return CheckResult.FAILED
+
+
+
+check = SynapseWorkspaceCMKEncryption()
