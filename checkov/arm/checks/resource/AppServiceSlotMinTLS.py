de9659ab14b0f3983a9349e2d068130a45e88c50
Rachel <bb50305030@gmail.com>
2024-07-18 21:14:34 +0300

    type return

--- checkov/arm/checks/resource/AppServiceSlotMinTLS.py
+++ checkov/arm/checks/resource/AppServiceSlotMinTLS.py
@@ -3,7 +3,7 @@ from checkov.common.models.enums import CheckCategories, CheckResult
 
 
 class AppServiceSlotMinTLS(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure the App service slot is using the latest version of TLS encryption"
         id = "CKV_AZURE_154"
         supported_resources = ("Microsoft.Web/sites", "Microsoft.Web/sites/slots")
@@ -11,10 +11,10 @@ class AppServiceSlotMinTLS(BaseResourceValueCheck):
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
                          missing_block_result=CheckResult.PASSED)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "properties/siteConfig/minTlsVersion"
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> str:
         return '1.2'
 
 
