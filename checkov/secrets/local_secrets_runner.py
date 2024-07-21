006ffa7c6045e26332ff980666c163629445bb91
pazbechor <pbechor@paloaltonetworks.com>
2024-07-21 16:19:46 +0300

    Add local debugger

---
+++ checkov/secrets/local_secrets_runner.py
@@ -0,0 +1,32 @@
+import json
+from checkov.main import secrets_runner
+from checkov.common.runners.runner_registry import RunnerRegistry
+from checkov.runner_filter import RunnerFilter
+from checkov.common.bridgecrew.platform_integration import bc_integration
+
+with open("/Users/pbechor/development/platform/src/microStacks/secretScanningStack/src/test/python/secret_checkov_scanner/secrets_policies.json") as secrets_policies_file:
+    default_regexes = json.load(secrets_policies_file)
+bc_integration.customer_run_config_response = {'secretsPolicies': default_regexes}
+
+def execute():
+    runner = secrets_runner(entropy_limit=4)
+    # 20 min less in order to finish processing, else put checkov's default (12h - 1200)
+
+
+
+    runner_registry = RunnerRegistry('', RunnerFilter(block_list_secret_scan=[],
+                                                          enable_secret_scan_all_files=True,
+                                                          enable_git_history_secret_scan=False,
+                                                          git_history_last_commit_scanned=None,
+                                                          git_history_timeout="checkov_timeout_str",
+                                                          checks=['BC_GIT_79']),
+                                         runner)
+
+    scan_reports = runner_registry.run(
+        root_folder="DESIRED_SCANNED_DIRECTORY",
+        external_checks_dir=[],
+        collect_skip_comments=True)
+
+
+if __name__ == "__main__":
+    execute()
