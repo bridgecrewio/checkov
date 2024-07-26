55fd0d4c98b67fc450807628aa545acd454b60f7
Taylor <tsmith.v11@gmail.com>
2024-07-26 15:20:36 -0700

    Support upper and lower

--- checkov/arm/checks/resource/KeyVaultDisablesPublicNetworkAccess.py
+++ checkov/arm/checks/resource/KeyVaultDisablesPublicNetworkAccess.py
@@ -21,7 +21,7 @@ class KeyVaultDisablesPublicNetworkAccess(BaseResourceValueCheck):
         properties = conf.get("properties", {})
         if self.get_inspected_key() in properties:
             conf_value = conf["properties"][self.get_inspected_key()]
-            if conf_value and conf_value == self.get_expected_value():
+            if conf_value and conf_value.lower() == self.get_expected_value():
                 return CheckResult.PASSED
 
         if properties and "networkAcls" in properties:
