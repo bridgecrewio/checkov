bba3e8ff1a11ac238eca493999b58d7275d1db48
esterKoren <150907204+esterKoren@users.noreply.github.com>
2024-07-29 15:48:44 +0300

    Update AzureDefenderDisabledForResManager.py

--- checkov/arm/checks/resource/AzureDefenderDisabledForResManager.py
+++ checkov/arm/checks/resource/AzureDefenderDisabledForResManager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict
+from typing import Any
 
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.arm.base_resource_check import BaseResourceCheck
