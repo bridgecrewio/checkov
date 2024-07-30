d47545039a0f849c26dd3410512df7dc3b6910c3
pninib <p7624859@gmail.com>
2024-07-30 22:50:58 +0300

    first

---
+++ checkov/arm/checks/resource/AzureContainerInstanceEnvVarSecureValueType.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import Any
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+import itertools
+
+
+class AzureContainerInstanceEnvVarSecureValueType(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure that Azure container environment variables are configured with secure values only"
+        id = "CKV_AZURE_235"
+        supported_resources = ("Microsoft.ContainerInstance/containerGroups",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
+        containers = itertools.chain(
+            conf.get('properties', {}).get('containers', []),
+            conf.get('properties', {}).get('initContainers', [])
+        )
+        for container in containers:
+            env_vars = container.get('properties', {}).get('environmentVariables', [])
+            if env_vars is not None and any('value' in env_var for env_var in env_vars):
+                return CheckResult.FAILED
+        return CheckResult.PASSED
+
+
+check = AzureContainerInstanceEnvVarSecureValueType()
---
+++ tests/arm/checks/resource/test_AzureContainerInstanceEnvVarSecureValueType.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+from checkov.runner_filter import RunnerFilter
+from checkov.arm.runner import Runner
+from checkov.arm.checks.resource.AzureContainerInstanceEnvVarSecureValueType import check
+
+
+class TestAzureContainerInstanceEnvVarSecureValueType(unittest.TestCase):
+    def test_summary(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        test_files_dir = os.path.join(current_dir, "example_AzureContainerInstanceEnvVarSecureValueType")
+
+        report = Runner().run(root_folder=str(test_files_dir), runner_filter=RunnerFilter(checks=[check.id]))
+
+        summary = report.get_summary()
+
+        passing_resources = {
+            'Microsoft.ContainerInstance/containerGroups.pass_1',
+            'Microsoft.ContainerInstance/containerGroups.pass_2',
+        }
+        failing_resources = {
+            'Microsoft.ContainerInstance/containerGroups.fail_1',
+            'Microsoft.ContainerInstance/containerGroups.fail_2',
+        }
+        skipped_resources = {}
+
+        passed_check_resources = {c.resource for c in report.passed_checks}
+        failed_check_resources = {c.resource for c in report.failed_checks}
+
+        self.assertEqual(summary['passed'], len(passing_resources))
+        self.assertEqual(summary['failed'], len(failing_resources))
+        self.assertEqual(summary['skipped'], len(skipped_resources))
+        self.assertEqual(summary['parsing_errors'], 1)
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
+
+
+if __name__ == '__main__':
+    unittest.main()
