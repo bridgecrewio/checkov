774830f4049050210ee9b8461051c1d935f93451
Austin Bakos <Austin.Bakos@libertymutual.com>
2024-08-13 11:38:19 -0400

    Checking types for KmsMasterKeyId and SqsManagedSseEnabled and default SqsManagedSseEnabled true if nether passed in

--- checkov/cloudformation/checks/resource/aws/SQSQueueEncryption.py
+++ checkov/cloudformation/checks/resource/aws/SQSQueueEncryption.py
@@ -15,6 +15,23 @@ class SQSQueueEncryption(BaseResourceCheck):
         kms_key_id_value = properties.get("KmsMasterKeyId")
         sse_enabled_value = properties.get("SqsManagedSseEnabled")
 
+        if sse_enabled_value:
+            if isinstance(sse_enabled_value, str):
+                if sse_enabled_value.lower() == "true":
+                    sse_enabled_value = True
+                elif sse_enabled_value.lower() == "false":
+                    sse_enabled_value = False
+                else:
+                    return CheckResult.UNKNOWN
+            elif not isinstance(sse_enabled_value, bool):
+                return CheckResult.UNKNOWN
+        elif isinstance(kms_key_id_value, str):
+            # New SQS queues have SSE enabled by default
+            sse_enabled_value = True
+
+        if kms_key_id_value and not isinstance(kms_key_id_value, str):
+            return CheckResult.UNKNOWN
+
         if kms_key_id_value or sse_enabled_value:
             return CheckResult.PASSED
         return CheckResult.FAILED
--- tests/cloudformation/checks/resource/aws/test_SQSQueueEncryption.py
+++ tests/cloudformation/checks/resource/aws/test_SQSQueueEncryption.py
@@ -16,7 +16,7 @@ class TestSQSQueueEncryption(unittest.TestCase):
         report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
-        self.assertEqual(summary['passed'], 2)
+        self.assertEqual(summary['passed'], 4)
         self.assertEqual(summary['failed'], 3)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
