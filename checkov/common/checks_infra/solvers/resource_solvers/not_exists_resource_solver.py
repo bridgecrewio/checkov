8d9dffda4f7debe58c8b276fb16dfa3b85ae839e
iifrach <iifrach@paloaltonetworks.com>
2024-08-14 11:31:30 +0300

    Change return type

--- checkov/common/checks_infra/solvers/resource_solvers/not_exists_resource_solver.py
+++ checkov/common/checks_infra/solvers/resource_solvers/not_exists_resource_solver.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Union
+from typing import Any, Optional
 
 from checkov.common.checks_infra.solvers.resource_solvers.exists_resource_solver import ExistsResourcerSolver
 from checkov.common.graph.checks_infra.enums import Operators
@@ -9,6 +9,6 @@ from checkov.common.graph.checks_infra.enums import Operators
 class NotExistsResourcerSolver(ExistsResourcerSolver):
     operator = Operators.NOT_EXISTS  # noqa: CCE003  # a static attribute
 
-    def get_operation(self, *args: Any, **kwargs: Any) -> Union[bool, None]:
+    def get_operation(self, *args: Any, **kwargs: Any) -> Optional[bool]:
         not_exists = not super().get_operation(*args, **kwargs)
         return not_exists if not not_exists else None
