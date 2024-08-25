198073b17ef654f3bb6521cf00bbd0de08d1b8de
Max Amelchenko <mamelchenko@paloaltonetworks.com>
2024-08-25 12:54:00 +0300

    fix tests

--- tests/terraform/checks/data/aws/test_CloudSplainingCredentialsExposure.py
+++ tests/terraform/checks/data/aws/test_CloudSplainingCredentialsExposure.py
@@ -35,7 +35,7 @@ class TestcloudsplainingPrivilegeEscalation(unittest.TestCase):
         self.assertEqual(summary["skipped"], 0)
         self.assertEqual(summary["parsing_errors"], 0)
 
-        self.assertEqual(['statement/[0]/actions/[0]/'], report.failed_checks[0].check_result.get('evaluated_keys'))
+        self.assertEqual(['statement/[0]/actions/[1]/'], report.failed_checks[0].check_result.get('evaluated_keys'))
 
         self.assertEqual(passing_resources, passed_check_resources)
         self.assertEqual(failing_resources, failed_check_resources)
