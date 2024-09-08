3e4f1bba4afa53c8d6752918227808af85170f6d
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2024-09-08 13:43:34 +0300

    fix(terraform): Fix foreach multi attributes in field (#6707)
    Fix(terraform): Fix foreach multi attributes in field

---
+++ tests/terraform/graph/variable_rendering/test_resources/dynamic_blocks_tfvars_merge/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "vmhosts" {
+  description = "VM hosts with configuration"
+  type = list(object({
+    name           = string
+    tags           = map(string)
+    private_ip     = string
+    ports          = list(number)
+  }))
+}
