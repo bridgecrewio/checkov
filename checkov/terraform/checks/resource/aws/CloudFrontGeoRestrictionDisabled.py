178b4a6f25dd426b6baed7f2041775d15ba6c62e
Taylor <tsmith.v11@gmail.com>
2024-09-12 00:14:04 -0700

    Add types and change ID

--- checkov/terraform/checks/resource/aws/CloudFrontGeoRestrictionDisabled.py
+++ checkov/terraform/checks/resource/aws/CloudFrontGeoRestrictionDisabled.py
@@ -4,17 +4,17 @@ from checkov.terraform.checks.resource.base_resource_negative_value_check import
 
 class CloudFrontGeoRestrictionDisabled(BaseResourceNegativeValueCheck):
 
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Ensure AWS CloudFront web distribution has geo restriction enabled"
-        id = "CKV_AWS_799"
-        supported_resources = ['aws_cloudfront_distribution']
+        id = "CKV_AWS_374"
+        supported_resources = ('aws_cloudfront_distribution',)
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return "restrictions/[0]/geo_restriction/[0]/restriction_type"
 
-    def get_forbidden_values(self):
+    def get_forbidden_values(self) -> list:
         return ["none"]
 
 
--- checkov/terraform/checks/resource/azure/StorageLocalUsers.py
+++ checkov/terraform/checks/resource/azure/StorageLocalUsers.py
@@ -3,17 +3,17 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 
 class StorageLocalUsers(BaseResourceValueCheck):
-    def __init__(self):
+    def __init__(self) -> None:
         name = "Avoid the use of local users for Azure Storage unless necessary"
-        id = "CKV_AZURE_999"
-        supported_resources = ['azurerm_storage_account']
+        id = "CKV_AZURE_244"
+        supported_resources = ('azurerm_storage_account',)
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self):
+    def get_inspected_key(self) -> str:
         return 'local_user_enabled'
 
-    def get_expected_value(self):
+    def get_expected_value(self) -> bool:
         return False
 
 
