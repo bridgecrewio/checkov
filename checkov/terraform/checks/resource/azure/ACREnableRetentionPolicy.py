0d1a54fc7f57e5b7fb34b312fd6250cf88673257
paddymorgan84 <patrickmorgan1984@gmail.com>
2024-10-08 14:27:10 +0000

    fix: Update CKV_AZURE_167 to correct check on retention policy

--- checkov/terraform/checks/resource/azure/ACREnableRetentionPolicy.py
+++ checkov/terraform/checks/resource/azure/ACREnableRetentionPolicy.py
@@ -1,5 +1,7 @@
 from checkov.common.models.enums import CheckCategories
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from typing import Any
+from checkov.common.models.consts import ANY_VALUE
 
 
 class ACREnableRetentionPolicy(BaseResourceValueCheck):
@@ -10,8 +12,11 @@ class ACREnableRetentionPolicy(BaseResourceValueCheck):
         categories = (CheckCategories.GENERAL_SECURITY,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self) -> str:
-        return "retention_policy/enabled"
+    def get_inspected_key(self):
+        return "retention_policy_in_days"
+
+    def get_expected_value(self) -> Any:
+        return ANY_VALUE
 
 
 check = ACREnableRetentionPolicy()
