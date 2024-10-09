02539e2036ad7884c533c5b33b39b79a2781866a
Rotem Avni <ravni@paloaltonetworks.com>
2024-10-09 18:34:07 +0300

    increase timeout

--- checkov/terraform/tf_parser.py
+++ checkov/terraform/tf_parser.py
@@ -724,7 +724,7 @@ def load_or_die_quietly(
             if file_name.endswith(".json"):
                 return cast("_Hcl2Payload", json.load(f))
             else:
-                parsing_timeout = int(os.getenv("HCL_PARSE_TIMEOUT_SEC", "1"))
+                parsing_timeout = int(os.getenv("HCL_PARSE_TIMEOUT_SEC", "10"))
 
                 def timeout_handler(signum, frame) -> None:  # type: ignore
                     logging.debug(f"reached timeout when parsing file {file} using hcl2")
