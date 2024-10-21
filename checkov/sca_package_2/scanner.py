b817b5483b09bf8b082aec9635a8786c521e837b
oshimko <oshimko@paloaltonetworks.com>
2024-10-21 15:18:26 +0300

    use EnvVarsConfig

--- checkov/sca_package_2/scanner.py
+++ checkov/sca_package_2/scanner.py
@@ -9,6 +9,7 @@ from typing import Any
 from requests import JSONDecodeError
 
 from checkov.common.bridgecrew.platform_integration import bc_integration
+from checkov.common.util.env_vars_config import env_vars_config
 from checkov.common.util.http_utils import request_wrapper
 
 from checkov.common.util.tqdm_utils import ProgressBar
@@ -45,7 +46,7 @@ class Scanner:
                 "repoId": bc_integration.repo_id,
                 "id": bc_integration.timestamp,
                 "repositoryId": "",
-                "enableDotnetCpm": os.getenv('ENABLE_DOTNET_CPM', 'False'),
+                "enableDotnetCpm": env_vars_config.ENABLE_DOTNET_CPM,
             }
 
             response = request_wrapper(
