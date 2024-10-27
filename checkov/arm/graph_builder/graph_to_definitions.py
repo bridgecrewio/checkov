db76b3e7aaa2cf2e1cf22de4b4962dce277b5162
Omri Yoffe <oyoffe@paloaltonetworks.com>
2024-10-27 10:14:06 +0200

    fix function name

--- checkov/arm/graph_builder/graph_to_definitions.py
+++ checkov/arm/graph_builder/graph_to_definitions.py
@@ -6,7 +6,7 @@ from typing import Any
 from checkov.arm.graph_builder.graph_components.blocks import ArmBlock
 
 
-def convert_graph_vertices_to_tf_definitions(
+def convert_graph_vertices_to_definitions(
     vertices: list[ArmBlock], root_folder: str | Path | None
 ) -> tuple[dict[Path, Any], dict[str, dict[str, Any]]]:
     # TODO
