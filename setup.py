8703acf4b3b8b8e3e9455def2fd609a66b6f90c1
tlevi <tomerlevi1983@gmail.co>
2024-11-12 13:40:19 +0200

    fix(pipfile) - revert back to specific botocore version

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         "junit-xml>=1.9,<2.0",
         "dpath==2.1.3",
         "pyyaml<7.0.0,>=6.0.0",
-        "boto3==1.35.49",
+        "boto3==1.34.25",
         "gitpython>=3.1.30,<4.0.0",
         "jmespath>=1.0.0,<2.0.0",
         "tqdm<5.0.0,>=4.65.0",
@@ -108,7 +108,7 @@ setup(
         "license-expression<31.0.0,>=30.1.0",
         "rustworkx>=0.13.0,<0.14.0",
         "pydantic<3.0.0,>=2.0.0",
-        "urllib3>=1.26.20,<2.0.0"
+        "urllib3",
     ],
     dependency_links=[],  # keep it empty, needed for pipenv-setup
     license="Apache License 2.0",
