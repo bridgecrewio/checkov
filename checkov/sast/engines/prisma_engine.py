44d28be48ec6d9fef9f60f8a8d2540e6c0454fb9
Barak Fatal <barakf156@gmail.com>
2024-11-13 15:22:58 +0200

    Used easier fix

--- checkov/sast/engines/prisma_engine.py
+++ checkov/sast/engines/prisma_engine.py
@@ -308,12 +308,12 @@ class PrismaEngine(SastEngine):
 
     @staticmethod
     def remove_none_conf_incidents_policies(data: Dict[str, Any]) -> None:
-        remove_list: List[Tuple[Any, Any]] = []
+        remove_list = []
         for lang, match in data.get('rule_match', dict()).items():
             for check in match.keys():
-                if bc_integration.customer_run_config_response is not None\
-                        and check not in bc_integration.customer_run_config_response['policyMetadata']:
-                    remove_list.append((lang, check))
+                if bc_integration.customer_run_config_response:
+                    if check not in bc_integration.customer_run_config_response.get('policyMetadata', []):
+                        remove_list.append((lang, check))
 
         for lang, check in remove_list:
             del data['rule_match'][lang][check]
