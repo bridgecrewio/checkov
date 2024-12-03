d091456f69d2e69d476d2e8fe9d282e803ba3820
Steve Vaknin <svaknin@paloaltonetworks.com>
2024-12-03 18:37:12 +0200

    Fix(kubernetes): fix json parsing

--- checkov/kubernetes/parser/k8_json.py
+++ checkov/kubernetes/parser/k8_json.py
@@ -1,18 +1,13 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Hashable
 from pathlib import Path
-from typing import Tuple, Dict, Any, List, TYPE_CHECKING
+from typing import Tuple, Dict, Any, List
 
-import yaml
-from yaml.loader import SafeLoader
-
+import json
+from checkov.common.parsers.json.decoder import SimpleDecoder
 from checkov.common.resource_code_logger_filter import add_resource_code_filter_to_logger
 from checkov.common.util.file_utils import read_file_with_any_encoding
-
-if TYPE_CHECKING:
-    from yaml import MappingNode
 
 logger = logging.getLogger(__name__)
 add_resource_code_filter_to_logger(logger)
@@ -20,16 +15,16 @@ add_resource_code_filter_to_logger(logger)
 
 def loads(content: str) -> list[dict[str, Any]]:
     """
-    Load the given YAML string
+    Load the given JSON string
     """
 
     content = "[" + content + "]"
     content = content.replace('}{', '},{')
     content = content.replace('}\n{', '},\n{')
 
-    template: list[dict[str, Any]] = yaml.load(content, Loader=SafeLineLoader)  # nosec  # custom safe loader
+    template: list[dict[str, Any]] = json.loads(content, cls=SimpleDecoder)
 
-    # Convert an empty file to an empty dict
+    # Convert an empty file to an empty list
     if template is None:
         template = []
 
@@ -51,13 +46,3 @@ def load(filename: Path) -> Tuple[List[Dict[str, Any]], List[Tuple[int, str]]]:
     template = loads(content)
 
     return template, file_lines
-
-
-class SafeLineLoader(SafeLoader):
-    def construct_mapping(self, node: MappingNode, deep: bool = False) -> dict[Hashable, Any]:
-        mapping = super().construct_mapping(node, deep=deep)
-        # Add 1 so line numbering starts at 1
-        # mapping['__line__'] = node.start_mark.line + 1
-        mapping['__startline__'] = node.start_mark.line + 1
-        mapping['__endline__'] = node.end_mark.line + 1
-        return mapping
