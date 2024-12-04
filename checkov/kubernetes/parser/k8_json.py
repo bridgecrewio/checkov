3554127daa284e078ac0488f82f79744f14c6a57
Steve Vaknin <svaknin@paloaltonetworks.com>
2024-12-04 16:14:16 +0200

    fix reintoduce parts

--- checkov/kubernetes/parser/k8_json.py
+++ checkov/kubernetes/parser/k8_json.py
@@ -1,14 +1,21 @@
 from __future__ import annotations
 
 import logging
+from collections.abc import Hashable
 from pathlib import Path
 from typing import Tuple, Dict, Any, List
+from typing import Tuple, Dict, Any, List, TYPE_CHECKING
 
 import json
+import yaml
+from yaml.loader import SafeLoader
 from checkov.common.parsers.json.decoder import SimpleDecoder
 from checkov.common.resource_code_logger_filter import add_resource_code_filter_to_logger
-from checkov.common.util.consts import END_LINE, START_LINE
+from checkov.common.util.consts import END_LINE, START_LINE, MAX_IAC_FILE_SIZE
 from checkov.common.util.file_utils import read_file_with_any_encoding
+
+if TYPE_CHECKING:
+    from yaml import MappingNode
 
 logger = logging.getLogger(__name__)
 add_resource_code_filter_to_logger(logger)
@@ -43,10 +50,31 @@ def load(filename: Path) -> Tuple[List[Dict[str, Any]], List[Tuple[int, str]]]:
         return [{}], []
 
     file_lines = [(idx + 1, line) for idx, line in enumerate(content.splitlines(keepends=True))]
+    file_size = len(content)
 
+    if file_size > MAX_IAC_FILE_SIZE:
+        # large JSON files take too much time, when parsed with `pyyaml`, compared to a normal 'json.loads()'
+        # with start/end line numbers of 0 takes only a few seconds
+        logging.info(
+            f"File {filename} has a size of {file_size} which is bigger than the supported 50mb, "
+            "therefore file lines will default to 0."
+            "This limit can be adjusted via the environment variable 'CHECKOV_MAX_IAC_FILE_SIZE'."
+        )
+        return json.loads(content, cls=CustomDecoder), file_lines
+    
     template = loads(content)
 
     return template, file_lines
+
+
+class SafeLineLoader(SafeLoader):
+    def construct_mapping(self, node: MappingNode, deep: bool = False) -> dict[Hashable, Any]:
+        mapping = super().construct_mapping(node, deep=deep)
+        # Add 1 so line numbering starts at 1
+        # mapping['__line__'] = node.start_mark.line + 1
+        mapping[START_LINE] = node.start_mark.line + 1
+        mapping[END_LINE] = node.end_mark.line + 1
+        return mapping
 
 
 class CustomDecoder(SimpleDecoder):
