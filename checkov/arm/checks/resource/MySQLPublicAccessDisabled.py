3afec0ec2a3a371b56861e1f9af57ffee1d9949a
Taylor <tsmith.v11@gmail.com>
2024-12-13 23:11:48 -0800

    add flexible servers

--- checkov/arm/checks/resource/MySQLPublicAccessDisabled.py
+++ checkov/arm/checks/resource/MySQLPublicAccessDisabled.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from checkov.common.models.enums import CheckCategories
 from checkov.arm.base_resource_value_check import BaseResourceValueCheck
 
@@ -6,18 +8,21 @@ class MySQLPublicAccessDisabled(BaseResourceValueCheck):
     def __init__(self) -> None:
         name = "Ensure 'public network access enabled' is set to 'False' for mySQL servers"
         id = "CKV_AZURE_53"
-        supported_resources = ("Microsoft.DBforMySQL/servers",)
+        supported_resources = ("Microsoft.DBforMySQL/servers","Microsoft.DBforMySQL/flexibleServers")
         categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self) -> str:
-        return "properties/publicNetworkAccess"
+        if self.entity_type == "Microsoft.DBforMySQL/servers":
+            return "properties/publicNetworkAccess"
+        else:
+            return "properties/network/publicNetworkAccess"
 
     def get_expected_value(self) -> str:
-        """
-        Returns the default expected value, governed by provider best practices
-        """
         return "disabled"
+
+    def get_expected_values(self) -> List[str]:
+        return ["disabled", "Disabled"]
 
 
 check = MySQLPublicAccessDisabled()
