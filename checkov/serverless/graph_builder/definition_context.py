1bfa487aa292122b375d65c0648bf2e0a7b6fe07
Omri Yoffe <oyoffe@paloaltonetworks.com>
2024-12-17 17:25:48 +0200

    different types line numbers

--- checkov/serverless/graph_builder/definition_context.py
+++ checkov/serverless/graph_builder/definition_context.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import cast, Any
 
-from checkov.common.parsers.node import StrNode
-from checkov.common.util.consts import START_LINE, END_LINE
+from checkov.common.parsers.node import StrNode, ListNode
+from checkov.common.util.consts import START_LINE, END_LINE, LINE_FIELD_NAMES
 from checkov.common.util.suppression import collect_suppressions_for_report
 from checkov.serverless.utils import ServerlessElements
 
@@ -34,35 +34,38 @@ def build_definitions_context(definitions: dict[str, dict[str, Any]], definition
 
 
 def add_resource_to_definitions_context(definitions_context: dict[str, dict[str, Any]], resource_key: str,
-                                        resource_attributes: dict[str, Any] | StrNode, definition_attribute: str,
+                                        resource_attributes: dict[str, Any] | ListNode | StrNode, definition_attribute: str,
                                         definitions_raw: dict[str, Any], file_path: str) -> None:
-    if not isinstance(resource_attributes, dict) and not isinstance(resource_attributes, StrNode):
+    if resource_key in LINE_FIELD_NAMES:
         return
 
-    start_line = None
-    end_line = None
-
     if isinstance(resource_attributes, dict):
-        start_line = resource_attributes[START_LINE]
-        end_line = resource_attributes[END_LINE]
+        start_line = resource_attributes[START_LINE] - 1
+        end_line = resource_attributes[END_LINE] - 1
 
-    elif isinstance(resource_attributes, StrNode):
+    elif isinstance(resource_attributes, ListNode):
         start_line = resource_attributes.start_mark.line
         end_line = resource_attributes.end_mark.line
 
-    definition_resource = {}
+    elif isinstance(resource_attributes, StrNode):
+        start_line = resource_attributes.start_mark.line + 1
+        end_line = resource_attributes.end_mark.line + 1
+
+    else:
+        return
+
+    definition_resource = {"start_line": start_line, "end_line": end_line}
 
     if resource_key is None and isinstance(resource_attributes, dict):
         resource_key = f"{resource_attributes.get('type')}.{resource_attributes.get('name')}"
-    int_start_line = cast(int, start_line)
-    int_end_line = cast(int, end_line)
+    int_start_line = cast(int, definition_resource["start_line"])
+    int_end_line = cast(int, definition_resource["end_line"])
     code_lines_for_suppressions_check = definitions_raw[file_path][int_start_line: int_end_line]
     definition_resource['skipped_checks'] = collect_suppressions_for_report(
         code_lines=code_lines_for_suppressions_check)
     if isinstance(resource_attributes, dict) and 'type' in resource_attributes:
         definition_resource["type"] = resource_attributes.get('type')
 
-    definition_resource["code_lines"] = definitions_raw[file_path][start_line: end_line + 1]
-    definition_resource["start_line"] = start_line + 1
-    definition_resource["end_line"] = end_line + 1
+    definition_resource["code_lines"] = definitions_raw[file_path][start_line - 1: end_line]
+
     definitions_context[file_path][definition_attribute][resource_key] = definition_resource
