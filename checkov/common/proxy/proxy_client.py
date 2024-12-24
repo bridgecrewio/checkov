db248daf60d6ffeefecf36c4ada9bc96eeb6a46d
lirshindalman <lirshindelman@gmail.com>
2024-12-24 14:16:41 +0200

    .

--- checkov/common/proxy/proxy_client.py
+++ checkov/common/proxy/proxy_client.py
@@ -3,17 +3,19 @@ from typing import Any, Mapping
 
 import requests
 
+from checkov.common.util.env_vars_config import env_vars_config
+
 
 class ProxyClient:
     def __init__(self) -> None:
-        self.proxy_ca_path = os.getenv('PROXY_CA_PATH', None)
+        self.proxy_ca_path = env_vars_config.PROXY_CA_PATH
         if self.proxy_ca_path is None:
             raise Exception("[ProxyClient] CA certificate path is missing")
 
     def get_session(self) -> requests.Session:
-        if not os.getenv('PROXY_URL', None):
+        if not env_vars_config.PROXY_URL:
             raise Exception('Please provide "PROXY_URL" env var')
-        proxy_url = os.getenv('PROXY_URL')
+        proxy_url = env_vars_config.PROXY_URL
         session = requests.Session()
         proxies = {
             "http": proxy_url,
@@ -34,10 +36,9 @@ def call_http_request_with_proxy(request: requests.Request) -> Any:
 
 
 def get_proxy_envs() -> Mapping[str, str] | None:
+    proxy_env = os.environ.copy()
     if os.getenv('PROXY_URL'):
-        proxy_env = os.environ.copy()
-        proxy_env["GIT_SSL_CAINFO"] = os.getenv('PROXY_CA_PATH')  # Path to the CA cert
-        proxy_env["http_proxy"] = os.getenv('PROXY_URL')  # Proxy URL
-        proxy_env["https_proxy"] = os.getenv('PROXY_URL')  # HTTPS Proxy URL (if needed)
-        return proxy_env
-    return None
+        proxy_env["GIT_SSL_CAINFO"] = env_vars_config.PROXY_CA_PATH # Path to the CA cert
+        proxy_env["http_proxy"] = env_vars_config.PROXY_URL  # Proxy URL
+        proxy_env["https_proxy"] = env_vars_config.PROXY_URL  # HTTPS Proxy URL (if needed)
+    return proxy_env
