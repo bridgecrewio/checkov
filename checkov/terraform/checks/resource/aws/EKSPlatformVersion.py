bce1c3490d6c55ecd9a0841f3818585a78567c69
Kyle Hodgetts <kyle.hodgetts@starlingbank.com>
2025-01-31 13:56:45 +0000

    Remove 1.24 as its support has run out

--- checkov/terraform/checks/resource/aws/EKSPlatformVersion.py
+++ checkov/terraform/checks/resource/aws/EKSPlatformVersion.py
@@ -25,7 +25,7 @@ class EKSPlatformVersion(BaseResourceValueCheck):
 
     def get_expected_values(self) -> list[Any]:
         # https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
-        return ["1.24", "1.25", "1.26", "1.27", "1.28", "1.29", "1.30", "1.31", "1.32"]
+        return ["1.25", "1.26", "1.27", "1.28", "1.29", "1.30", "1.31", "1.32"]
 
 
 check = EKSPlatformVersion()
