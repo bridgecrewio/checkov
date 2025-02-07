86fd595ba706981057ef00ce65798c0dd01e873b
Joshua Brule <brule.joshua@mayo.edu>
2025-02-07 14:57:57 -0600

    Added additional purpose exceptions for Google Subnetwork private_ip_google_access check

--- checkov/terraform/checks/resource/gcp/GoogleSubnetworkPrivateGoogleEnabled.py
+++ checkov/terraform/checks/resource/gcp/GoogleSubnetworkPrivateGoogleEnabled.py
@@ -5,6 +5,8 @@ from typing import Any
 from checkov.common.models.enums import CheckCategories, CheckResult
 from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
+# private_ip_google_access is not supported for subnetworks with the following purpose set
+PURPOSE_EXCEPTIONS = ["INTERNAL_HTTPS_LOAD_BALANCER", "REGIONAL_MANAGED_PROXY", "GLOBAL_MANAGED_PROXY"]
 
 class GoogleSubnetworkPrivateGoogleEnabled(BaseResourceValueCheck):
     def __init__(self) -> None:
@@ -16,7 +18,7 @@ class GoogleSubnetworkPrivateGoogleEnabled(BaseResourceValueCheck):
 
     def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
         purpose = conf.get("purpose")
-        if purpose and isinstance(purpose, list) and purpose[0] == "INTERNAL_HTTPS_LOAD_BALANCER":
+        if purpose and isinstance(purpose, list) and purpose[0] in PURPOSE_EXCEPTIONS:
             return CheckResult.UNKNOWN
 
         return super().scan_resource_conf(conf)
