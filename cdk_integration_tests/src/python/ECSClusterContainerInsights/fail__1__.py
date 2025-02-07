cc2b79bb3c676186f13657454626322cb3b92821
Mike Urbanski <murbanski@paloaltonetworks.com>
2025-02-07 13:22:00 -0600

    handle python cdk

--- cdk_integration_tests/src/python/ECSClusterContainerInsights/fail__1__.py
+++ cdk_integration_tests/src/python/ECSClusterContainerInsights/fail__1__.py
@@ -1,20 +1,19 @@
-from aws_cdk import core
+import aws_cdk as core
+from constructs import Construct
 from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ec2 as ec2
 
 class MyECSClusterStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Define ECS Cluster with Cluster Settings
-        cluster = ecs.CfnCluster(
-            self, 'MyECSCluster',
-            cluster_name='my-ecs-cluster',
-            cluster_settings=[{
-                'name': 'containerInsights',
-                'value': 'disabled'
-            }]
-            # Other properties for your ECS Cluster
-        )
+        vpc = ec2.Vpc(self, "Vpc",
+                      ip_protocol=ec2.IpProtocol.DUAL_STACK
+                      )
+
+        cluster = ecs.Cluster(self, "EcsCluster", vpc=vpc, container_insights=False)
+        cluster2 = ecs.Cluster(self, "EcsCluster", vpc=vpc)
+        cluster3 = ecs.Cluster(self, "EcsCluster", vpc=vpc, container_insights_v2=ecs.ContainerInsights.DISABLED)
 
 app = core.App()
 MyECSClusterStack(app, "MyECSClusterStack")
