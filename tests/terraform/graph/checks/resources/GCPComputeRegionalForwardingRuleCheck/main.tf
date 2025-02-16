c26ca78b6152ef0addc0b53dc848cfa06a95a3aa
Taylor <tsmith.v11@gmail.com>
2025-02-15 21:42:38 -0800

    fix check3

--- tests/terraform/graph/checks/resources/GCPComputeRegionalForwardingRuleCheck/main.tf
+++ tests/terraform/graph/checks/resources/GCPComputeRegionalForwardingRuleCheck/main.tf
@@ -24,3 +24,17 @@ resource "google_compute_forwarding_rule" "fail_missing_lbscheme" {
 
   // Additional required configuration as needed...
 }
+
+resource "google_compute_forwarding_rule" "fail2" {
+  name                  = "l7-ilb-forwarding-rule"
+  provider              = google-beta
+  region                = "europe-west1"
+  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network               = google_compute_network.ilb_network.id
+  subnetwork            = google_compute_subnetwork.ilb_subnet.id
+  network_tier          = "PREMIUM"
+}
