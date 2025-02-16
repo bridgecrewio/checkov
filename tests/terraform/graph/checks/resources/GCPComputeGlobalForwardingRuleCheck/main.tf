fb204707df28e40af166da0cc976803001821923
Taylor <tsmith.v11@gmail.com>
2025-02-15 22:13:59 -0800

    Add check 4

--- tests/terraform/graph/checks/resources/GCPComputeGlobalForwardingRuleCheck/main.tf
+++ tests/terraform/graph/checks/resources/GCPComputeGlobalForwardingRuleCheck/main.tf
@@ -1,47 +1,40 @@
-resource "google_compute_forwarding_rule" "pass" {
+resource "google_compute_global_forwarding_rule" "pass" {
   name                  = "passing-forwarding-rule"
-  region                = "us-central1"
   load_balancing_scheme = "EXTERNAL"
   target                = "https://www.googleapis.com/compute/v1/projects/my-project/global/targetHttpProxies/my-target-proxy"
 
   // Additional required configuration as needed...
 }
 
-resource "google_compute_forwarding_rule" "fail" {
+resource "google_compute_global_forwarding_rule" "fail" {
   name                  = "failing-forwarding-rule"
-  region                = "us-central1"
   load_balancing_scheme = "EXTERNAL"
   target                = "https://www.googleapis.com/compute/v1/projects/my-project/global/targetSslProxies/my-target-proxy"
 
   // Additional required configuration as needed...
 }
 
-resource "google_compute_forwarding_rule" "fail_missing_lbscheme" {
+resource "google_compute_global_forwarding_rule" "fail_missing_lbscheme" {
   name                  = "failing-forwarding-rule"
-  region                = "us-central1"
   # load_balancing_scheme = "EXTERNAL" # Default is EXTERNAL
   target                = "https://www.googleapis.com/compute/v1/projects/my-project/global/targetSslProxies/my-target-proxy"
 
   // Additional required configuration as needed...
 }
 
-resource "google_compute_forwarding_rule" "fail2" {
-  name                  = "l7-ilb-forwarding-rule"
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "l7-xlb-forwarding-rule"
   provider              = google-beta
-  region                = "europe-west1"
-  depends_on            = [google_compute_subnetwork.proxy_subnet]
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
-  target                = google_compute_region_target_http_proxy.default.id
-  network               = google_compute_network.ilb_network.id
-  subnetwork            = google_compute_subnetwork.ilb_subnet.id
-  network_tier          = "PREMIUM"
+  target                = google_compute_target_http_proxy.default.id
+  ip_address            = google_compute_global_address.default.id
 }
 
-resource "google_compute_region_target_http_proxy" "default" {
-  name     = "l7-ilb-target-http-proxy"
+# http proxy
+resource "google_compute_target_http_proxy" "default" {
+  name     = "l7-xlb-target-http-proxy"
   provider = google-beta
-  region   = "europe-west1"
-  url_map  = google_compute_region_url_map.default.id
+  url_map  = google_compute_url_map.default.id
 }
--- tests/terraform/graph/checks/test_yaml_policies.py
+++ tests/terraform/graph/checks/test_yaml_policies.py
@@ -553,6 +553,9 @@ class TestYamlPolicies(unittest.TestCase):
     def test_GCPComputeRegionalForwardingRuleCheck(self):
         self.go("AzureSpringCloudTLSDisabled")
 
+    def test_GCPComputeGlobalForwardingRuleCheck(self):
+        self.go("AzureSpringCloudTLSDisabled")
+
 
     def test_registry_load(self):
         registry = Registry(parser=GraphCheckParser(), checks_dir=str(
