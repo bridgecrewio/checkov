6c3bb18db16d21a480633929eb55c8ab2360d8a6
Barak Fatal <barakf156@gmail.com>
2025-03-26 15:42:05 +0200

    Added comment

--- tests/cloudformation/graph/checks/test_yaml_policies_with_runner.py
+++ tests/cloudformation/graph/checks/test_yaml_policies_with_runner.py
@@ -5,6 +5,10 @@ from checkov.common.graph.graph_builder import CustomAttributes
 
 
 def test_connected_node_in_results_is_never_tuple():
+    """
+    Validates we correctly update the connected_node.
+    It cannot be a tuple, as this is unserializable to json.
+    """
     file_dir = os.path.dirname(__file__)
     dir_path = os.path.join(file_dir, f"resources", "ALBRedirectHTTPtoHTTPS")
     external_checks_dir = os.path.join(os.path.dirname(os.path.dirname(dir_path)), f"test_checks")
