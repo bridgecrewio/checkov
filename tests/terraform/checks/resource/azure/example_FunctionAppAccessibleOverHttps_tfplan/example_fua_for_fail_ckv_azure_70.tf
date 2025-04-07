7413111330af8ef7f00ce26e9c3bde0dc8c9b012
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2025-04-07 16:25:48 +0300

    fix(terraform): Update FunctionAppsAccessibleOverHttps (#7084)
    * fix_CKV_AZURE_70
    
    * add test
    
    * add test
    
    * add test

---
+++ tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+  description = "resource_group_name"
+  type = string
+  default = "default_group_name"
+}
+
+variable "location" {
+  description = "Azure location name"
+  type = string
+  default = "westeurope"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                      = "example-linux-functionapp"
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  service_plan_id           = azurerm_app_service_plan.example.id
+  storage_account_name      = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  site_config {
+    https_only = true
+    
+  }
+}
+
+output "function_app_endpoint" {
+  value = azurerm_linux_function_app.example.default_hostname
+}
