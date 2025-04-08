cce79ca8ec582341c972b97fa49538b7857fa0ad
lirshindalman <lirshindelman@gmail.com>
2025-04-08 11:02:03 +0300

    rename test

--- tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
+++ tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
@@ -11,11 +11,11 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure location name"
   type = string
-  default = "westeurope"
+  default = "west"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = "examples"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
