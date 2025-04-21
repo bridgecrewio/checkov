17977cd982d091b033702e716372c4442f3871f2
Yuval Michaeli <ymichaeli@paloaltonetworks.com>
2025-04-21 09:07:56 +0300

    CR changes: apply the filter on the function itself

--- tests/terraform/graph/checks/resources/LambdaOpenCorsPolicy/main.tf
+++ tests/terraform/graph/checks/resources/LambdaOpenCorsPolicy/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_lambda_function" "example_lambda_function" {
+resource "aws_lambda_function" "valid_lambda_function" {
   function_name = "example_lambda_function"
   handler       = "index.handler"
   runtime       = "nodejs14.x"
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "example_lambda_function" {
 }
 
 resource "aws_lambda_function_url" "valid_lambda_function_url" {
-  function_name = aws_lambda_function.example_lambda_function.function_name
+  function_name = aws_lambda_function.valid_lambda_function.function_name
   cors {
     allow_origins = ["https://example.com"]
     allow_methods = ["GET", "POST"]
@@ -19,29 +19,61 @@ resource "aws_lambda_function_url" "valid_lambda_function_url" {
   authorization_type = "AWS_IAM"
 }
 
+resource "aws_lambda_function" "valid_lambda_function_no_cors_definition" {
+  function_name = "example_lambda_function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.example_role.arn
+  filename      = "lambda_function_payload.zip"
+}
+
 resource "aws_lambda_function_url" "valid_lambda_function_url_no_cors_definition" {
-  function_name = aws_lambda_function.example_lambda_function.function_name
+  function_name = aws_lambda_function.valid_lambda_function_no_cors_definition.function_name
   authorization_type = "AWS_IAM"
 }
 
+resource "aws_lambda_function" "valid_lambda_function_only_allow_origins_star" {
+  function_name = "example_lambda_function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.example_role.arn
+  filename      = "lambda_function_payload.zip"
+}
+
 resource "aws_lambda_function_url" "valid_lambda_function_url_only_allow_origins_star" {
-  function_name = aws_lambda_function.example_lambda_function.function_name
+  function_name = aws_lambda_function.valid_lambda_function_only_allow_origins_star.function_name
   cors {
     allow_origins = ["*"]
   }
   authorization_type = "AWS_IAM"
 }
 
+resource "aws_lambda_function" "valid_lambda_function_only_allow_methods_star" {
+  function_name = "example_lambda_function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.example_role.arn
+  filename      = "lambda_function_payload.zip"
+}
+
 resource "aws_lambda_function_url" "valid_lambda_function_url_only_allow_methods_star" {
-  function_name = aws_lambda_function.example_lambda_function.function_name
+  function_name = aws_lambda_function.valid_lambda_function_only_allow_methods_star.function_name
   cors {
     allow_methods = ["*"]
   }
   authorization_type = "AWS_IAM"
 }
 
+resource "aws_lambda_function" "invalid_lambda_function" {
+  function_name = "example_lambda_function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.example_role.arn
+  filename      = "lambda_function_payload.zip"
+}
+
 resource "aws_lambda_function_url" "invalid_lambda_function_url" {
-  function_name = aws_lambda_function.example_lambda_function.function_name
+  function_name = aws_lambda_function.invalid_lambda_function.function_name
   cors {
     allow_origins = ["*"]
     allow_methods = ["*"]
