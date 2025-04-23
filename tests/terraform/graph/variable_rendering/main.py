25c219035635bb2d7b3e2f0bbc2677313d32f74b
lshindelman <lshindelman@paloaltonetworks.com>
2025-04-23 15:15:40 +0300

    empty_string

--- tests/terraform/graph/variable_rendering/main.py
+++
@@ -1,8 +0,0 @@
-from checkov.terraform.graph_builder.variable_rendering.safe_eval_functions import evaluate
-
-
-
-
-input_str = 'try("local.foo.boop", "{}")'
-result = evaluate(input_str)
-result2 = evaluate(input_str)
