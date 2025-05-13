b91e7c8fabde06033b643cfb0412b31d6b0e06ba
Barak Fatal <35402131+bo156@users.noreply.github.com>
2025-05-13 09:54:42 +0300

    fix(terraform): Fixed crash when using variable rendering inside a list of len > 1 (#7151)
    Fixed crash when using variable rendering inside a list of len > 1

--- tests/terraform/graph/variable_rendering/test_resources/list_entry_module_var/module/main.tf
+++ tests/terraform/graph/variable_rendering/test_resources/list_entry_module_var/module/main.tf
@@ -24,3 +24,30 @@ resource "aws_security_group" "sg" {
     cidr_blocks     = ["10.0.0.0/16", var.cidr_sg]
   }
 }
+
+variable "empty_ingress" {
+  type = list
+}
+
+resource "aws_security_group" "multiple_ingress_sg" {
+  name            = "example"
+  vpc_id          = var.vpc_id
+
+  ingress = [var.empty_ingress]
+  ingress = [
+    {
+    from_port       = 23
+    to_port         = 23
+    protocol        = "TCP"
+    cidr_blocks     = [var.cidr_sg]
+  },
+    var.empty_ingress
+  ]
+
+  egress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "TCP"
+    cidr_blocks     = ["10.0.0.0/16", var.cidr_sg]
+  }
+}
