a867f173d46133f5c884b234d8a723730d3432c9
Omri Yoffe <oyoffe@paloaltonetworks.com>
2025-05-14 11:40:51 +0300

    add check for hcl2 start and end line

--- checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
@@ -1,5 +1,8 @@
 from typing import Optional, Any, Dict
 from collections.abc import Collection
+
+import hcl2
+
 from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver import BaseAttributeSolver
 from checkov.common.util.consts import START_LINE, END_LINE
@@ -12,6 +15,9 @@ class IsEmptyAttributeSolver(BaseAttributeSolver):
         attr = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
 
         if isinstance(attr, (list, Collection)):
-            return len(attr) == 0 or (len(attr) == 2 and START_LINE in attr and END_LINE in attr)
+            if len(attr) == 0 \
+                    or (len(attr) == 2 and START_LINE in attr and END_LINE in attr) \
+                    or (len(attr) == 2 and hcl2.START_LINE in attr and hcl2.END_LINE in attr):
+                return True
 
         return False
