5578376ee51fc3b49b26c66cd5d7edb550ceaf0d
cshayner <cshayner@paloaltonetworks.com>
2025-05-14 10:19:27 +0300

    git-history allow scan by commits list

--- checkov/secrets/runner.py
+++ checkov/secrets/runner.py
@@ -159,6 +159,7 @@ class Runner(BaseRunner[None, None, None]):
             file_names: Iterable[str] | None = None,
             entropy_limit: Optional[float] = None):
         super().__init__(file_extensions, file_names)
+        self.commits_to_scan = []
         self.secrets_coordinator = SecretsCoordinator()
         self.history_secret_store = GitHistorySecretStore()
         self.entropy_limit = entropy_limit or float(os.getenv('CHECKOV_ENTROPY_KEYWORD_LIMIT', '3'))
@@ -260,7 +261,7 @@ class Runner(BaseRunner[None, None, None]):
                     git_history_scanner = GitHistoryScanner(
                         root_folder, secrets, self.history_secret_store, runner_filter.git_history_timeout)
                     settings.disable_filters(*['detect_secrets.filters.common.is_invalid_file'])
-                    git_history_scanner.scan_history(last_commit_scanned=runner_filter.git_history_last_commit_scanned)
+                    git_history_scanner.scan_history(last_commit_scanned=runner_filter.git_history_last_commit_scanned, commits_to_scan=self.commits_to_scan)
                     logging.info(f'Secrets scanning git history for root folder {root_folder}')
                 else:
                     files_to_scan += _find_files_from_root_folder(root_folder, runner_filter)
