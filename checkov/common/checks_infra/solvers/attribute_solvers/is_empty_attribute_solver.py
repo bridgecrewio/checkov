bbf2d7a5aa718360bf1bcc587c988243ca20c040
omriyoffe-panw <oyoffe@paloaltonetworks.com>
2025-05-14 15:53:17 +0300

    fix(general): exclude __start_line__ and __end_line__ from is empty solver (#7156)
    * exclude __start_line__ and __end_line__ from is empty check
    
    * add is not empty test

--- checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
@@ -1,7 +1,11 @@
 from typing import Optional, Any, Dict
-from collections.abc import Sized
+from collections.abc import Collection
+
+import hcl2
+
 from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver import BaseAttributeSolver
+from checkov.common.util.consts import START_LINE, END_LINE
 
 
 class IsEmptyAttributeSolver(BaseAttributeSolver):
@@ -10,7 +14,10 @@ class IsEmptyAttributeSolver(BaseAttributeSolver):
     def _get_operation(self, vertex: Dict[str, Any], attribute: Optional[str]) -> bool:
         attr = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
 
-        if isinstance(attr, (list, Sized)):
-            return len(attr) == 0
+        if isinstance(attr, (list, Collection)):
+            if len(attr) == 0 \
+                    or (len(attr) == 2 and START_LINE in attr and END_LINE in attr) \
+                    or (len(attr) == 2 and hcl2.START_LINE in attr and hcl2.END_LINE in attr):
+                return True
 
         return False
