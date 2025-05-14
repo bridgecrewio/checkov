ca272d6cfee5a61cedba103204cae6e07409364d
Omri Yoffe <oyoffe@paloaltonetworks.com>
2025-05-14 11:10:29 +0300

    exclude __start_line__ and __end_line__ from is empty check

--- checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
+++ checkov/common/checks_infra/solvers/attribute_solvers/is_empty_attribute_solver.py
@@ -2,6 +2,7 @@ from typing import Optional, Any, Dict
 from collections.abc import Sized
 from checkov.common.graph.checks_infra.enums import Operators
 from checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver import BaseAttributeSolver
+from checkov.common.util.consts import START_LINE, END_LINE
 
 
 class IsEmptyAttributeSolver(BaseAttributeSolver):
@@ -11,6 +12,7 @@ class IsEmptyAttributeSolver(BaseAttributeSolver):
         attr = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
 
         if isinstance(attr, (list, Sized)):
-            return len(attr) == 0
+            if len(attr) == 0 or (len(attr) == 2 and START_LINE in attr and END_LINE in attr):
+                return True
 
         return False
