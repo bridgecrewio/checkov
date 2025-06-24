1e04b7c3eade844d13f1f6ee6056e2ae7bd52ddd
mayblo <105641951+mayblo@users.noreply.github.com>
2025-06-24 13:40:43 +0300

    feat(kubernetes): include hidden folders in scan (#7219)
    * Add --include-hidden flag support
    
    * runner filter and not arg
    
    * logs
    
    * return
    
    * adding include_hidden to runner_filter
    
    * include_hidden to build_graph_from_source_directory abstractmethod
    
    * include_hidden
    
    * flake
    
    * tests
    
    * fix path
    
    * tests
    
    * env var
    
    * return
    
    * lint
    
    * assertIn
    
    * test
    
    * test
    
    * test
    
    * tests
    
    * MonkeyPatch
    
    * env_vars_config
    
    * IGNORE_HIDDEN_DIRECTORIES
    
    * tests
    
    * test
    
    * another try
    
    * tests
    
    ---------
    
    Co-authored-by: May Blonder <mblonder@paloaltonetworks.com>

--- tests/kubernetes/graph/test_kubernetes_utils.py
+++ tests/kubernetes/graph/test_kubernetes_utils.py
@@ -1,10 +1,12 @@
 import os
 
-
 from tests.kubernetes.graph.base_graph_tests import TestGraph
-from checkov.kubernetes.kubernetes_utils import build_resource_id_from_labels, PARENT_RESOURCE_KEY_NAME
+from checkov.kubernetes.kubernetes_utils import build_resource_id_from_labels, PARENT_RESOURCE_KEY_NAME, should_include_path
 
 TEST_DIRNAME = os.path.dirname(os.path.realpath(__file__))
+RELATIVE_PATH = os.path.join("resources", "definitions")
+PATH_HIDDEN = "/Users/mblonder/dev/checkov/tests/kubernetes/graph/resources/definitions/.hidden/graph_check.yaml"
+PATH_NOT_HIDDEN = "/Users/mblonder/dev/checkov/tests/kubernetes/graph/resources/definitions/not_hidden/graph_check.yaml"
 
 
 class TestKubernetesUtilsZ(TestGraph):
@@ -23,3 +25,23 @@ class TestKubernetesUtilsZ(TestGraph):
         labels = {}
         result = build_resource_id_from_labels(resource_type, namespace, labels, resource)
         self.assertEqual(result, "Pod.namespace.deployment_name.default")
+    
+    def test_should_include_path_include_hidden(self) -> None:
+        ignore_hidden_dir = False
+
+        should_include_hidden = should_include_path(PATH_HIDDEN, ignore_hidden_dir)
+        should_include_not_hidden = should_include_path(PATH_NOT_HIDDEN, ignore_hidden_dir)
+
+        self.assertEqual(should_include_hidden, True)
+        self.assertEqual(should_include_not_hidden, True)
+    
+    def test_should_include_path_not_include_hidden(self) -> None:
+        ignore_hidden_dir = True
+
+        should_include_hidden = should_include_path(PATH_HIDDEN, ignore_hidden_dir)
+        should_include_not_hidden = should_include_path(PATH_NOT_HIDDEN, ignore_hidden_dir)
+
+        self.assertEqual(should_include_hidden, False)
+        self.assertEqual(should_include_not_hidden, True)
+
+
