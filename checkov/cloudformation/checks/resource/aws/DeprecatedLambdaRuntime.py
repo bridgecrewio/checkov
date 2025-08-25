de17486618fc0495f1b53ce576d31b684ea1a449
Rory Kiefer <rory.kiefer@fanduel.com>
2025-08-25 12:49:52 -0400

    amended all lambda runtime deprecations thru Jun 30 2026

--- checkov/cloudformation/checks/resource/aws/DeprecatedLambdaRuntime.py
+++ checkov/cloudformation/checks/resource/aws/DeprecatedLambdaRuntime.py
@@ -21,9 +21,16 @@ class DeprecatedLambdaRuntime(BaseResourceNegativeValueCheck):
                 "nodejs10.x", "nodejs8.10", "nodejs4.3", "nodejs6.10", "dotnetcore1.0", "dotnetcore2.0",
                 "nodejs4.3-edge", "nodejs", "java8", "python3.7", "go1.x", "provided", "ruby2.7", "nodejs14.x",
                 "nodejs16.x", "python3.8", "dotnet7", "dotnet6"
+                # , "java8.al2" # Uncomment on Jun 30, 2026
+                # , "java11" # Uncomment on Jun 30, 2026
+                # , "java17" # Uncomment on Jun 30, 2026
                 # , "nodejs18.x" # Uncomment on Sept 1, 2025
+                # , "nodejs20.x" # Uncomment on Apr 30, 2026
                 # , "provided.al2" # Uncomment on Jun 30, 2026
-                # , "python3.9" # Uncomment on Nov 3, 2025
+                # , "python3.9" # Uncomment on Dec 15, 2025
+                # , "python3.10" # Uncomment on Jun 30, 2026
+                # , "python3.11" # Uncomment on Jun 30, 2026
+                # , "ruby3.2" # Uncomment on Mar 31, 2026
                 ]
 
 
