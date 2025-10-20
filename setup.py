99aa7ad4db343f162c5c77a2c3d962061ba12cd6
lshindelman <lshindelman@paloaltonetworks.com>
2025-10-20 15:57:57 +0300

    fix urllib3 dependency

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
         "rustworkx>=0.13.0,<1.0.0",
         "pydantic<3.0.0,>=2.0.0",
         "asteval==1.0.6",
-        "urllib3>=1.26.20"
+        "urllib3>=2.5.0"
     ],
     dependency_links=[],  # keep it empty, needed for pipenv-setup
     license="Apache License 2.0",
