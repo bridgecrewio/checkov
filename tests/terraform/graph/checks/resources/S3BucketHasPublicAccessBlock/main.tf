ffd74bb9cb7e9db9a835d37b6e6ebc97b17253cb
Floris Van den Abeele <floris@aikido.dev>
2025-11-07 10:41:11 +0100

    test: add failing S3BucketHasPublicAccessBlock terraform test case when count is used

--- tests/terraform/graph/checks/resources/S3BucketHasPublicAccessBlock/main.tf
+++ tests/terraform/graph/checks/resources/S3BucketHasPublicAccessBlock/main.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "bucket_good_1" {
   bucket = "bucket_good"
 }
 
+resource "aws_s3_bucket" "bucket_good_2" {
+  count  = var.cur_bucket_name != "" ? 1 : 0
+  bucket = "bucket_good_2"
+}
+
 resource "aws_s3_bucket" "bucket_bad_1" {
   bucket = "bucket_bad_1"
 }
@@ -23,6 +28,18 @@ resource "aws_s3_bucket_public_access_block" "access_good_1" {
 
   block_public_acls   = true
   block_public_policy = true
+}
+
+// Test for checkov failing to find the associated public_access_block resource when 'count' is used.
+// checkov 3.2.478 fixed this in https://github.com/bridgecrewio/checkov/pull/7303/files#diff-77c177a4c101efaf3685daff654840ef935d65f190afb9e1b68bae198b3b56f5
+resource "aws_s3_bucket_public_access_block" "access_good_2" {
+  count                   = var.cur_bucket_name != "" ? 1 : 0
+  bucket                  = aws_s3_bucket.bucket_good_2[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket_public_access_block" "access_bad_1" {
