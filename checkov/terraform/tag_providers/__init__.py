042aa54f884197b60a3a68d1c75fadee28af72e6
Barak Fatal <35402131+bo156@users.noreply.github.com>
2025-11-10 16:41:21 +0200

    fix(terraform): get_resource_tags handles more cases (#7365)
    Updated get_resource_tags to handle more cases

--- checkov/terraform/tag_providers/__init__.py
+++ checkov/terraform/tag_providers/__init__.py
@@ -12,11 +12,20 @@ def get_resource_tags(resource_type: str, entity_config: Dict[str, Any]) -> Opti
     if not isinstance(entity_config, dict):
         return None
 
-    if "_" not in resource_type:
-        return None  # probably not a resource block
-    provider = resource_type[: resource_type.index("_")]
-    provider_tag_function = provider_tag_mapping.get(provider)
+    provider_tag = get_provider_tag(resource_type)
+    provider_tag_function = provider_tag_mapping.get(provider_tag) if provider_tag else None
     if provider_tag_function:
         return provider_tag_function(entity_config)
     else:
         return None
+
+
+def get_provider_tag(resource_type: str) -> Optional[str]:
+    provider_tag = None
+    if 'aws' in resource_type:
+        provider_tag = "aws"
+    elif 'azure' in resource_type:
+        provider_tag = "azure"
+    elif 'gcp' in resource_type or 'google' in resource_type:
+        provider_tag = "gcp"
+    return provider_tag
