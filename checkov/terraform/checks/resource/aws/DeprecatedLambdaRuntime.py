4617eaa9c0360c52343063782f66d325042cdb8e
Rory Kiefer <rory.kiefer@fanduel.com>
2025-12-15 11:16:12 -0500

    updates: tests using python3.9 lambda runtime updated to use 3.13 instead; added python3.9 to deprecated runtime datastructure

--- checkov/terraform/checks/resource/aws/DeprecatedLambdaRuntime.py
+++ checkov/terraform/checks/resource/aws/DeprecatedLambdaRuntime.py
@@ -20,8 +20,7 @@ class DeprecatedLambdaRuntime(BaseResourceNegativeValueCheck):
         return ["dotnetcore3.1", "nodejs12.x", "python3.6", "python2.7", "dotnet5.0", "dotnetcore2.1", "ruby2.5",
                 "nodejs10.x", "nodejs8.10", "nodejs4.3", "nodejs6.10", "dotnetcore1.0", "dotnetcore2.0",
                 "nodejs4.3-edge", "nodejs", "java8", "python3.7", "go1.x", "provided", "ruby2.7", "nodejs14.x",
-                "nodejs16.x", "python3.8", "dotnet7", "dotnet6", "nodejs18.x"]
-        # , "python3.9"    # Uncomment on Dec 15, 2025
+                "nodejs16.x", "python3.8", "dotnet7", "dotnet6", "nodejs18.x", "python3.9"]
         # , "ruby3.2"      # Uncomment on Mar 31, 2026
         # , "nodejs20.x"   # Uncomment on Apr 30, 2026
         # , "provided.al2" # Uncomment on Jun 30, 2026
--- tests/terraform/checks/resource/aws/example_LambdaCodeSigningConfigured/main.tf
+++ tests/terraform/checks/resource/aws/example_LambdaCodeSigningConfigured/main.tf
@@ -3,7 +3,7 @@
 resource "aws_lambda_function" "pass" {
   function_name = "test-env"
   role          = ""
-  runtime       = "python3.9"
+  runtime       = "python3.13"
   code_signing_config_arn = "123123123"
 }
 
@@ -12,5 +12,5 @@ resource "aws_lambda_function" "pass" {
 resource "aws_lambda_function" "fail" {
   function_name = "stest-env"
   role          = ""
-  runtime       = "python3.9"
+  runtime       = "python3.13"
 }
