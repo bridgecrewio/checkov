15034630503ba3e123d8bf21e363d743f04df8f2
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2025-12-23 20:52:26 +0100

    Do not flag public access if network ACLs are set

--- tests/terraform/checks/resource/azure/example_StorageAccountDisablePublicAccess/main.tf
+++ tests/terraform/checks/resource/azure/example_StorageAccountDisablePublicAccess/main.tf
@@ -37,3 +37,21 @@ resource "azurerm_storage_account" "fail2" {
   }
 }
 
+resource "azurerm_storage_account" "pass2" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  network_rules {
+    default_action = "Deny"
+    virtual_network_subnet_ids = [
+      "/subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/example-rg/providers/Microsoft.Network/virtualNetworks/example-vnet/subnets/example-subnet"
+    ]
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
