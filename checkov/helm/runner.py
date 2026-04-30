a1858508d663858530144546d497386699396d64
aidanhall34 <aidan.hall34@outlook.com>
2026-04-30 19:07:49 +1000

    chore(lint): fix dead var lint

--- checkov/helm/runner.py
+++ checkov/helm/runner.py
@@ -157,7 +157,7 @@ class Runner(BaseRunner[_KubernetesDefinitions, _KubernetesContext, "KubernetesG
         logging.info(f"Checking necessary system dependencies for {self.check_type} checks.")
         try:
             proc = subprocess.Popen([self.helm_command, 'version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # nosec
-            o, e = proc.communicate()
+            o, _ = proc.communicate()
             oString = str(o, 'utf-8')
             if "Version:" in oString:
                 helmVersionOutput = oString[oString.find(':') + 2: oString.find(',') - 1]
